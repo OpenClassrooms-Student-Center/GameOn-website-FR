66526b89d32dd7f82fcb6fa90a3f0efe8b48f103
T3events <49736855+T3events@users.noreply.github.com>
2020-08-01 11:35:21 -0400

    Add files via upload

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,22 +1,23 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-// ex. lement to close
-const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-// close modal event ex.
-closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// Close modal form
-function closeModal() {
-  modalbg.style.display = "none";
-}
 
