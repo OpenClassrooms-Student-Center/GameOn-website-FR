492983776ffdda69d5ac083285eeef188819fd60
fournier-alexis <pro.fournieralexis@gmail.com>
2021-01-11 08:23:48 +0100

    Vérification des données saisies en javascript

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,13 @@
+let error_list = {
+    error_prenom: "Veuillez entrer 2 caractères ou plus pour le champ du prénom",
+    error_nom: "Veuillez entrer 2 caractères ou plus pour le champ du nom",
+    error_mail: "Veuillez entrer un email valide",
+    error_naissance: "Veuillez saisir une date de naissance",
+    error_tournois: "",
+    error_villes: "Veuillez sélectionner une ville",
+    error_conditions: "Veuillez accepter les conditions"
+}
+
 function editNav() {
     const x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -12,7 +22,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalConfirm = document.querySelectorAll("#confirm-message");
 const form = document.getElementById("formulaire");
-const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,29 +29,45 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
     modalbg.style.display = "block";
-    form.addEventListener('submit', cancelRefresh);
+    form.addEventListener('submit', verifyForm);
+    //launch input events
+    document.getElementById("first").addEventListener("change", change_prenom);
+    document.getElementById("last").addEventListener("change", change_nom);
+    document.getElementById("email").addEventListener("change", change_mail);
+    document.getElementById("birthdate").addEventListener("change", change_naissance);
+    document.getElementById("quantity").addEventListener("change", change_tournois);
+    document.getElementById("villes").addEventListener("change", change_villes);
+    document.getElementById("checkbox1").addEventListener("change", change_conditions);
 }
 
 /**
  * Lorsque l'on ferme le formulaire on supprime les valeurs entrées
- * et on reset l'affichage
+ * et on reset l'affichage ainsi que les erreurs du formulaires
  */
 function closeForm() {
     modalbg.style.display = "none";
     form.style.display = "block";
     modalConfirm[0].style.display = "none";
-    for (const tc of form.getElementsByClassName("text-control")){
-        if (tc.type === "number"){
+    for (const tc of form.getElementsByClassName("text-control")) {
+        if (tc.type === "number") {
             tc.value = 0;
-        }
-        else {
+        } else {
             tc.value = "";
         }
     }
-    for (const ci of form.getElementsByClassName("checkbox-input")){
-        if (ci.checked){
+    for (const ci of form.getElementsByClassName("checkbox-input")) {
+        if (ci.checked) {
             ci.checked = false;
         }
+    }
+    error_list = {
+        error_prenom: "Veuillez entrer 2 caractères ou plus pour le champ du prénom",
+        error_nom: "Veuillez entrer 2 caractères ou plus pour le champ du nom",
+        error_mail: "Veuillez entrer un email valide",
+        error_naissance: "Veuillez saisir une date de naissance",
+        error_tournois: "",
+        error_villes: "Veuillez sélectionner une ville",
+        error_conditions: "Veuillez accepter les conditions"
     }
 }
 
@@ -51,10 +76,107 @@ function closeForm() {
  * Permet d'afficher le message de validation de l'envoi
  * @param event "submit"
  */
-function cancelRefresh(event){
-    form.style.display = "none";
-    modalConfirm[0].style.display = "block";
+function verifyForm(event) {
+    const inputs_required = document.getElementsByClassName("error");
+    let everithing_valid = true;
+    let i = 0
+    for (const v in error_list){
+        if (error_list[v] !== ""){
+            everithing_valid = false;
+        }
+        inputs_required[i].textContent = error_list[v];
+        i++;
+    }
+    if (everithing_valid){
+        form.style.display = "none";
+        modalConfirm[0].style.display = "block";
+    }
     event.preventDefault()
 }
 
+/**
+ * Vérifie que le prénom fait au minimum 2 caractères
+ */
+function change_prenom() {
+    if (this.value.length > 1) {
+        error_list.error_prenom = "";
+    } else {
+        error_list.error_prenom = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
+    }
 
+}
+
+/**
+ * Vérifie que le nom fait au minimum 2 caractères
+ */
+function change_nom() {
+    if (this.value.length > 1) {
+        error_list.error_nom = "";
+    } else {
+        error_list.error_nom = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
+    }
+}
+
+/**
+ * Vérifie que le mail est valide
+ */
+function change_mail() {
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (regex.test(this.value.toLowerCase())) {
+        error_list.error_mail = "";
+    } else {
+        error_list.error_mail = "Veuillez entrer un email valide";
+    }
+
+}
+
+/**
+ * Vérifie que la date de naissance est sélectionné
+ */
+function change_naissance() {
+    if (this.value === "") {
+        error_list.error_naissance = "Veuillez saisir une date de naissance";
+    } else {
+        error_list.error_naissance = "";
+    }
+
+}
+
+/**
+ * Vérifie que le nombre de tournois est supérieur à 0
+ */
+function change_tournois() {
+    if (parseInt(this.value) >= 0 && parseInt(this.value) <= 99) {
+        error_list.error_tournois = "";
+    } else {
+        error_list.error_tournois = "Le nombre doit être compris entre 0 et 99"
+    }
+}
+
+/**
+ * Vérifie qu'au moins une ville à été sélectionné
+ */
+function change_villes() {
+    let one_checked = false;
+    for (let i = 0; i < this.children.length; i += 2) {
+        if (this.children[i].checked) {
+            one_checked = true;
+        }
+    }
+    if (one_checked) {
+        error_list.error_villes = "";
+    } else {
+        error_list.error_villes = "Veuillez sélectionner une ville";
+    }
+}
+
+/**
+ * Vérifie que les conditions ont été accepté
+ */
+function change_conditions() {
+    if (this.checked) {
+        error_list.error_conditions = "";
+    } else {
+        error_list.error_conditions = "Veuillez accepter les conditions";
+    }
+}
