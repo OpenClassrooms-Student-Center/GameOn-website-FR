e6f998e5dc9b074b25f037555a4aaffccc31e93f
Amandine Arias <amandine.arias@gmail.com>
2021-02-12 13:36:40 +0100

    feat (modal) : add close function on button (x) (#1)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const bground = document.querySelector(".bground");
+const closeModalBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +21,16 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal event
+closeModalBtn.addEventListener("click", function(event) {
+  event.preventDefault ();
+  closeModal();
+  });
+
+  // close modal form
+  function closeModal() {
+    bground.style.display = "none";
+  }
 
 
