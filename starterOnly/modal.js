ce4ddee76fa7f00dfa3204c7efa25f23001f85f2
Amandine Arias <amandine.arias@gmail.com>
2021-02-12 18:38:30 +0100

    feat (labels) : add html labels and links (#2)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,6 +14,24 @@ const formData = document.querySelectorAll(".formData");
 const bground = document.querySelector(".bground");
 const closeModalBtn = document.querySelector(".close");
 
+// html labels links
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
+const checkbox1 = document.getElementById("checkbox1");
+const checkbox2 = document.getElementById("checkbox2");
+
+// Validity
+const formValid = document.getElementById("submit");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -32,5 +50,3 @@ closeModalBtn.addEventListener("click", function(event) {
   function closeModal() {
     bground.style.display = "none";
   }
-
-
