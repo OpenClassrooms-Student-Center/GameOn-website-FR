ccc6e61c96228e8f608fbd52411912806e70f218
Hugues <hugues.aubry@gmail.com>
2021-02-15 21:29:05 +0100

    first commit !
    ajout de la fonction pour fermer la modale en cliquant sur la croix
    ajout du label aucune , bouton radio pour que l'utilisateur n'ayant participer a aucun évenement puisse entrer un champs de choix de villes.
    une correction dans le htlm répétition du pronom "je" ligne 210
    ajout de fonction de validation lors de l'envoie réussi du formulaire .
    ajout d'un message générale lorsqu'un champ n'est pas renseigné .

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,6 +7,7 @@ function editNav() {
   }
 }
 
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -20,4 +21,50 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal
+function closeModal() {
+  modalbg.style.display = "none"
+}
 
+console.log(modalBtn);
+
+
+let firstName = document.getElementById("first");
+let lastName = document.getElementById("last");
+let email = document.getElementById("email");
+let birthdate = document.getElementById("birthdate");
+let quantityNumber = document.getElementById("quantity");
+
+// validation du formulaire lors de l'envoi
+document.getElementById("inscription").addEventListener("submit", function(submitElement) {
+
+  let erreur;
+
+  if (!quantityNumber.value) {
+    erreur = "veuillez renseigner un nombre de participation"
+  }
+
+  if (!birthdate.value) {
+    erreur = "veuillez renseigner un l'année de naissance"
+  }
+
+  if (!email.value) {
+    erreur = "veuillez renseigner un email"
+  }
+
+  if (!lastName.value) {
+    erreur = "veuillez renseigner un nom de famille"
+  }
+
+  if (!firstName.value) {
+    erreur = "veuillez renseigner un Prénom"
+  }
+
+  if (erreur) {
+    submitElement.preventDefault();
+    document.getElementById("erreur").innerHTML = erreur;
+    return false;
+  } else {
+    alert('formulaire envoyé !');
+  }
+});
