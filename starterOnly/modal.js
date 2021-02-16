7e0b5ec5c96f79d220fbb3d73086c1d5483129c4
Hugues <hugues.aubry@gmail.com>
2021-02-16 22:06:51 +0100

    [DEV] second commit transform de la validation du formulaire en moins de ligne. général + ajout de validation d'erreur unique.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,9 +1,3 @@
-let firstName = document.getElementById("first");
-let lastName = document.getElementById("last");
-let email = document.getElementById("email");
-let birthdate = document.getElementById("birthdate");
-let quantityNumber = document.getElementById("quantity");
-
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -32,38 +26,22 @@ function closeModal() {
   modalbg.style.display = "none"
 }
 
-console.log(modalBtn);
+// console.log(modalBtn);
 
 
-let firstName = document.getElementById("first");
-let lastName = document.getElementById("last");
-let email = document.getElementById("email");
-let birthdate = document.getElementById("birthdate");
-let quantityNumber = document.getElementById("quantity");
 
 // validation du formulaire lors de l'envoi
-document.getElementById("inscription").addEventListener("submit", function(submitElement) {
+document.forms["reserve"].addEventListener("submit", function(submitElement) {
 
   let erreur;
+  let inputs = this;
 
-  if (!quantityNumber.value) {
-    erreur = "veuillez renseigner un nombre de participation"
-  }
-
-  if (!birthdate.value) {
-    erreur = "veuillez renseigner un l'année de naissance"
-  }
-
-  if (!email.value) {
-    erreur = "veuillez renseigner un email"
-  }
-
-  if (!lastName.value) {
-    erreur = "veuillez renseigner un nom de famille"
-  }
-
-  if (!firstName.value) {
-    erreur = "veuillez renseigner un Prénom"
+  for (let i = 0; i < inputs.length; i++) {
+    if (!inputs[i].value) {
+      erreur = "veuillez renseigner tous les champs !";
+      break;
+    }
+    console.log(inputs[i])
   }
 
   if (erreur) {
@@ -71,6 +49,54 @@ document.getElementById("inscription").addEventListener("submit", function(submi
     document.getElementById("erreur").innerHTML = erreur;
     return false;
   } else {
-    alert('formulaire envoyé !');
+    alert('Merci, Votre formulaire à bien été envoyé !');
   }
-});
+ });
+
+ document.getElementById("first").addEventListener("input", function() {
+  if (this.value == "") {
+  document.getElementById("errorFirstName").innerHTML = "Veuillez entrez un Prénom valide";
+  } else {
+    document.getElementById("errorFirstName").innerHTML = "";
+  }
+ })
+
+ document.getElementById("last").addEventListener("input", function() {
+  if (this.value == "") {
+  document.getElementById("errorLastName").innerHTML = "Veuillez entrez un Nom valide";
+  } else {
+    document.getElementById("errorLastName").innerHTML = "";
+  }
+ })
+
+
+
+// regex 2 caractere : [a-z]
+
+
+ //   // if (!quantityNumber.value) {
+//   //   erreur = "veuillez renseigner un nombre de participation"
+//   // }
+
+//   // if (!birthdate.value) {
+//   //   erreur = "veuillez renseigner un l'année de naissance"
+//   // }
+
+//   // if (!email.value) {
+//   //   erreur = "veuillez renseigner un email"
+//   // }
+
+//   // if (!lastName.value) {
+//   //   erreur = "veuillez renseigner un nom de famille"
+//   // }
+
+//   // if (!firstName.value) {
+//   //   erreur = "veuillez renseigner un Prénom"
+//   // }
+
+// console.log(document.forms["reserve"]);
+// // let firstName = document.getElementById("first");
+// // let lastName = document.getElementById("last");
+// // let email = document.getElementById("email");
+// // let birthdate = document.getElementById("birthdate");
+// // let quantityNumber = document.getElementById("quantity");
