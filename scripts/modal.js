d97dad32c341c8d0e576a51b97472323fcb572c3
DAMEVIN <raphael.damevin@gmail.com>
2021-03-02 16:59:51 +0100

    changements

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,57 +1,94 @@
-
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modal = document.getElementById('modal');
+const $registrationForm = document.querySelector('.registration-form');
+
+
+// Functions
+//  -> dédié à l'affichage
 
 // Fonction pour ouvrir et fermer la modal
 function modalDisplay(displayStyle) {
   modal.style.display = displayStyle 
 }
 
-// Fonction pour que lorsque l'utilisateur clique sur le 
-// background la modal se ferme
+//  -> dédié aux vérifications
+function checkFirstName() {
+  const firstNameInput = document.getElementById('first').value;
+  const $firstErrorMsg = document.querySelector("#firstErrorMsg");
+  const isFirstNameValid = firstNameInput.length < 2;
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-     modal.style.display = 'none';
-   }
- }
-
-// Alerte lorsque l'utilisateur envoie les informations
-function validate() {
-  let errorFund = this.getErrors()
-  if(errorFund) {
-    return false
+  if (isFirstNameValid) {
+    $firstErrorMsg.style.display = "block";
+  } else {
+    $firstErrorMsg.style.display = "none";
   }
-  else {
-    alert('Merci ! Votre réservation a été reçue.')
-    return true
-  }
+  return isFirstNameValid;
 }
 
-// Fonction pour les champs de la modal
+function checkLastName() {
+  const lastNameInput = document.getElementById('last').value;
+  const $lastErrorMsg = document.querySelector('#lastErrorMsg');
+  const isLastNameValid = lastNameInput.length < 2;
 
-function getErrors() {
-  let errorFund = false;
-  let first = document.getElementById('first').value;
-  let last = document.getElementById('last').value;
-  let email = document.getElementById('email').value;
-  let mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  
-  if (first.length < 2 ) {
-    errorFund = true
-    document.getElementById("firstErrorMsg").textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
-  } 
-  if (last.length < 2 ) {
-    errorFund = true
-    document.getElementById("lastErrorMsg").textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du nom';
+  if (isLastNameValid) {
+    $lastErrorMsg.style.display = "block";
+  } else {
+    $lastErrorMsg.style.display = "none";
   }
-  if (email.value.match(mailFormat)) {
-    errorFund = true
-    document.getElementById("emailErrorMsg").textContent = 'Veuillez entrer un email valide';
-  }
-  return errorFund
+  return isLastNameValid;
 }
+
+function checkEmail() {
+  const emailInput = document.getElementById('email').value;
+  const emailFormat = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;";
+  const $emailErrorMsg = document.querySelector('#emailErrorMsg');
+  let isEmailValid = true;
+  console.log(emailFormat);
+
+  if (emailInput.value.match(emailFormat)) {
+    let isEmailValid = false;
+    $emailErrorMsg.style.display = "block";
+
+  } else {
+    $emailErrorMsg.style.display = "none";
+    let isEmailValid = true;
+  }
+  return 
+}
+
+
+function isFormValid() {
+  checkFirstName();
+  checkLastName();
+  checkEmail();
+}
+
+
+// Event Listeners
+$registrationForm.addEventListener('submit', function(event) {
+  event.preventDefault()
+
+  if (isFormValid()) {
+    console.log("=====")
+    console.log("tu peux passer à l'étape d'après")
+    console.log("=====")
+  } else {
+    console.log("=====")
+    console.log("C'est pété, il y a un truc pas clair dans le formulaire")
+    console.log("=====")
+  }
+
+})
+
+/**
+ * querySelector -> ça retourne un élément de type noeud
+ * querySelectorAll -> ça retourne un tableau (de noeuds ?)
+ */
+
+
+// À l'événement submit, déclenche la fonction à sa droite
+
+
