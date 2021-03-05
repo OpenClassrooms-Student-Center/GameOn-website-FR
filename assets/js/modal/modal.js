fe310f1b4355549f9fb2021cef2571fd5b047935
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-03-05 16:42:36 +0100

    cleaning to prepare another branch for refactor in POO

--- assets/js/modal/modal.js
+++ assets/js/modal/modal.js
@@ -6,52 +6,19 @@ const closeBtn = document.querySelectorAll(".close, .btn-close");
 const modalbg = document.querySelector(".bground");
 const modalSuccessMessage = document.querySelector(".reservation-accepted");
 
-// // click : launch modal
-// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// click : launch modal
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// // click : close modal
-// closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+// click : close modal
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
-// // launch modal
-// function launchModal() {
-//   modalbg.style.display = "block";
-// }
-
-// // close modal
-// function closeModal() {
-//   modalbg.style.display = "none";
-//   modalSuccessMessage.style.display = "none";
-// }
-
-// ********* TEST class Modal *********
-class Modal {
-
-  constructor(open, close, modalBackground, modalSuccessMessage) {
-    this.open = open;
-    this.close = close;
-    this.modalBackground = modalBackground;
-    this.modalSuccessMessage = modalSuccessMessage;
-  }
-
-  openModal() {
-    this.open.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        this.modalBackground.style.display = "block";
-      });
-    });
-  }
-
-  closeModal() {
-    this.close.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        this.modalBackground.style.display = "none";
-        this.modalSuccessMessage.style.display = "none";
-      });
-    });
-  }
+// launch modal
+function launchModal() {
+  modalbg.style.display = "block";
 }
 
-const modalInscription = new Modal(modalBtn, closeBtn, modalbg, modalSuccessMessage);
-
-modalInscription.openModal();
-modalInscription.closeModal();
+// close modal
+function closeModal() {
+  modalbg.style.display = "none";
+  modalSuccessMessage.style.display = "none";
+}
