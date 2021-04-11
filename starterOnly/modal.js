e36e630f1f9a14b6fb73cb004422c388e22c32e3
Redavela <kodadreda67@gmail.com>
2021-04-11 13:25:56 +0200

    step 1 closeModal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,28 @@ function editNav() {
 }
 
 // DOM Elements
+
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementById('close');
 
 // launch modal event
+
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+
+closeBtn.addEventListener('click', closeModal);
+
 // launch modal form
+
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
