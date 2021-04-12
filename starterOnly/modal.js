e8605524ddb28ab28bb367a2fd25e626e7c58ab4
y.arcorio <y.arcorio@acreos.eu>
2021-04-12 22:49:58 +0200

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,28 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
+// MODAL EVENT
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalClose.addEventListener("click", closeModal);
 
-// launch modal form
+// MODAL FORM
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
+function closeModal() {
+    modalbg.style.display = "none";
+}
 
+function editNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
