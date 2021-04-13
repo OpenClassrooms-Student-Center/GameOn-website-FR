c3c0f9d99555fb14b4944e4a9cb2529cb7ba0fc4
y.arcorio <y.arcorio@acreos.eu>
2021-04-13 21:43:30 +0200

    validate form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -4,12 +4,20 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalClose = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
+const firstName = document.getElementsByName("first");
+const lastName = document.getElementsByName("last");
+const email = document.getElementsByName("email");
+const birthdate = document.getElementsByName("birthdate");
+const quantity = document.getElementsByName("quantity");
 
-// MODAL EVENT
+
+// EVENTS
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalClose.addEventListener("click", closeModal);
 
-// MODAL FORM
+
+
+// FUNCTIONS
 function launchModal() {
     modalbg.style.display = "block";
 }
@@ -24,5 +32,31 @@ function editNav() {
         x.className += " responsive";
     } else {
         x.className = "topnav";
+    }
+}
+
+function validate() {
+    let valide = true;
+
+    const regexName = /^[a-zA-Z '\-éèêëçäàï]{5,}$/;
+    const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const regexNumber = /^[0-9]$/;
+
+    if (!firstName.value.match(regexName)) {
+        valide = false;
+        firstName.setCustomValidity("");
+    }
+    if (!lastName.value.match(regexName)) {
+        valide = false;
+        lastName.setCustomValidity("Le nom doit contenir au minimum 2 caractères");
+    }
+    if (!email.value.match(regexEmail)) {
+        valide = false;
+        email.setCustomValidity("L'adresse électronique n'est pas valide");
+    }
+    if (valide) {
+        return valide;
+    } else {
+        event.preventDefault();
     }
 }
