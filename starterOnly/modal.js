5291a598f87d3fd64b3a6ef277903e95d759dd54
Noor Makria <noor.makria@gmail.com>
2021-04-24 17:23:17 +0200

    validation des inputs, à réduire en mettant en place une fonction errorTreatment

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,23 +8,25 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground"); // toute la modale
-const modalBtn = document.querySelectorAll(".modal-btn"); // bouton pour ouvrir la modale
-const formData = document.querySelectorAll(".formData"); //
-const closeBtn = document.querySelector(".close"); // bouton X pour fermer la modale
-const contentBg = document.querySelector(".content");
+const modalbg = document.querySelector(".bground"); // toute la modale entière
+const modalBtn = document.querySelectorAll(".modal-btn"); // bouton "Je M'inscris" pour ouvrir la modale
+const formData = document.querySelectorAll(".formData"); // Chaque ligne du formulaire
+const closeBtnX = document.querySelector(".close"); // bouton X pour fermer la modale
+const contentBg = document.querySelector(".content"); // modal contenu entier (span quit "X" + modal-body)
 const modalBody = document.querySelector(".modal-body");
 const main = document.querySelector("main");
 const body = document.querySelector("body");
 const form = document.querySelector("form");
-const firstNameInput = document.getElementById("first");
-const lastNameInput = document.getElementById("last");
-const emailInput = document.getElementById("email");
-const birthDateInput = document.getElementById("birthdate");
-const quantityInput = document.getElementById("quantity");
-const allSpan = document.querySelectorAll('span.error-alert'); // span des messages d'erreur
-const condUtilisation = document.getElementById("checkbox1"); // checkbox conditions d'utilisation
-const confirmationBox = document.querySelector(".confirmation-box")
+const firstNameInput = document.getElementById("first"); // input text control 1
+const lastNameInput = document.getElementById("last"); // input text control 2
+const emailInput = document.getElementById("email"); // input text control 3
+const birthDateInput = document.getElementById("birthdate"); // input text control 4
+const quantityInput = document.getElementById("quantity"); // input text control 5
+const allInputs = document.querySelectorAll('.text-control'); // les inputs textcontrol (prénom nom email date naissance tournois)
+const allSpans = document.querySelectorAll('span.error-alert'); // span des messages d'erreur
+const allCheckboxLocations = document.querySelectorAll('.checkbox-input.city'); // checkboxes des villes
+const condUtilisation = document.getElementById("checkbox1"); // checkboxes conditions d'utilisation
+const confirmationBox = document.querySelector(".confirmation-box") // la modal confirmation
 const exitBtnModal = document.querySelector(".btn-exitModal"); // bouton fermer modal
 
 
@@ -33,11 +35,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-    // on remet tout à 0
+    // on remet tous les input à 0
     firstNameInput.value = "";
     lastNameInput.value = "";
     emailInput.value = "";
     birthDateInput.value = "";
+    quantityInput.value = "";
     // on retire la confirmationBox
     confirmationBox.style.display = "none";
     // on affiche le form
@@ -45,81 +48,171 @@ function launchModal() {
     // on affiche toute la modal
     modalbg.style.display = "block";
 }
+
 // ------------------------------------------------
 // FERMETURE DE LA MODALE
 // ------------------------------------------------
-closeBtn.addEventListener('click', function(event) {
+closeBtnX.addEventListener('click', function(event) {
     modalbg.style.display = "none"; // change le style en display: none;
 });
 exitBtnModal.addEventListener('click', function(event) {
     modalbg.style.display = "none"; // change le style en display: none;
 });
+
+
+// FONCTION VALIDATION DES INPUTS (NOM PRENOM NB DE TOURNOI)
+// ----------------------------------------------------
+// function validateInput(input, span, errorMessage) {
+//     console.log("Verification type du champ", input.type);
+//     if(input.type == "text") {
+//         if(input.value == "" || input.value.length < 2) { // -------on écrit la condition (si la valeur de l'input est vide ou si inférieur à 2 caractères)
+//             error = errorMessage;
+//             if (error) { // -------------------- *** (IF) Si on a bien une erreur: 
+//                 span.innerHTML = error; // ----- j'affiche l'erreur dans le span correspondant
+//                 input.classList.add("red-border");
+//                 return false; // --------------------- On met false pour être sûr que rien ne se passe
+//             } else { // ------------------------ *** (ELSE) Si on n'a pas d'erreur:
+//                 span.innerHTML = ""; // -------- On vide le span
+//                 input.classList.remove("red-border");
+//             };
+//         }    
+//     } else {
+//         console.log("Verification champ number", input.type);
+//         if(input.value == "") { // on vérifie que la valeur de la date de naissance absente (si c'est le cas retourne true)
+//             error = errorMessage;
+//         };
+//         if (error) {
+//             span.innerHTML = error;
+//             input.classList.add("red-border");
+//             return false;
+//         } else {
+//             span.innerHTML = "";
+//             input.classList.remove("red-border");
+//         };
+//     }  
+// }
+// validateInput(allInputs[0], allSpans[0], "Veuillez entrer 2 caractères ou plus pour le champ du prénom.")
+// validateInput(allInputs[1], allSpans[1], "Veuillez entrer 2 caractères ou plus pour le champ du nom.")
+// validateInput(allInputs[4], allSpans[4], "Ce champ est obligatoire.")
 // ------------------------------------------------
 // VALIDATION / MESSAGES ERREUR
 // ------------------------------------------------
+
 document.forms["reserve"].addEventListener("submit", function(e) {
     e.preventDefault();
     let error; // On crée une variable error qui va contenir les différents messages d'erreur
     // traitement cas par cas (input unique)
+
     // VALIDATION PRENOM
-    if(firstNameInput.value == "" || firstNameInput.value.length < 2) { // -------on écrit la condition (si la valeur de l'input est vide ou si inférieur à 2 caractères)
-        error = "Veuillez entrer 2 caractères ou plus pour le champ du nom"; // --on indique le message d'erreur correspondant si la condition est true
+    if(allInputs[0].value == "" || allInputs[0].value.length < 2) { // -------on écrit la condition (si la valeur de l'input est vide ou si inférieur à 2 caractères)
+    error = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     };
     if (error) { // -------------------- *** (IF) Si on a bien une erreur: 
-        e.preventDefault(); // --------------- j'empeche la soumission du formulaire (comportement par défaut du formulaire)
-        allSpan[0].innerHTML = error; // ----- j'affiche l'erreur dans le span correspondant
+        e.preventDefault();
+        allSpans[0].innerHTML = error; // ----- j'affiche l'erreur dans le span correspondant
+        allInputs[0].classList.add("red-border");
         return false; // --------------------- On met false pour être sûr que rien ne se passe
     } else { // ------------------------ *** (ELSE) Si on n'a pas d'erreur:
-        allSpan[0].innerHTML = ""; // -------- On vide le span
+        allSpans[0].innerHTML = ""; // -------- On vide le span
+        allInputs[0].classList.remove("red-border");
     };
 
     // VALIDATION NOM
-    if(lastNameInput.value == "" || lastNameInput.value.length < 2) {
-        error = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
+    if(allInputs[1].value == "" || allInputs[1].value.length < 2) {
+    error = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     };
-    if (error) {
+    if (error) { 
         e.preventDefault();
-        allSpan[1].innerHTML = error;
+        allSpans[1].innerHTML = error;
+        allInputs[1].classList.add("red-border");
         return false;
     } else {
-        allSpan[1].innerHTML = "";
+        allSpans[1].innerHTML = "";
+        allInputs[1].classList.remove("red-border");
     };
 
     // VALIDATION EMAIL
     const regexEmail = /\S+@\S+\.\S+/; // On définit une variable regex pour la validation des mails
-    if(emailInput.value.search(regexEmail) === -1) { // on vérifie que la valeur de l'email ne verifie pas cette regex (ca retourne true dans ce cas)
+    if(allInputs[2].value.search(regexEmail) === -1) { // on vérifie que la valeur de l'email ne verifie pas cette regex (ca retourne true dans ce cas)
         error = "Rentrez un email valide"; // on indique le message d'erreur correspondant si la condition est true
     };
     if (error) {
         e.preventDefault();
-        allSpan[2].innerHTML = error;
+        allSpans[2].innerHTML = error;
+        allInputs[2].classList.add("red-border");
         return false;
     } else {
-        allSpan[2].innerHTML = "";
+        allSpans[2].innerHTML = "";
+        allInputs[2].classList.remove("red-border");
     };
 
     // VALIDATION BIRTHDAY 
-    if(birthDateInput.value == "") { // on vérifie que la valeur de la date de naissance absente (si c'est le cas retourne true)
-        error = "Vous devez entrer votre date de naissance";
+    if(allInputs[3].value == "") { // on vérifie que la valeur de la date de naissance absente (si c'est le cas retourne true)
+        error = "Vous devez entrer votre date de naissance.";
     };
     if (error) {
         e.preventDefault();
-        allSpan[3].innerHTML = error;
+        allSpans[3].innerHTML = error;
+        allInputs[3].classList.add("red-border");
         return false;
     } else {
-        allSpan[3].innerHTML = "";
+        allSpans[3].innerHTML = "";
+        allInputs[3].classList.remove("red-border");
     };
+
+    // VALIDATION TOURNOIS
+    if(allInputs[4].value == "") { // on vérifie que la valeur de la date de naissance absente (si c'est le cas retourne true)
+        error = "Ce champ est obligatoire.";
+    };
+    if (error) {
+        e.preventDefault();
+        allSpans[4].innerHTML = error;
+        allInputs[4].classList.add("red-border");
+        return false;
+    } else {
+        allSpans[4].innerHTML = "";
+        allInputs[4].classList.remove("red-border");
+    };
+
+    // VALIDATION CHECKBOXES VILLES
+    // for (let i=0; i<allCheckboxLocations.length; i++) {
+    //     if(allCheckboxLocations[i].checked) {
+    //         allSpans[5].innerHTML = "";
+    //         console.log("OK!");            
+    //         break;
+    //     } else {
+    //         allSpans[5].innerHTML = "Vous devez choisir une option.";
+    //         console.log("NOT OK!"); 
+    //         i++;             
+    //     }
+    // };
 
     // VALIDATION CONDITIONS GENERALES
     if(!condUtilisation.checked) { // on vérifie que la checkbox n'est pas checkée (si c'est le cas c'est true)
-        error = "Vous devez vérifier que vous acceptez les termes et conditions";            
+        error = "Vous devez vérifier que vous acceptez les termes et conditions.";            
     };
+    errorTreatment()
     if (error) {
         e.preventDefault();
-        allSpan[5].innerHTML = error;
+        allSpans[6].innerHTML = error;
+        condUtilisation.classList.add("red-border");
         return false;
     } else {
-        allSpan[5].innerHTML = "";
+        allSpans[6].innerHTML = "";
+        condUtilisation.classList.remove("red-border");
+    }
+
+
+    const errorTreatment = (input, span) => {
+        if (error) {
+            e.preventDefault();
+            allSpans[6].innerHTML = error;
+            condUtilisation.classList.add("red-border");
+            return false;
+        } else {
+            allSpans[6].innerHTML = "";
+            condUtilisation.classList.remove("red-border");
+        }
     }
 // ------------------------------------------------
 // CONFIRMATION
@@ -130,63 +223,3 @@ document.forms["reserve"].addEventListener("submit", function(e) {
     confirmationBox.style.display = "flex";
     
 });
-
-
-// ------------------------------------------------
-// ESSAI
-// ------------------------------------------------
-// Validation Form
-
-// firstNameInput.addEventListener('input', (e) => {
-//     if(e.target.value.length < 2) {
-//         allSpan[0].style.display = 'inline';
-//     } else {
-//         allSpan[0].style.display = 'none';
-//     }
-// });
-
-// lastNameInput.addEventListener('input', (e) => {
-//     if(e.target.value.length < 2) {
-//         allSpan[1].style.display = 'inline';
-//     } else {
-//         allSpan[1].style.display = 'none';
-//     }
-// });
-
-// emailInput.addEventListener('input', (e) => {
-//     const regexEmail = /\S+@\S+\.\S+/;
-//     if(e.target.value.search(regexEmail) === 0) {
-//         allSpan[2].style.display = 'none';
-//     } else if(e.target.value.search(regexEmail) === -1) {
-//         allSpan[2].style.display = 'inline';
-//     }
-// });
-
-// ------------------------------------------------
-// ESSAI
-// ------------------------------------------------
-
-// function validate(e) {
-    // form.addEventListener("submit", function() {
-    //     alert('Formulaire envoyé !');
-        // e.preventDefault();
-
-        // if (firstNameInput.value == "") {
-        //     allSpan[0].innerHTML = 'icoucou';
-        // }
-
-    // });
-// };
-
-
-// ---------------------QUESTIONS------------------
-// ???? Pourquoi ne fonctionne pas ???? ci-dessus 
-// ???? preferer rajouter le contenant du message d'erreur dans le html ou préférer utiliser append child ????
-
-// ---------------------QUESTIONS------------------
-// - Est ce que priviligier la validation js uniquement ? ou peut utiliser attribut html ? Impression que attribut html prioritaire ... et que peut pas utiliser les 2 ?
-// - onsubmit="return validate(event) ???? faut il englober le code dans cette fonction ?
-// - A quoi correspond l'input A combien de tournoi ... ?
-// - Vous devez choisir une option ... a quoi correspond ce message d'erreur ??
-// - Conseil refacto ...
-// - Breakpoints ()
