5ce547e89503c1c429a7406c36726b044f8c87a2
ChrisP <petitcolaschristoper@gmail.com>
2021-04-24 12:01:19 +0200

    Correction issue sur la fermeture de la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,38 @@ const modalbg = document.querySelector(".background_modal");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const coches = document.getElementsByClassName("checkbox-label");
+const agree = document.getElementsByClassName("checkbox2-label");    
+const cases = document.querySelectorAll(".checkbox-icon");;            
+const aggreCoche = document.querySelectorAll(".agree-icon");  
+        
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display= "block";
+  modalbg.style.position="absolute";
+  modalbg.style.zIndex="1000000";
 }
 
 
+// remplissage personnalisé des coches lors de la selection utilisateur
+
+for( let i = 0; i < coches.length; i++){
+  coches[i].addEventListener("click", () => { 
+    cases[i].classList.toggle("fillCoche");
+  } )}
+  
+  for( let i = 0; i < agree.length; i++){
+    agree[i].addEventListener("click", () => { 
+      aggreCoche[i].classList.toggle("fillCoche2");
+    } )}
+
+
+    const closeModal = document.querySelector(".close_modal");  // Je recupere le bouton permettant de fermer la modal
+
+    closeModal.addEventListener("click", () => {    // j'ecoute l'evenement click sur le bouton pour apppliquer la fonction de fermeture de la fenetre 
+      modalbg.style.display= "none";
+    })
+    
