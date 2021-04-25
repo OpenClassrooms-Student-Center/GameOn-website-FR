3e473849050f42d6e022843b18a945b2c4c2e2f9
ChrisP <petitcolaschristoper@gmail.com>
2021-04-25 11:25:42 +0200

    fixes #1, Ajout de la fonction fermeture de la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,25 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Ciblage du span avec class close
+const btnCloseModal = document.querySelector("span.close");       
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Fermeture de la modal 
+const closeModal= () => modalbg.style.display = "none";
+
+//Ajout d'un evenement clique sur le bouton pour appeler la fonction qui fermera la modal
+btnCloseModal.addEventListener("click", closeModal);                        
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
+
+
 
 
