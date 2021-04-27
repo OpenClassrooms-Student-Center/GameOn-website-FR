849147075a10f2789293f20e0c858848d4b9395d
Koch Christopher <koch.christopher@hotmail.fr>
2021-04-27 19:12:57 +0200

    réduction des lignes code JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,24 +15,8 @@ const closeX = document.querySelectorAll(".close");
 const formDisplay = document.getElementById("validation_clear");
 const validationText= document.getElementById("validation_text");
 const validationButton= document.getElementById("validation--button");
-const firstNameInput = document.getElementById("first");
-const lastNameInput = document.getElementById("last");
-const emailInput = document.getElementById("email");
-const birthdateInput = document.getElementById("birthdate");
-const quantityInput = document.getElementById("quantity");
-const condition = document.getElementById("checkbox1");
-const locationInput = document.getElementsByName("location");
-const loc1 = document.getElementById ('location1');
-const loc2 = document.getElementById ('location2');
-const loc3 = document.getElementById ('location3');
-const loc4 = document.getElementById ('location4');
-const loc5 = document.getElementById ('location5');
-const loc6 = document.getElementById ('location6');
 const inputs = document.getElementsByTagName("input");
 const form = document.getElementsByClassName("formData");
-
-//Variables need to be true for modal validation
-let FirstNameValid,LastNameValid, emailValid, birthdateValid, quantityValid,conditionValid = true, locationValid;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -61,17 +45,13 @@ function validate(){
   formDisplay.style.display = "none";
   validationText.style.display= "block";
   validationButton.style.display= "block";
-  if (inputs.length >0) {
     for (var i=0; i < inputs.length -1; i++){
     inputs[i].value = "";
     inputs[i].checked = false;
     }
-  }
-  if (form.length >0) {
     for (var h=0; h < form.length; h++) {
       form[h].setAttribute("data-error-visible", undefined)
     } 
-  }
 }
 
 // Error messages function
@@ -87,61 +67,53 @@ function error(e,texte,custom) {
 }
 
 
-//Function wich check if the input is correct
+//Function which check if the input is correct
 function fieldValidation(e) {
   if (e.type === "text"){
         if (e.value.length <2) {
           error (e,"Nom trop court !","Votre nom doit contenir au moins 2 caractères")
+          inputValid=false;
         }else if (!e.value.match(/^[A-Za-z\é\è\ê\-]+$/)){ 
           error (e,"Pas de caractères spéciaux!","Votre nom ne doit pas contenir de caractères spéciaux")
+          inputValid=false;
         } else  {
           noError(e);
-          FirstNameValid=true;
-          LastNameValid=true;
+          inputValid=true;
         }
   } else if (e.type === "email") {
         if (e.value.match(/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i)) {
-          emailValid=true;
+          inputValid=true;
           noError(e);
         } else {
-          emailValid=false;
-          error(e,"Email non valide !","'"+emailInput.value+ "' n'est pas une adresse mail valide");
+          inputValid=false;
+          error(e,"Email non valide !","'"+e.value+ "' n'est pas une adresse mail valide");
         }
   } else if (e.type ==="date") {
         if (e.value.match (/^[0-9]/)) {
-          birthdateValid=true;
+          inputValid=true;
           noError(e);
         } else {
-          birthdateValid=false;
+          inputValid=false;
           error(e,"Date anniversaire non correcte !","Veuillez entrer votre date de naissance");
         }
   } else if (e.type ==="number") {
         if (!e.value.match (/^[0-9]/) || e.value > 99) {
           error (e,"Erreur sur la quantité !","Veuillez entrer le nombre de participation à des tournois GameOn");
-          quantityValid=false;
+          inputValid=false;
         } else {
-          quantityValid=true;
+          inputValid=true;
           noError(e);
         }
-
   } else if (e.type ==="radio") {
-    for (i=0;  locationInput[i].checked ==true; i++){
-    locationValid= true;
-          noError(e);
-          console.log("ok");}
-        /*if (loc1.checked || loc2.checked || loc3.checked || loc4.checked || loc5.checked || loc6.checked) {
-          locationValid= true;
-          noError(e)
-        } else {
-          locationValid= false;
-          e.setCustomValidity("Veuillez choisir une ville");
-        }*/
+          if (e.checked) {
+            inputValid= true;
+        }
   } else if (e.type === "checkbox"){
           if (e.checked){
             noError(e);
-            conditionValid=true;
+            inputValid=true;
         } else {
-            conditionValid=false;
+          inputValid=false;
             error(e,"CGU non coché !","Veuillez lire et accepter les conditions d'utilisation")
         }
   }
@@ -154,10 +126,9 @@ for (var i=0; i < inputs.length; i++){
      })}
 
 // Check if all the fields are correctly entered (==true) If it is, it launch the validation message
-const isValidAllFieldValid = FirstNameValid == true && LastNameValid == true && emailValid == true && birthdateValid == true && quantityValid == true && conditionValid == true && locationValid==true
 document.forms["reserve"].addEventListener("submit", function(e) {
   e.preventDefault();
-  if (FirstNameValid == true && LastNameValid == true && emailValid == true && birthdateValid == true && quantityValid == true && conditionValid == true && locationValid==true) {
+  if (inputValid) {
     validate(); 
   } 
 });
