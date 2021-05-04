b2d59f7ba552f49320796a23e359fab77dc39cb8
Redavela <kodadreda67@gmail.com>
2021-05-04 02:12:46 +0200

    Ajout du message de confirmation + réglage css fin des modifications

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -13,8 +13,9 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.getElementById('close');
-const modalBodyForm = document.querySelector(".modal-body__form")
-const confirmation = document.querySelector(".confirmation")
+const modalBodyForm = document.querySelector(".form");
+const succesMessage = document.getElementById('message_confirmation');
+const modalBox = document.querySelector(".bground");
 
 // launch modal event
 
@@ -35,97 +36,156 @@ function launchModal() {
 
 function closeModal() {
   modalbg.style.display = "none";
-  confirmation.style.display = "none";
 }
 
 
-// Validation des données 
+// Récuperation des éléments du formulaire
 
-let myform = document.getElementById("inscription");
+const myform = document.getElementById("inscription");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const mail =document.getElementById("email");
+const birth = document.getElementById("birthdate");
+const quantity = document.getElementById('quantity')
+const city = document.querySelectorAll("input[type=radio]");
+const choice = document.getElementById("checkbox1");
+
+
+// Récupération des messages d'erreur
+const lastErr = document.getElementById('lastErr');
+const firstErr = document.getElementById('firstErr');
+const emailErr = document.getElementById('emailErr');
+const birthErr = document.getElementById('birthErr');
+const quantityErr = document.getElementById('quantityErr');
+const cityErr = document.getElementById('cityErr');
+const choiceErr = document.getElementById('choiceErr');
 
 
 
+// Création variable qui récupère la date renseignée
+const birthDate = new Date(birth.value);
+// Création d'une variable qui récupère la date du jour
+const today = new Date(Date.now());
+// Création d'une variable de 90 ans
+const old = new Date(1931, 12, 31);
 
-myform.addEventListener('submit', function(e){
-  // Récuperation des éléments du formulaire
-  let first = document.getElementById("first");
-  let last = document.getElementById("last");
-  let mail =document.getElementById("email");
-  let birth = document.getElementById("birthdate");
-  let quantity = document.getElementById('quantity')
-  let city = document.querySelectorAll("input[type=radio]");
-  let choice = document.getElementById("checkbox1");
+// Création de variable regex pour le nom et le mail
+const regName = /^[a-zA-Z]{2}/;
+const regMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+// Condition pour la validation du formulaire
+myform.addEventListener('submit', (e) => {
   
+  e.preventDefault();
+  if( firstN() && lastN() && email() && birthday() && quantityy() && cities() && choices() ) {
+    //Close ModalBox and OPEN SuccesBox
+      modalBox.style.display = "none";
+      succesMessage.style.display = "block";  
+      //Close succesBox
+      document.querySelectorAll('.close_confirmation').forEach(button =>{
+      button.addEventListener('click', button => {
+      if (succesMessage.style.display === "block"){
+          succesMessage.style.display = 'none';
+      }})});
+      //Reset the form  
+      myform.reset();
+    } 
+    return true;
+  })
 
-  // Récupération des messages d'erreur
-  let lastErr = document.getElementById('lastErr');
-  let firstErr = document.getElementById('firstErr');
-  let emailErr = document.getElementById('emailErr');
-  let birthErr = document.getElementById('birthErr');
-  let quantityErr = document.getElementById('quantityErr');
-  let cityErr = document.getElementById('cityErr');
-  let choiceErr = document.getElementById('choiceErr');
-
-
-
-  // Création variable qui récupère la date renseignée
-  let birthDate = new Date(birth.value);
-  // Création d'une variable qui récupère la date du jour
-  let today = new Date(Date.now());
-  // Création d'une variable de 90 ans
-  let old = new Date(1931, 12, 31);
-
-  // Création de variable regex pour le nom et le mail
-  let regName = /^[a-zA-Z]{2}/;
-  let regMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-  // Condition pour la validation du formulaire
-
+// conditions pour le prénom avec une regex qui n'accepte que les consttres 
+function firstN(){
   if(!regName.test(first.value.trim())){
     firstErr.textContent = "Le champs Prénom est requis et il faut 2 lettres minimum";
     first.style.borderColor ='red';
-    e.preventDefault();
+    return false;
   } else {
     document.getElementById("firstErr").innerHTML = "";
     first.style.borderColor = "black";
+    return true;
   }
+}
 
+  // conditions pour le nom avec une regex qui n'accepte que les consttres
+function lastN(){
   if(!regName.test(last.value.trim())){
     lastErr.textContent = "Le champs Nom est requis et il faut 2 lettres minimum";
     last.style.borderColor ='red';
-    e.preventDefault();
+    return false;
   } else {
     document.getElementById("lastErr").innerHTML = "";
     last.style.borderColor = "black";
+    return true;
   }
+}
 
+  // conditions pour le mail avec une regex qui demande un vrai mail
+function email(){
   if(!regMail.test(mail.value.trim())){
     emailErr.textContent = "Le champs Mail est incorrect...";
     mail.style.borderColor ='red';
-    e.preventDefault();
-
+    return false;
   } else {
     document.getElementById("emailErr").innerHTML = "";
     mail.style.borderColor = "black";
+    return true;
   }
+}
 
+  // conditions pour la date de naissance, qu'elle ne soit ni trop ancienne ni dans le futur
+function birthday(){
   if(birth.value === "" || birthDate.getTime() > today.getTime() || birthDate.getTime() < old.getTime()){
     birthErr.textContent = "Vous devez entrer votre date de naissance";
     birth.style.borderColor ='red';
-    e.preventDefault();
+    return false;
   }else{
     birthErr.textContent = "";
     birth.style.borderColor ='black';
+    return true;
   }
+}
 
+  // conditions pour le nombre de participation (entre 0 et 99)
+function quantityy(){
   if(quantity.value ==="" || quantity.value >= 100) {
     quantityErr.textContent = "Veuillez entrer un nombre entre 0 et 99";
     quantity.style.borderColor = 'red';
-    e.preventDefault();
+    return false;
   }else {
     quantityErr.textContent = "";
     quantity.style.borderColor = 'black';
+    return true;
   }
-  
+}
 
-})
+  // conditions pour voir si une ville à bien était sélectionner
+function cities() { 
+  let i =0;
+  let isChecked = false;
+  while ( i < city.length) {
+    if (city[i].checked) {
+      isChecked=true;
+      break;
+    } else {
+      i++
+    }
+  }
+  if(!isChecked) {
+    cityErr.textContent ='Veuillez choisir une ville'
+    return false;
+    } else {
+      cityErr.textContent='';
+      return true;
+    }
+}
+    // conditions pour verifier que les conditions sont acceptées
+function choices() {
+  if(!choice.checked) {
+    choiceErr.textContent ='Veuillez accepter les conditions'
+    return false;
+  }else {
+    choiceErr.textContent ='';
+    return true;
+  }
+}
+
