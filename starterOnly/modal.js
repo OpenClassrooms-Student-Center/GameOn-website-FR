f1e67f4af247621ae9e32893d96dffd01973d69d
elodie baeza <elodie.baeza@gmail.com>
2021-05-06 17:08:48 +0200

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,3 +22,10 @@ function launchModal() {
 }
 
 
+// close modal event
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+ 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
