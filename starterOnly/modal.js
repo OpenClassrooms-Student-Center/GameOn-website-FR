80afc7e4951f85c0aa1a1986097e8e1373ef9d83
elodie baeza <elodie.baeza@gmail.com>
2021-05-07 21:20:50 +0200

    validation form fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -42,64 +42,56 @@ const form = document.getElementById("reserve");
  const btnSubmit = document.getElementById('btnSubmit');
  const checkRadio = document.querySelectorAll('input[type="radio"]');
  const checkbox = document.querySelectorAll('input[type="checkbox"]');
+ const inputs = form.getElementsByTagName('input');
 
-form.addEventListener("submit", function(e) {
-	let messageErreur;
-	
-	const inputs = this.getElementsByTagName('input');
+// valide si prenom n'est pas vide et au mini 2 caractères
+function validateFirst(){
+	return (inputs["first"].value !== '' && inputs["first"].value.length >= 2);
+}
 
-	// Traitement générique
-	if (!inputs["first"].value || inputs["first"].value.length < 2) {
-		messageErreur = true ;
-	}
+// valide si nom n'est pas vide et au mini 2 caractères
+function validateLast(){
+	return (inputs["last"].value !== '' && inputs["last"].value.length >= 2);
+}
 
-	if (!inputs["last"].value || inputs["last"].value.length < 2) {
-		messageErreur = true ;
-	}
+// valide syntaxe dans champ email
+function validateEmail(){
+	return (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(inputs["email"].value));
+}
 
-	if (!inputs["email"].value || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(inputs["email"].value) == false) {
-		messageErreur = true ;
-	}
+// valide syntaxe dans champ date de naissance
+function validateDate(){
+	return (/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(inputs["birthdate"].value));
+} 
 
-	if (/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(inputs["birthdate"].value) == false){
-		messageErreur = true;
-	} 
-
-	for (let i = 0; i < checkRadio.length; i++) {
-		if (checkRadio[i].checked) {
+// valide si ville cochée
+function validateRadio(){
+	for (radio of checkRadio){
+		if(radio.checked)
+			return true;
 			break;
-		} else if (i = checkRadio.length - 1){
-			messageErreur = true;
-		}
+	}
+}
+
+// valide si conditions d'utilisations cochée
+function validateCheckbox1(){
+	return (checkbox[0].checked);
+}
+
+// vérification des contraintes lors de la validation du formulaire
+form.addEventListener("submit", function() {
+	//cré un tableau des contraintes valides et non valides	
+	const inputsItems = [validateFirst(),validateLast(),validateEmail(),validateDate(),validateRadio(),validateCheckbox1()];
+
+	let isValid = true;
+
+	//parcours du tableau des contrainte, sort de la boucle à la premiere valeur false et passe isValid à false
+	for (item of inputsItems){
+		if(item = false)
+			isValid = false;
+			break;
 	}
 
-	if (!checkbox[0].checked){
-		messageErreur = true;
-	}
- 
-	if (messageErreur) {
-		e.preventDefault();
-		document.getElementById("erreur").innerHTML = 'remplir tous les champs';
-		return false;
-	} else {
-		e.preventDefault(true);
-		document.getElementById("erreur").innerHTML = '';
-		alert('Formulaire envoyé !');
-		clearValue(5 ,inputs);
-		unchecked(checkRadio.length, checkRadio);
-		unchecked(checkbox.length, checkbox);
-		return false
-	}
+	console.log(isValid);
+
 });
-
-function clearValue(a , b){
-	for (let i=0 ; i<a ; i++){
-		b[i].value = '';
-	}
-}
-
-function unchecked(a , b){
-	for (let i=0 ; i<a ; i++){
-		b[i].checked =  false;
-	}
-}
