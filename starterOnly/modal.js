1b763111a6f1cdcf93c79ff01b891f076236ee52
Corentin Clichy <cor.clichy@gmail.com>
2021-05-11 14:13:08 -0400

    setup the forked project and change the showmodal animation to be able to have smooth transition when closing the modal. Added the close modal feature

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,24 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const content = document.querySelectorAll(".content")[0];
+
+const closeModalIcon = document.querySelector(".close");
+
+console.log(closeModalIcon);
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+closeModalIcon.addEventListener("click", closeModal);
+
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.classList.add("bground--show");
+  content.classList.add("content--show");
 }
 
-
+function closeModal() {
+  content.classList.remove("content--show");
+  modalbg.classList.remove("bground--show");
+}
