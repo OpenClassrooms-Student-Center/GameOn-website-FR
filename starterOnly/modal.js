08308a373d22d7c5cb9441ddf759f44d99470131
Corentin Clichy <cor.clichy@gmail.com>
2021-05-11 14:56:11 -0400

    added closing feature when clicking outside the content element

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,27 +9,33 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
 const content = document.querySelectorAll(".content")[0];
 
-const closeModalIcon = document.querySelector(".close");
+// EVENTS LISTENERS
 
-console.log(closeModalIcon);
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-closeModalIcon.addEventListener("click", closeModal);
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".modal-btn")) {
+    //Case if user click on any element with classes .modal-btn
+    launchModal();
+  } else if (
+    e.target.matches(".close") ||
+    e.target.closest(".content") === null
+  ) {
+    //Case if user click either on close element or any element outside the content div.
+    closeModal();
+  }
+});
 
 // launch modal form
 function launchModal() {
+  // adding specific classes to bgground et content to display them
   modalbg.classList.add("bground--show");
   content.classList.add("content--show");
 }
 
 function closeModal() {
+  // removing specific classes to bgground et content to display them
   content.classList.remove("content--show");
   modalbg.classList.remove("bground--show");
 }
