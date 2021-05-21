08fd0a2d11065d5e445a08fae03145722d4c07f3
Amar <a.zirouel@gmail.com>
2021-05-21 13:06:11 +0200

    fixes #3, ajout des messages d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,7 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const submitBtn = document.querySelector(".btn-submit");
-const form={                                                //objet contenant les champs du formulaire
+// objet contenant les champs du formulaire
+const form = {
   first: document.getElementById("first"),
   last: document.getElementById("last"),
   email: document.getElementById("email"),
@@ -21,14 +22,25 @@ const form={                                                //objet contenant le
   city: document.querySelectorAll("input[type=radio]"),
   terms: document.getElementById("checkbox1")
 };
-const regex ={
+// objet qui créé la div qui va afficher le message d'erreur
+const errorMsg = {
+  first: document.createElement("div"),
+  last: document.createElement("div"),
+  email: document.createElement("div"),
+  birthdate: document.createElement("div"),
+  quantity: document.createElement("div"),
+  city: document.createElement("div"),
+  terms: document.createElement("div")
+};
+// expression reguliere pour valider les differentes chaines de caractères
+const regex = {
   mail: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
-  name: /^[a-zA-Z-]{1,}[^\d.+*/$%µ!§:;,?={}²&~"#()`@]$/, 
+  name: /^[a-zA-ZÀ-Ÿà-ÿ]+([\s\'\.\-][a-zA-ZÀ-Ÿà-ÿ]+)?([\s\'\.\-][a-zA-ZÀ-Ÿà-ÿ]+)*$/,
   quantity: /^\d{1,}$/
 };
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach(btn => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
@@ -38,59 +50,84 @@ function launchModal() {
 // fermer la modale
 const closeModalBtn = document.querySelector(".close");
 
-closeModalBtn.addEventListener("click", function(){
+closeModalBtn.addEventListener("click", function () {
   modalbg.style.display = "none";
 });
 
+// application du style pour les msg d'erreur
+for (const property in errorMsg) {
+  errorMsg[property].style.color = "#FF4E60";
+  errorMsg[property].style.fontSize = "12px";
+}
 
 // validation d'un champ de texte
-function validText(entry, regex){
-  if(entry.match(regex)){
-    return true
-  } else{
-    return false
+function validText(entry, regex, container, msg, index, msg2) {
+  formData[index].appendChild(container);
+  if(entry.length < 2 && msg2.length > 0){
+    container.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du " + msg2 + ".";
+    return false;
+  }
+  if (entry.match(regex)) {
+    container.innerHTML = "";
+    return true;
+  } else {
+    container.innerHTML = msg;
+    return false;
   }
 }
 
 // validation de la date de naissance
-function isDateValid(){
+function isDateValid() {
   let date = new Date(form.birthdate.value).getTime();
-  let year = form.birthdate.value.substring(0,2);
-  if (parseInt(year) < 19 || isNaN(date) || Date.now() < date){
+  let year = form.birthdate.value.substring(0, 2);
+  formData[3].appendChild(errorMsg.birthdate);
+  if (parseInt(year) < 19 || isNaN(date) || Date.now() < date) {
+    errorMsg.birthdate.innerHTML = "Vous devez saisir une date de naissance.";
     return false;
   } else {
+    errorMsg.birthdate.innerHTML = "";
     return true;
   }
 }
 
 // validation de la selection de la ville
-function isCityValid(){
-  for (let i=0; i< form.city.length; i++){
-    if (form.city[i].checked){
+function isCityValid() {
+  formData[5].appendChild(errorMsg.city);
+  for (let i = 0; i < form.city.length; i++) {
+    if (form.city[i].checked) {
+      errorMsg.city.innerHTML = "";
       return true;
     }
   }
+  errorMsg.city.innerHTML = "Vous devez choisir une option.";
   return false;
 }
 
 // validation des conditions d'utilisation
-function isTermsValid(){
-  if (form.terms.checked){
+function isTermsValid() {
+  formData[6].appendChild(errorMsg.terms);
+  if (form.terms.checked) {
+    errorMsg.terms.innerHTML = "";
     return true;
   } else {
+    errorMsg.terms.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
     return false;
   }
 }
 
 // ecoute du bouton submit
-submitBtn.addEventListener("click", function(event){
-    if( validText(form.first.value, regex.name) &&
-    validText(form.last.value, regex.name) &&
-    validText(form.email.value, regex.mail) &&
-    validText(form.quantity.value, regex.quantity) &&
-    isDateValid() && isCityValid() && isTermsValid()){
-      console.log("ok");
-    } else{
-      event.preventDefault();
-    }
+submitBtn.addEventListener("click", function (event) {
+  let validForm = validText(form.first.value, regex.name, errorMsg.first, "Veuillez saisir un prénom correct.", 0, "prénom") &
+                  validText(form.last.value, regex.name, errorMsg.last, "Veuillez saisir un nom correct.", 1, "nom") &
+                  validText(form.email.value, regex.mail, errorMsg.email, "Veuillez saisir une adresse mail correcte.", 2, "") &
+                  isDateValid() & 
+                  validText(form.quantity.value, regex.quantity, errorMsg.quantity, "Veuillez entrer un nombre entier positif.", 4, "") &
+                  isCityValid() &
+                  isTermsValid();
+  console.log(validForm);
+  if (validForm == true) {
+    console.log("ok");
+  } else {
+    event.preventDefault();
+  }
 });
