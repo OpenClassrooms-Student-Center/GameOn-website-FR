cffe8a24dbb9c1c84faf96193331068e695d53a8
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-05-25 08:51:47 +0200

    Issue 2 - test sur le prenom

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,6 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const modalBtnFermeture = document.getElementById("btn-close-modale");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,11 +20,45 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//close modal
+//fermer modal
 
-modalBtnFermeture.addEventListener("click",function(){
+document.getElementById("btn-close-modale").addEventListener("click",function(){
   modalbg.style.display = "none";
 });
 
+
+
+
+
+// valider les données du prénom
+
+//variable
+const prenomFormulaire = document.getElementById("first");
+const affichageValiditePrenom = document.getElementById("validite-prenom");
+
+
+// ecoute
+prenomFormulaire.addEventListener("input", function(valeur){
+    function isValidNumber(e){
+      return /\D/.test(e);
+    };
+    let caractereSaisie = valeur.target.value;
+    isValidNumber(caractereSaisie);
+
+    
+   switch(isValidNumber){
+      case "true" : affichageValiditePrenom.textContent = "je suis vrai";
+      break;
+      case "false" : affichageValiditePrenom.textContent = "je suis faut";
+      break;
+   };
+    
+   
+    
+    console.log("isvalidnumber : " + isValidNumber(caractereSaisie));
+    console.log("saisie : " + caractereSaisie);
+    console.log("validite prenom :" + affichageValiditePrenom.innerText);
+  }
+);
 
 
