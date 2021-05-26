e9b41d2f462181fd61351b019407a6989d8d9439
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-05-26 11:08:00 +0200

    Issue 2 : validation prenom et nom

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,4 +26,61 @@ document.getElementById("btn-close-modale").addEventListener("click",function(){
   modalbg.style.display = "none";
 });
 
+// VALIDER FORMULAIRE
 
+// déclaration variable
+formulaire = document.getElementById("formulaire");
+prenom = document.getElementById("first");
+nom = document.getElementById("last");
+mail = document.getElementById("email");
+
+
+// boite à outil
+function nePasSubmit(typeEvenement){
+  typeEvenement.preventDefault();
+};
+
+function validerNomPrenom(text){
+  if (text.value != "" && text.value.length >= 2) {
+    return 1;
+  } else {
+    alert(text.value + " : est INVALIDE : doit avoir au moins 2 caractères");
+    return 0;
+  };
+};
+
+function validerMail(mailAValider){
+  let testResult = /[@]/.test(mailAValider.value);
+  if (testResult == true) {
+    alert("Test : le mail est valide");
+  } else {
+    alert("Test : le mail est invalide");
+  };
+};
+
+
+// déclencheur
+formulaire.addEventListener("submit",function (evenement){
+  nePasSubmit(evenement);
+  validerNomPrenom(prenom);
+  validerNomPrenom(nom);
+  validerMail(mail);
+});
+
+
+
+
+
+
+
+
+
+/*
+function validerEnvoyer (typeEvenement){
+  if (validerNomPrenom = 1) {
+    alert("test : le formulaire est valide")
+  } else {
+    alert("test : le formulaire est non valide")
+  }
+};
+*/
