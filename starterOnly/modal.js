eb438aeaa559d8574027f338c2f2e0db784f02e8
kirmann <k.dkle@hotmail.fr>
2021-05-27 17:29:14 +0200

    implémentation du code de fermeture de la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal evant
+close.addEventListener('click', closeModal);
 
+// close modal form
+function closeModal(){
+  modalbg.style.display = "none";
+}
