b32d7ea2b4c39e4afc21c3bd5504add34c7e5070
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-06-03 15:32:24 +0200

    issue 2 : affichage message validation

--- README.md
+++ README.md
@@ -8,29 +8,3 @@
 ISSUE 2 :
 
 (1) Lier les labels aux entrées dans le HTML en utilisant les attributs "for" et "id" dans le code existant. Corriger le code HTML quand nécessaire.
-(2) Utiliser du JavaScript pur (pas de jQuery) pour terminer le formulaire :
-
-Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
-Les données doivent être saisies correctement :
-(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
-(3) L'adresse électronique est valide.
-(4) Pour le nombre de concours, une valeur numérique est saisie.
-(5) Un bouton radio est sélectionné.
-(6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
-Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
-
-
-ISSUE 3 :
-
-Des messages d'erreur spécifiques doivent apparaître sous l'entrée qui n'est pas correcte. Les messages d'erreur doivent s'afficher sous le champ de saisie associé. Exemples :
-
-"Veuillez entrer 2 caractères ou plus pour le champ du nom."
-"Vous devez choisir une option."
-"Vous devez vérifier que vous acceptez les termes et conditions."
-"Vous devez entrer votre date de naissance."
-
-
-ISSUE 4 :
-
-Après une validation réussie, inclure un message de confirmation de la soumission réussie pour l'utilisateur (ex. "Merci ! Votre réservation a été reçue.")
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,11 +7,23 @@ function editNav() {
   };
 };
 
-// DOM Elements affichage formulaire
-const modalbg = document.querySelector(".bground");
+// AFFICHER / FERMER MODALE
+// DOM Elements affichage Modale
+const modalbg = document.getElementById("formulaire-inscription");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+// launch modal event et launch modal form
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+function launchModal() {
+  modalbg.style.display = "block";
+}
+// Fermer modal
+document.getElementById("btn-close-modale").addEventListener("click",function(){
+  modalbg.style.display = "none";
+});
 
+
+// VALIDER FORMULAIRE
 // DOM elements Validation formulaire
 const formulaire = document.getElementById("formulaire");
 const prenom = document.getElementById("first");
@@ -21,6 +33,7 @@ const dateNaissance = document.getElementById("birthdate");
 const nombreTournoi = document.getElementById("quantity");
 const choixVille = document.getElementsByName("location");
 const conditionUtilisation = document.getElementById("checkbox1");
+const btnValiderFormulaire = document.getElementById("btn-valider");
 const erreurPrenom = document.getElementById("erreur-prenom");
 const erreurNom = document.getElementById("erreur-nom");
 const erreurMail = document.getElementById("erreur-mail");
@@ -28,143 +41,100 @@ const erreurNaissance = document.getElementById("erreur-naissance");
 const erreurTournoi = document.getElementById("erreur-tournoi");
 const erreurVille = document.getElementById("erreur-ville");
 const erreurCondition = document.getElementById("erreur-condition");
-
-
-// launch modal event et launch modal form
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-// Fermer modal
-document.getElementById("btn-close-modale").addEventListener("click",function(){
-  modalbg.style.display = "none";
-});
-
-// VALIDER FORMULAIRE
-
-// boite à outil
-function nePasSubmit(typeEvenement){
-  typeEvenement.preventDefault();
-};
-
-function validerNomPrenom(text){
-  if (text.value != "" && text.value.length >= 2) {
-    return 1;
-  } else {
-    return 0;
-  };
-};
-
-function validerMail(mailAValider){
-  let testResult = /[\s\S]{1,}@([a-zA-Z0-9-_]{1,}[.])+[a-zA-Z]{2,3}$/.test(mailAValider.value);
-  //^([a-zA-Z0-9_-])+([.]?[a-zA-Z0-9_-]{1,})*@([a-zA-Z0-9-_]{2,}[.])+[a-zA-Z]{2,3}$
-  if (testResult == true) {
-    return 1;
-  } else {
-    return 0;
-  };
-};
-
-function validerDateNaissance(dateAniv){
-  let anneNaissance = new Date(dateAniv.value).getFullYear();
-  let anneePresent = new Date().getFullYear();
-  let comparerAnnee = anneePresent - anneNaissance;
-  if(comparerAnnee >= 15){
-    return 1;
-  } else {
-    return 0;
-  };
-};
-
-function validerNombreTournoi(qt){
-  let testResult = /^(?!\-)[0-9]{1,}$/.test(qt.value);
-  if (testResult == true && qt.value < 99){
-    return 1;
-  } else {
-    return 0;
-  };  
-};
-
-function validerSiUneVilleChoisie(villePossible){
-  let chercherVilleNonChoisie = 0;
-  for (let ville of villePossible){
-    if(ville.checked == false){
-      chercherVilleNonChoisie++;
-    };
-  };
-  if(chercherVilleNonChoisie == villePossible.length){
-      return 0;
-    } else {
-      return 1;
-  };
-};
-
-function validerConditionUtilisation(caseCocher){
-  if(caseCocher.checked == true){
-    return 1;
-  } else {
-    return 0;
-  };
-};
-
-// déclencheur de validation et message erreur
-formulaire.addEventListener("submit",function (evenement){
+const fermerValidation1 = document.getElementById("btn-close-form-envoye");
+const fermerValidation2 = document.getElementById("btn-fermer-form-envoye");
+const message = document.getElementById("message-formulaire-envoye");
+// Variable formulaire
+const couleurDefaut = "#fc4059";
+const couleurValide = "";
+// Fonction test des champs du formulaire
+function testFormulaire(evenement){
   let nombreTestReussi = 0;
-  
-  if(validerNomPrenom(prenom) == 1){
+  let chercherVilleNonChoisie = 0;
+  // Test prénom
+  if(prenom.value != "" && prenom.value.length >= 2){
     erreurPrenom.innerText = "";
+    prenom.style.borderColor =couleurValide;
     nombreTestReussi++;
   } else {
     erreurPrenom.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-  };
-  
-  if (validerNomPrenom(nom) == 1){
+    prenom.style.borderColor =couleurDefaut;
+  }
+  // Test nom 
+  if (nom.value != "" && nom.value.length >= 2){
     erreurNom.innerText = "";
+    nom.style.borderColor = couleurValide;
     nombreTestReussi++;
   } else {
     erreurNom.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-  };
-
-  if (validerMail(mail) == 1){
+    nom.style.borderColor = couleurDefaut;
+  }
+  // Test mail
+  if (/[\s\S]{1,}@([a-zA-Z0-9-_]{1,}[.])+[a-zA-Z]{2,3}$/.test(mail.value)){
     erreurMail.innerText = "";
+    mail.style.borderColor = couleurValide;
     nombreTestReussi++;
   } else {
-    erreurMail.innerText = "Veuiller entrer un couriel valide : forme attendue couriel@couriel.xxx";
-  };
-
-  if (validerDateNaissance(dateNaissance) == 1){
+    erreurMail.innerText = "Veuiller entrer un couriel valide";
+    mail.style.borderColor = couleurDefaut;
+  }
+  // Test age
+  if (new Date().getFullYear() - new Date(dateNaissance.value).getFullYear() >= 18){
     erreurNaissance.innerText = "";
+    dateNaissance.style.borderColor = couleurValide;
     nombreTestReussi++;
   } else {
-    erreurNaissance.innerText = "Vous devez entrer votre date de naissance. Et être au moins dans votre 15ème année";
-  };
-
-  if (validerNombreTournoi(nombreTournoi) == 1){
+    erreurNaissance.innerText = "Vous devez entrer votre date de naissance. Et être majeur";
+    dateNaissance.style.borderColor = couleurDefaut;
+  }
+  // Test nb tournois
+  if (/^(?!\-)[0-9]{1,}$/.test(nombreTournoi.value) && nombreTournoi.value < 99){
     erreurTournoi.innerText = "";
+    nombreTournoi.style.borderColor = couleurValide;
     nombreTestReussi++;
   } else {
     erreurTournoi.innerText = "Veuillez saisir un nombre de participation entre 0 et 99";
-  };
-
-  if (validerSiUneVilleChoisie(choixVille) == 1){
+    nombreTournoi.style.borderColor = couleurDefaut;
+  }
+  // Test choix ville
+  for (let ville of choixVille){
+    if(ville.checked == false){
+      chercherVilleNonChoisie++;
+    }
+  }
+  if(chercherVilleNonChoisie != choixVille.length){
     erreurVille.innerText = "";
     nombreTestReussi++;
   } else {
     erreurVille.innerText = "Veuillez choisir une ville";
-  };
-
-  if (validerConditionUtilisation(conditionUtilisation) == 1){
+  }
+  // Test condition utilisation
+  if (conditionUtilisation.checked){
     erreurCondition.innerText = "";
     nombreTestReussi++;
   } else {
     erreurCondition.innerText = "Vous devez vérifier que vous acceptez les termes et conditions.";
-  };
-
-  if (nombreTestReussi == 7){
-    alert("Le formulaire est envoyé, Merci de votre participation")
-  } else {
-    nePasSubmit(evenement);
   }
-});
+  // Gestion comportement formulaire et message formulaire valide
+  if (nombreTestReussi == 7){
+    return true;
+  } else {
+    return false;
+  }
+}
+// Fonction fermeture du message de validation formulaire
+fermerValidation1.addEventListener("click",function(){
+  message.style.display = "none";
+})
+fermerValidation2.addEventListener("click",function(){
+  message.style.display = "none";
+})
+// Déclencheur de validation et message erreur
+formulaire.addEventListener("submit", function(submit){
+  let test = testFormulaire();
+  if (test) {
+    message.style.display = "block";
+  }else{
+    submit.preventDefault();
+  }
+})
