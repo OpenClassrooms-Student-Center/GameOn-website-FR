c85eba5acbd29f72e26d7d81976f11b6cab0a9ab
florian <ffoezon@gmail.com>
2021-06-07 18:40:37 +0200

    insertion des commentaires

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,6 +14,21 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const form = document.getElementById("inscription");
 
+/* Expressions Régulières*/
+
+let emailRegex = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/ ;
+let nameRegex = /^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
+let numberRegex = /^[0-9 \-]+$/;
+
+/* boléen */
+let isValid = true;
+
+/* listes */
+let errorMessage = ["Entrez un prénom valide!",
+"Entrez un nom valide!","Entrez un email valide!","veuillez insérer une date",
+"veuillez insérer un chiffre!","selectionnez au moins une ville!",
+"Veuillez accepter les conditions!"];
+let regexList = [nameRegex,nameRegex,emailRegex,numberRegex,numberRegex];
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -22,7 +37,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalClose.forEach((close) => close.addEventListener("click", closeModal));
 
 /* submit form event */
-form.addEventListener("submit", formValidity);
+form.addEventListener("submit", validate);
 
 // launch modal form
 function launchModal() {
@@ -32,46 +47,85 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
-/* verify form validity */
-function formValidity(e) {
-  let prénom = form.elements["first"];
-  let nom = form.elements["last"];
-  let email = form.elements["email"];
-  let eventsQuantity = form.elements["quantity"];
-  let emailregex = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/ ;
-  let nameregex = /^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
 
-  if (nameregex.exec(prénom.value) == null) {
-    document.getElementById("nom").innerHTML = "Entrez un nom valide!"; 
+/* generate message */
+generateErrorMessage();
+
+/* Générer les messages d'erreurs */
+function generateErrorMessage(){
+  for (i = 0; i < 7; i++) {
+    formData[i].dataset.error = errorMessage[i]; 
+ }
+}
+
+/* verify form validity */
+function validate(e) {
+  textValid()
+  citiesValid();
+  isConditionSelected();
+
+  if (check() == false) {
     e.preventDefault();
   }
-  if (nameregex.exec(nom.value) == null) {
-    document.getElementById("prénom").innerHTML = "Entrez un prénom valide!";
-    e.preventDefault();
+  else {
+    closeModal();
+    alert("super!");
   }
-  if(emailregex.exec(email.value) == null) {
-    document.getElementById("mail").innerHTML = "Entrez un email valide!";
-    e.preventDefault();
-  }
+}
+
+/* fonctions éléments formulaire */
+function textValid() {
+  
+  for (i = 0; i < 5; i++) {
+    if (regexList[i].exec(form.elements[i].value) == null) {
+      form.elements[i].dataset.error = form.elements[i].value; 
+      formData[i].dataset.errorVisible = "true";
+      form.elements[i].dataset.errorVisible = "true";
+    }
+    else {
+      formData[i].dataset.errorVisible = "false";
+      form.elements[i].dataset.errorVisible = "false";
+    } 
+ }
+}
+
+function citiesValid(){
   if (isCheckboxSelected() == false){
-    document.getElementById("cities").innerHTML = "selectionnez au moins une ville!";
-    e.preventDefault();
+    formData[5].dataset.errorVisible = "true";
+    return false;
   }
-  if (eventsQuantity.value == ""){
-    document.getElementById("tournaments").innerHTML = "veuillez insérer un chiffre!";
-    e.preventDefault();
+  else {
+    formData[5].dataset.errorVisible = "false";
+  }
+}
+
+/* verify condition box selected */
+function isConditionSelected(){
+  if (document.getElementById('checkbox1').checked){
+    formData[6].dataset.errorVisible = "false";
+  }
+  else {
+    formData[6].dataset.errorVisible = "true";
+    return false;
   }
 }
 
 /* verify checkbox checked */
 
 function isCheckboxSelected(){
-  let MaxChoice = 6
-  for (i=1; i<MaxChoice+1; i++){
+  for (i=1; i<7; i++){
     if(document.getElementById('location'+i).checked) {
       return true;
     }
   }
 return false
 }
+function check () {
+  for ( i = 0 ; i<7; i++){
+    if (formData[i].dataset.errorVisible = "true"){
+      return false
+    }
+  }
+}
+
 
