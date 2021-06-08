3ac3f765d28b001b5ac5b457ec298508448fe7da
franzmongin <franz.mongin@gmail.com>
2021-06-08 19:14:42 +0200

    add close action to modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,22 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelectorAll(".content .close");
+
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
