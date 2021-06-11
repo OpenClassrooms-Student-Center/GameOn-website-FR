bd75139f80b0624113a9698e2e4e1b29a931ffb5
kirmann <k.dkle@hotmail.fr>
2021-06-11 14:13:56 +0200

    validation jusqu'au boutton  radion

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,37 +29,67 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-  // first name verification
+// test preventdefault
+document.getElementById("formGlobal").addEventListener("submit", function (e) {
+  let erreur;
 
-let formGlobal = document.getElementById('formGlobal');
+  //first name validation
+  const first = document.getElementById("first");
+  if (!first.value) {
+    erreur = "prénom manquant";
+    document.getElementById("firstSpan").innerHTML = erreur;
+  } else if (first.value.length < 2) {
+    erreur = "veuillez rentrer un prénom de 2 caractères minimum";
+    document.getElementById("firstSpan").innerHTML = erreur;
+  } else {
+    document.getElementById("firstSpan").innerHTML = "";
+  }
 
-formGlobal.first.addEventListener('change', function(){
-  validFirst(this);
-});
+  //last name validation
+  const last = document.getElementById("last");
+  if (!last.value) {
+    erreur = "nom manquant";
+    document.getElementById("lastSpan").innerHTML = erreur;
+  } else if (last.value.length < 2) {
+    erreur = "veuillez entrer un nom de 2 carctères minimum";
+    document.getElementById("lastSpan").innerHTML = erreur;
+  } else {
+    document.getElementById("lastSpan").innerHTML = "";
+  }
 
-const validFirst = (firstName) => {
-  let testFirst = (/^[a-zA-Z]/.test(firstName.value) && firstName.value.length>=2);
-  if(testFirst){
-    document.getElementById('firstSpan').innerHTML = ""
+  //email validation
+  const email = document.getElementById("email");
+  if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) {
+    document.getElementById("mailSpan").innerHTML = "";
+  } else {
+    erreur = "veuillez renseigner un email valide";
+    document.getElementById("mailSpan").innerHTML = erreur;
+  }
+
+  //birth date validation
+  const today = new Date().toISOString().split("T")[0];
+  let birthDate = document.getElementById("birthdate");
+  console.log(birthDate.value)
+  
+  if(birthDate.value > today) {
+    erreur =
+      "veuillez sélectionner une date de naissance antérieure au " + today;
+    document.getElementById("birthSpan").innerHTML = erreur;
+  } else {
+    document.getElementById("birthSpan").innerHTML = "";
+  }
+
+  // quantity of tournament validation
+  const quantity = document.getElementById('quantity');
+  if(/[0-9]/.test(quantity.value)){
+    document.getElementById("quantitySpan").innerHTML = "";
   }
   else{
-    document.getElementById('firstSpan').innerHTML = "veuillez renseigner un prénom valide de 2 caractères minimum"
+    erreur = "entrer un nombre entre 0 et 99";
+    document.getElementById("quantitySpan").innerHTML = erreur;
 
   }
-}
-
-// last name validation
-formGlobal.last.addEventListener('change', function(){
-  validLast(this);
+  if (erreur) {
+    e.preventDefault();
+  }
 });
-
-const validLast = (lastName) => {
-  let testLast = (/^[a-zA-Z]/.test(lastName.value) && lastName.value.length>=2);
-  if(testLast){
-    document.getElementById('lastSpan').innerHTML = "";
-  }
-  else{
-    document.getElementById('lastSpan').innerHTML = "veuillez renseigner un nom valide de 2 caractères minimum";
-
-  }
-}
