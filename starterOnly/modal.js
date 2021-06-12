8ccafa21e185a579563711bf53341754b9e092a0
Benoit <raymond.benoit@orange.fr>
2021-06-12 22:48:04 +0200

    fix(modal): add function close modal on click X - Close #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,10 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// Close modal form
+close.addEventListener("click", () => {
+  modalbg.style.display = "none";
+})
 
 
