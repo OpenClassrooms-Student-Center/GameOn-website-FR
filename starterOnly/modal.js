4fa5bdc918a16ac384f3d00ee92be3a38f3b52dd
Arthur Béquié <arthurbequie@gmail.com>
2021-06-16 17:12:54 +0200

    OP4-4: close modal button done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.forEach((cls) => cls.addEventListener("click",closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal event
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
 
 
