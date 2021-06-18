00564711f005017c3258c4bad7f388bb75741792
kirmann <k.dkle@hotmail.fr>
2021-06-18 09:26:13 +0200

    réécriture du code pour la validation de la date

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,7 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const close = document.querySelector(".close");
 const formGlobal = document.getElementById("formGlobal");
+const validationPage = document.querySelector(".sendValidation");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,30 +29,30 @@ close.addEventListener("click", closeModal);
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
-};
+}
 
-
-formGlobal.addEventListener("submit", function(e){
+formGlobal.addEventListener("submit", function (e) {
   let erreur;
 
   //email validation
-  
-  if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData[2].children[2].value)){
+
+  if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData[2].children[2].value)) {
     formData[2].removeAttribute("data-error");
     formData[2].removeAttribute("data-error-visible");
-  }else{
-    erreur = "veuillez entrer un mail valide"
-    formData[2].setAttribute("data-error", erreur)
-    formData[2].setAttribute("data-error-visible","true")
+  } else {
+    erreur = "veuillez entrer un mail valide";
+    formData[2].setAttribute("data-error", erreur);
+    formData[2].setAttribute("data-error-visible", "true");
   }
 
   //birth date validation
-  const today = new Date().toISOString().split("T")[0]; //défini la date d'aujourd'hui
-  let birthDate = document.getElementById("birthdate");
+  let birthDate = document.getElementById("birthdate");//récupère la date de naissance entrée par l'utilisateur
+  let parseBirthDate = Date.parse(birthDate.value);//converti la date entrée en milliseconde utc
+  const today = Date.now();// défini la date d'aujourd'hui en ms utc
 
-  if (birthDate.value > today || birthDate.value == "") {
+  if (parseBirthDate > today || !parseBirthDate) {
     erreur =
-      "veuillez sélectionner une date de naissance antérieure au " + today;
+      "veuillez sélectionner une date de naissance antérieure à aujourd'hui";
     birthDate.parentElement.setAttribute("data-error-visible", "true");
     birthDate.parentElement.setAttribute("data-error", erreur);
   } else {
@@ -70,20 +71,17 @@ formGlobal.addEventListener("submit", function(e){
     formData[4].setAttribute("data-error", erreur);
   }
 
-  
   //town validation
 
-  if(!formGlobal.location.value){      
-    erreur = "selectionnez une ville";      
-    formData[5].setAttribute("data-error-visible","true");
-    formData[5].setAttribute("data-error",erreur);    
-  }else{
+  if (!formGlobal.location.value) {
+    erreur = "selectionnez une ville";
+    formData[5].setAttribute("data-error-visible", "true");
+    formData[5].setAttribute("data-error", erreur);
+  } else {
     formData[5].removeAttribute("data-error-visible");
-    formData[5].removeAttribute("data-error");    
+    formData[5].removeAttribute("data-error");
   }
 
-
-  
   if (!formData[6].children[0].checked) {
     erreur = "merci d'accepter les conditions d'utilisation";
     formData[6].setAttribute("data-error-visible", "true");
@@ -93,9 +91,11 @@ formGlobal.addEventListener("submit", function(e){
     formData[6].removeAttribute("data-error");
   }
 
-  if(erreur) {
+  if (erreur) {
     e.preventDefault();
-  } else {
-    alert("formulaire envoyé")
   }
 });
+
+console.log(auj);
+console.log(parseDate);
+console.log(birthDate.value);
