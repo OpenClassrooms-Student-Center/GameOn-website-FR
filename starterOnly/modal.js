8d7e99352e2d8dfed5f471b115a59c31c5be64b6
florian <ffoezon@gmail.com>
2021-06-18 14:15:02 +0200

    code terminé

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,6 +7,8 @@ function editNav() {
   }
 }
 
+
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalbg2 = document.querySelector(".bground2");
@@ -22,9 +24,6 @@ let emailRegex = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0
 let nameRegex = /^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
 let numberRegex = /^[0-9 \-]+$/;
 
-/* boléen */
-let isValid = true;
-
 /* listes */
 let errorMessage = ["Entrez un prénom valide!",
 "Entrez un nom valide!","Entrez un email valide!","veuillez insérer une date",
@@ -34,16 +33,14 @@ let regexList = [nameRegex,nameRegex,emailRegex,numberRegex,numberRegex];
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 /* close modal event */
 modalClose.forEach((close) => close.addEventListener("click", closeModal));
 
 /* submit form event */
 form.addEventListener("submit", validate);
 
+/* close modal2 form */
 form2.addEventListener("click", closeModal);
-
-
 
 // launch modal form
 function launchModal() {
@@ -57,7 +54,6 @@ function closeModal() {
   modalbg.style.display = "none";
   modalbg2.style.display = "none";
 }
-
 /* verify form validity */
 function validate(e) {
   generateErrorMessage();
@@ -66,22 +62,23 @@ function validate(e) {
   isConditionSelected();
   if(check() == false) {
     e.preventDefault();
-  }  
+  }
+  else{
+    e.preventDefault();
+    modalbg.style.display = "none";
+    launchModal2();
+  }
 }
-launchModal2();
-
 /* Générer les messages d'erreurs */
 function generateErrorMessage(){
   for (i = 0; i < 7; i++) {
     formData[i].dataset.error = errorMessage[i]; 
  }
 }
-
 /* fonctions éléments formulaire */
 function textValid() {
-  
   for (i = 0; i < 5; i++) {
-    if (regexList[i].exec(form.elements[i].value) == null) {
+    if (regexList[i].exec(form.elements[i].value.trim()) == null) {
       formData[i].dataset.errorVisible = "true";
     }
     else {
@@ -89,7 +86,7 @@ function textValid() {
     } 
  }
 }
-
+/* vrify cities selected */
 function citiesValid(){
   if (isCheckboxSelected() == false){
     formData[5].dataset.errorVisible = "true";
