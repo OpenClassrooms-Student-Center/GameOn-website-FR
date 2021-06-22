a6507f0dec1503467fe0571152337de1a081cabb
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-06-22 12:53:06 +0200

    complete close modal issue

--- js/modal.js
+++ js/modal.js
@@ -8,9 +8,10 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalbg = document.querySelector(".bground") ;
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,11 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// add modal close event
+closeBtn.forEach( (close) => close.addEventListener("click", closeModal)) ;
+
+//add close modal function
+function closeModal(){
+  modalbg.style.display = "none" ;
+}
 
