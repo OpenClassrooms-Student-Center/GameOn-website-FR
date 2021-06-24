c2d9d204a7377dd6604fb54f59a388168bb22da2
Lisa Tashtiyeva <tashtiyeva.lisa@gmail.com>
2021-06-24 16:51:44 +0200

    Ajout de la fonctionnalité "fermer la modale"

--- modal.js
+++ modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose= document.querySelector("span.close");
+
+//----Gestion de l'affichage de la modale-----//
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "block"; //permet de 'rendre visible' la modale"
 }
 
-
+// close modal
+modalClose.addEventListener("click",closeModal);
+function closeModal() {
+  modalbg.style.display = "none"; //permet de ne pas 'rendre visible' la modale"
+};
