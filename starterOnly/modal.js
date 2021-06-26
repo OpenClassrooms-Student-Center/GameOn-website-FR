cacd41090fda68c2a2593d1bc95ea1c854b052b6
kirmann <k.dkle@hotmail.fr>
2021-06-26 16:43:32 +0200

    reset du formulaire à la confirmation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -194,12 +194,14 @@ formGlobal.addEventListener("submit", function (e) {
     
     closeModal();
     valid.style.display="block";
+    formGlobal.reset();
     e.preventDefault();
-  } else {
+    
     numberOfTownChecked = 0;
     e.preventDefault();
   }
 });
+
 
 // fermeture de la page de confirmation
 
