03ea6f2650aba10c6f4e47590a93f1768f766f7d
Arthur Béquié <arthurbequie@gmail.com>
2021-06-29 18:52:09 +0200

    The form modal is done.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,17 +1,12 @@
-function editNav() {
-  let x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
+/**
+ * @type {Element} - Dom elements
+ */
+const modalBg = document.querySelector(".bground");
 const messageModal = document.querySelector(".message-modal");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const content = document.querySelector(".content");
+const form = document.getElementById("form");
 const formData = document.querySelectorAll(".formData");
+const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelectorAll(".close");
 const closeMessageBtn = document.querySelectorAll(".btn-message");
 const firstName = document.getElementById("first");
@@ -24,42 +19,66 @@ const conditions = document.getElementById("checkbox1");
 const submitButton = document.getElementsByClassName("btn-submit");
 const error = document.getElementsByClassName("error-message");
 
-//Regular expressions
+/**
+ * @type {RegExp} - Regular expressions that allow to check the consistency of the data input
+ */
 const firstRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð'-]{2,}$/i;
 const lastRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð' -]{2,}$/i;
-const emailRegex = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/i;
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const quantityRegex = /^([\d])+$/i;
 
-
-// Launch modal form
-function launchModal() {
-    modalbg.style.display = "block";
+/**
+ * Function that allow to the navigation menu to be responsive
+ */
+function editNav() {
+    let topNav = document.getElementById("myTopnav");
+    if (topNav.className === "topnav") {
+        topNav.className += " responsive";
+    } else {
+        topNav.className = "topnav";
+    }
 }
-function closeModal() {
-    modalbg.style.display = "none";
+
+/**
+ * Functions that allow to open the form and message modal.
+ */
+function openModal() {
+    modalBg.style.display = "block";
+    modalBg.style.animation = "modalopen 0.8s";
 }
-
-
-function launchModalMessage() {
+function openModalMessage() {
     messageModal.style.display = "block";
+    modalBg.style.display = "none";
+}
+
+/**
+ * Functions that allow to close the form and message modal.
+ */
+function closeModal() {
+    modalBg.style.animation = "modalclose 3s forwards";
 }
 function closeModalMessage() {
     messageModal.style.display = "none";
+    form.submit();
 }
 
-// Launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+/**
+ * Event that listen the opening events of the form modal.
+ */
+modalBtn.forEach((btn) => btn.addEventListener("click", openModal));
 
-// Closing modal event
+/**
+ * Event that listen the closing of the form and message modals
+ */
 closeBtn.forEach((cls) => cls.addEventListener("click",closeModal));
-
 closeMessageBtn.forEach((cls) => cls.addEventListener("click",closeModalMessage));
 
-
-//Validation Event
+/**
+ * This function is call when the form is submitted. It's role is to prevent the submission in the cases where the data
+ * aren't consistent with what's expected.
+*/
 function validate(event) {
     const toValidate = Object.values(event.target);
-    console.log(formData[6].firstElementChild);
     function isChecked() {
         let acc = 0;
         for (let i = 5; i < 11; i+=1) {
@@ -109,101 +128,104 @@ function validate(event) {
         formData[6].setAttribute("data-error","Vous devez accepter les conditions d'utilisations");
         formData[6].setAttribute("data-error-visible","true");
     } else {
-        return launchModalMessage();
+        event.preventDefault();
+        openModalMessage();
     }
 }
 
-// Message validation events
+/**
+ * Theses functions listen all the inputs and send a validation message in the case where the input data aren't
+ * consistent
+ * @param event - Form submission event
+ */
 function onInputFirst(event) {
     if (event.target.value.length < 2) {
         if (event.target.value.length === 0) {
-            formData[0].setAttribute("data-error","Le champ du prénom ne peut être vide");
-            formData[0].setAttribute("data-error-visible","true");
+            event.target.parentElement.setAttribute("data-error","Le champ du prénom ne peut être vide");
+            event.target.parentElement.setAttribute("data-error-visible","true");
         } else {
-            formData[0].setAttribute("data-error","Le champ du prénom doit contenir 2 caractères ou plus");
-            formData[0].setAttribute("data-error-visible","true");
+            event.target.parentElement.setAttribute("data-error","Le champ du prénom doit contenir 2 caractères ou plus");
+            event.target.parentElement.setAttribute("data-error-visible","true");
         }
     } else if (!firstRegex.test(event.target.value)) {
-        formData[0].setAttribute("data-error","Le champ du prénom doit contenir que des caractères alphabétiques");
-        formData[0].setAttribute("data-error-visible","true");
+        event.target.parentElement.setAttribute("data-error","Le champ du prénom doit contenir que des caractères alphabétiques");
+        event.target.parentElement.setAttribute("data-error-visible","true");
     } else {
-        formData[0].removeAttribute("data-error");
-        formData[0].removeAttribute("data-error-visible");
+        event.target.parentElement.removeAttribute("data-error");
+        event.target.parentElement.removeAttribute("data-error-visible");
     }
 }
-
 function onInputLast(event) {
     if (event.target.value.length < 2) {
         if (event.target.value.length === 0) {
-            formData[1].setAttribute("data-error","Le champ du nom ne peut être vide");
-            formData[1].setAttribute("data-error-visible","true");
+            event.target.parentElement.setAttribute("data-error","Le champ du nom ne peut être vide");
+            event.target.parentElement.setAttribute("data-error-visible","true");
         } else {
-            formData[1].setAttribute("data-error","Le champ du nom doit contenir 2 caractères ou plus");
-            formData[1].setAttribute("data-error-visible","true");
+            event.target.parentElement.setAttribute("data-error","Le champ du nom doit contenir 2 caractères ou plus");
+            event.target.parentElement.setAttribute("data-error-visible","true");
         }
     } else if (!lastRegex.test(event.target.value)) {
-        formData[1].setAttribute("data-error","Le champ du nom doit contenir que des caractères alphabétiques");
-        formData[1].setAttribute("data-error-visible","true");
+        event.target.parentElement.setAttribute("data-error","Le champ du nom doit contenir que des caractères alphabétiques");
+        event.target.parentElement.setAttribute("data-error-visible","true");
     } else {
-        formData[1].removeAttribute("data-error");
-        formData[1].removeAttribute("data-error-visible");
+        event.target.parentElement.removeAttribute("data-error");
+        event.target.parentElement.removeAttribute("data-error-visible");
     }
 }
-
 function onInputEmail(event) {
     if (event.target.value === "") {
-        formData[2].setAttribute("data-error","Le champ e-mail ne peut pas être vide");
-        formData[2].setAttribute("data-error-visible","true");
+        event.target.parentElement.setAttribute("data-error","Le champ e-mail ne peut pas être vide");
+        event.target.parentElement.setAttribute("data-error-visible","true");
     } else if (!event.target.value.includes("@")) {
-        formData[2].setAttribute("data-error","Le champ e-mail doit contenir un @");
-        formData[2].setAttribute("data-error-visible","true");
+        event.target.parentElement.setAttribute("data-error","Le champ e-mail doit contenir un @");
+        event.target.parentElement.setAttribute("data-error-visible","true");
     } else if (!emailRegex.test(event.target.value)) {
-        formData[2].setAttribute("data-error","Le champ du courriel doit être conforme au format e-mail");
-        formData[2].setAttribute("data-error-visible","true");
+        event.target.parentElement.setAttribute("data-error","Le champ du courriel doit être conforme au format e-mail");
+        event.target.parentElement.setAttribute("data-error-visible","true");
     } else {
-        formData[2].removeAttribute("data-error");
-        formData[2].removeAttribute("data-error-visible");
+        event.target.parentElement.removeAttribute("data-error");
+        event.target.parentElement.removeAttribute("data-error-visible");
     }
 }
-
 function onInputBirth(event) {
     if (event.target.value === "") {
-        formData[3].setAttribute("data-error","Le champ de la date de naissance ne peut pas être vide");
-        formData[3].setAttribute("data-error-visible","true");
+        event.target.parentElement.setAttribute("data-error","Le champ de la date de naissance ne peut pas être vide");
+        event.target.parentElement.setAttribute("data-error-visible","true");
     } else {
-        formData[3].removeAttribute("data-error");
-        formData[3].removeAttribute("data-error-visible");
+        event.target.parentElement.removeAttribute("data-error");
+        event.target.parentElement.removeAttribute("data-error-visible");
     }
 }
-
 function onInputQuant(event) {
     if (event.target.value === "") {
-        formData[4].setAttribute("data-error","Le champ des participations doit contenir un nombre");
-        formData[4].setAttribute("data-error-visible","true");
+        event.target.parentElement.setAttribute("data-error","Le champ des participations doit contenir un nombre");
+        event.target.parentElement.setAttribute("data-error-visible","true");
     } else {
-        formData[4].removeAttribute("data-error");
-        formData[4].removeAttribute("data-error-visible");
+        event.target.parentElement.removeAttribute("data-error");
+        event.target.parentElement.removeAttribute("data-error-visible");
     }
 }
-
-// Check the case when user submitted the form and the validation avoid the submission because of a radio not checked.
-// Listen on all the radio input and if one is checked, remove the validation message
-checkbox.forEach((rad) => rad.addEventListener("input", function (event){
-    if (event.target.checked){
-        formData[5].removeAttribute("data-error");
-        formData[5].removeAttribute("data-error-visible");
-    }
-}))
-
 function onInputConditions(event){
     if (!event.target.checked) {
-        formData[6].setAttribute("data-error","Vous devez accepter les conditions d'utilisations");
-        formData[6].setAttribute("data-error-visible","true");
+        event.target.parentElement.setAttribute("data-error","Vous devez accepter les conditions d'utilisations");
+        event.target.parentElement.setAttribute("data-error-visible","true");
     } else {
-        formData[6].removeAttribute("data-error");
-        formData[6].removeAttribute("data-error-visible");
+        event.target.parentElement.removeAttribute("data-error");
+        event.target.parentElement.removeAttribute("data-error-visible");
     }
 }
+
+/**
+ * When the user submitted the form, check the case where the validation avoid the submission because
+ * of a radio not checked. Listen on all the radio input and if one is checked, remove the validation message.
+ * A event listener is use rather than a function to avoid of listening every radio input.
+ */
+checkbox.forEach((rad) => rad.addEventListener("input", function (event){
+    if (event.target.checked){
+        event.target.parentElement.removeAttribute("data-error");
+        event.target.parentElement.removeAttribute("data-error-visible");
+    }
+}))
 
 
 
