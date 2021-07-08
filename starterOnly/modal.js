5d605865d57caa334dbafe5dae39d9413de5f41b
Mélyssa Auberton <melyssa.auberton@gmail.com>
2021-07-08 18:15:58 +0200

    fixes #1, ajout de la fonction closeModal pour rendre le bouton X fonctionnel et fermer la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event
+modalCloseBtn.addEventListener("click", closeModal);
 
+// launch modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
