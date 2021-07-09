d50292af4881d775b20d714e73f944cdf17c723c
Benoît Raymond <raymond.benoit@orange.fr>
2021-07-09 02:34:44 +0200

    fix/Résolution du bug de fermeture de la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,8 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal form
+close.addEventListener("click", () => {
+  modalbg.style.display = "none";
+})
 
