bf87fc298af8bbf8cbf4d71363cbad18a1e1e4b1
Quentin <quentin.bonnin@outlook.fr>
2021-07-11 12:18:13 -0400

    fix OpenClassrooms-Student-Center#1 btn close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,18 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
