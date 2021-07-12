631fe4941ba5144783baffe33ffe1e05fc5a4c21
kirmann <k.dkle@hotmail.fr>
2021-07-12 11:36:34 +0200

    changement de var en const dans la fonction editnav

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
