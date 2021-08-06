5ca3e85e62ff8af7d04fd10af526840be44e35ec
Anthony LC <gad_77@hotmail.fr>
2021-08-06 04:27:25 +0200

    Correction des commentaires JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,7 +13,7 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelectorAll("#close");
 
-// Mes VARIABLES
+// VARIABLES
 let myForm = document.getElementById("myForm");
 let firstName = document.getElementById("first_name");
 let lastName = document.getElementById("last_name");
@@ -28,70 +28,63 @@ let locationSix = document.getElementById("location6");
 let checkCondition = document.getElementById("checkbox1");
 let msgConfirm = document.getElementById("message-confirm");
 
-// MES FONCTIONS
-// A l'envoi on vérifie que la fonction checkInputs return true //
+// My FUNCTIONS
+// When sending the form we check that checkInputs return true
 myForm.addEventListener("submit", (e) => {
   e.preventDefault();
   checkInputs();
-  // Si return true alors on fait apparaitre le msg de validation
   if (checkInputs() == true) {
     msgConfirm.style.display = "block";
     messageValid();
-    // Sinon on return false est le formulaire ne s'envoie pas.
   } else {
     return false;
   }
 });
 
 function checkInputs() {
-  // Conditions if NAME //
+  // Conditions if FIRSTNAME
   let firstError = document.getElementById("first_error");
-  // Si firstName est égal a vide ou inferieux a deux = msg d'erreur + return false.
   if (firstName.value == "" || firstName.value.length < 2) {
     firstError.innerHTML =
-      "Veuillez entrer 2 caractères ou plus pour le champs du prénom";
+      "Veuillez entrer 2 caractères ou plus pour le champs du prénom.";
     firstError.style.color = "Red";
     firstError.style.fontSize = "20px";
     return false;
-    // Sinon pas de msg d'erreur + return true.
   } else {
     firstError.innerHTML = "";
   }
 
-  // Conditions if LASTNAME //
+  // Conditions if LASTNAME
   let lastError = document.getElementById("last_error");
-  // Si lastName est égal a vide ou inferieur a deux = msg d'erreur + return false.
   if (lastName.value == "" || lastName.value.length < 2) {
     lastError.innerHTML =
-      "Veuillez entrer 2 caractères ou plus pour le champs du nom";
+      "Veuillez entrer 2 caractères ou plus pour le champs du nom.";
     lastError.style.color = "red";
     lastError.style.fontSize = "20px";
     return false;
-    // Sinon pas de msg d'erreur + return true.
   } else {
     lastError.innerHTML = "";
   }
 
-  // Conditions if EMAIL //
+  // Conditions if EMAIL
   let emailError = document.getElementById("email_error");
   let myRegex =
     /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
-  // Si myRegex est different de la valeur de email ou si email est égal a vide = msg d'erreur + return false.
   if (!myRegex.test(email.value) || email.Value == "") {
-    emailError.innerHTML = "Veuillez rentrer une adresse mail valide";
+    emailError.innerHTML = "Veuillez rentrer une adresse mail valide.";
     emailError.style.color = "red";
     emailError.style.fontSize = "20px";
     return false;
-    // Sinon pas de msg d'erreur + return true.
   } else {
     emailError.innerHTML = "";
   }
 
+  //Conditions if BIRTHDATE
   let birthdate = document.getElementById("birthdate");
   let birthdateError = document.getElementById("birthdate_error");
   let regex = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
   if (!regex.test(birthdate.value)) {
-    birthdateError.innerHTML = "Veuillez rentrer une adresse mail valide";
+    birthdateError.innerHTML = "Vous devez entrer votre date de naissance.";
     birthdateError.style.color = "red";
     birthdateError.style.fontSize = "20px";
     return false;
@@ -99,9 +92,8 @@ function checkInputs() {
     birthdateError.innerHTML = "";
   }
 
-  // Conditions if TOURNOI //
+  // Conditions if TOURNOI
   let numberError = document.getElementById("tournoi_error");
-  // Si tournoiNumber est égal à 0 ou si la valeur de tournoiNumber n'es pas un nombre = msg d'erreur + return false.
   if (tournoiNumber.value == "" || isNaN(tournoiNumber.value)) {
     numberError.innerHTML = "Veuillez rentrer au moins un chiffre";
     numberError.style.color = "red";
@@ -111,9 +103,8 @@ function checkInputs() {
     numberError.innerHTML = "";
   }
 
-  // Conditions if CHECKBOX //
+  // Conditions if CHECKBOX
   let locationError = document.getElementById("location_error");
-  // Si au moins un des six inputs est checked = pas de msg d'erreur + return true.
   if (
     locationUn.checked ||
     locationDeux.checked ||
@@ -123,24 +114,21 @@ function checkInputs() {
     locationSix.checked
   ) {
     locationError.innerHTML = "";
-    // Sinon si aucun input n'est checked = msg d'erreur + return false
   } else {
-    locationError.innerHTML = "Vous devez choisir une option";
+    locationError.innerHTML = "Vous devez choisir une option.";
     locationError.style.color = "red";
     locationError.style.fontSize = "20px";
     return false;
   }
 
-  // Conditions if REGLEMENT //
+  // Conditions if REGLEMENT
   let conditionError = document.getElementById("condition_error");
-  // Si checkbox n'est pas coché = msg d'erreur + return false.
   if (!checkCondition.checked) {
     conditionError.innerHTML =
-      "Vous devez verifier que vous acceptez les termes et conditions";
+      "Vous devez verifier que vous acceptez les termes et conditions.";
     conditionError.style.color = "red";
     conditionError.style.fontSize = "20px";
     return false;
-    // Sinon pas de msg d'erreur + return true.
   } else {
     conditionError.innerHTML = "";
   }
@@ -148,7 +136,7 @@ function checkInputs() {
 }
 
 function messageValid() {
-  // Au clique sur l'input le message de validation disparait + envoie du formulaire.
+  // When clicking on the button, the validation message disappears + send form
   let cliqueOk = document.getElementById("validate");
   cliqueOk.addEventListener("click", () => {
     msgConfirm.style.display = "none";
