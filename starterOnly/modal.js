2a04b78f2191d1d1b1c86ad2cf8d3d0b16d67b67
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-08-18 22:39:31 +0200

    add validations to form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -32,3 +32,64 @@ closeBtn.addEventListener('click', closeModal);
 function closeModal() {
     modalbg.style.display = 'none';
 }
+
+// ***************** Implémenter entrées du formulaire*******************
+const firstNameInputEl = document.getElementById('first');
+const lastNameInputEl = document.getElementById('last');
+const emailInputEl = document.getElementById('email');
+const birthDateInputEl = document.getElementById('birthdate');
+const numberParticipateEl = document.getElementById('quantity');
+const location1 = document.getElementById('location1');
+const location2 = document.getElementById('location2');
+const location3 = document.getElementById('location3');
+const location4 = document.getElementById('location4');
+const location5 = document.getElementById('location5');
+const location6 = document.getElementById('location6');
+const checkBox1 = document.getElementById('checkbox1');
+
+const validate = () => {
+    let isFormValid = true;
+
+    //first name validation
+    const firstName = firstNameInputEl.value;
+    const isFirstNameValid = Validation.minLength(firstName, 2) && Validation.isRequired(firstName);
+    isFormValid = isFormValid && isFirstNameValid;
+
+    //last name validation
+    const lastName = lastNameInputEl.value;
+    const isLastNameValid = Validation.minLength(lastName, 2) && Validation.isRequired(lastName);
+    isFormValid = isFormValid && isLastNameValid;
+
+    //email validation
+    const email = emailInputEl.value;
+    const isEmailValid = Validation.checkEmail(email);
+    isFormValid = isFormValid && isEmailValid;
+
+    //birthdate validation
+    const birthDate = birthDateInputEl.value;
+    const isBirthDateValid = Validation.checkBirthDate(birthDate);
+    isFormValid = isFormValid && isBirthDateValid;
+
+    //participate count validation
+    const numberParticipate = numberParticipateEl.value;
+    const isNumberParticipateValid = Validation.checkNumber(numberParticipate);
+    isFormValid = isFormValid && isNumberParticipateValid;
+
+    //location validation
+    const locationValues = [
+        location1.checked,
+        location2.checked,
+        location3.checked,
+        location4.checked,
+        location5.checked,
+        location6.checked,
+    ];
+    const isLocationValid = Validation.isAnyChecked(locationValues);
+    isFormValid = isFormValid && isLocationValid;
+
+    //is condition accept
+    const checkBoxValid = checkBox1.checked;
+    isFormValid = isFormValid && checkBoxValid;
+
+    return isFormValid;
+};
