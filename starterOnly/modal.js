af2c4322d42b901802f292999492215b78da9d2c
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-08-18 22:27:26 +0200

    add pretier configuration

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,34 +1,34 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById('myTopnav');
+    if (x.className === 'topnav') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'topnav';
+    }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeBtn = document.getElementById("close-btn");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
+const closeBtn = document.getElementById('close-btn');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = 'block';
 }
 
 // ******************* Ferme modal***************
 
 // close modal event
 
-closeBtn.addEventListener("click", closeModal);
+closeBtn.addEventListener('click', closeModal);
 
 // close Modal function
 
 function closeModal() {
-  modalbg.style.display = "none";
+    modalbg.style.display = 'none';
 }
