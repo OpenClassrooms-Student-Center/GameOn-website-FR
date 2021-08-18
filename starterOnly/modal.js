f2f1ab0b059f5257294846bedbce4edc69c588c0
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-08-18 13:30:26 +0200

    close modal by on click X icon

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementById("close-btn");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,14 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// ******************* Ferme modal***************
 
+// close modal event
+
+closeBtn.addEventListener("click", closeModal);
+
+// close Modal function
+
+function closeModal() {
+  modalbg.style.display = "none";
+}
