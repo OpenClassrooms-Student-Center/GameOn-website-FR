763fa99c1ea1d2987e102d8a3210499636779502
Dequé <carolinedeque@gmail.com>
2021-09-13 12:53:33 +0200

    ajout #2 implementer les entrées du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,3 +25,73 @@ function launchModal() {
 closeModal.addEventListener("click", () => {
   modalbg.style.display = "none";
 });
+
+//Regex
+let checkfirst,
+  checkLast = /^[a-zA-Z-]*$/;
+let checkMail =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let checkQuantity = /^[0-9]*$/;
+
+//Functions Form
+
+function validateFirst() {
+  let first = form.elements["first"];
+  let error = document.getElementById("error");
+  if (checkfirst.test(first.value) === false) {
+    first.classlist.add("input-error");
+    error.innerText = "Le prénom doit contenir plus de 2 caractères";
+    return false;
+  } else {
+    email.classlist.remove("input-error");
+    email.classlist.add("input-validate");
+    error.innerText = "";
+    return true;
+  }
+}
+function validateLast() {
+  let last = form.elements["last"];
+  let error = document.getElementById("error");
+  if (
+    checkLast.test(last.value) === false &&
+    (last.value.length > 0 || last.value.length < 2)
+  ) {
+    last.classlist.add("input-error");
+    error.innerText = "Le nom doit contenir plus de 2 caractères";
+    return false;
+  } else {
+    email.classlist.remove("input-error");
+    email.classlist.add("input-validate");
+    error.innerText = "";
+    return true;
+  }
+}
+function validateEmail() {
+  let email = form.elements["email"];
+  let error = document.getElementById("error");
+  if (checkMail.test(email.value) === false) {
+    email.classlist.add("input-error");
+    error.innerText = "Saississez un email valide";
+    return false;
+  } else {
+    email.classlist.remove("input-error");
+    email.classlist.add("input-validate");
+    error.innerText = "";
+    return true;
+  }
+}
+
+function validateQuantity() {
+  let quantity = form.elements["quantity"];
+  let error = document.getElementById("error");
+  if (checkQuantity.test(quantity.value) === false) {
+    quantity.classlist.add("input-error");
+    error.innerText = "Le nombre de tournoi doit être indiqué ";
+    return false;
+  } else {
+    quantity.classlist.remove("input-error");
+    quantity.classlist.add("input-validate");
+    error.innerText = "";
+    return true;
+  }
+}
