a3761eb74f56796c55e22ead5101ecdbbf3e321c
Stebenwolf <benjamin@the-org.eu>
2021-09-22 22:44:31 +0200

    ADD validate form function (warning otherwise)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,8 +22,15 @@ function launchModal() {
 
 // Close Modal function
 // when we click the x, the modal display property is changed to none.
+const contentbox = document.querySelector(".content");
 function closeModal() {
+  // Première option: on ferme purement et simplement
   modalbg.style.display = "none";
+  // Seconde option: on fait remonter avec animation, opacity = 0
+  /* modalbg.style.animation = "modalclose 1s forwards"; */ // pb: on ne peut plus afficher le modal...
+  /* contentbox.style.animation = "modalclose 1s forwards";
+  modalbg.style.animation = "modalclosepure 1s ease-in 1s forwards"; */
+  
 }
 
 
@@ -32,53 +39,50 @@ function closeModal() {
 // otherwise the form cannot be submitted (and the values entered stay)
 function validate() {
 
-  // on récupère la longueur du prénom
-  const formFirst = document.querySelector("#first").value;
-  
-  // on récupère la longueur du nom
-  const formLast = document.querySelector("#last").value;
-  
-  // on récupère la valeur de l'input email
-  const formEmail = document.querySelector("#email").value;
-  
-  // on récupère le nombre de concours
-  const formQuantity = document.querySelector("#quantity").value;
-  
-  // un bouton radio est-il sélectionné?
-  // cf plus bas: fonction radioChecked 
-
-  // on récupère la valeur associée au check des conditions générales
-  const formGeneral = document.querySelector("#checkbox1").checked;
+  // on récupère les valeurs des différents champs
+  const formFirst = document.querySelector("#first").value; // prénom
+  const formLast = document.querySelector("#last").value; // nom
+  const formEmail = document.querySelector("#email").value; // email
+  const formBirthdate =  document.querySelector("#birthdate").value; // date naissance
+  const formQuantity = document.querySelector("#quantity").value; // nombre de concours
+  // une ville est elle sélectionnée ? cf fonction radioChecked plus bas 
+  const formGeneral = document.querySelector("#checkbox1").checked; // conditions générales acceptées
 
   // on va à présent tester pour chacune des valeurs si elle correspond à ce qui est attendu.
-  // Si oui, tout se déroule "en silence", si non on récupère un message d'erreur dans la console.
-  if (formFirst.length < 2) {
-    console.log("Prénom trop court Coucou");
-  }
-  if (formLast.length < 2) {
-    console.log("Nom trop court");
-  }
-  if (!validateEmail(formEmail)) {
-    console.log("Email paaaas ok");
-  }
-  if (!Number(formQuantity)) {
-    console.log("Paaaas numérique le nombre de concours");
-  }
-  if (!radioChecked()) {
-    console.log("Paaaas de ville sélectionnée!!");
-  }
-  if (!formGeneral) {
-    console.log("CG paaaaas acceptées");
+  // Si oui, tout se déroule "en silence", si non on affiche un message d'erreur dans la console.
+ 
+  // par souci de simplicité/éviter répétitions, on stocke l'ensemble des conditions dans une array
+  const conditions = [
+    formFirst.length < 2,
+    formLast.length < 2,
+    !validateEmail(formEmail),
+    formBirthdate.length === 0 || validateBirthdate(formBirthdate),
+    !Number.isInteger(Number(formQuantity)) || Number(formQuantity) < 0 || Number(formQuantity) > 99 || formQuantity === "",
+    !radioChecked(),
+    !formGeneral
+  ];
+
+  // on récupère l'ensemble des éléments ayant la classe "warning"
+  const warningText = document.querySelectorAll(".warning");
+
+  // on créé une variable qui va stocker le nombre de champs invalide
+  let invalidInput = 0;
+
+  // pour chaque condition, on vérifie si elle est vérifiée
+  for (let i = 0; i<conditions.length; i++) {
+    if (conditions[i]) { // si oui (ie, si le champ est invalide): 
+      warningText[i].style.display = "inline-block"; // on affiche un message d'erreur 
+      invalidInput++; // et on incrémente le nombre de champs invalide
+    } else { // si non (ie si le champ est valide)
+      warningText[i].style.display =" none"; // on fait disparaitre un éventuel message d'erreur
+    }
   }
 
-  // On vérifie que l'ensemble des conditions est respecté. Si l'un des champs n'est pas valide on empêche la validation du formulaire.
-  if (formFirst.length < 2 || formLast.length < 2 || !validateEmail(formEmail) || !Number(formQuantity) || !radioChecked() || !formGeneral) {
+  // si le nombre d'entrées invalides est supérieur à 0, on bloque l'envoi.
+  if (invalidInput > 0) {
     return false;
-  } else {
-    return true;
   }
 }
-
 
 // Validate email function
 // Source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript?page=1&tab=votes#tab-top
@@ -98,4 +102,15 @@ function radioChecked() {
     }
   }
   return checkedItems;
+}
+
+// Validate Birthdate
+// Adapted from: https://stackoverflow.com/questions/6177975/how-to-validate-date-with-format-mm-dd-yyyy-in-javascript
+function validateBirthdate(testDate) {
+  var date_regex = /^(19|20)\d{2}\/(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])$/;
+  if (!(date_regex.test(testDate))) {
+    return false;
+  } else {
+    return true;
+  }
 }
