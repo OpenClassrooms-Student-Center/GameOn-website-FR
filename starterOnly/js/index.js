ab4a9a00e2ab1df9cc6c60fc5e5f44851e1ef445
greg <lokkotarakun@gmail.com>
2021-09-24 16:13:36 +0200

    Finalisation de la validation du formulaire

--- starterOnly/js/index.js
+++ starterOnly/js/index.js
@@ -8,18 +8,25 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const closeBtn = document.querySelector(".close");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector(".bground"),
+  modalBtn = document.querySelectorAll(".modal-btn"),
+  closeBtn = document.querySelector(".close"),
+  formData = document.querySelectorAll(".formData"),
+  firstname = document.querySelector("#firstname"),
+  lastname = document.querySelector("#lastname"),
+  email = document.querySelector("#email"),
+  birthdate = document.querySelector("#birthdate"),
+  quantity = document.querySelector("#quantity"),
+  terms = document.querySelector("#terms"),
+  errorMsg = document.querySelectorAll(".errorMsg"),
+  newsletter = document.querySelector("#newsletter");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+modalBtn.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    modalbg.style.display = "block";
+  })
+);
 
 // close modal event
 closeBtn.addEventListener("click", closeModal);
@@ -28,20 +35,11 @@ function closeModal() {
 }
 
 // afficher message d'erreur
-let firstname = document.querySelector("#firstname"),
-    lastname = document.querySelector("#lastname"),
-    email = document.querySelector("#email"),
-    birthdate = document.querySelector("#birthdate"),
-    quantity = document.querySelector("#quantity"),
-    terms = document.querySelector("#terms");
-
-let errorMsg = document.querySelectorAll(".errorMsg"),
-    option = document.querySelectorAll(".checkbox-input");
-
 const inputs = [firstname, lastname, email, birthdate, quantity, terms];
+
 for (let i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener("input", ()=> {
-    if(!inputs[i].validity.valid) {
+  inputs[i].addEventListener("input", () => {
+    if (!inputs[i].validity.valid) {
       errorMsg[i].classList.remove("hidden");
       inputs[i].classList.remove("valid");
       inputs[i].classList.add("invalid");
@@ -49,8 +47,54 @@ for (let i = 0; i < inputs.length; i++) {
       inputs[i].classList.remove("invalid");
       inputs[i].classList.add("valid");
       errorMsg[i].classList.add("hidden");
-    } 
+    }
   });
 }
 
+let choice,
+  option = document.querySelectorAll(".option"),
+  isChecked = false;
 
+for (let i = 0; i < option.length; i++) {
+  const elt = option[i];
+  elt.addEventListener("click", () => {
+    isChecked = option[i].checked;
+    choice = option[i].value;
+  });
+}
+
+function checkForm() {
+  if (
+    firstname.validity.valid &&
+    lastname.validity.valid &&
+    email.validity.valid &&
+    birthdate.validity.valid &&
+    quantity.validity.valid &&
+    isChecked &&
+    terms.checked
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function submitForm() {
+  event.preventDefault();
+  if (checkForm()) {
+    const info = {
+      firstname: firstname.value,
+      lastname: lastname.value,
+      email: email.value,
+      birthdate: birthdate.value,
+      quantity: quantity.value,
+      option: choice,
+      terms: terms.checked,
+      newsletter: newsletter.checked,
+    };
+    console.log(info);
+    console.log("on change le contenu pour afficher les remerciements");
+  } else {
+    console.log("oops une erreur");
+  }
+}
