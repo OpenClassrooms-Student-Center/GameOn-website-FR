fc93a9e1659555e94442099f8a509eaa99054efb
nicolas le meur <nicolas.lemeur9@gmail.com>
2021-09-30 15:29:35 +0200

    #issue 1 : fermer la modale
    -ajout de la constante 'closeModalBtn' dans le DOM, qui pointe ver la classe '.close' dans le document html
    -ajout de l'evenement qui lance la fonction auclique
    -ajout de la fonction closeModal() pour desactiver la fenetre modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -11,13 +11,28 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll(".close");  // LA CONSTANTE POINTE VERS l'element avec la class .close
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+// Pour chaque "element" pointé dans: closeModalBtn à l'interieur du DOM
+// => au clique sur cet élement, utilise la function closeModal()
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+    //lorceque closeModal() est appellé, on modifie la propriété display
+    // de la classe(.bground) associé à la constante (modalbg) dans le DOM en 'none'
+
+
 
 
