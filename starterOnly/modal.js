cccb3e681973e2f86bdc4ca9d82df4cece9e2eaa
DevWeb13 <giuliano.loic@sfr.fr>
2021-10-01 10:24:44 +0200

    feat : fermeture modale #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,24 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+console.log(modalBtn);
 const formData = document.querySelectorAll(".formData");
+//Modal close
+const close = document.querySelector(".close");
+// console.log(close);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
 }
 
-
+// close modal
+function closeModal() {
+  modalBg.style.display = "none";
+}
+close.addEventListener("click", closeModal);
