2fb4e85cb761d76d7c3e05433674629a465118ce
rayanadir <88830520+rayanadir@users.noreply.github.com>
2021-10-05 15:47:27 +0200

    Ajout fonctionnalité fermer formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,26 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById('form');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
-
+function closeForm() {
+    form.style.display = "none";
+}
