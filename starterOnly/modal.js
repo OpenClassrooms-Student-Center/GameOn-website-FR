085a24a0c88d0c242b10fb23cd99e2dfb0ff6eef
rayanadir <88830520+rayanadir@users.noreply.github.com>
2021-10-05 22:03:54 +0200

    mise à jour vérification nom prénom email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,9 +14,11 @@ const formData = document.querySelectorAll(".formData");
 const form = document.getElementById('form');
 const erreur_prenom = document.querySelector(".erreur_prenom");
 const erreur_nom = document.querySelector(".erreur_nom");
+const erreur_email = document.querySelector(".erreur_email");
 
 erreur_prenom.style.display = "none";
 erreur_nom.style.display = "none";
+erreur_email.style.display = "none";
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -35,13 +37,17 @@ async function checkFirstName() {
     const firstName = document.getElementById('first').value;
     const length = document.getElementById('first').value.length;
     const erreur_prenom = document.querySelector(".erreur_prenom");
-    if (length < 2) {
-        console.log("invalide");
-        erreur_prenom.style.display = "initial";
-        erreur_prenom.style.color = "red";
-        erreur_prenom.style.fontSize = "12px";
+    if (length !== 0) {
+        if (length < 2) {
+            console.log("invalide");
+            erreur_prenom.style.display = "initial";
+            erreur_prenom.style.color = "red";
+            erreur_prenom.style.fontSize = "14px";
+        } else {
+            console.log("valide");
+            erreur_prenom.style.display = "none";
+        }
     } else {
-        console.log("valide");
         erreur_prenom.style.display = "none";
     }
 }
@@ -50,25 +56,35 @@ async function checkLastName() {
     const lastName = document.getElementById('last').value;
     const length = document.getElementById('last').value.length;
     const erreur_nom = document.querySelector(".erreur_nom");
-    if (length < 2) {
-        console.log("invalide");
-        erreur_nom.style.display = "initial";
-        erreur_nom.style.color = "red";
-        erreur_nom.style.fontSize = "12px";
+    if (length !== 0) {
+        if (length < 2) {
+            console.log("invalide");
+            erreur_nom.style.display = "initial";
+            erreur_nom.style.color = "red";
+            erreur_nom.style.fontSize = "14px";
+        } else {
+            console.log("valide");
+            erreur_nom.style.display = "none";
+        }
     } else {
-        console.log("valide");
         erreur_nom.style.display = "none";
     }
 }
 
 async function checkEmail() {
     const email = document.getElementById('email').value;
-    const length = document.getElementById('last').value.length;
-    for (let i = 0; i < length; i++) {
-        if (email[i] !== '@') {
-            console.log("email invalide");
+    const length = document.getElementById('email').value.length;
+    var regex = /[@]/g;
+    console.log(email.search(regex));
+    if (length !== 0) {
+        if (email.search(regex) == -1) {
+            erreur_email.style.display = "initial";
+            erreur_email.style.color = "red";
+            erreur_email.style.fontSize = "14px";
         } else {
-            console.log("email valide");
+            erreur_email.style.display = "none";
         }
+    } else {
+        erreur_email.style.display = "none";
     }
 }
