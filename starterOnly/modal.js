2e8ccf4359bb3074f8ff4e25feecc4d5552c6aeb
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-10-06 17:57:53 +0200

    Event close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,14 +7,22 @@ function editNav() {
   }
 }
 
-// DOM Elements
+/*---------- DOM ELEMENTS ----------*/
+
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelectorAll("#close");
 
+/*---------- EVENT ----------*/
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//close modal event
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
+/*---------- FUNCTION ----------*/
 
 // launch modal form
 function launchModal() {
