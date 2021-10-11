9b5b37a290e7c32499eb5a4e16084d1be0d44052
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2021-10-11 10:36:01 +0200

    moduf validatelocation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -116,7 +116,7 @@ function removeAlerts() {
 //Validation location
 function validateLocation() {
   const input = locationInput;
-  for (var i = 0; i < input.length; i++) {
+  for (let i = 0; i < input.length; i++) {
     const validityState = input[i].checked;
     if (validityState) {
       return true;
