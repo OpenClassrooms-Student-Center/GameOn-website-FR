493a4b716d79ea20960f81bd05a12c3e03b174ba
rayanadir <rayan.dahmena@gmail.com>
2021-10-14 18:51:25 +0200

    simplification du code + correction image

--- modal.js
+++ modal.js
@@ -13,15 +13,15 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const form = document.getElementById('form');
 const content = document.querySelector('.content');
-const content_valid = document.querySelector('.content-valid');
-const date = document.getElementById('birthdate');
-const quantity = document.getElementById('quantity');
 const hero_section = document.querySelector('.hero-section');
 const topnav = document.querySelector('.topnav');
 const footer = document.querySelector('footer');
 const confirmed = document.querySelector('.confirmed');
 
-content_valid.style.display = "none";
+var modalBody = document.getElementById('modal-body');
+var initial = modalBody.innerHTML;
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -29,21 +29,16 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
     modalbg.style.display = "block";
+    content.setAttribute('content-mobile', true);
+    modalbg.setAttribute('bground-mobile', true);
 }
 
 // fermer formulaire
 function closeForm() {
     form.style.display = "none";
+    modalBody.innerHTML = initial;
 }
 
-// fermer la confirmation
-function closeConfirmation() {
-    content_valid.style.display = "none";
-    modalbg.style.display = 'none';
-    hero_section.style.display = 'initial';
-    topnav.style.display = "initial";
-    footer.style.display = "initial";
-}
 
 // verification du nom et prénom
 function checkFirstAndLastName(input, type) {
@@ -53,6 +48,7 @@ function checkFirstAndLastName(input, type) {
     if (test) {
         input.parentElement.removeAttribute('data-error');
         input.parentElement.removeAttribute('data-error-visible');
+        return true;
     } else {
         if (type == 'first') {
             input.parentElement.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
@@ -61,6 +57,7 @@ function checkFirstAndLastName(input, type) {
             input.parentElement.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du nom.');
         }
         input.parentElement.setAttribute('data-error-visible', true);
+        return false;
     }
 }
 
@@ -71,102 +68,113 @@ function checkEmail(input) {
     if (test) {
         input.parentElement.removeAttribute('data-error');
         input.parentElement.removeAttribute('data-error-visible');
+        return true;
     } else {
         input.parentElement.setAttribute('data-error', 'Vous devez entrer une adresse email valide.');
         input.parentElement.setAttribute('data-error-visible', true);
+        return false;
     }
 }
+
+
+
 
 // verification de la date de naissance
 function checkBirthdate(input) {
     if (input.value) {
         input.parentElement.removeAttribute('data-error');
         input.parentElement.removeAttribute('data-error-visible');
+        const currentYear = new Date();
+        const selectedYear = new Date(document.getElementById("birthdate").value);
+
+        // Vérification de la majorité de l'utilisateur
+        if (checkAge(currentYear, selectedYear) >= 18) {
+            input.parentElement.removeAttribute('data-error');
+            input.parentElement.removeAttribute('data-error-visible');
+            return true;
+        } else {
+            input.parentElement.setAttribute('data-error', 'Vous devez être majeur.');
+            input.parentElement.setAttribute('data-error-visible', true);
+        }
     } else {
         input.parentElement.setAttribute('data-error', 'Vous devez entrer votre date de naissance.');
         input.parentElement.setAttribute('data-error-visible', true);
+        return false;
     }
 }
 
 // verification du nombre
 function checkQuantity(input) {
-    if (input.value) {
+    var regex = /^\d$/;
+    const test = regex.test(input.value);
+    if (test) {
         input.parentElement.removeAttribute('data-error');
         input.parentElement.removeAttribute('data-error-visible');
+        return true
+    } else {
+        input.parentElement.setAttribute('data-error', 'Vous devez entrer un nombre.');
+        input.parentElement.setAttribute('data-error-visible', true);
+        return false;
     }
 }
 
 // fonction de la validation
 function validate() {
+    const first = document.getElementById('first');
+    const last = document.getElementById('last');
+    const date = document.getElementById('birthdate');
+    const quantity = document.getElementById('quantity');
     const radio_value = document.querySelector('input[name="location"]:checked');
+    const email = document.getElementById('email');
+
+    let isFirstValid = false;
+    let isLastValid = false;
+    let isEmailValid = false;
+    let isBirthdateValid = false;
+    let isQuantityValid = false;
+    let isRadioValid = false;
+    let isCheckboxValid = false;
+
     if (radio_value == null) {
         const radio_parent = document.querySelector('input[name="location"]').parentNode;
         radio_parent.setAttribute('data-error', 'Vous devez choisir une option.');
         radio_parent.setAttribute('data-error-visible', true);
+    } else {
+        isRadioValid = true;
     }
     const check_value = document.getElementById('checkbox1').checked;
     if (check_value == false) {
         const check_parent = document.querySelector('input[id="checkbox1"]').parentNode;
         check_parent.setAttribute('data-error', 'Vous devez vérifier que vous acceptez les termes et conditions.');
         check_parent.setAttribute('data-error-visible', true);
-    }
-    const date = document.getElementById('birthdate').value;
-    if (date == '') {
-        const date_parent = document.querySelector('input[id="birthdate"]').parentNode;
-        date_parent.setAttribute('data-error', 'Vous devez entrer votre date de naissance.');
-        date_parent.setAttribute('data-error-visible', true);
-    }
-    const quantity = document.getElementById('quantity').value;
-    if (!quantity) {
-        const quantity_parent = document.querySelector('input[id="quantity"]').parentNode;
-        quantity_parent.setAttribute('data-error', 'Vous devez entrer un nombre.');
-        quantity_parent.setAttribute('data-error-visible', true);
-    }
-    const first = document.getElementById('first').value;
-    if (!first) {
-        const first_parent = document.querySelector('input[id="first"]').parentNode;
-        first_parent.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
-        first_parent.setAttribute('data-error-visible', true);
-    }
-    const last = document.getElementById('last').value;
-    if (!last) {
-        const last_parent = document.querySelector('input[id="last"]').parentNode;
-        last_parent.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du nom.');
-        last_parent.setAttribute('data-error-visible', true);
-    }
-    const email = document.getElementById('email').value;
-    if (!email) {
-        const email_parent = document.querySelector('input[id="email"]').parentNode;
-        email_parent.setAttribute('data-error', 'Vous devez entrer une adresse email valide.');
-        email_parent.setAttribute('data-error-visible', true);
-    }
-    if (radio_value !== null && check_value !== false && date !== '' && quantity && first && last && email) {
-        console.log("valid true");
-        return true;
     } else {
-        console.log("valid false");
+        isCheckboxValid = true;
     }
+
+    isFirstValid = checkFirstAndLastName(first, 'first');
+    isLastValid = checkFirstAndLastName(last, 'last');
+    isEmailValid = checkEmail(email);
+    isBirthdateValid = checkBirthdate(date);
+    isQuantityValid = checkQuantity(quantity);
+
+    return isBirthdateValid && isCheckboxValid && isEmailValid && isFirstValid && isLastValid && isQuantityValid && isRadioValid;
 }
 
 // déclenchement de l'événement de la validation du formulaire
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     if (validate() == true) {
-        modalbg.style.display = 'none';
-        hero_section.style.display = 'none';
-        content_valid.style.display = "initial";
-        confirmed.style.color = "white";
-        confirmed.style.display = "flex"
-        confirmed.setAttribute('confirmed', true);
-        topnav.style.display = "none";
-        footer.style.display = "none";
-        document.querySelector('form').reset();
-        const check_parent = document.querySelector('input[id="checkbox1"]').parentNode;
-        check_parent.removeAttribute('data-error');
-        check_parent.removeAttribute('data-error-visible');
-        const radio_parent = document.querySelector('input[name="location"]').parentNode;
-        radio_parent.removeAttribute('data-error');
-        radio_parent.removeAttribute('data-error-visible');
+        document.querySelector('form').reset()
+        const modalBody = document.getElementById('modal-body');
+        modalBody.innerHTML = `
+                <p class="confirmed">
+                            Merci ! Votre réservation a été reçue.
+                        </p>
+                        <button class="btn-close" onclick="closeForm()">Fermer</button>
+                `;
+        //modalbg.style.backgroundColor="initial";        
+        content.setAttribute('content-mobile', true);
+        modalbg.setAttribute('bground-mobile', true);
     }
 })
 
@@ -177,4 +185,16 @@ function closeConfirmation() {
     confirmed.style.display = "none";
     topnav.style.display = "initial";
     footer.style.display = "initial";
+}
+
+// Vérifier âge
+
+function checkAge(currentDate, birthDate) {
+    var diff = Math.floor(currentDate.getTime() - birthDate.getTime());
+    var day = 1000 * 60 * 60 * 24;
+    var days = Math.floor(diff / day);
+    var years = Math.floor(days / 365.25);
+    console.log("age : " + years + " ans");
+
+    return years;
 }
