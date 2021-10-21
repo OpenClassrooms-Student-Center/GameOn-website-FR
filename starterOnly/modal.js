15944d19351e2b80db69d8f7431596ca42fc4abe
Laurent <laurentsingamalom@gmail.com>
2021-10-21 10:38:50 +0400

    Upload Issues 1 & 2
    - Visualiser et tester l'interface utilisateur dans les dernières versions de Chrome et de Firefox, ainsi que dans les versions mobile et desktop. Corriger les erreurs d'affichage existantes.
    - Tester toutes les fonctionnalités des boutons et des entrées de formulaire (tester les valeurs correctes et incorrectes)
    - Après une validation réussie, inclure un message de confirmation de la soumission réussie pour l'utilisateur (ex. "Merci ! Votre réservation a été reçue.")

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,25 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("reserve");
+const closeBtnRed = document.getElementById("closeBtnRed");
+const confirmationMsg = document.getElementById("confirmationMsg");
 
-// launch modal event
+const heroSection = document.querySelector('.hero-section');
+
+
+// variable mobile media query
+let mediaQueryMobile = window.matchMedia("(max-width: 540px)");
+
+
+/// launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  // if mobile screen, heroSection doesn't appear
+  if(mediaQueryMobile.matches){
+    heroSection.style.display = "none";
+  }
 }
-
-
