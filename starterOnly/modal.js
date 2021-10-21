eac3c91a52b6a377a7bfd21207437d7058b29dfc
inesodtap <odoultapiaines@gmail.com>
2021-10-21 10:46:27 +0200

    :white_check_mark: Error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,23 +28,89 @@ const modalCloseBtn = document.querySelectorAll(".close");
 // close modal event
 modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 // close modal form
-function closeModal(){
+function closeModal() {
   modalbg.style.display = 'none';
 }
 
 //TODO #2 Implémenter entrées du formulaire
-const firstName = document.getElementById("first");
-const errorFirstName = document.getElementById("errorFirstName");
-const lastName = document.getElementById("last");
-const errorLastName = document.getElementById("errorLastName");
-const email = document.getElementById("email");
-const errorEmail = document.getElementById("errorEmail");
-const birthdate = document.getElementById("birthdate");
-const errorBirthdate = document.getElementById("errorBirthdate");
-const quantityTournamenent = document.getElementById("quantity");
-const errorQuantity = document.getElementById("errorQuantity");
-const location = document.querySelector('input[name="location"]');
-const errorLocation = document.getElementById("errorLocations");
-const policy = document.getElementById("checkbox1");
-const errorPolicy = document.getElementById("errorPolicy");
 
+function checkFirstName() {
+  const firstName = document.getElementById("first").value;
+
+  if (firstName.length < 2) {
+
+    document.getElementById("errorFirstName").classList.remove("hidden")
+  }
+}
+
+function checkLastName() {
+  const lastName = document.getElementById("last").value;
+
+  if (lastName.length < 2) {
+
+    document.getElementById("errorLastName").classList.remove("hidden")
+  }
+}
+
+function checkEmailName() {
+  const email = document.getElementById("email").value;
+
+  var regex = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
+
+  if (regex.test(email.value)) {
+    return true;
+  }
+
+  else {
+    document.getElementById("errorEmail").classList.remove("hidden")
+  }
+}
+
+function checkBirthdate() {
+  const birthdate = document.getElementById("birthdate").value;
+
+  if (!birthdate)
+
+  document.getElementById("errorBirthdate").classList.remove("hidden")
+}
+
+function checkQuantity() {
+  var x = document.getElementById("quantity").value;
+
+  if ((x < 0 || x > 99 || !x)) {
+    document.getElementById("errorQuantity").classList.remove("hidden")
+  }
+
+  else {
+    return false;
+  }
+}
+
+function checkLocation() {
+  var getSelectedValue = document.querySelector('input[name="location"]:checked');
+
+  if (getSelectedValue !=null) {
+    return true;
+  }
+
+  else {
+    document.getElementById("errorLocations").classList.remove("hidden")
+  }
+}
+ 
+function checkPolicy() {
+  
+  if (document.getElementById("checkbox1").checked) {
+    return true;
+  }
+
+  else {
+    document.getElementById("errorPolicy").classList.remove("hidden")
+  }
+}
+
+function validate() {
+  checkFirstName() & checkLastName() & checkEmailName() & checkBirthdate() & checkQuantity() & checkLocation() & checkPolicy();
+}
+
+document.getElementById("submitBtn").addEventListener("click", validate)
