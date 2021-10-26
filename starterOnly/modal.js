123cc2c3168e6e75e5323bea26544b4100493603
nicolas le meur <nicolas.lemeur9@gmail.com>
2021-10-26 18:30:16 +0200

    check localisation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -52,15 +52,13 @@ const location4=document.getElementById('location4');
 const location5=document.getElementById('location5');
 const location6=document.getElementById('location6');
 const checkbox1=document.getElementById('checkbox1');
-const checkbox2=document.getElementById('checkbox2');
+const checkbox2=document.getElementById('checkbox2'); //cg
 
 // Validate - Function
 
 
 // submit
   document.getElementById('submit').addEventListener("submit", function(e){
-
-    alert('ok');
 
 //regex (expression reguliere)
     let FirstLastRegex = new RegExp(
@@ -78,17 +76,40 @@ const checkbox2=document.getElementById('checkbox2');
 
 
 // erreur
-    const locationBoxQuantity = 6;
-    let checkCount = 0;
-    for (let i=0; i<locationBoxQuantity-1; i++ ){
-      if((location+i).checked){
-        checkCount++;
-      }
+
+    if(checkbox1.checked === false){
+
+      erreur="vous devez accepter les conditions";
     }
+
+
+    var checkCount = 0;
+    if(location1.checked === true){
+      checkCount++;
+    }
+    if(location2.checked === true){
+      checkCount++;
+    }
+
+    if(location3.checked === true){
+      checkCount++;
+    }
+    if(location4.checked === true){
+      checkCount++;
+    }
+    if(location5.checked === true){
+      checkCount++;
+    }
+    if(location6.checked === true){
+      checkCount++;
+    }
+
     if (checkCount>1){
       erreur = "vous ne pouvez pas selectionner plusieurs localisation";
     } else if (checkCount<1){
       erreur = "vous n'avez pas choisi de localisation";
+    } else {
+      erreur=" ";
     }
 
     if(!quantityTournament.value){
@@ -97,32 +118,40 @@ const checkbox2=document.getElementById('checkbox2');
       erreur ="Nous n'avons pas organisé autant de tournois";
     }
 
-    if(email.value.length > 41 || email.value.length <3 ){
-      erreur ="un email doit contenir ENTRE 3 ET 40 CARACTERES caractères";
-    }
 
 
+
+    var birdthdateValidity = false;
     if (!birthdate.value){  // on verifie que le champs est different de "" (n'est pas vide)
       erreur = "veuillez renseigner votre date de naissance";
     } else if (BirthdateRegex.test(birthdate.value) == false){
       erreur="une date de naissance ne peut contenir que des chiffres";
     } else if (birthdate.value.length != 10){
       erreur="une date de naissance contient 8 chiffres";
+    } else{
+      birdthdateValidity = true;
     }
 
 
 
-
+    var emailValidity = false;
     if (!email.value){
       erreur = "veuillez renseigner une addresse mail";
     }
+    else if(email.value.length > 41 || email.value.length <3 ){
+      erreur ="un email doit contenir ENTRE 3 ET 40 CARACTERES caractères";
+    }
+
     else if (EmailRegex.test(email.value) == false) { //on test la validité des caractère avec la regex
       erreur = "vous devez renseigné une addresse valide";
+    }
+    else{
+      emailValidity = true;
     }
 
 
 
-
+    var lastNameValidity = false;
     if (!lastName.value) {
       erreur = "veuillez renseigner votre nom";
     } else if (lastName.value.length < 2 || lastName.value.length > 40) {
@@ -130,22 +159,26 @@ const checkbox2=document.getElementById('checkbox2');
 
     } else if (FirstLastRegex.test(lastName.value) == false) { //on test la validité des caractère avec la regex
       erreur = "un nom ne doit contenir que des lettres";
+    } else{
+      lastNameValidity = true;
     }
 
 
 
-
+      var FirstNameValidity = false;
       if (!firstName.value) {
+        firstName.closest('.formData').dataset.error = "sss"; // on definit la propriété error de l'objet cutomisable dataset
+        firstName.closest('.formData').dataset.errorVisible = 'true';
+
         erreur = "veuillez renseigner votre prénom";
       } else if (firstName.value.length < 2 || firstName.value.length > 40) {
         erreur = "un prénom doit contenir entre 2 et 40 caractères";
 
       } else if (FirstLastRegex.test(firstName.value) == false) { //on test la validité des caractère avec la regex
         erreur = "un prenom ne doit contenir que des lettres";
+      } else{
+        FirstNameValidity = true;
       }
-
-
-
 
 
 
@@ -156,16 +189,7 @@ const checkbox2=document.getElementById('checkbox2');
 
 
   });
-  /*
-  if(firstName){
-    // le champ n'est pas vide
 
-
-  } else{
-    //le champ est vide
-  }
-
-*/
 
 
 
