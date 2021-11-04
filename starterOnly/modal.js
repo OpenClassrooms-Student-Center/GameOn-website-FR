fd01d917121fbff3b8a0836c90d22781d916be72
Marcos Meneghetti <meneghettimarcos@outlook.com>
2021-11-04 16:53:44 +0100

    message de confirmation reussie pour l'utilisateur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,20 +10,22 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formAllData = document.getElementsByName("reserve");
+const resetForm = document.getElementById("formReserve");
 const closeModal = document.querySelector(".close");
 const submitBtn = document.querySelector(".btn-submit");
+// submitBtn.disabled = true;
 
 //form elements
 
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
-const birthDate = document.getElementById("birthdate");
 const quantityNbr = document.getElementById("quantity");
 const radioBtns = document.getElementsByName("location");
 const todayDateForm = document.getElementById("birthdate");
 const conditionAccept = document.querySelector("#checkbox1");
+
+console.log(todayDateForm.value);
 
 //today day
 const todayDate = new Date().toISOString().slice(0, 10);
@@ -36,6 +38,7 @@ const locationErrorMsg = document.querySelector(".location-error");
 const quantityErrorMsg = document.querySelector(".quantity-error");
 const dateErrorMsg = document.querySelector(".date-error");
 const conditionErrorMsg = document.querySelector(".conditions-error");
+const submitErrorMsg = document.querySelector(".submit-error");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -51,8 +54,12 @@ closeModal.addEventListener("click", () => {
 });
 
 //confirm inputs
-
 //first name
+firstName.addEventListener("focus", (e) => {
+  submitErrorMsg.style.display = "none";
+  locationErrorMsg.style.display = "none";
+});
+
 firstName.addEventListener("input", (e) => {
   if (e.target.value.length < 2) {
     e.target.style.border = "3px solid red";
@@ -66,6 +73,12 @@ firstName.addEventListener("input", (e) => {
 });
 
 //last name
+
+lastName.addEventListener("focus", (e) => {
+  submitErrorMsg.style.display = "none";
+  locationErrorMsg.style.display = "none";
+});
+
 lastName.addEventListener("input", (e) => {
   if (e.target.value.length < 2) {
     e.target.style.border = "3px solid red";
@@ -77,7 +90,14 @@ lastName.addEventListener("input", (e) => {
     submitBtn.removeAttribute("disabled");
   }
 });
+
 //email
+
+email.addEventListener("focus", (e) => {
+  submitErrorMsg.style.display = "none";
+  locationErrorMsg.style.display = "none";
+});
+
 email.addEventListener("input", (e) => {
   if (e.target.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
     e.target.style.border = "3px solid #51d115";
@@ -90,7 +110,31 @@ email.addEventListener("input", (e) => {
   }
 });
 
+//verifying birth date
+
+todayDateForm.addEventListener("focus", (e) => {
+  submitErrorMsg.style.display = "none";
+  locationErrorMsg.style.display = "none";
+});
+
+todayDateForm.addEventListener("input", (e) => {
+  if (!todayDateForm) {
+    dateErrorMsg.style.display = "inline";
+    submitBtn.setAttribute("disabled");
+  } else if (todayDateForm.value === todayDate) {
+    dateErrorMsg.style.display = "inline";
+    submitBtn.setAttribute("disabled");
+  } else {
+    dateErrorMsg.style.display = "none";
+    submitBtn.removeAttribute("disabled");
+  }
+});
+
 //city of tournement
+quantityNbr.addEventListener("focus", (e) => {
+  submitErrorMsg.style.display = "none";
+  locationErrorMsg.style.display = "none";
+});
 
 quantityNbr.addEventListener("change", (e) => {
   locationErrorMsg.style.display = "none";
@@ -164,19 +208,6 @@ function displayCheck(e) {
   }
 }
 
-//verifying birth date
-todayDateForm.addEventListener("change", () => {
-  // birthDate.value = todayDate;
-
-  if (todayDateForm.value === todayDate) {
-    dateErrorMsg.style.display = "inline";
-    submitBtn.setAttribute("disabled");
-  } else {
-    dateErrorMsg.style.display = "none";
-    submitBtn.removettribute("disabled");
-  }
-});
-
 //checking conditions
 conditionAccept.addEventListener("change", (e) => {
   if (!e.target.checked) {
@@ -185,5 +216,48 @@ conditionAccept.addEventListener("change", (e) => {
   } else {
     conditionErrorMsg.style.display = "none";
     submitBtn.removeAttribute("disabled");
+  }
+});
+
+//submit event
+submitBtn.addEventListener("click", ($event) => {
+  if (quantityNbr > 0) {
+    for (i = 0; i < radioBtns.length; i++) {
+      if (!radioBtns[i].checked) {
+        locationErrorMsg.style.display = "inline";
+      } else {
+        locationErrorMsg.style.display = "none";
+      }
+    }
+  }
+
+  if (
+    firstName.value === "" ||
+    lastName.value === "" ||
+    email.value === "" ||
+    todayDateForm.value === ""
+  ) {
+    console.log(todayDateForm.value);
+
+    submitErrorMsg.style.display = "inline";
+    submitBtn.disabled = true;
+  } else {
+    $event.preventDefault();
+
+    //hide all error messages
+    firstErrorMsg.style.display = "none";
+    lastErrorMsg.style.display = "none";
+    emailErrorMsg.style.display = "none";
+    dateErrorMsg.style.display = "none";
+    locationErrorMsg.style.display = "none";
+    submitErrorMsg.style.display = "none";
+
+    //hide borders
+    firstName.style.border = "0px solid #fff";
+    lastName.style.border = "0px solid #fff";
+    email.style.border = "0px solid #fff";
+
+    alert(`Merci, ${firstName.value}! Votre réservation a été reçue.`);
+    resetForm.reset();
   }
 });
