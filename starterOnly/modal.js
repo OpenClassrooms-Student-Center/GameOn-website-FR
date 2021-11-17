c736bebacc226694124103e801bbafd249f96d09
yxwangOCR <wang.yixuan@live.fr>
2021-11-17 23:38:26 +0100

    error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,57 +6,120 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
 // DOM Elements
 const closeBtn = document.querySelector(".close");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelector(".formData");
+const submitButton = document.querySelector(".btn-submit");
+const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const emailAddressInput = document.getElementById("email");
+const birthDateInput = document.getElementById("birthdate");
+const checkBoxInput = document.querySelectorAll(".checkbox-label");
+const checkCondition = document.querySelector(".checkbox1-label > .checkbox-icon")
 
-
-//#1 Fermer la modale: ajouter la fonctionnalité au bouton (x): 
-// close modal form:
 const closeModal = function(){
   modalbg.style.display="none";
 };
-// launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-// launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.addEventListener("click", closeModal)
 
-//问题： 另一个方法为什么不可以？ 
-//const closeBtn = document.getElementsByClassName(".close");为什么不可以？
-// closeBtn.onclick = function(){//  为什么不可以?
-//  modalbg.style.display="none";}
-
-//#2 Implémenter entrées du formulaire: 
-//(1) Lier les labels aux entrées dans le HTML en utilisant les attributs "for" et "id" dans le code existant. 
-//Corriger le code HTML quand nécessaire.使用现有代码中的“for”和“id”属性将标签链接到 HTML 中的条目。必要时更正 HTML 代码。
-//(2) Utiliser du JavaScript pur (pas de jQuery) pour terminer le formulaire: Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
-//const submitButton = document.getElementById("#submitButton"); //===> "null"
-//const submitButton = document.getElementsByClassName(".btn-submit"); ===> "Uncaught SyntaxError: Invalid or unexpected token", 为什么retour不一样？ 
-
-//Les données doivent être saisies correctement :
-//(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-const submitButton = document.querySelector(".btn-submit");
-const firstNameInput = document.getElementById("#first");
-const onSubmit = function(){
-  //console.log("firstNameInput.value");
-  if(firstNameInput.value.length<2){
-    console.log('no valided name');
-  }else{
-    console.log('OK');
-  }
-}
-formData.addEventListener("submit", onSubmit);//Uncaught TypeError: formData.addEventListener is not a function
-submitButton.addEventListener("click", onSubmit); 
-
-//(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
-//(3) L'adresse électronique est valide.
 //(4) Pour le nombre de concours, une valeur numérique est saisie.
 //(5) Un bouton radio est sélectionné.
 //(6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
 //Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
+
+const onSubmit = function(){
+  //first name: 
+  if(firstNameInput.value.length<2){
+    errorFirstName.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
+  }else{
+    errorFirstName.innerHTML = "";
+    return true;
+  }
+
+  //last name:
+  if(lastNameInput.value.length<2){
+    errorLastName.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom de famille";
+  }else{
+    errorLastName.innerHTML = "";
+    return true;
+  }
+
+  //email address: 
+  //regular expression to match required email address format: regular = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(emailAddressInput.value.match(mailFormat)){
+    errorEmailAddress.innerHTML="";
+    return true;
+  }
+  else{
+    errorEmailAddress.innerHTML = "Veuillez fournir une adresse email valide";
+  }
+
+  //birthday: 
+  //regular expression to match required date format: regular = /^\d{1,2}\/\d{1,2}\/\d{4}$/  or /^\d{2}\/\d{2}\/\d{4}$/;
+  let dateFormat = /^\d{2}\/\d{2}\/\d{4}$/;
+  if(birthDateInput.value !="" && !birthDateInput.value.match(dateFormat)){   
+    errorBirthDate.innerHTML="";
+    return true;
+  }
+  else{
+    errorBirthDate.innerHTML = "Vous devez entrer votre date de naissance.";
+  }
+}
+
+ //city option:
+ function isChecked(){
+  let newYork = querySelector('location1').checked;
+  let sanFrancisco = querySelector('location2').checked;
+  let seattle = querySelector('location3').checked;
+  let chicago = querySelector('location4').checked;
+  let boston = querySelector('location5').checked;
+  let portland = querySelector('location6').checked;
+if(newYork==false && sanFrancisco==false && seattle==false && chicago==false && boston==false && portland==false){
+ErrorOption.innerHTML="Vous devez choisir une ville.";
+}else{
+ErrorOption.innerHTML=""
+return true;
+} 
+}
+
+//Terms and conditions:
+if(checkCondition.checked){ 
+  ErrorCondition.innerHTML="Vous devez vérifier que vous acceptez les termes et conditions."
+}else{
+  ErrorCondition.innerHTML="";
+  //alert('Merci ! Votre réservation a été reçue.');
+}
+
+formData.addEventListener("submit", onSubmit);
+firstNameInput.addEventListener("submit", onSubmit);
+lastNameInput.addEventListener("submit", onSubmit);
+emailAddressInput.addEventListener("submit", onSubmit);
+birthDateInput.addEventListener("submit", onSubmit);
+submitButton.addEventListener("click", onSubmit); 
+//checkCondition.addEventListener("submit", onSubmit);=> not work 
+//isChecked.addEventListener("submit", onSubmit);
+
+
+
+
+
+
+
+
+
+
+
+//草稿：
+//for(let i=0;i<checkBoxInput.length;i++){
+//if(!checkBoxInput[i].checked || checkBoxInput[i]<1){ // if(checkBoxInput[i].checked && i>=1),  ErrorOption.innerHTML="" ?
+//ErrorOption.innerHTML="Vous devez choisir une ville.";
+//  }else{
+//  ErrorOption.innerHTML=""
+// }
