b435cb5f44c22fe05a2eaf25d0bf530bc08770af
yxwangOCR <wang.yixuan@live.fr>
2021-11-18 02:21:15 +0100

    checkbox fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,8 +16,8 @@ const firstNameInput = document.getElementById("first");
 const lastNameInput = document.getElementById("last");
 const emailAddressInput = document.getElementById("email");
 const birthDateInput = document.getElementById("birthdate");
-const checkBoxInput = document.querySelectorAll(".checkbox-label");
-const checkCondition = document.querySelector(".checkbox1-label > .checkbox-icon")
+const checkBoxInput = document.forms[0].location;
+const checkCondition = document.getElementById("checkbox1");
 
 const closeModal = function(){
   modalbg.style.display="none";
@@ -27,11 +27,6 @@ function launchModal() {
 }
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.addEventListener("click", closeModal)
-
-//(4) Pour le nombre de concours, une valeur numérique est saisie.
-//(5) Un bouton radio est sélectionné.
-//(6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
-//Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
 
 const onSubmit = function(){
   //first name: 
@@ -75,27 +70,24 @@ const onSubmit = function(){
 
  //city option:
  function isChecked(){
-  let newYork = querySelector('location1').checked;
-  let sanFrancisco = querySelector('location2').checked;
-  let seattle = querySelector('location3').checked;
-  let chicago = querySelector('location4').checked;
-  let boston = querySelector('location5').checked;
-  let portland = querySelector('location6').checked;
-if(newYork==false && sanFrancisco==false && seattle==false && chicago==false && boston==false && portland==false){
-ErrorOption.innerHTML="Vous devez choisir une ville.";
-}else{
-ErrorOption.innerHTML=""
-return true;
-} 
+  for(let i=0;i<checkBoxInput.length;i++){
+    if(checkBoxInput[i].checked){ 
+      ErrorOption.innerHTML="";
+      return true;
+    }else{
+      ErrorOption.innerHTML="Vous devez choisir une ville."
+}
+  }
+
+ //terms & condition option: 
+  if(checkCondition.checked){
+    ErrorCondition.innerHTML="";
+    return true;
+  }else{
+    ErrorCondition.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions."
+  }
 }
 
-//Terms and conditions:
-if(checkCondition.checked){ 
-  ErrorCondition.innerHTML="Vous devez vérifier que vous acceptez les termes et conditions."
-}else{
-  ErrorCondition.innerHTML="";
-  //alert('Merci ! Votre réservation a été reçue.');
-}
 
 formData.addEventListener("submit", onSubmit);
 firstNameInput.addEventListener("submit", onSubmit);
@@ -103,23 +95,3 @@ lastNameInput.addEventListener("submit", onSubmit);
 emailAddressInput.addEventListener("submit", onSubmit);
 birthDateInput.addEventListener("submit", onSubmit);
 submitButton.addEventListener("click", onSubmit); 
-//checkCondition.addEventListener("submit", onSubmit);=> not work 
-//isChecked.addEventListener("submit", onSubmit);
-
-
-
-
-
-
-
-
-
-
-
-//草稿：
-//for(let i=0;i<checkBoxInput.length;i++){
-//if(!checkBoxInput[i].checked || checkBoxInput[i]<1){ // if(checkBoxInput[i].checked && i>=1),  ErrorOption.innerHTML="" ?
-//ErrorOption.innerHTML="Vous devez choisir une ville.";
-//  }else{
-//  ErrorOption.innerHTML=""
-// }
