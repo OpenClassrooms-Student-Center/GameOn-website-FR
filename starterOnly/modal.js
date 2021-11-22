e804b5607322324f97fe5da7a5e23c21a1016cfd
Stéphane <stephane1920@gmail.com>
2021-11-22 12:22:43 +0100

    add event close modal btn [x]

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,15 +9,21 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalClose = document.querySelector(".close")
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalClose.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
