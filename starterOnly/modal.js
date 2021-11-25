5aea57e406e1ca7c3a2307b419c5ca88391e23f0
yann-lcrx <yannick84v@gmail.com>
2021-11-25 17:27:56 +0100

    feat: implement form entries

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,21 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.getElementsByClassName('close')[0];
+const submitBtn = document.getElementsByClassName('btn-submit')[0];
+const termsOfUse = document.getElementById('terms-of-use');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // close modal event
 closeBtn.addEventListener('click', () => closeModal())
+
+// validate form
+submitBtn.addEventListener('click', (submission) => {
+  if (!cityIsChecked || !termsAreAgreed) {
+    submission.preventDefault;
+  }
+})
 
 // launch modal form
 function launchModal() {
@@ -29,3 +38,16 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// check "city" input completion 
+function cityIsChecked() {
+  for (let i = 1; i < 7; i++) {
+    if (document.getElementById(`location${i}`).checked) {
+      return true
+    }    
+  }
+}
+
+// check agreement to terms of use
+function termsAreAgreed() {
+  return termsOfUse.checked
+}
