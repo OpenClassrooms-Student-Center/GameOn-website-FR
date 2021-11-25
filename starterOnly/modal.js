bc441ca45195746c62d9ed305cfa05c21b9ca24c
yxwangOCR <wang.yixuan@live.fr>
2021-11-25 08:14:08 +0100

    sending message fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,8 +16,13 @@ const firstNameInput = document.getElementById("first");
 const lastNameInput = document.getElementById("last");
 const emailAddressInput = document.getElementById("email");
 const birthDateInput = document.getElementById("birthdate");
+const quantityInput = document.getElementById("quantity");
 const locationCheckbox = document.querySelectorAll("input[name=location]");
 const checkCondition = document.getElementById("checkbox1");
+let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let dateFormat = /^\d{2}\/\d{2}\/\d{4}$/;
+let i=0;
+
 
 const closeModal = function(){
   modalbg.style.display="none";
@@ -28,19 +33,18 @@ function launchModal() {
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.addEventListener("click", closeModal)
 
-
 const nameValidation = function(){
 if(firstNameInput.value.length<2){
   errorFirstName.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
 }else{
   errorFirstName.innerHTML = "";
-  return true;
+  
 }
 if(lastNameInput.value.length<2){
   errorLastName.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom de famille";
 }else{
   errorLastName.innerHTML = "";
-  return true;
+  
 }
 }
 
@@ -67,7 +71,7 @@ const birthdayValidation = function(){
 }
 
 const locationChecked = function(){
-  for(let i=0;i<locationCheckbox.length;i++){
+  for(i=0;i<locationCheckbox.length;i++){
     if(locationCheckbox[i].checked){ 
       ErrorOption.innerHTML="";
       return true;
@@ -85,19 +89,69 @@ const termsChecked = function(){
      ErrorCondition.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions."
    }
  }
- 
 
-const onSubmit = function(){
+ const validation = function () {
+  firstNameInput.value;
+  lastNameInput.value;
+  emailAddressInput.value;
+  birthDateInput.value;
+  quantityInput.value;
+  locationCheckbox.value;
+  checkCondition.value;
+  
+  if(firstNameInput.value.length>=2 
+    && lastNameInput.value.length >=2 
+    && emailAddressInput.value.match(mailFormat) 
+    && birthDateInput.value !="" 
+    && !birthDateInput.value.match(dateFormat) 
+    && checkCondition.checked === true
+    && checkCondition.checked === true){
+    console.log("Sending test")
+    alert("Informations envoyées avec succès！")
+  }else{
+    alert("Les informations saisies sont incorrectes.")
+  }
+
+ }
+
+ const onSubmit = function(){
   nameValidation()
   emailValidation()
   birthdayValidation()
   locationChecked()
-  termsChecked()
+  termsChecked()  
 }
 
+submitButton.addEventListener("click",(e)=>{
+e.preventDefault();
+onSubmit();
+validation();
+//console.log("Send Form")
+});
 
 
+
+
+
+
+//form.addEventListener("submit",sendForm);
+  
+/*
+ function onSubmit(e){
+ e.preventDefault();
+ console.log("formSending");
+ const dataForm = new DataForm(formSending);
+ const first = dataForm.get('first');
+ const last = dataForm.get('last');
+ const email = dataForm.get('email');
+ const birthdate = dataForm.get('birthdate');
+ const quantity = dataForm.get('quantity');
+ const location = dataForm.get('location');
+ const check = dataForm.get('check');
+ console.log('Form',{first, last, email, birthdate, quantity, location, check});
  
+ }
+ */
 
 
  /*
