f312e862b032c65e1466af60499287062534db03
yxwangOCR <wang.yixuan@live.fr>
2021-11-29 09:48:53 +0100

    Form css redo

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,130 +1,94 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-// DOM Elements
+/** @format */
+
+const signUpBtn = document.querySelector(".btn-signup");
 const closeBtn = document.querySelector(".close");
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
 const form = document.querySelector(".form");
-const submitButton = document.querySelector(".btn-submit");
-const firstNameInput = document.getElementById("first");
-const lastNameInput = document.getElementById("last");
-const emailAddressInput = document.getElementById("email");
-const birthDateInput = document.getElementById("birthdate");
-const quantityInput = document.getElementById("quantity");
-const locationCheckbox = document.querySelectorAll("input[name=location]");
-const checkCondition = document.getElementById("checkbox1");
-let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-let dateFormat = /^\d{2}\/\d{2}\/\d{4}$/;
-let i=0;
+const firstName = document.querySelector("#first");
+const lastName = document.querySelector("#last");
+const email = document.querySelector("#email");
+const birthday = document.querySelector("#birthday");
+const quantity = document.querySelector("#quantity");
+const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const dateFormat = /^\d{2}\/\d{2}\/\d{4}$/;
 
-
-const closeModal = function(){
-  modalbg.style.display="none";
-};
-function launchModal() {
+// Launch & close modal:
+const launchModal = function () {
   modalbg.style.display = "block";
-}
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-closeBtn.addEventListener("click", closeModal)
+};
 
-const nameValidation = function(){
-if(firstNameInput.value.length<2){
-  errorFirstName.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
-}else{
-  errorFirstName.innerHTML = "";
-  
-}
-if(lastNameInput.value.length<2){
-  errorLastName.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom de famille";
-}else{
-  errorLastName.innerHTML = "";
-  
-}
-}
+const closeModal = function () {
+  modalbg.style.display = "none";
+};
 
-const emailValidation = function(){
-  //let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(emailAddressInput.value.match(mailFormat)){
-    errorEmailAddress.innerHTML="";
-    return true;
-  }
-  else{
-    errorEmailAddress.innerHTML = "Veuillez fournir une adresse email valide";
-  }
-}
+signUpBtn.addEventListener("click", launchModal);
+closeBtn.addEventListener("click", closeModal);
 
-const birthdayValidation = function(){
-  //let dateFormat = /^\d{2}\/\d{2}\/\d{4}$/;
-  if(birthDateInput.value !="" && !birthDateInput.value.match(dateFormat)){   
-    errorBirthDate.innerHTML="";
-    return true;
-  }
-  else{
-    errorBirthDate.innerHTML = "Vous devez entrer votre date de naissance.";
-  }
-}
-
-const locationChecked = function(){
-  for(i=0;i<locationCheckbox.length;i++){
-    if(locationCheckbox[i].checked){ 
-      ErrorOption.innerHTML="";
-      return true;
-    }else{
-      ErrorOption.innerHTML="Vous devez choisir une ville."
-}
-  }
-}
-
-const termsChecked = function(){
-   if(checkCondition.checked){
-     ErrorCondition.innerHTML="";
-     return true;
-   }else{
-     ErrorCondition.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions."
-   }
- }
-
- const validation = function () {
-  if(firstNameInput.value.length>=2 
-    && lastNameInput.value.length >=2 
-    && emailAddressInput.value.match(mailFormat) 
-    && birthDateInput.value !="" 
-    && !birthDateInput.value.match(dateFormat) 
-    && checkCondition.checked === true
-    && checkCondition.checked === true){
-    //console.log("Sending test")
-    alert("Informations envoyées avec succès！")
-  }else{
-    alert("Les informations saisies sont incorrectes.")
-  }
- }
-
- const onSubmit = function(){
-  nameValidation()
-  emailValidation()
-  birthdayValidation()
-  locationChecked()
-  termsChecked()  
-}
-
-submitButton.addEventListener("click",(e)=>{
-e.preventDefault();
-onSubmit();
-validation();
-//console.log("Send Form")
+//Error message:
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInput();
 });
 
+function checkInput() {
+  //get values from inputs
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const birthdayValue = birthday.value;
+  const quantityValue = quantity.value;
 
+  if (firstNameValue.length < 2) {
+    // show error message
+    setError(
+      firstName,
+      "Please enter 2 or more characters for the first name field"
+    );
+  } else {
+    // show success icon
+    setSuccess(firstName);
+  }
 
- /*
-birthday: 
-regular expression to match required date format: regular = /^\d{1,2}\/\d{1,2}\/\d{4}$/  or /^\d{2}\/\d{2}\/\d{4}$/;
-email address: 
-regular expression to match required email address format: regular = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-*/
+  if (lastNameValue.length < 2) {
+    // show error message
+    setError(
+      lastName,
+      "Please enter 2 or more characters for the last name field"
+    );
+  } else {
+    // show success icon
+    setSuccess(lastName);
+  }
+
+  if (emailValue === "") {
+    // show error message
+    setError(email, "Email cannot be empty");
+  } else if (!isEmail(emailValue)) {
+    setError(email, "Please provide a valid email address");
+  } else {
+    // show success icon
+    setSuccess(email);
+  }
+}
+
+function setError(input, message) {
+  const formData = input.parentElement;
+  const small = formData.querySelector("small");
+
+  //add error message inside small element
+  small.innerHTML = message;
+
+  //add error class
+  formData.className = "formData error";
+}
+
+function setSuccess(input) {
+  const formData = input.parentElement;
+
+  //add error class
+  formData.className = "formData success";
+}
+
+function isEmail(email) {
+  return emailFormat.test(email);
+}
