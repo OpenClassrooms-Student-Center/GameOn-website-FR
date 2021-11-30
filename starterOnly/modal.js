449dc6b15529d63236c6c7c80ab3722c8b74f9f2
yxwangOCR <wang.yixuan@live.fr>
2021-11-30 02:35:33 +0100

    success message fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,22 +14,21 @@ const checkbox1 = document.getElementById("checkbox-1");
 const conditionMessage = document.getElementById("conditionMessage");
 const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const submitButton = document.getElementById("submitButton");
+const thanksMessage = document.getElementById("thanks-message");
 
 // Launch & close modal:
 const launchModal = function () {
   modalbg.style.display = "block";
 };
-
 const closeModal = function () {
   modalbg.style.display = "none";
 };
-
 signUpBtn.addEventListener("click", launchModal);
 closeBtn.addEventListener("click", closeModal);
 
 //Error message:
-// Setup 2 classes = Error & Success:
-function setError(input, message) {
+// Setup 2 functions = isError() & isSuccess():
+function isError(input, message) {
   const formData = input.parentElement;
   const small = formData.querySelector("small");
   //add error message inside small element
@@ -37,7 +36,8 @@ function setError(input, message) {
   //add error class
   formData.className = "formData error";
 }
-function setSuccess(input) {
+
+function isSuccess(input) {
   const formData = input.parentElement;
   //add success class
   formData.className = "formData success";
@@ -48,115 +48,99 @@ function isEmail(email) {
 
 // Function for Text input part : isInput():
 function isInput() {
-  //get values from inputs
+  //get input value:
   const firstNameValue = firstName.value.trim();
   const lastNameValue = lastName.value.trim();
   const emailValue = email.value.trim();
   const birthdayValue = birthday.value;
   const quantityValue = quantity.value;
-
   if (firstNameValue.length < 2) {
     // show error message
-    setError(
+    isError(
       firstName,
       "Please enter 2 or more characters for the first name field"
     );
   } else {
     // show success green icon
-    setSuccess(firstName);
+    isSuccess(firstName);
   }
-
   if (lastNameValue.length < 2) {
     // show error message
-    setError(
+    isError(
       lastName,
       "Please enter 2 or more characters for the last name field"
     );
   } else {
     // show success green icon
-    setSuccess(lastName);
+    isSuccess(lastName);
   }
-
   if (emailValue === "") {
     // show error message
-    setError(email, "Email cannot be empty");
+    isError(email, "Email cannot be empty");
   } else if (!isEmail(emailValue)) {
-    setError(email, "Please provide a valid email address");
+    isError(email, "Please provide a valid email address");
   } else {
     // show success green icon
-    setSuccess(email);
+    isSuccess(email);
   }
-
   if (birthdayValue === "") {
     // show error message
-    setError(birthday, "Birthday cannot be empty");
+    isError(birthday, "Birthday cannot be empty");
   } else {
     // show success green icon
-    setSuccess(birthday);
+    isSuccess(birthday);
   }
-
   if (quantityValue === "") {
     // show error message
-    setError(quantity, "Tournament cannot be empty");
+    isError(quantity, "Tournament cannot be empty");
   } else {
     // show success green icon
-    setSuccess(quantity);
+    isSuccess(quantity);
   }
   //for Radio checkbox part :
-
   for (const location of locations) {
-    if (location.checked == true) {
-      setSuccess(locationMessage);
+    if (location.checked) {
+      isSuccess(locationMessage);
       break;
     } else {
-      setError(locationMessage, "You have to choose a city");
+      isError(locationMessage, "You have to choose a city");
     }
   }
-
-  /*
-  if (locations.checked) {
-    setSuccess(locationMessage);
-  } else {
-    setError(locationMessage, "You have to choose a city");
-  }
-*/
   //for checkbox part :
   if (checkbox1.checked) {
-    setSuccess(conditionMessage);
+    isSuccess(conditionMessage);
   } else {
-    setError(conditionMessage, "You have to accept the terms and conditions");
+    isError(conditionMessage, "You have to accept the terms and conditions");
+  }
+
+  if (
+    firstNameValue.length > 1 &&
+    lastNameValue.length > 1 &&
+    emailValue.match(emailFormat) &&
+    birthdayValue != "" &&
+    quantityValue != "" &&
+    checkbox1.checked
+  ) {
+    form.remove();
+    thanksMessage.style.display = "block";
+    submitButton.innerHTML = submitButton.innerHTML.replace("Go", "Close");
+  } else {
+    form.style.display = "block";
   }
 }
-
-/*
-const isSuccess = function () {
-  console.log("success");
-  form.style.opacity = "0";
-};
-*/
 
 //Validation & sumbit the form :
-function validation() {
-  if (locations.checked === true && checkbox1.checked === true) {
-    //console.log("OK");
-  } else {
-    //console.log("Not validated");
-  }
+function onSubmit() {
+  isInput();
 }
-
-const onSubmit = function () {
-  isInput();
-};
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  isInput();
-});
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
   onSubmit();
-  validation();
-  //isSuccess();
-  //console.log("Send Form");
+  isInput();
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  isInput();
 });
