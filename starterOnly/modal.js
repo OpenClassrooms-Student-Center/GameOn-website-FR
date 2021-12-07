5b069ddb2e3abb668f8881dafa9c6f79a76a3e31
Thomas Monnier <thomahawok0@gmail.com>
2021-12-07 20:05:09 +0100

    Fix issue 1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");      /* Issue 1 - Select the element that has the calsse .close - Sélectonne l'élément qui à la calsse .close */ 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event
+modalClose.addEventListener("click", closeModal); /* Issue 1 - Listen to the mouse click on the .close element. On click call the 'closeModal' method - Ecoute le click de la sourie sur l'élément .close. Au click appel la fonction (méthode) 'closeModal'   */
 
+// close modal form
+function closeModal() {                            
+  modalbg.style.display = "none";   /* Issue 1 - Apply the "none" style on modalbd, which has the .bground class - Applique le syle "none" sur modalbd, qui corespont à l'élement qui à la classe .bground */
+}
