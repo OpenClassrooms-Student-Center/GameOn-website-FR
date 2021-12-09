674dd4ba744238dbdbd0c6d4feb3665fced679a1
Thomas Monnier <thomahawok0@gmail.com>
2021-12-09 15:57:44 +0100

    Fix issue 2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,6 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelector(".close");      /* Issue 1 - Select the element that has the calsse .close - Sélectonne l'élément qui à la calsse .close */ 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,10 +20,53 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// *** Issue 1
+// Select the element with the calsse .close - Sélectonne l'élément qui à la calsse .close  
+const modalClose = document.querySelector(".close"); 
+
 // close modal event
-modalClose.addEventListener("click", closeModal); /* Issue 1 - Listen to the mouse click on the .close element. On click call the 'closeModal' method - Ecoute le click de la sourie sur l'élément .close. Au click appel la fonction (méthode) 'closeModal'   */
+// Listen to the mouse click on the .close element. On click call the 'closeModal' method - Ecoute le click de la sourie sur l'élément .close. Au click appel la fonction (méthode) 'closeModal'
+modalClose.addEventListener("click", closeModal); 
 
 // close modal form
+// Apply the "none" style on modalbd, which has the .bground class - Applique le syle "none" sur modalbd, qui corespont à l'élement qui à la classe .bground 
 function closeModal() {                            
-  modalbg.style.display = "none";   /* Issue 1 - Apply the "none" style on modalbd, which has the .bground class - Applique le syle "none" sur modalbd, qui corespont à l'élement qui à la classe .bground */
+  modalbg.style.display = "none";   
 }
+
+// *** Issue 2
+
+const first = document.querySelector("#first"); 
+const last = document.querySelector("#last"); 
+const email = document.querySelector("#email");
+const birthdate = document.querySelector("#birthdate");
+const quantity = document.querySelector("#quantity");
+const location1 = document.querySelector("#location1");
+const location2 = document.querySelector("#location2");
+const location3 = document.querySelector("#location3");
+const location4 = document.querySelector("#location4");
+const location5 = document.querySelector("#location5");
+const location6 = document.querySelector("#location6");
+const checkbox1 = document.querySelector("#checkbox1");
+
+
+function validate() {
+  if (first.checkValidity() &&
+      last.checkValidity() && 
+      email.checkValidity() && 
+      birthdate.checkValidity() &&
+      quantity.checkValidity() &&
+      (location1.checked || 
+       location2.checked ||
+       location3.checked ||
+       location4.checked ||
+       location5.checked ||
+       location6.checked) &&
+      checkbox1.checked) 
+        return true;
+
+  else
+      return false; 
+  
+}
+
