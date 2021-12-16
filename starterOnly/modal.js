4ad77ee8b25981942ab8655826727bd1028de3fe
Thomas Monnier <thomahawok0@gmail.com>
2021-12-16 09:54:55 +0100

    Fixe issue 3 V2 - less code

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  var x = document.querySelector("#myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -21,15 +21,15 @@ function launchModal() {
 }
 
 // *** Issue 1
-// Select the element with the calsse .close - Sélectonne l'élément qui à la calsse .close  
+// Select the element with the calsse .close - Sélectonne l"élément qui à la calsse .close  
 const modalClose = document.querySelector(".close"); 
 
 // close modal event
-// Listen to the mouse click on the .close element. On click call the 'closeModal' method - Ecoute le click de la sourie sur l'élément .close. Au click appel la fonction (méthode) 'closeModal'
+// Listen to the mouse click on the .close element. On click call the "closeModal" method - Ecoute le click de la sourie sur l"élément .close. Au click appel la fonction (méthode) "closeModal"
 modalClose.addEventListener("click", closeModal); 
 
 // close modal form
-// Apply the "none" style on modalbd, which has the .bground class - Applique le syle "none" sur modalbd, qui corespont à l'élement qui à la classe .bground 
+// Apply the "none" style on modalbd, which has the .bground class - Applique le syle "none" sur modalbd, qui corespont à l"élement qui à la classe .bground 
 function closeModal() {                            
   modalbg.style.display = "none";   
 }
@@ -47,13 +47,13 @@ const location4 = document.querySelector("#location4");
 const location5 = document.querySelector("#location5");
 const location6 = document.querySelector("#location6");
 const checkbox1 = document.querySelector("#checkbox1");
-const button = document.querySelector("#btn-submit");
-const form = document.getElementById('form');
+const submit = document.querySelector("#submit");
+const form = document.querySelector("#form");
 
 // function valdate
 
 function validate() {
-  if (first.checkValidity() &&   // PM - checkValidity = Méthodes de l'API de validation des contraintes
+  if (first.checkValidity() &&   // PM - checkValidity = Méthodes de l"API de validation des contraintes
       last.checkValidity() && 
       email.checkValidity() && 
       birthdate.checkValidity() &&
@@ -81,106 +81,130 @@ function validateLocation() {
 
 // *** Issue 3
 
-function alert() {
-  first.addEventListener('change', function(event) {
-    if(event.target.validity.valid){
-      document.getElementById("formFirst").setAttribute('data-error-visible', false);
-    } else
-      document.getElementById("formFirst").setAttribute('data-error-visible', true);
-      document.getElementById("formFirst").setAttribute('data-error',"Veuillez entrer 2 caractères ou plus pour le prénom.");
-    }
-  );
 
-  last.addEventListener('change', function(event) {
-    if(event.target.validity.valid){
-      document.getElementById("formLast").setAttribute('data-error-visible', false);
-    } else
-      document.getElementById("formLast").setAttribute('data-error-visible', true);
-      document.getElementById("formLast").setAttribute('data-error',"Veuillez entrer 2 caractères ou plus pour le prénom.");
-    }
-  );
+let inputIds = document.querySelectorAll("div.formData");
 
-  email.addEventListener('change', function(event) {
-    if(event.target.validity.valid){
-      document.getElementById("formEmail").setAttribute('data-error-visible', false);
-    } else
-      document.getElementById("formEmail").setAttribute('data-error-visible', true);
-      document.getElementById("formEmail").setAttribute('data-error',"Veuillez entrer 2 caractères ou plus pour le prénom.");
-    }
-  );
+for(let i = 0; i < inputIds.length; i++) {
+  let errors = [
+    "Veuillez entrer 2 caractères ou plus pour le prénom.",
+    "Veuillez entrer 2 caractères ou plus pour le prénom.",
+    "Veuillez entrer une adresse email valide.",
+    "Veuillez indiquez une date.",
+    "Veuillez un nombre."
+  ];
 
-  birthdate.addEventListener('change', function(event) {
-    if(event.target.validity.valid){
-      document.getElementById("formBirth").setAttribute('data-error-visible', false);
-    } else
-      document.getElementById("formBirth").setAttribute('data-error-visible', true);
-      document.getElementById("formBirth").setAttribute('data-error',"Veuillez entrer 2 caractères ou plus pour le prénom.");
-    }
-  );
+  inputIds[i].addEventListener("input", function noneValide(event ) {
+    inputIds[i].setAttribute("data-error-visible", !event.target.validity.valid);
+    inputIds[i].setAttribute("data-error", errors[i]);
+  });
+};
+
+
+
+
+/*
+  targetInputId.addEventListener("input", (e) => noneValide(e, "Veuillez entrer 2 caractères ou plus pour le prénom.")); 
   
-  quantity.addEventListener('change', function(event) {
-    if(event.target.validity.valid){
-      document.getElementById("formQuantity").setAttribute('data-error-visible', false);
-    } else
-      document.getElementById("formQuantity").setAttribute('data-error-visible', true);
-      document.getElementById("formQuantity").setAttribute('data-error',"Veuillez entrer 2 caractères ou plus pour le prénom.");
-    }
-  );
+  targetInputId.addEventListener("input", (e) => noneValide(e, "Veuillez entrer 2 caractères ou plus pour le prénom.")); 
+  targetInputId.addEventListener("input", (e) => noneValide(e, "targetInputId",  "Veuillez entrer une adresse email valide.")); 
+  targetInputId.addEventListener("input", (e) => noneValide(e, "targetInputId",  "Veuillez indiquez une date.")); 
+  targetInputId.addEventListener("input", (e) => noneValide(e, "targetInputId",  "Veuillez un nombre.")); 
+  
+  function noneValide(event , errorMessage ) {
+    targetInputId.setAttribute("data-error-visible", !event.target.validity.valid);
+    targetInputId.setAttribute("data-error", errorMessage);
+  }
+*/
 
-  checkbox1.addEventListener('change', function(event) {
-    if(event.target.checked){
-      document.getElementById("formCheckbox1").setAttribute('data-error-visible', false);
-    } else
-      document.getElementById("formCheckbox1").setAttribute('data-error-visible', true);
-      document.getElementById("formCheckbox1").setAttribute('data-error',"Veuillez entrer 2 caractères ou plus pour le prénom.");
-    }
-  );
+/*
+// alert message
+document.querySelector("#formFirst").addEventListener("input", (e) => noneValide(e, "#formFirst",  "Veuillez entrer 2 caractères ou plus pour le prénom.")); 
+document.querySelector("#formLast").addEventListener("input", (e) => noneValide(e, "#formLast",  "Veuillez entrer 2 caractères ou plus pour le prénom.")); 
+document.querySelector("#formEmail").addEventListener("input", (e) => noneValide(e, "#formEmail",  "Veuillez entrer une adresse email valide.")); 
+document.querySelector("#formBirth").addEventListener("input", (e) => noneValide(e, "#formBirth",  "Veuillez indiquez une date.")); 
+document.querySelector("#formQuantity").addEventListener("input", (e) => noneValide(e, "#formQuantity",  "Veuillez un nombre.")); 
+function noneValide(event , id, errorMessage ) {
+  document.querySelector(id).setAttribute("data-error-visible", !event.target.validity.valid);
+  document.querySelector(id).setAttribute("data-error", errorMessage);
+}
+*/
 
-  if (validateLocation()) {
-    document.getElementById("formLocation").setAttribute('data-error-visible', false);
-  } else {
-    document.getElementById("formLocation").setAttribute('data-error-visible', true);
-    document.getElementById("formLocation").setAttribute('data-error', "Veuillez sélectionner une ville.");
+document.querySelector("#formCheckbox1").addEventListener("change", (e) => noneCheck(e, "#formCheckbox1", "Veuillez accepter les conditions d'utilisation.")); 
+document.querySelector("#formLocation").addEventListener("change", (e) => noneCheck(e, "#formLocation",  "Veuillez sélectionner une ville.")); 
+  function noneCheck(event , id, errorMessage ) {
+    document.querySelector(id).setAttribute("data-error-visible", !event.target.checked);
+    document.querySelector(id).setAttribute("data-error", errorMessage);
+}
+  
+  
+// alert mesage at click on submit without form completed
+
+document.querySelector("#submit").addEventListener("click", checkFirst )
+
+function checkFirst() {
+  document.querySelector("#formFirst").addEventListener("invalid", noneCheckFirst("#formFirst", "Veuillez entrer 2 caractères ou plus pour le prénom."));
+  document.querySelector("#formLast").addEventListener("invalid", noneCheckFirst("#formLast", "Veuillez entrer 2 caractères ou plus pour le prénom."));
+  document.querySelector("#formEmail").addEventListener("invalid", noneCheckFirst("#formEmail",  "Veuillez entrer une adresse email valide."));
+  document.querySelector("#formBirth").addEventListener("invalid", noneCheckFirst("#formBirth",  "Veuillez indiquez une date.")); 
+  document.querySelector("#formQuantity").addEventListener("invalid", noneCheckFirst("#formQuantity",  "Veuillez un nombre.")); 
+    function noneCheckFirst( id, errorMessage ) {
+      document.querySelector(id).setAttribute("data-error-visible", true);
+      document.querySelector(id).setAttribute("data-error", errorMessage);
+    }
+
+  if (validateLocation() == false) {
+    document.querySelector("#formLocation").setAttribute("data-error-visible", true);
+    document.querySelector("#formLocation").setAttribute("data-error", "Veuillez sélectionner une ville.");
+  };
+
+  if (checkbox1.checked == false) {
+    document.querySelector("#formCheckbox1").setAttribute("data-error-visible", true);
+    document.querySelector("#formCheckbox1").setAttribute("data-error", "Veuillez accepter les conditions d/'utilisation.");
   };
 }
 
-form.addEventListener('bubmit' , alert());
 
 
 
-first.addEventListener('invalid', () => {
-  document.getElementById("formFirst").setAttribute('data-error-visible', true);
-  document.getElementById("formFirst").setAttribute('data-error',"Veuillez entrer 2 caractères ou plus pour le prénom.");
+// *** Issue 3
+/*
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+ // document.querySelector("#submit").Value="Formulaire Envoyé !";
+  alert("Formulaire envoyé !");
+ //document.querySelectorAll(".formData").style.display = "none";
+ 
 });
+*/
+/*
 
-last.addEventListener('invalid', () => {
-  document.getElementById("formLast").setAttribute('data-error-visible', true);
-  document.getElementById("formLast").setAttribute('data-error',"Veuillez entrer 2 caractères ou plus pour le nom.");
-});
+// Create Text element
+const validationText = document.createElement("p");
+validationText.innerHTML = "Formulaire Envoyé !";
+validationText.style.cssText = "text-align: center; padding: 30vh 0.5vw";
 
-
-email.addEventListener('invalid', () => {
-  document.getElementById("formEmail").setAttribute('data-error-visible', true);
-  document.getElementById("formEmail").setAttribute('data-error',"Veuillez entrer une adresse email valide.");
-});
-
-birthdate.addEventListener('invalid', () => {
-  document.getElementById("formBirth").setAttribute('data-error-visible', true);
-  document.getElementById("formBirth").setAttribute('data-error',"Vous devez avoir 1 an minimum pour participer.");
-});
-
-quantity.addEventListener('invalid', () => {
-  document.getElementById("formQuantity").setAttribute('data-error-visible', true);
-  document.getElementById("formQuantity").setAttribute('data-error', "Veuillez entrer une valeur comprise entre 0 et 99.");
-});
+// Création de l"élément boutton
+const validationButton = document.createElement("button");
+validationButton.classList.add("btn-submit");
+validationButton.classList.add("final-submit");
+validationButton.style.cssText = "margin: 0px 14%; text-align: center;";
+validationButton.innerHTML = "Close";
 
 
-if (validateLocation() == false) {
-  document.getElementById("formLocation").setAttribute('data-error-visible', true);
-  document.getElementById("formLocation").setAttribute('data-error', "Veuillez sélectionner une ville.");
-};
+// Ajout dans le DOM
 
-if (checkbox1.checked == false) {
-  document.getElementById("formCheckbox1").setAttribute('data-error-visible', true);
-  document.getElementById("formCheckbox1").setAttribute('data-error', "Veuillez accepter les conditions d'utilisation.");
-};
+const modalBody = document.querySelector(".modal-body");
+
+modalBody.appendChild(validationText);
+modalBody.appendChild(validationButton);
+//modalbg.after(newModal);
+
+// Fermeture de la modale sur la croix et le boutton
+//validationButton.addEventListener("click", () => newModal.remove());
+//closeIcon.addEventListener("click", () => newModal.remove());
+
+
+// Gestion de l"affichage de la modale de confirmation
+const DISPLAY_VALIDATION_POPUP = "DISPLAY_VALIDATION_POPUP";
+
+*/
