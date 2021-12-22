2bdc26962dd3133fb0a1b7ec273aa37ead7fe727
JOJOBAN <91075976+JOJOBAN@users.noreply.github.com>
2021-12-22 14:52:38 +0100

    modification html

--- js/modal.js
+++ js/modal.js
@@ -24,15 +24,15 @@ function launchModal() {
 //Fermeture du modal
 bouton.addEventListener("click", () => (modalbg.style.display = "none"));
 
-//Validation du formulaire
-function valider() {
-  // si la valeur du champ prenom est non vide
-  if (document.formData.prenom.value != "") {
-    // alors on envoie le formulaire
-    document.formData.submit();
-  } else {
-    // sinon on affiche un message
-    alert("Saisissez le prénom");
-  }
-}
-console.log(".formData");
+// //Validation du formulaire
+// function valider() {
+//   // si la valeur du champ prenom est non vide
+//   if (document.formData.prenom.value != "") {
+//     // alors on envoie le formulaire
+//     document.formData.submit();
+//   } else {
+//     // sinon on affiche un message
+//     alert("Saisissez le prénom");
+//   }
+// }
+// console.log(".formData");
