546b1f95429f48dfb25b094ab598f01f9228cb8c
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2021-12-23 16:09:59 +0100

    ajustement close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,7 +8,9 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalbg = document.getElementById("bground");
+const closeModalBtn = document.querySelector(".closeModal");
+
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
@@ -20,4 +22,58 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//close modal event
+closeModalBtn.addEventListener("click", closeModal);
 
+//close modal form
+function closeModal() {
+  modalbg.style.display = 'none';
+  closeModalBtn.removeEventListener('click', closeModal)
+}
+
+
+
+// //message erreur adr mail
+
+let form = document.forms["reserve"]
+let verifMail = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+
+form.addEventListener('submit', function(e){
+  
+  let prenom = form.querySelector('input[name="first"]')
+  let nom = form.querySelector('input[name="last"]')
+  let email = document.querySelector('email')
+  let dateDeNaissance = form.querySelector('input[name="birthdate"]')
+
+
+  if(!email.value.trim().match(verifMail))
+  {
+    alert('l\'adresse email n\'est pas valide')
+    e.preventDefault()
+    e.removeEventListener()
+  }
+})
+
+
+
+
+
+/*
+const getUsers = async function () {
+  let data = await fetch()
+  .then(response => response.json())
+  console.log(data)
+}
+
+getUsers()
+
+let email = document.querySelector('#email')
+email.focus()
+
+document.querySelector('form').addEventListener('submit', function(e){
+    let mentions = document.querySelector('#mentions')
+    if(!mentions.checked) {
+        alert('Vous n\'avez pas accepté les CGU')
+        e.preventDefault()
+    }
+})*/
