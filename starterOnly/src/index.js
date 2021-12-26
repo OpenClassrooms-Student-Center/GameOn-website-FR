87ba538133ca7618de35a320fc6d64ac1429f7a6
Nicolas Ka <n.ka@lepermislibre.fr>
2021-12-26 15:28:30 +0100

    markup cleaning

--- starterOnly/src/index.js
+++ starterOnly/src/index.js
@@ -1,10 +1,12 @@
 import { editNav, capitalize } from "./utils.js";
+import * as validator from "./validators.js";
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeModalBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("signup");
 
 // TEXT FORMATTING
 capitalize(document.querySelector(".btn-signup.modal-btn"));
@@ -23,4 +25,10 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-//
+const formEl = document.forms.signup;
+const formDataEl = new formData(formEl);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(formDataEl);
+});
--- starterOnly/src/validators.js
+++ starterOnly/src/validators.js
@@ -1,16 +1,30 @@
 export const firstNameValidator = (string) => {
-    return string && string.length > 0 ? true : false 
-}
+  return string && string.length > 0 ? true : false;
+};
 
 export const lastNameValidator = (string) => {
-    return string && string.length > 0 ? true : false 
-}
+  return string && string.length > 0 ? true : false;
+};
 
 export const emailValidator = (string) => {
-    const regex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
-    return string.match(regex) ? true : false;
-}
+  const regex = new RegExp(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+  return string.match(regex) ? true : false;
+};
 
 export const isNumericValidator = (string) => {
-    return typeof parseInt(string) === Number ? true : false
-}
+  return typeof parseInt(string) === Number ? true : false;
+};
+
+export const radioValidator = (options, string) => {
+  let result = false;
+  options.forEach((option) => {
+    if (string === option) return (result = true);
+  });
+  return result;
+};
+
+export const hasAcceptedGCU = (element) => {
+  return element.checked === true ? true : false;
+};
