83e3a9d8acc1e2e3fd40f3e50bddc14c648d5010
Nicolas Ka <n.ka@lepermislibre.fr>
2021-12-31 10:26:18 +0100

    all issues resolved

--- starterOnly/src/utils.js
+++ starterOnly/src/utils.js
@@ -1,16 +1,12 @@
 export function editNav() {
-    const x = document.getElementById("myTopnav"); // no vars since es6
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-  
-  // auto captitalize
+  const nav = document.getElementById("myTopnav"); // no vars since es6
+  nav.classList.toggle("responsive"); // Simplifying burger menu logic using classlist toggle.
+}
+
+// Auto captitalize first letter
 export const capitalize = (element) => {
-    const textContent = element.innerHTML;
-    element.innerHTML = `${textContent
-      .charAt(0)
-      .toUpperCase()}${textContent.slice(1).toLowerCase()}`;
-  };
+  const textContent = element.innerHTML;
+  element.innerHTML = `${textContent.charAt(0).toUpperCase()}${textContent
+    .slice(1)
+    .toLowerCase()}`;
+};
--- starterOnly/src/validators.js
+++ starterOnly/src/validators.js
@@ -1,30 +1,79 @@
-export const firstNameValidator = (string) => {
+// The validators are in a separated file so if I need to use them elsewhere 
+// I can just import validators. It also make the code more consice and easier to read. 
+
+const firstNameValidator = (string) => {
   return string && string.length > 0 ? true : false;
 };
 
-export const lastNameValidator = (string) => {
+const lastNameValidator = (string) => {
   return string && string.length > 0 ? true : false;
 };
 
-export const emailValidator = (string) => {
+const emailValidator = (string) => {
+  // Some regular expression to check if the format of the email 
+  // is right(example: 'nicolas@email.fr' is valid)
   const regex = new RegExp(
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
   return string.match(regex) ? true : false;
 };
 
-export const isNumericValidator = (string) => {
-  return typeof parseInt(string) === Number ? true : false;
+const birthDateValidator = (string) => {
+  return string && string.length > 0 ? true : false;
 };
 
-export const radioValidator = (options, string) => {
-  let result = false;
-  options.forEach((option) => {
-    if (string === option) return (result = true);
-  });
-  return result;
+const isNumericValidator = (string) => {
+  return parseInt(string) ? true : false;
 };
 
-export const hasAcceptedGCU = (element) => {
-  return element.checked === true ? true : false;
+const radioValidator = (selection) => {
+  return selection !== "" ? true : false;
 };
+
+const hasAcceptedGCUValidator = (value) => {
+  return value ? true : false;
+};
+
+const validators = function (el) {
+  // I use a switch to validate the currentEl value based on its name.
+  switch (el.name) {
+    case "firstName":
+      return (
+        firstNameValidator(el.value) ||
+        "Votre prénom doit faire au moins 2 caractères"
+      );
+    case "lastName":
+      return (
+        lastNameValidator(el.value) ||
+        "Votre nom doit faire au moins 2 caractères"
+      );
+    case "email":
+      return emailValidator(el.value) || "Veuillez rentrer un email valide";
+    case "birthdate":
+      return (
+        birthDateValidator(el.value) ||
+        "Veuillez rentrer votre date de naissance"
+      );
+    case "quantity":
+      return (
+        isNumericValidator(el.value) ||
+        "Veuillez renseigner le nombre de tournois"
+      );
+    case "location":
+      return (
+        radioValidator(el.value) || "Veuillez selectionner un lieu de tournois"
+      );
+    case "hasAcceptedGCU":
+      return (
+        hasAcceptedGCUValidator(el.value) ||
+        "Vous devez aggréer à nos conditions générales d'utilisation"
+      );
+
+    // In case there is an input with no specific validation, so
+    // if I want to add an optionnal input, it will be validated by default
+    default:
+      return true;
+  }
+};
+
+export default validators;
