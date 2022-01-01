cc3eac0e67071f68ba526f22b9fb242668015e24
FrekoJazz <jason_tirard6@hotmail.com>
2022-01-01 01:19:39 +0100

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const firstName = document.querySelector()
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal)); //les fonctions => n'ont pas de noms
 
 // launch modal form
-function launchModal() {
+function launchModal() { //
   modalbg.style.display = "block";
 }
+// Close modal
+closeBtn.addEventListener("click", closeModal)
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+// validation du formulaire
 
