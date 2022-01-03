233843d6e990dc8d43cbf740828826d0bee80247
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-01-03 19:12:00 +0900

    Fermer la modal + correction HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const modalClose = document.querySelector(".close") //Création et chargement de la constante 'modalClose' et accès à la class .close du DOM  
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +20,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//Fermeture du questionnaire
+modalClose.addEventListener("click", close); //Ecoute event clic de souris sur l'element close, nommé 'close'
 
+//Appel de la fonction 'close' pour cacher le formulaire
+function close () {
+  modalbg.style.display = "none"; 
+}
