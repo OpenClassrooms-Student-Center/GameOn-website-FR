f37ef959f16d1f1a4080e483b2b1e5c2228955aa
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-01-04 16:03:46 +0100

    alignement css test message modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,6 +14,7 @@ const closeModalBtn = document.querySelector(".closeModal");
 const modalBtn = document.querySelector(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const toggleMenu = document.querySelectorAll(".toggleMenu");
+const modalThanks = document.getElementById("thanks");
 
 
 /*TOGGLE MENU*/
@@ -30,6 +31,9 @@ modalBtn.addEventListener("click", launchModal);
 // launch modal form
 function launchModal(){
   modalbg.style.display = "block";
+  window.scrollTo(0, 0);
+  modalbg.scrollTo(0,0);
+  document.body.style.overflow = "hidden";
 }
 
 /*FERMETURE MODAL*/
@@ -47,8 +51,7 @@ function closeModal(){
 let form = document.forms["reserve"];
 
 //format text et mail
-let verifMail =
-  /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+let verifMail = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
 let textFormat = /^[a-zA-Z\é\è\-\^\']{2,30}$/;
 
 form.addEventListener("submit", function(e){
@@ -71,7 +74,7 @@ form.addEventListener("submit", function(e){
   if(first.value == "" || textFormat.test(first.value) == false){
     error.textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
     error.style.color = "red";
-    error.fontSize = '0.5rem';
+    error.style.fontSize = '0.75rem';
   }else{
     error.innerHTML = "";
     formErrors++;
@@ -81,14 +84,14 @@ form.addEventListener("submit", function(e){
   if(last.value == "" || textFormat.test(last.value) == false){
     error1.textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
     error1.style.color = "red";
-    error1.fontSize = '0.5rem';
+    error.style.fontSize = '0.75rem';;
   }else{
     error1.innerHTML = "";
     formErrors++;
   }
 
   //label mail
-  if (!email.value.trim().match(verifMail)){
+  if(!email.value.trim().match(verifMail)){
     alert("l'adresse email n'est pas valide");
     e.removeEventListener();
   }
@@ -97,7 +100,7 @@ form.addEventListener("submit", function(e){
   if(birthdate.value == ""){
     error2.textContent = 'Vous devez entrer votre date de naissance.';
     error2.style.color = "red";
-    error2.fontSize = '0.5rem';
+    error.style.fontSize = '0.75rem';
   }else{
     error2.innerHTML = "";
     formErrors++;
@@ -107,7 +110,7 @@ form.addEventListener("submit", function(e){
   if(quantity.value == ""){
     error3.textContent = 'Vous devez saisir un nombre.';
     error3.style.color = "red";
-    error3.fontSize = '0.5rem';
+    error3.style.fontSize = '0.75rem';
   }else{
     error3.innerHTML = "";
     formErrors++;
@@ -127,37 +130,57 @@ form.addEventListener("submit", function(e){
   }else{
     errorMessage.textContent = "Vous devez choisir une option.";
     errorMessage.style.color = "red";
-    errorMessage.fontSize = "0.5rem";
+    errorMessage.style.fontSize = "0.75rem";
   }
 
   if(!terms.checked){
     errorTerms.textContent =
       "Vous devez vérifier que vous acceptez les termes et conditions.";
     errorTerms.style.color = "red";
-    errorTerms.style.fontSize = "0.5em";
+    errorTerms.style.fontSize = "0.75rem";
   }else{
     errorTerms.innerHTML = "";
     formErrors++;
   }
 
-  console.log(formErrors);
-  if(formErrors === 7) {
-    reserve.innerHTML = "Votre reservation <br> a bien été reçue";
-    btnClose.style.display = "block";
-    reserve.style = "display: flex;  justify-content: center; align-item : center";
-    document.getElementById("reserve").reset();
-    closeDelay();
+  if (formErrors > 0){
+    console.log('azeaze')
+    return false
+  }else{
+    form.reset();
+    showModalThanks()
   }
 
-  function closeDelay(){
-    window.setTimeout(reloadPage, 5000);
-  }
 
-  function reloadPage(){
-    location.reload();
-  }
-});
 
+    // console.log(formErrors);
+    // if(formErrors === 7) {
+      //   reserve.innerHTML = "Votre reservation <br> a bien été reçue";
+      //   btnClose.style.display = "block";
+      //   reserve.style = "display: flex;  justify-content: center; align-item : center";
+      //   document.getElementById("reserve").reset();
+      //   closeDelay();
+      // }
+      
+      // function closeDelay(){
+        //   window.setTimeout(reloadPage, 5000);
+        // }
+        
+        // function reloadPage(){
+          //   location.reload();
+          // }
+        });
+        
+        function showModalThanks(){
+          formulaire.style.display= 'none';
+          modalThanks.style.display = '';
+        }
+        
+        
+        function showModalForm(){
+        modalThanks.style.display = 'none';
+        formulaire.style.display = '';
+        }
 /*
 const getUsers = async function () {
   let data = await fetch()
