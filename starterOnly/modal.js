20a39f802fab2ff47b5320a56cc1c93142efe563
MarilyneD <marilynedestombes@gmail.com>
2022-01-05 17:38:14 +0100

    issue1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close") //constant which retrieves the element corresponding to the cross
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +19,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
-}
+}  
 
+
+//waiting for a click on the close class and then launching the function closeModal
+close.addEventListener('click', closeModal); 
+
+
+//function allowing to close the modal when clicking on the cross
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
