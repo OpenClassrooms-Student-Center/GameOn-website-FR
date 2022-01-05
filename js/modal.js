ced46f73825893c6aaf73cd7cbb9052f47c8f023
JOJOBAN <91075976+JOJOBAN@users.noreply.github.com>
2022-01-05 14:28:26 +0100

    refactorisation code input

--- js/modal.js
+++ js/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const bouton = document.querySelector(".close");
+const submitBtn = document.querySelector(".btn-submit");
 const confirmationCloseBtn = document.querySelectorAll(".btn-close");
 const regexName = /[a-zA-Z]+/g;
 const regexEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
@@ -41,99 +42,142 @@ function launchModal() {
 bouton.addEventListener("click", () => (modalbg.style.display = "none"));
 
 //Empêcher la page de se recharger
-formData.forEach((form) =>
-  form.addEventListener("submit", (e) => e.preventDefault())
-);
-//Fonction de contrôle du prenom
+submitBtn.addEventListener("click", (e) => e.preventDefault());
+
+//Fonctions pour le formulaire
+function goodBorder(element) {
+  changeBorder(element, "green");
+}
+
+function badBorder(element) {
+  changeBorder(element, "red");
+}
+
+function changeBorder(element, color) {
+  element.style.border = "2px solid " + color;
+}
+
+function removeInvisible(element) {
+  element.nextElementSibling.classList.remove("invisible");
+}
+
+//Fonctions de contrôle des inputs
 first.addEventListener("blur", (e) => {
   checkPrenom();
 });
-function checkPrenom() {
-  const firstValue = first.value.trim();
-  console.log(firstValue);
-  if (regexName.exec(first.value) === null || first.length < 2) {
-    first.nextElementSibling.classList.remove("invisible");
-    first.style.border = "2px solid red";
-    console.log("Erreur! Veuillez entrer un prénom valide");
-  } else {
-    first.style.border = "2px solid green";
-    first.nextElementSibling.innerHTML = "";
-    console.log("Bravo vous avez un prénom valide");
-  }
-}
-
-//Fonction de contrôle du nom
 last.addEventListener("blur", (e) => {
   checkNom();
 });
-function checkNom() {
-  const lastValue = last.value.trim();
-  console.log(lastValue);
-  if (regexName.exec(last.value) === null || last.length < 2) {
-    last.nextElementSibling.classList.remove("invisible");
-    last.style.border = "2px solid red";
-    console.log("Erreur! Veuillez entrer un nom valide");
-  } else {
-    last.style.border = "2px solid green";
-    last.nextElementSibling.innerHTML = "";
-    console.log("Bravo vous avez un nom valide");
-  }
-}
-
-//validation du formulaire de l'email
 email.addEventListener("blur", (e) => {
   checkEmail();
 });
-
-function checkEmail() {
-  const emailValue = email.value.trim();
-  console.log(emailValue);
-  if (regexEmail.exec(email.value) === null) {
-    last.nextElementSibling.classList.remove("invisible");
-    email.style.border = "2px solid red";
-    console.log("Erreur! Veuillez entrer une adresse email valide");
-    return false;
-  } else {
-    email.style.border = "2px solid green";
-    console.log("Bravo vous avez un email valide");
-    return true;
-  }
-}
-
-//Fonction de contrôle de la date de naissance
 birthdate.addEventListener("blur", (e) => {
   checkBirthdate();
 });
-function checkBirthdate() {
-  const birthdateValue = birthdate.value.trim();
-  console.log(birthdateValue);
-  if (birthdate.value == "") {
-    birthdate.style.border = "2px solid red";
-    birthdate.nextElementSibling.classList.remove("invisible");
-    console.log("Erreur! Veuillez entrer une date de naissance valide");
-  } else {
-    birthdate.style.border = "2px solid green";
-    birthdate.nextElementSibling.innerHTML = "";
-    console.log("Bravo vous avez une date de naissance valide");
-  }
-}
-
-//Fonction de validation du nombre de tournois
 quantity.addEventListener("blur", (e) => {
   checkQuantity();
 });
+
+//Fonction de contrôle du prénom
+function checkPrenom() {
+  const firstValue = first.value.trim();
+  if (regexName.exec(firstValue) === null || first.length < 2) {
+    removeInvisible(first);
+    badBorder(first);
+    return false;
+  }
+  goodBorder(first);
+  first.nextElementSibling.innerHTML = "";
+  return true;
+}
+
+//Fonction de contrôle du nom
+function checkNom() {
+  const lastValue = last.value.trim();
+  if (regexName.exec(lastValue) === null || last.length < 2) {
+    removeInvisible(last);
+    badBorder(last);
+    return false;
+  }
+  goodBorder(last);
+  last.nextElementSibling.innerHTML = "";
+  return true;
+}
+
+//validation du formulaire de l'email
+function checkEmail() {
+  const emailValue = email.value.trim();
+  if (regexEmail.exec(emailValue) === null) {
+    removeInvisible(email);
+    badBorder(email);
+    return false;
+  }
+  goodBorder(email);
+  email.nextElementSibling.innerHTML = "";
+  return true;
+}
+
+//Fonction de contrôle de la date de naissance
+function checkBirthdate() {
+  const birthdateValue = birthdate.value;
+  if (birthdateValue == "") {
+    removeInvisible(birthdate);
+    badBorder(birthdate);
+    return false;
+  }
+  goodBorder(birthdate);
+  birthdate.nextElementSibling.innerHTML = "";
+  return true;
+}
+
+//Fonction de validation du nombre de tournois
 function checkQuantity() {
   const quantityValue = quantity.value;
-  console.log(quantityValue);
-  if (quantity.value == "") {
-    quantity.style.border = "2px solid red";
-    quantity.nextElementSibling.classList.remove("invisible");
-    console.log("Erreur! Veuillez entrer un nombre de tournois valide");
-  } else {
-    quantity.style.border = "2px solid green";
-    quantity.nextElementSibling.innerHTML = "";
-    console.log("Bravo vous avez un nombre de tournois valide");
+  if (quantityValue === "" || quantityValue > 99) {
+    removeInvisible(quantity);
+    badBorder(quantity);
+    return false;
   }
+  goodBorder(quantity);
+  quantity.nextElementSibling.innerHTML = "";
+  return true;
+}
+
+//Fonction de validation location
+function checkLocation() {
+  const location1Value = location1.value;
+  const location2Value = location2.value;
+  const location3Value = location3.value;
+  const location4Value = location4.value;
+  const location5Value = location5.value;
+  const location6Value = location6.value;
+  if (
+    location1Value === "" ||
+    location2Value === "" ||
+    location3Value === "" ||
+    location4Value === "" ||
+    location5Value === "" ||
+    location6Value === ""
+  ) {
+    removeInvisible(location1);
+    badBorder(location1);
+    return false;
+  }
+  goodBorder(location1);
+  location1.nextElementSibling.innerHTML = "";
+  return true;
+}
+
+//Fonction de validation checkbox
+function checkCheckbox() {
+  if (checkbox.checked === false) {
+    removeInvisible(checkbox);
+    badBorder(checkbox);
+    return false;
+  }
+  goodBorder(checkbox);
+  checkbox.nextElementSibling.innerHTML = "";
+  return true;
 }
 
 // //Fonction de validation du formulaire
