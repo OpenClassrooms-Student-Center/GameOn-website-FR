85f462168352bb1e5721b0f1ad38790e30dbb52f
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-01-13 10:46:47 +0100

    test radiovalue

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,16 +1,18 @@
 
 
 // DOM Elements
+const toggleMenu = document.querySelectorAll(".toggleMenu")
+
 const modalbg = document.getElementById("bground");
-const closeModalBtn = document.querySelector(".closeModal");
-const modalBtn = document.querySelector(".btn-signup");
-const formData = document.querySelectorAll(".formData");
-const toggleMenu = document.querySelectorAll(".toggleMenu");
 const modalThanks = document.getElementById("thanks");
+
+const modalBtn = document.querySelector(".btn-signup");
+const closeModalBtn = document.querySelector(".closeModal");
+const thanksCloseBtn = document.getElementById("closeAll");
 
 
 /*TOGGLE MENU*/
-toggleMenu.forEach((el) => el.addEventListener("click", (e) => {
+toggleMenu.forEach((el) => el.addEventListener("click", () => {
   document.body.classList.toggle('menu-open');
 }
 ));
@@ -21,13 +23,10 @@ modalBtn.addEventListener("click", launchModal);
 //close modal event
 closeModalBtn.addEventListener("click", closeModal);
 //Thank modal
-
-
-
+thanksCloseBtn.addEventListener("click", closeModalThanks);
 
 
 /*FONCTION MODAL*/
-
 // launch modal form
 function launchModal(){
   modalbg.style.display = "block";
@@ -43,28 +42,31 @@ function closeModal(){
   document.body.classList.remove('overflow');
 }
 
-//modal thanks
+//open modal thanks
 function showModalThanks(){
   form.style.display= 'none';
   modalThanks.style.display = 'block';
   document.querySelector('#closeAll').addEventListener("click", closeModal);
 }
-
-
-function showModalForm(){
+//close modal thanks
+function closeModalThanks(){
 modalThanks.style.display = 'none';
-form.style.display = 'block';
+modalbg.style.display = 'none';
 }
+
+
 
 //variable formulaire
 let form = document.forms["reserve"];
-
 //format text et mail
 let verifMail = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
 let textFormat = /^[a-zA-Z\é\è\-\^\']{2,30}$/;
 
+
 form.addEventListener("submit", function(e){
   e.preventDefault();
+  
+  let formErrors = 0;
 
   //constantes
   const first = form.querySelector('input[name="first"]');
@@ -72,13 +74,15 @@ form.addEventListener("submit", function(e){
   const email = form.querySelector('input[name="email"]');
   const birthdate = form.querySelector('input[name="birthdate"]');
   const quantity = form.querySelector('input[name="quantity"]');
+  const radiolocation = document.querySelector('input[name="location"]');
+  const terms = document.getElementById('terms');
 
   const error = document.getElementById("error");
   const error1 = document.getElementById("error1");
   const error2 = document.getElementById("error2");
   const error3 = document.getElementById("error3");
+  const errorMessage = document.getElementById("errorMessage");
 
-  let formErrors = 0;
 
   //label prenom
   if(first.value == "" || textFormat.test(first.value) == false){
@@ -94,7 +98,7 @@ form.addEventListener("submit", function(e){
   if(last.value == "" || textFormat.test(last.value) == false){
     error1.textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
     error1.style.color = "red";
-    error.style.fontSize = '0.75rem';;
+    error.style.fontSize = '0.75rem';
     formErrors++;
   }else{
     error1.innerHTML = "";
@@ -127,26 +131,71 @@ form.addEventListener("submit", function(e){
   }
 
   //location
-  
-  if(
-    !document.getElementById("location1").checked ||
-    !document.getElementById("location2").checked ||
-    !document.getElementById("location3").checked ||
-    !document.getElementById("location4").checked ||
-    !document.getElementById("location5").checked ||
-    !document.getElementById("location6").checked)
-    {
-    formErrors++;
-    errorMessage.textContent = "Vous devez choisir une option.";
+
+  let radiovalue;
+  radiolocation.forEach((el) => {
+    if (el.checked) radioValue = el.value;
+  }
+  );
+  if (!radioValue) {
+    errorMessage.textContent = "vous devez choisir une option.";
     errorMessage.style.color = "red";
     errorMessage.style.fontSize = "0.75rem";
+    formErrors++
   }else{
-    errorMessage.innerHTML = '';
+    errorMessage.innerHTML = "";
   }
 
+  // switch (radiolocation){
+  //   case 0: document.getElementById("location1").checked
+  //   break
+  //   case 1: document.getElementById("location2").checked
+  //   break
+  //   default document.getElementsByClassName("checkbox-input").innerHTML = "Vous devez choisir une option."
+  // }
+  
+// if(!radiolocation.checked) {
+//   errorMessage.textContent = "vous devez choisir une option.";
+//   errorMessage.style.color = "red";
+//   errorMessage.style.fontSize = "0.75rem";
+//   formErrors++;
+// }else{
+//   errorMessage.innerHTML = "";
+// }
+
+  /* errorMessage.innerHTML = "";
+  // if(
+  //   document.getElementById("location1").checked ||
+  //   document.getElementById("location2").checked ||
+  //   document.getElementById("location3").checked ||
+  //   document.getElementById("location4").checked ||
+  //   document.getElementById("location5").checked ||
+  //   document.getElementById("location6").checked
+  // ){
+  // }else{
+  //   errorMessage.textContent = "Vous devez choisir une option.";
+  //   errorMessage.style.color = "red";
+  //   errorMessage.style.fontSize = "0.75rem";
+  //   formErrors++;
+  /* }
+
+
+
+  /* if(('input[type=radio]:checked').length === 0) {
+  //   errorMessage.textContent = "Vous devez choisir une option.";
+  //   errorMessage.style.color = "red";
+  //   errorMessage.style.fontSize = "0.75rem";
+  //   formErrors++;
+  // }else{
+  //   errorMessage.innerHTML = '';
+ }*/
+
+
+
+  //Conditions and subscribe
+  
   if(!terms.checked){
-    errorTerms.textContent =
-      "Vous devez vérifier que vous acceptez les termes et conditions.";
+    errorTerms.textContent = "Vous devez vérifier que vous acceptez les termes et conditions.";
     errorTerms.style.color = "red";
     errorTerms.style.fontSize = "0.75rem";
     formErrors++;
@@ -155,11 +204,13 @@ form.addEventListener("submit", function(e){
   }
 
   if (formErrors > 0){
+    return false
   }else{
     form.reset();
-    showModalThanks()
+    closeModal;
+    showModalThanks();
   }
-
+window.location.reload
 });
         
         
