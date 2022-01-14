d21ddf4fa280fbbedf89f8d9b1ab116034e0836e
MarilyneD <marilynedestombes@gmail.com>
2022-01-14 21:24:00 +0100

    issue 2 et issue 3 terminées

--- starterOnly/js/openclosemodal.js
+++ starterOnly/js/openclosemodal.js
@@ -11,23 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const close = document.querySelector(".close") //constant which retrieves the element corresponding to the cross
+const close = document.querySelector(".close"); //constant which retrieves the element corresponding to the cross
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// launch modal form and reset data
 function launchModal() {
+  formulary.reset();
   modalbg.style.display = "block";
-}  
-
-
-//waiting for a click on the close class and then launching the function closeModal
-close.addEventListener('click', closeModal); 
-
-
-//function allowing to close the modal when clicking on the cross
-function closeModal() {
-  modalbg.style.display = "none";
 }
 
+//waiting for a click on the close class and then launching the function closeModal
+close.addEventListener("click", closeModal);
+
+//function allowing to close the modal when clicking on the cross
+function closeModal(disabled) {
+  modalbg.style.display = "none";
+}
