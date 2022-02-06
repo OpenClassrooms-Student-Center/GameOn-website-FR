d872cee2cbb6105b2558d854ae486df43f3843f2
nasrlassaar <89594360+nasrlassaar@users.noreply.github.com>
2022-02-06 11:23:06 +0100

    feat: add close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,23 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
+
+console.log(closeBtn);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "";
+}
