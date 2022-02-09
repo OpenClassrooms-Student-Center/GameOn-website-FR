3ee675eb2339e1e851d6148e294c54045c093c48
Madeline <grammaticomadeline@hotmail.fr>
2022-02-09 16:24:33 +0100

    feat(form) :  add validCheckbox object

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -74,50 +74,52 @@ const formValidate = {
 
 const form = document.getElementById("reverve");
 form.addEventListener("submit",function validate(e) {
-  // const inputs = form.getElementsByClassName("text-control");
-  // let asError = false;
-  // console.log(inputs);
+  const inputs = form.getElementsByClassName("text-control");
+  let asError = false;
+  console.log(inputs);
 
-  // for (let i = 0; i< inputs.length; i++) {
-  //   for(const item in formValidate){
+  for (let i = 0; i< inputs.length; i++) {
+    for(const item in formValidate){
      
-  //     if (inputs[i].name === formValidate[item].name){
-  //       console.log(inputs[i].value);
-  //       console.log("regex " + formValidate[item].regex.test(inputs[i].value));
-  //       const input = document.getElementById(inputs[i].id);
-  //       const hasRegexValid = formValidate[item].regex.test(inputs[i].value)
-  //       console.log(hasRegexValid);
+      if (inputs[i].name === formValidate[item].name){
+        console.log(inputs[i].value);
+        console.log("regex " + formValidate[item].regex.test(inputs[i].value));
+        const input = document.getElementById(inputs[i].id);
+        const hasRegexValid = formValidate[item].regex.test(inputs[i].value);
+        console.log(hasRegexValid);
 
-  //       if (!formValidate[item].regex.test(inputs[i].value)) {
-  //         console.log("erreur");
-  //         asError = true;
-  //         console.log("asError " + asError);
-  //         if (input.nextElementSibling == null) {
-  //           let p = document.createElement("p");
-  //           input.parentNode.appendChild(p);
-  //           console.log("p créé")
-  //         }
-  //         let pError = input.nextElementSibling;
-  //         console.log(pError);
-  //         console.log(formValidate[item].errorCustom)
-  //         pError.innerHTML = formValidate[item].errorCustom;
+        if (!formValidate[item].regex.test(inputs[i].value)) {
+          console.log("erreur");
+          asError = true;
+          console.log("asError " + asError);
+          if (input.nextElementSibling == null) {
+            let p = document.createElement("p");
+            input.parentNode.appendChild(p);
+            console.log("p créé")
+          }
+          let pError = input.nextElementSibling;
+          console.log(pError);
+          console.log(formValidate[item].errorCustom)
+          pError.innerHTML = formValidate[item].errorCustom;
 
-  //       } else if (formValidate[item].regex.test(inputs[i].value)) {
-  //         if (input.nextElementSibling == null) {
-  //           console.log("tout est ok");
-  //         }
-  //         else {
-  //           let pError = input.nextElementSibling;
-  //         pError.innerHTML = "";
-  //         console.log("effacement message erreur");
-  //         };
+        } else if (formValidate[item].regex.test(inputs[i].value)) {
+          console.log("regex ok");
+          if (input.nextElementSibling == null) {
+            console.log("tout est ok");
+          }
+          else {
+            let pError = input.nextElementSibling;
+          pError.innerHTML = "";
+          console.log("effacement message erreur");
+          };
           
-  //       };
-  //     };
-  //   };
-  // };
+        };
+      };
+    };
+  };
   validateRadios();
-  if (asError == true) {
+  console.log(validateRadios())
+  if (asError == true|| validateRadios() == false) {
     e.preventDefault();
     return false
   };
@@ -125,18 +127,32 @@ form.addEventListener("submit",function validate(e) {
   alert('Formulaire envoyé !');
 });
 
+validChecbox = {
+  localisation: {
+    name: "location",
+    error: "Vous devez choisir une option.",
+    required: true
+  },
+  checkbox1: {
+    name: "checkbox1",
+    error: "Vous devez vérifier que vous acceptez les termes et conditions.",
+    required: true
+  },
+  checkbox2: {
+    name: "checkbox2",
+    required: false,
+  }
+}
 function validateRadios() {
   let formValid = true;
   let inputwarps = document.getElementsByClassName("inputWarp");
 
   console.log("validateRadios");
-  console.log(inputwarps)
   for (let i = 0; i< inputwarps.length; i++) {
     const checkbox = inputwarps[i].getElementsByClassName("checkbox-input");
     let hasInputWarpsValid = false
     for (let b = 0; b < checkbox.length; b++) {
-      console.log(checkbox)
-      // console.log('je suis dans checkbox ' + checkbox)
+
       if (checkbox[b].checked) {
         hasInputWarpsValid = true
         console.log('checkbox checked')
@@ -146,7 +162,6 @@ function validateRadios() {
     if (!hasInputWarpsValid) {formValid = false};
     console.log("formValid = " + formValid)
   }
-  if (!formValid) alert("must check some option");
-  return formValid;
+  if (!formValid)return false;
 }
 
