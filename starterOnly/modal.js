ce1d383c9619f77dc25358b3f56142054a7bb290
FrekoJazz <jason_tirard6@hotmail.com>
2022-02-12 02:13:27 +0100

    modal de fin a terminer

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,6 +10,7 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn2 = document.querySelectorAll(".modal-btn2");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 const myForm = document.querySelector("form");
@@ -21,13 +22,15 @@ const quantity = document.getElementById("quantity");
 const locations = document.getElementsByName("location");
 const checkbox1 = document.getElementById("checkbox1");
 const checkbox2 = document.getElementById("checkbox2");
+const btnSubmit = document.querySelector("input[type=submit]")
 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal)); //les fonctions => n'ont pas de noms
+modalBtn2.forEach((btn) => btn.addEventListener("click", closeModal)); //les fonctions => n'ont pas de noms
 
 // launch modal form
-function launchModal() { //
+function launchModal() {
   modalbg.style.display = "block";
 }
 // Close modal
@@ -49,27 +52,25 @@ function removeError(input) {
 //déclarations des variables
 let regexName = /^([a-zA-Z]{2,30}\s*)+/;
 let regexMail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-let regexNombre = /^(0|[1-9]\d*)$/;
-let eventError = true;
 
-// Evenement pour le Prenom OK !
-eventError = true;
+// Evénement pour le Prénom OK !
+let eventErrorFirst = true;
 first.addEventListener("input", function (e) {
   if (regexName.test(first.value) == true) {
     removeError(e.target.parentNode);
-    eventError = false;
+    eventErrorFirst = false;
   }
   else {
     displayError(e.target.parentNode, "le prénom doit comporter minimum 2 caractères");
   }
 });
 
-// Evenement pour le Nom OK !
-eventError = true;
+// Evénement pour le Nom OK !
+let eventErrorLast = true;
 last.addEventListener("input", function (e) {
   if (regexName.test(last.value) == true) {
     removeError(e.target.parentNode);
-    eventError = false;
+    eventErrorLast = false;
   }
   else {
     displayError(e.target.parentNode, "le nom doit comporter minimum 2 caractères");
@@ -77,58 +78,59 @@ last.addEventListener("input", function (e) {
 });
 
 
-// Evenement pour l'email OK !
-eventError = true;
+// Evénement pour l'email OK !
+let eventErrorEmail = true;
 email.addEventListener("input", function (e) {
   if (regexMail.test(email.value)) {
     removeError(e.target.parentNode);
-    eventError = false;
+    eventErrorEmail = false;
   }
   else {
     displayError(e.target.parentNode, "Veillez renseigner une adresse mail valide");
   }
 });
 
-//Evenement pour le Nombre de tournois Ok !
-eventError = true;
-quantity.addEventListener("input", function (e) {
-  let quantityValid = parseInt(quantity.value);
-  if (quantityValid >= 0 && quantityValid <= 99) {
-    removeError(e.target.parentNode);
-    eventError = false;
-  }
-  else {
-    displayError(e.target.parentNode, "Veillez renseigner le nombre de tournois que vous avez réalisé");
-  }
-});
-
-//Evenement pour la date d'anniversaire OK !
+//Evénement pour la date de naissance OK !
 const today = new Date().toISOString().split("T")[0];
 timestamp = Date.parse(today);
 birthdate.max = today;
-eventError = true;
+let eventErrorBirthdate = true;
 birthdate.addEventListener("input", function (e) {
   let birthdateTime = Date.parse(birthdate.value);
   if (birthdateTime < timestamp) {
     removeError(e.target.parentNode);
-    eventError = false;
+    eventErrorBirthdate = false;
   }
   else {
     displayError(e.target.parentNode, "Veillez renseigner une date de naissance valide");
   }
 });
 
-// location OK !
+//Evénement pour le Nombre de tournois Ok !
+let eventErrorQuantity = true;
+quantity.addEventListener("input", function (e) {
+  let quantityValid = parseInt(quantity.value);
+  if (quantityValid >= 0 && quantityValid <= 99) {
+    removeError(e.target.parentNode);
+    eventErrorQuantity = false;
+  }
+  else {
+    displayError(e.target.parentNode, "Veillez renseigner le nombre de tournois que vous avez réalisé");
+  }
+});
+
+
+// Evénement pour la localisation OK !
+let eventErrorLocation = true;
 for (let i = 0; i < locations.length; i++) {
   locations[i].addEventListener("change", function (e) {
-    eventError = true;
     let error = true;
     for (let u = 0; u < locations.length; u++) {
       if (locations[u].checked) {
         error = false;
         valeur = locations[u].value;
         removeError(e.target.parentNode);
-        eventError = false;
+        eventErrorLocation = false;
         break
       }
     }
@@ -139,12 +141,12 @@ for (let i = 0; i < locations.length; i++) {
   });
 }
 
-// la case des conditions générales est cochée, l'autre case est facultative  OK ! (Appelle l'event 6 fois ????)
-eventError = true;
+// Evénement pour la conditions générales OK !
+let eventErrorCheckbox1 = true;
 checkbox1.addEventListener("input", function (e) {
   if (checkbox1) {
     removeError(e.target.parentNode);
-    eventError = false;
+    eventErrorCheckbox1 = false;
   }
   else {
     displayError(e.target.parentNode, "cette case est obligatoire");
@@ -153,38 +155,46 @@ checkbox1.addEventListener("input", function (e) {
 
 //validation du formulaire: faire un tableau pour recuperer toutes les infos de eventError dans les inputs !
 
-/*function validate(e) {
-  e.preventDefault();
-  alert("Votre inscription est bien pris en compte");
-
-  const first = document.getElementById("first").value;
-  const last = document.getElementById("last").value;
-  const email = document.getElementById("email").value;
-  const birthdate = document.getElementById("birthdate").value;
-  const quantity = document.getElementById("quantity").value;
-  const locations = document.getElementsByName("location").value;
-  const checkbox1 = document.getElementById("checkbox1").value;
-  const checkbox2 = document.getElementById("checkbox2").value;
-}*/
-
-document.forms["reserve"].addEventListener("submit", function (e) {
-
-  let erreur;
-
-  let inputs = this;
-
-  for (let i = 0; i < inputs.length; i++) {
-    if (!inputs[i].value) {
-      erreur = ("Veillez renseigner tous les champs du formulaire");
+/*let inputs = [eventErrorFirst, eventErrorLast, eventErrorEmail, eventErrorBirthdate, eventErrorQuantity, eventErrorLocation, eventErrorCheckbox1]
+function validate() {
+  for (let i in inputs) {
+    if (!inputs[i].every()) {
+      alert("Votre inscription est bien pris en compte"); // ajouter du texte avec javascript, lui dire qu'il est un enfant de ... , et penser a la lui mettre une class que j'aurais definie en CSS
+    }
+    return false;
+    /*if (inputs[i].some()) {
+      alert("FAUX !");
     }
   }
+}*/
 
-  if (erreur) {
-    e.preventDefault();
-    alert("Veillez renseigner tous les champs du formulaire");
-    return false;
+function validate() {
+
+  if (!eventErrorFirst) {
+    btnSubmit.addEventListener("click", launchModal); {
+      while (myForm.firstChild) {
+        myForm.removeChild(myForm.firstChild);
+      }
+      const p = document.createElement("p");
+      myForm.appendChild(p);
+      p.innerHTML = "Merci pour votre Inscription";
+      p.classList.add("formEnd");
+
+      const button = document.createElement("button");
+      myForm.appendChild(button);
+      button.innerHTML = "Fermer";
+      button.classList.add("btn-close");
+      button.classList.add("modal-btn2");
+      modalBtn2.addEventListener("click", closeModal);
+      function closeModal() {
+        modalbg.style.display = "none";
+      }
+    }
   }
+  //alert("Votre inscription est bien pris en compte"); // ajouter du texte avec javascript, lui dire qu'il est un enfant de ... , et penser a la lui mettre une class que j'aurais definie en CSS
+
   else {
-    alert("Votre inscription est bien pris en compte");
+    alert("FAUX !");
   }
-})
+  return false;
+}
