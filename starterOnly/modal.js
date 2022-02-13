b5ecf023910c23c4e5438d924277149e43fb1627
nasrlassaar <89594360+nasrlassaar@users.noreply.github.com>
2022-02-13 12:44:58 +0100

    faire des conditions pour fermer le modal et de passer a l'etape suivante

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,5 +26,70 @@ function launchModal() {
 
 // Close modal form
 function closeModal() {
-  modalbg.style.display = "";
+  modalbg.style.display = "none";
+}
+// From https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+function validateEmail(email) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
+
+function validate(event) {
+  // Prevent refresh
+  event.preventDefault();
+
+  // First name
+  const firstNameInput = document.querySelector("#first");
+  const firstNameValue = firstNameInput.value;
+  if (firstNameValue.length < 2) {
+    return;
+  }
+
+  // Last name
+  const lastNameInput = document.querySelector("#last");
+  const lastNameValue = lastNameInput.value;
+  if (lastNameValue.length < 2) {
+    return;
+  }
+
+  // Email
+  const emailInput = document.querySelector("#email");
+  const emailValue = emailInput.value;
+  const isEmailValid = validateEmail(emailValue);
+  if (!isEmailValid) {
+    return;
+  }
+
+  // Quantity
+  const quantityInput = document.querySelector("#quantity");
+  const quantityValue = quantityInput.value;
+  if (quantityValue.length === 0) {
+    return;
+  }
+
+  // Location
+  const locationInputs = document.getElementsByName("location");
+  let hasLocationChecked = false;
+
+  for (const locInput of locationInputs) {
+    if (locInput.checked) {
+      hasLocationChecked = true;
+      break;
+    }
+  }
+
+  if (!hasLocationChecked) {
+    return;
+  }
+
+  // Checkbox1
+  const checkbox1Input = document.querySelector("#checkbox1");
+  if (!checkbox1Input.checked) {
+    return;
+  }
+
+  closeModal();
 }
