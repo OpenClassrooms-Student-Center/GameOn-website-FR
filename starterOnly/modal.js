84dbb3c677987d7be67636a21302196e09d413c4
Anthony COLAS <anthonycolas.france@gmail.com>
2022-02-24 16:11:17 +0100

    fix: modal closing ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,24 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Listener for closing the modal
+const closeBtn = document.querySelector(".close");
+const bgModal = document.querySelector(".bground");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  bgModal.style.display = "none";
 }
 
 
