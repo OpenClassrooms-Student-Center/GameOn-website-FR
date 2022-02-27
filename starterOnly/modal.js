8ccb25e687c77124925918dd031244510f2207ee
Anthony COLAS <anthonycolas.france@gmail.com>
2022-02-27 18:05:53 +0100

    fix Issue #5 OK - Fix html and responsive

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,7 +1,7 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += "responsive";
+    x.className += " responsive";
   } else {
     x.className = "topnav";
   }
