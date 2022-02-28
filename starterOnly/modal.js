4b9a15780f4a134946ca945a5d61fe39710789b2
Hentiofyle <92610008+Hentiofyle@users.noreply.github.com>
2022-02-28 09:35:20 +0100

    Update modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll("#close-btn")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//close modal event 
+
+closeModalBtn.forEach(btn => btn.addEventListener("click", closeModal))
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal form 
 
+function closeModal(){
+  modalbg.style.display="none";
+}
