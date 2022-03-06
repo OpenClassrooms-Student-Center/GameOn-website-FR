b6b9ee06e8368603a9fc5b27b71963980e973a02
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-03-06 15:15:31 +0100

    code nettoyé

--- js/modal.js
+++ js/modal.js
@@ -1,27 +1,37 @@
-function editNav() {
-	var x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
-}
-
 // DOM Elements
+
+const topNav = document.getElementById("myTopnav");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeIcon = document.querySelector(".close");
+const closeModalIcon = document.querySelector(".close");
+const navIcon = document.querySelector(".nav-icon");
+const modalForm = document.getElementById("modal-form");
+
+// Menu burger
+navIcon.addEventListener("click", editNav);
+function editNav() {
+	if (topNav.className === "topnav") {
+		topNav.className += " responsive";
+	} else {
+		topNav.className = "topnav";
+	}
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+	modalbg.style.display = "block";
 }
 
-// close modal event 
-closeIcon.addEventListener('click', function() {
-  modalbg.style.display = "none";
-})
+// close modal event
+closeModalIcon.addEventListener("click", function () {
+	modalbg.style.display = "none";
+});
+
+// Form on submit event
+modalForm.addEventListener("submit", function () {
+	return validate();
+});
