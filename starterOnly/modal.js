3ee9993d4c0f7e5fff07e629f600113bf59d93db
Andreas <andreas95560@gmail.com>
2022-03-16 17:58:37 +0100

    revoir ouverture / fermeture modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,23 +7,42 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalCloseBtn = document.querySelectorAll(".close");
-const modalValidationBtn = document.querySelectorAll(".btn-submit")
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-// Close modal event
-modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
-
-//close modal form
-function closeModal() {
-  modalbg.style.display = "none";
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const modalbg = document.querySelector(".bground");
+  const modalBtn = document.querySelectorAll(".modal-btn");
+  const modalCloseBtn = document.querySelectorAll(".close");
+  const keyCodes = { escape: 27 }
+  
+  const openModal = function () {
+    modalbg.classList.add('openModal');
+    modalbg.classList.replace('closeModal', 'openModal' );
+  };
+  const closeModal = function () {
+    modalbg.classList.add('closeModal');
+    modalbg.classList.replace('openModal', 'closeModal');
+  };
+  // ouverture de la modal au click du boutton
+  modalBtn.forEach((btn) =>
+    btn.addEventListener("click", () => {
+      openModal();
+    })
+  );
+  // Fermeture de la modal avec la croix
+  modalCloseBtn.forEach((btn) =>
+    btn.addEventListener("click", () => {
+      closeModal();
+    })
+  );
+  // Fermeture de la modal quand on click a l'exterieur de celle ci
+  window.addEventListener("click", (e) => {
+    if (e.target === modalbg) {
+      closeModal();
+    }
+  });
+  // fermeture de la modal avec la touche echap
+  document.addEventListener("keydown", (e) => {
+    if (e.which === keyCodes.escape) {
+      closeModal();
+    }
+  });
+});
