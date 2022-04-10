14980a2bef4576110b8bbaef14d6cd5821a4667a
Benjamin Siret <benjaminsiret@outlook.fr>
2022-04-10 15:47:03 +0200

    add close-modal function on X

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event
+closeModalBtn.addEventListener("click", closeModal);
 
+// close modal function
+function closeModal() {
+  modalbg.style.display = "none";
+}
