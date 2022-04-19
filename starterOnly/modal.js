cf81babfe427ec96d7ae793576f987f3007069e4
Erik <93394265+Erik-TH@users.noreply.github.com>
2022-04-19 15:48:05 +0200

    modal.js : close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,25 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
+const btnCloseModal = document.getElementById("btn-close-modal");
 
-// launch modal event
+// launch modal event - open
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// launch modal form - open
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close Modal form
+const closeModal = () => {
+  modalbg.style.display = "none";
+};
 
+// open modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal)); // changer nom
+
+// close modal event
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+btnCloseModal.addEventListener("click", closeModal);
