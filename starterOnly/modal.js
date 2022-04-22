c17d07e57e2b18b5badc46c5133982309b9621a3
nathalie loussouarn <natel@hotmail.com>
2022-04-22 11:46:37 +0200

    function close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,11 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
+modalBtn.forEach((btn) => btn.addEventListener("click", (event) => {
+  // launch modal form
   modalbg.style.display = "block";
-}
+}));
+
+// close modal event
+closeBtn.addEventListener("click", (event) => {
+  // delay before close modal
+  window.setTimeout(function() {
+      // close modal form
+      modalbg.style.display = "none";
+  }, 1000)
+});
+
