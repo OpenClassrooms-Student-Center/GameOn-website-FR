7d2b30e941eeff034bae6c9f9001ac5b7a462d4b
LoicFmi <fayemi.loic@gmail.com>
2022-04-26 17:20:46 +0200

    Start mail validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,10 +22,25 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
 
 // close modal event
 modalCross.addEventListener("click", closeModal);
+
+// validate email form
+function validate(input) {
+  var mailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if(input.value.match(mailRegex)) {
+  alert("Valid email address!");
+  document.reserve.email.focus();
+  return true;
+  } else {
+  alert("You have entered an invalid email address!");
+  document.reserve.email.focus();
+  return false;
+  }
+}
 
