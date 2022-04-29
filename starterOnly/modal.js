0c95f3801a0d24569b60d2e748e19942e9984bb5
bbeatrice <techerbaretbeatrice@gmail.com>
2022-04-29 13:31:58 +0200

    fix display

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -19,5 +20,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
 
 
