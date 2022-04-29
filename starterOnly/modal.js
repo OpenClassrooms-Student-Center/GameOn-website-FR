fc28f0607c6a77a0291053f1b9a8ef035f4f4ae3
bbeatrice <techerbaretbeatrice@gmail.com>
2022-04-29 15:20:41 +0200

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,14 +11,18 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.forEach((btn) => btn.addEventListener ("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
