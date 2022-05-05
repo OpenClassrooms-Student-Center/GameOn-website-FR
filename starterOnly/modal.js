0deebfa3234295ed28fea198fed78edaf6bee128
shiftytab <rodrigoestevesramos@gmail.com>
2022-05-05 12:17:13 +0200

    Fermeture du modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.getElementById('close-modal');
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +22,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Fermeture du modal
+closeModal.onclick = () => {
+  modalbg.style.display = "none";
+}
