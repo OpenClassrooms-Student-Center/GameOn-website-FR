2f80122f812d6d1301a88003563f6110995f6034
laminemessaci <lamine.messaci18@gmail.com>
2022-05-19 08:49:56 +0200

    delete img backgroud

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,17 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelector('.close');
+const submitBtn = document.querySelector(".btn-submit");
+const modalThanks = document.querySelector(".merci")
+
+  closeModal.addEventListener("click", ()=> modalbg.style.display = "none")
+
+ submitBtn.addEventListener("click", ()=>{
+    console.log('submit-btn clicked ')
+
+  })
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +29,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+
+// launch modal form
+function launchModalThanks() {
+  modalThanks.style.display = "block";
 }
 
 
