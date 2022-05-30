3c8d0791d9c79cbec5ad20bbcea3f1671fe814c3
unknown <kayv@hotmail.fr>
2022-05-30 14:36:12 +0200

    Email + date

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -37,6 +37,8 @@ function closeModal(){ // fonction qui arrete l'affichage du modal
 let myFirstname = document.getElementById('first');
 let myLastname = document.getElementById('last');
 let myEmail = document.getElementById('email');
+let myBirthday = document.getElementById('birthdate');
+let myQuantity = document.getElementById('quantity');
 
 // Validation
 
@@ -45,30 +47,72 @@ let btnSubmit = document.querySelector('.btn-submit');
 btnSubmit.addEventListener("click", function(event){
   event.preventDefault();
   checkName();
-
+  checkLastName();
+  checkEmail(myEmail);
+  checkBirthdate();
+  checkQuantity();
 });
+
+// Check Name
 function checkName(){
   if (myFirstname.value == "" || myFirstname.value.length < 2 ){
     myFirstname.style.border = "2px solid red";
     return false;
   }
+  else{
+     myFirstname.style.border = "none";
+     return true;
+  }
+}
+// Check Last Name
+function checkLastName(){
   if (myLastname.value == "" || myLastname.value.length < 2 ){
     myLastname.style.border = "2px solid red";
     return false;
   }
-}
-/*
-function validate(){
-  if(myFirstname==""){
-    alert("Entrez un prenom");
-    return false;
-  }
   else{
+    myLastname.style.border = "none";
     return true;
   }
 }
-/*
-function not_validate(){
 
-}
-*/
+/* Email */
+function checkEmail(myEmail) {
+let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+  if(emailRegExp.test(myEmail.value)){
+    myEmail.style.border = "none";
+  }
+  else{
+    myEmail.style.border = "2px solid red";
+  }
+}  // date 
+
+function checkBirthdate(){
+  // take current date and user birthday
+  const birthdate = new Date(myBirthday.value);
+  const currentDay = new Date();
+  
+  // calculs
+  const ageYear = currentDay.getFullYear() - birthdate.getFullYear(); // year
+  let ageMonth = currentDay.getMonth() - birthdate.getMonth(); // month
+
+
+  if(ageYear <= 17)
+  {
+    myBirthday.style.border = "2px solid red";
+    return false;
+  }
+    else if(ageYear = 17 && ageMonth < 0){
+      myBirthday.style.border = "2px solid red";
+      return false;
+    }
+    else{
+      myBirthday.style.border = "none";
+    } 
+  }
+
+  // quantity
+
+  function checkQuantity(){
+    
+  }
