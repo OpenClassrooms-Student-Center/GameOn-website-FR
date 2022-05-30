e878d21526f73e880faa8aacbdab1c08dfabe2db
Fablaney <fabrmoto@gmail.com>
2022-05-30 10:56:07 +0200

    menu pac/mobile OK

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,38 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+// Bouton burger du menu
+function navMenu()
+{
+    var x = document.getElementById("myTopnav");
+
+    if(x.className === "topnav")
+    {
+        x.className += " responsive";
+    }
+    else
+    {
+        x.className = "topnav";
+    }
 }
+
+
+
+
+
+
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+
+const modalBtn = document.querySelectorAll(".btn-signup");
+
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+function launchModal()
+{
+    modalbg.style.display = "block";
 }
 
 
