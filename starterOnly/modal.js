25005a512789e95ce6a7281207ea85a835b952be
Fablaney <fabrmoto@gmail.com>
2022-05-30 18:08:39 +0200

    vérification form JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,17 +16,12 @@ function navMenu()
 
 
 
-
-
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 
 const modalBtn = document.querySelectorAll(".btn-signup");
 
 const formData = document.querySelectorAll(".formData");
-
-
 
 
 
@@ -39,11 +34,61 @@ function launchModal()
     modalbg.style.display = "block";
 }
 
-
-
-
 // close le modal avec X
 function closeModal()
 {
     modalbg.style.display = "none";
 }
+
+
+
+
+
+
+
+
+// écouter le formulaire et validation des champs
+// function validateForm()
+// {
+
+//     alert('test');
+//     let errNom = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+//     let errDateNaiss = "Vous devez entrer votre date de naissance.";
+//     let errOption = "Vous devez choisir une option.";
+//     let errConditions = "Vous devez vérifier que vous acceptez les termes et conditions.";
+
+//     var first = document.getElementById("first").value;
+//     var p = document.getElementById("password").value;
+
+//     if(first== "")
+//     {
+//         alert(errNom);
+//         return false;
+//     }
+//     if(p == "")
+//     {
+//         alert("Please enter you Password");
+//         return false;
+//     }
+
+//     alert("All datas are valid!, send it to the server!")
+
+//     return true;
+// }
+
+function validateForm()
+{
+    // alert('test');
+    let errNom = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    let errDateNaiss = "Vous devez entrer votre date de naissance.";
+    let errOption = "Vous devez choisir une option.";
+    let errConditions = "Vous devez vérifier que vous acceptez les termes et conditions.";
+
+    let first = document.forms["myForm"]["first"].value;
+
+    alert(first);
+    // if (x == "") {
+    //   alert("Name must be filled out");
+    //   return false;
+    // }
+  }
