7f57fcfb333d0e50e8676d4881cbfa1cf9c6abd7
julien Levieux <julien.levieux@drotek.com>
2022-06-01 11:56:56 +0200

    feature : close modal on click(x)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeButton = document.querySelector('.close')
+
+
+// close modal form (mouve out the modal from layout using display: none)
+const closeModal = () => modalbg.style.display = "none"
+// close modal event (adding event listeners to the selected element for close modal event)
+closeButton.addEventListener("click", closeModal)
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
 
 
