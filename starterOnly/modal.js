feec2db728bfe26a019ebcc3608316c8c3cacb3d
Pascal Allau <pascal.allau@etu.toulouse-inp.fr>
2022-06-05 22:45:43 +0200

    fix #4; add confirmation message when send successfull

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,14 +6,12 @@ import { valid, validate } from './validation.js';
  */
 const reserve = document.querySelector('#reserve');
 /**
- * @const {HTMLElement}  .bground  modal background
- */
-const modalbg = document.querySelector('.bground');
-/**
  * @const {HTMLButtonElement}  .modal-btn  modal buttons
  */
 const modalBtn = document.querySelectorAll('.modal-btn');
 
+let modal = null;
+let previousActiveElement = null;
 /**
  * If the class name of the element with the id of 'myTopnav' is equal to 'topnav', then add
  * 'responsive' to the class name. Otherwise, set the class name to 'topnav'
@@ -27,23 +25,47 @@ function editNav() {
     x.className = 'topnav';
   }
 }
-
+function closeModal() {
+  modal.removeAttribute('style');
+  modal.setAttribute('aria-hidden', 'true');
+  modal.removeAttribute('aria-modal');
+  modal.removeEventListener('click', closeModal);
+  modal
+    .querySelector('span[data-dismiss="dialog"]')
+    .removeEventListener('click', closeModal);
+  modal.lastElementChild.removeEventListener('click', closeModal);
+  // restoring focus
+  previousActiveElement.focus();
+  modal = null;
+}
+function setToOpenModal(el) {
+  el.setAttribute('style', 'display: block');
+  el.removeAttribute('aria-hidden');
+  el.setAttribute('aria-modal', 'true');
+  el.querySelectorAll('[data-dismiss="dialog"]').forEach((elt) =>
+    elt.addEventListener('click', closeModal)
+  );
+  modal = el;
+}
 /**
  * When the user clicks the button, the modal background is displayed
  */
-function launchModal() {
-  modalbg.setAttribute('style', 'display: block');
-}
-
-function closeModal() {
-  modalbg.setAttribute('style', 'display: none');
+function launchModal(ev) {
+  /**
+   * @const btn
+   * @type {EventTarget}
+   */
+  const btn = ev.target;
+  // @ts-ignore
+  const el = document.getElementById(btn.getAttribute('aria-haspopup'));
+  if (!modal) {
+    previousActiveElement = document.activeElement;
+    setToOpenModal(el);
+  }
 }
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
-document
-  .querySelectorAll('[data-dismiss="dialog"]')
-  .forEach((btn) => btn.addEventListener('click', closeModal));
 
 /* Adding an event listener to each input element with the form attribute of "reserve" and calling the
 valid function on each change. */
@@ -103,6 +125,45 @@ Array.from(document.querySelectorAll('span[role="checkbox"]')).forEach(
   }
 );
 /**
+ * It creates a modal window with a success message and displays it
+ * @param   {String} firstname - the first name of the user
+ * @param   {String} lastname - the last name of the user
+ */
+function displaySuccessModal(firstname, lastname) {
+  /**
+   * @const
+   * @type {HTMLElement}
+   */
+  const div = document.createElement('div');
+  div.setAttribute('id', 'successModal');
+  div.setAttribute('class', 'modal successModal');
+  div.setAttribute('role', 'dialog');
+  div.innerHTML = `
+    <div class="modal-content" role="document">
+      <span
+        tabindex="0"
+        role="button"
+        id="dismissSuccessModal"
+        class="modal-close"
+        aria-label="Fermer la fenêtre modale"
+        title="Fermer la fenêtre modale"
+        data-dismiss="dialog"
+      ></span>
+      <div class="modal-body">
+        <h3><span>${firstname} ${lastname}</span> Merci pour votre inscription ! </h3>
+        <button class="button btn-submit" data-dismiss="dialog">Fermer</button>
+      </div>
+    </div>
+  `;
+  document.querySelector('main').appendChild(div);
+  /**
+   * @const
+   * @type  {HTMLElement}
+   */
+  const successModal = document.querySelector('#successModal');
+  setToOpenModal(successModal);
+}
+/**
  * The above code is listening for a submit event on the form. If the form is valid, it will send the
 form data to the console. If the form is not valid, it will prevent the form from submitting.
  *
@@ -122,6 +183,8 @@ reserve.addEventListener('submit', (ev) => {
   // new Response(new FormData(reserve)).text().then(console.log);
   // send form by Ajax
   closeModal();
+  // @ts-ignore
+  displaySuccessModal(data.first, data.last);
   // @ts-ignore
   reserve.reset();
   document.querySelectorAll('input[form="reserve"]').forEach((el) => {
