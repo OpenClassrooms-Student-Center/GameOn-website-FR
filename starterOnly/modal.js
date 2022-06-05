057bde50dd8ec3c62057c42b08fc7b19a192bbfe
Pascal Allau <pascal.allau@etu.toulouse-inp.fr>
2022-06-05 11:21:12 +0200

    fix #1; close modal with span X

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,46 @@
+// DOM Elements
+/**
+ *
+ *
+ * @const {HTMLElement}  .bground  [.bground description]
+ *
+ */
+const modalbg = document.querySelector('.bground');
+/**
+ *
+ * @const {HTMLButtonElement}  .modal-btn  [.modal-btn description]
+ *
+ */
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
+
+/**
+ * If the class name of the element with the id of 'myTopnav' is equal to 'topnav', then add
+ * 'responsive' to the class name. Otherwise, set the class name to 'topnav'
+ */
+
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  const x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
+/**
+ * When the user clicks the button, the modal background is displayed
+ */
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.setAttribute('style', 'display: block');
 }
 
+function closeModal() {
+  modalbg.setAttribute('style', 'display: none');
+}
 
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
+document
+  .querySelectorAll('[data-dismiss="dialog"]')
+  .forEach((btn) => btn.addEventListener('click', closeModal));
