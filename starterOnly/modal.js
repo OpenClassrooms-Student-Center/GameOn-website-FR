0b048ff820be9898a4ddc83bc7feed6d5ef5afa4
SuperSandrine <sandrine.mestas@gmail.com>
2022-06-13 10:27:17 +0200

    activation de la fermeture de la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,28 @@
+// DOM Elements
+//let modal = null;
+
+const modalBg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalClosure = document.querySelector(".closeModal");
+const modalContent = document.querySelector(".content");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalBg.style.display = "block";
+}
+
+//Close modal with X button
+// >function:
+function closeModal() {
+  modalBg.style.display = "none";
+}
+// >call of function:
+modalClosure.addEventListener("click", closeModal);
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,18 +31,3 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-
