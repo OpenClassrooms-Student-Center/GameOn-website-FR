f303925a05f9ee2329611416234382a58f48fc3d
unknown <kayv@hotmail.fr>
2022-06-19 01:13:56 +0200

    correction js

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -12,7 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBtnClose = document.querySelectorAll(".close"); // creation d'un dom element sur l'element tout les element de la classe "close"
 const formData = document.querySelectorAll(".formData");
-const modalbgEnd = document.querySelector(".bgroundClose");
+const modalbgEnd = document.querySelector('#close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -40,7 +40,7 @@ let myLastname = document.getElementById('last');
 let myEmail = document.getElementById('email');
 let myBirthday = document.getElementById('birthdate');
 let myQuantity = document.getElementById('quantity');
-let myLocalisation = document.getElementsByName("location");
+let myLocalisation = document.querySelectorAll(".checkbox-input");
 let myCheckbox = document.getElementById('checkbox1');
 
 //Element Error message
@@ -53,30 +53,33 @@ let myLocalisationError = document.getElementById('error__message__localisation'
 let myCheckboxError = document.getElementById('error__message__checkbox');
 
 
-// Error
-let errors= 0;
 
 // Validation
 let btnSubmit = document.querySelector('.btn-submit');
+
+
+ 
 btnSubmit.addEventListener("click", function(event){
   event.preventDefault();
-  checkName();
-  checkLastName();
-  checkEmail(myEmail);
-  checkBirthdate();
-  checkQuantity();
-  checkLocation();
-  checkCheckBox();
+  let name =  checkName();
+  let lastname = checkLastName();
+  let email = checkEmail(myEmail);
+  let birthday  = checkBirthdate();
+  let quantity = checkQuantity();
+  let localisation = checkQuantity();
+  let checkbox = checkCheckBox();
   if(
-    checkName() == true && 
-    checkLastName() == true && 
-    checkEmail(myEmail)== true && 
-    checkBirthdate() == true && 
-    checkQuantity() == true &&
-    checkLocation() == true &&
-    checkCheckBox() == true
+    name == false || 
+    lastname == false ||
+    email == false ||
+    birthday == false ||
+    quantity == false ||
+    localisation == false ||
+    checkbox == false 
   ){
-    alert("oui");
+    
+  }
+  else{
     modalbg.style.display = "none";
     modalbgEnd.style.display = "block";
   }
@@ -89,12 +92,12 @@ btnSubmit.addEventListener("click", function(event){
 function checkName(){
    if (myFirstname.value == "" || myFirstname.value.length < 2 ){
     myFirstname.style.border = "2px solid red";
-    myFirstnameError.style.visibility = "visible";
+    myFirstnameError.classList.remove("oui");
     return false;
   }
   else{
      myFirstname.style.border = "none";
-     myFirstnameError.style.visibility = "hidden";
+      myFirstnameError.classList.add("oui");
      return true;
   }
   console.log(errors);
@@ -170,19 +173,24 @@ function checkBirthdate(){
 
   //Localisation
   function checkLocation(){
+    console.log("sdfsd");
     let valid = false;
     for(let i=0; i<myLocalisation.length;i++){
       if(myLocalisation[i].checked)
       {
         valid = true;
-        myLocalisationError.style.visibility = "none";
+        myLocalisationError.classList.add("oui");
+        console.log("salut");
         return true;
       }
     }
     if(valid == false){
-      myLocalisationError.style.visibility = "visible";
+      myLocalisationError.classList.remove("oui");
+      console.log("s");
       return false;
     }
+          console.log("sdfsd");
+
 }
 
 //Checkbox
