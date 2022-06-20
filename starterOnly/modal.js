90f37e8f015db3ea624fce7df6e30573be751f5f
SuperSandrine <sandrine.mestas@gmail.com>
2022-06-20 14:21:07 +0200

    créations de variables afin de remplir les conditions de validations requises par le clientet compléter la fonction validate()

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,39 +20,12 @@ function closeModal() {
 // >play of function:
 modalClosure.addEventListener("click", closeModal);
 
-//the form should be valid when submit:
-//THOMAS: La soumission est faite directement dans le html (si j'ai bien compris)?
-//Du coup je n'ai pas à jouer la fonction
-//THOMAS: Est ce que je dois rajouter un contrôle au moment de
-// la validation du formulaire? ou est-ce que mes Checker suffise au contrôle?
-function validate() {
-  if (checkboxCGU.checked) {
-    // note pour moi: pour l'instant: si la checkboxCGU est validée
-    // un message s'affiche en alert et
-    // alors on peut envoyer le formulaire : return true
-    // TODO:  faire le contrôle des données, si les données sont ok, formulaire valide.
-    // et dire ce qu'il faut envoyé et où
-    alert("ça marche");
-    return true;
-  } else {
-    alert("veuillez cocher les CGU");
-    // sinon on indique de ne pas envoyer le formulaire
-    return false;
-  }
-}
-
-//prevent de Default behaviour on validation while coding:
-//empêche la fermeture de la modale au clic 'je m'inscris':
-// TODO: to remove when code will be ready.
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-
 //------------------------Datas controls-------------------
-// Control of datas class text-control:
+// Control of datas class text-control (text and number in input):
 const inputsText = document.querySelectorAll(".text-control");
 // une liste de 5 eléments
 let firstName, lastName, email, birthdate, quantity;
+let A, B, C, D;
 // des variables vides
 
 const firstNameChecker = (value) => {
@@ -64,12 +37,15 @@ const firstNameChecker = (value) => {
     containerFN.setAttribute("data-error-visible", true);
     // containerFN.classList.add("error");
     firstName = null;
+    A = 0;
   } else if (value == null || value == "") {
-    containerFN.classList.add("error");
+    containerFN.classList.add("erreur");
     firstName = null;
+    A = 0;
   } else {
-    containerFN.classList.remove("error");
+    containerFN.classList.remove("erreur");
     firstName = value;
+    A = 1;
   }
 };
 
@@ -78,29 +54,34 @@ const lastNameChecker = (value) => {
   //est-ce que ça me sélectionne l'input ou toute la div? = l'input
   //THOMAS: est-ce génant pour l'application des classes css error?
   if (value.length > 0 && value.length < 2) {
-    containerLN.classList.add("error");
+    containerLN.classList.add("erreur");
     // pour appliquer set Attribute, il faut appeler le selectionner le .formdata
     // document
     //   .querySelector(".formData #lastName")
     //   .setAttribute("formData[data-error-visible]", true);
     lastName = null;
+    B = 0;
   } else if (value == null || value == "") {
-    containerLN.classList.add("error");
+    containerLN.classList.add("erreur");
     lastName = null;
+    B = 0;
   } else {
-    containerLN.classList.remove("error");
+    containerLN.classList.remove("erreur");
     lastName = value;
+    B = 1;
   }
 };
 
 const emailChecker = (value) => {
   const containerE = document.querySelector("#email");
   if (!value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i)) {
-    containerE.classList.add("error");
+    containerE.classList.add("erreur");
     email = null;
+    C = 0;
   } else {
-    containerE.classList.remove("error");
+    containerE.classList.remove("erreur");
     email = value;
+    C = 1;
   }
 };
 // email regex from : https://regexr.com/3e48o
@@ -112,11 +93,13 @@ const birthdateChecker = (value) => {
 const quantityChecker = (value) => {
   const containerQ = document.querySelector("#quantity");
   if (!value.match(/^[0-9]{0,1}[0-9]$/i)) {
-    containerQ.classList.add("error");
+    containerQ.classList.add("erreur");
     quantity = null;
+    D = 0;
   } else {
-    containerQ.classList.remove("error");
+    containerQ.classList.remove("erreur");
     quantity = value;
+    D = 1;
   }
 };
 
@@ -151,9 +134,9 @@ inputsText.forEach((input) => {
 //input, (fonction blablaChecker écrite juste avant et appeler pour chaque cas)
 //
 
-// Control of datas class checkbox-label:
+// Control of datas class checkbox-label (radio and checkbox):
 const inputsCheckbox = document.querySelectorAll(".checkbox-input");
-let place, checkboxNewsVar;
+let place, checkboxNewsVar, E;
 
 // checké une location,
 // si la checkbox est validé, rajouté la donnée dans le tableau
@@ -163,37 +146,41 @@ inputsCheckbox.forEach((input) => {
       case "location1":
         if (location1.checked) {
           place = e.target.value;
+          E = 1;
         }
         break;
       case "location2":
         if (location2.checked) {
           place = e.target.value;
+          E = 1;
         }
         break;
       case "location3":
         if (location3.checked) {
           place = e.target.value;
+          E = 1;
         }
         break;
       case "location4":
         if (location4.checked) {
           place = e.target.value;
+          E = 1;
         }
         break;
       case "location5":
         if (location5.checked) {
           place = e.target.value;
+          E = 1;
         }
         break;
       case "location6":
         if (location6.checked) {
           place = e.target.value;
+          E = 1;
         }
         break;
       case "checkboxCGU":
         if (checkboxCGU.checked) {
-          // rien ne se passe, THOMAS: à quoi sert le required du html si je met
-          //tout ce code?
         } else {
           alert("cochezzzzzz les CGU !!!!!!!");
         }
@@ -201,6 +188,9 @@ inputsCheckbox.forEach((input) => {
       case "checkboxNews":
         if (checkboxNews.checked) {
           checkboxNewsVar = true;
+        }
+        if (!checkboxNews.checked || checkboxNewsVar == undefined) {
+          checkboxNewsVar = false;
         } else {
           checkboxNewsVar = false;
         } // notons que si l'utilisateurs ne coche et décoche pas la newsletter,
@@ -213,40 +203,35 @@ inputsCheckbox.forEach((input) => {
   });
 });
 
-// function locationChoosen() {
-//   // ou
-//   // const locationChoosen= ()=>{}
-//   if (location1.checked) {
-//     alert("c'est coché");
-//   } else {
-//     alert("c'est pas coché");
-//   }
-// }
+//the form should be valid when submit:
+//THOMAS: La soumission est faite directement dans le html (si j'ai bien compris)?
+//Du coup je n'ai pas à jouer la fonction
+//THOMAS: Est ce que je dois rajouter un contrôle au moment de
+// la validation du formulaire? ou est-ce que mes Checker suffise au contrôle?
+// THOMAS: à quoi sert le required du html si je met
+//tout ce code?
+function validate() {
+  if (A + B + C + D + E == 5 && checkboxCGU.checked) {
+    // note pour moi: pour l'instant: si la checkboxCGU est validée
+    // un message s'affiche en alert et
+    // alors on peut envoyer le formulaire : return true
+    // TODO:  faire le contrôle des données, si les données sont ok, formulaire valide.
+    // et dire ce qu'il faut envoyé et où
+    alert("ça marche");
+    return true;
+  } else {
+    alert("y a un problème");
+    // sinon on indique de ne pas envoyer le formulaire
+    return false;
+  }
+}
 
-// inputsCheckbox.forEach((input) => {});
-// // }= (e) => {
-// //   if (location1.checked) {
-// //     location = e.value;
-// //   }
-// //   console.log(location);
-// // };
-// // // inputsCheckbox.forEach((elementIC) => {
-// // //   elementIC.addEventListener("i", (e) => {
-// // //   }
-// // // });
-// // function validate() {
-// //   if (checkboxCGU.checked) {
-// //     // note pour moi: pour l'instant: si la box checkbox est validée
-// //     // un message s'affiche en alert et
-// //     // alors on peut envoyer le formulaire : return true
-// //     // TODO:  faire le contrôle des données sont ok
-// //     // et dire ce qu'il faut envoyé et où
-// //     alert("ça marche");
-// //     return true;
-// //   } else {
-// //     alert("veuillez cocher les CGU");
-// //     // sinon on indique de ne pas envoyer le formulaire
-// //     return false;
+//prevent de Default behaviour on validation while coding:
+//empêche la fermeture de la modale au clic 'je m'inscris':
+// TODO: to remove when code will be ready.
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
 function editNav() {
   var x = document.getElementById("myTopnav");
