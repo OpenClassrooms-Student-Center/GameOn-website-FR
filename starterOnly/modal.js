12af81da93758cb07d0579f55991c3eda4bd271a
Mwardentoumz <thomas.thivolet@gmail.com>
2022-06-21 15:32:37 +0200

    Update modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClose = document.querySelectorAll(".close"); // Ajout d'un sélecteur sur tous les élements de la classe "close"
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,11 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//ajout évenement sur la classe close
+modalBtnClose.forEach((btnClose) => btnClose.addEventListener("click", closeModal))
+
+//close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
