b3613c66183edb356ac940eb0183ee25f89446e6
Mwardentoumz <thomas.thivolet@gmail.com>
2022-07-10 19:11:16 +0200

    All is valid noww
    corrected all issues

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -34,7 +34,7 @@ const location5 = document.getElementById("location5")
 const location6 = document.getElementById("location6")
 const radios = document.getElementsByName("location")
 const errLoc = document.getElementById("errorLocation")
-const condition1 = document.getElementById("checkbox2")
+const condition1 = document.getElementById("checkbox1")
 const errconditions = document.getElementById("errorAcceptedPolicy")
 const birthDate = document.getElementById("birthdate")
 const errBirth = document.getElementById("errorBirthdate")
@@ -45,6 +45,13 @@ let checkedMail = false
 let checkedBirth = false
 let checkedQ = false
 let checkedCond = false
+let fieldname = ""
+let fieldfamily = ""
+let fieldmail = ""
+let fieldbirth = ""
+let fieldQ = ""
+let fieldCond = ""
+let fieldLoc = ""
 
 //TODO 1 !!!
 // launch modal event
@@ -71,17 +78,20 @@ function closeModal() {
 
 // create  functions to validate the first name
 function validateName(firstName){
-  if (firstName === "" || firstName.length < 2 || !(/^[A-Z]{2,25}/gi.test(firstName))){
-    errorFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ Prénom";
-    errorFirst.style.display = "block";
-    errorFirst.style.color = "red";
-    errorFirst.style.fontSize = "12px"
-    first.style.borderColor = "red"
-    checkedName = false;
-  }else{
-    checkedName = true
-    errorFirst.style.display = "none";
-  }
+ 
+    if (firstName === null || firstName.length < 2 || !(/^[A-Z]{2,25}/gi.test(firstName))){
+      errorFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ Prénom";
+      errorFirst.style.display = "block";
+      errorFirst.style.color = "red";
+      errorFirst.style.fontSize = "12px"
+      first.style.borderColor = "red"
+      checkedName = false;
+    }else{
+      checkedName = true
+      fieldname = firstName;
+      first.style.border = "none";
+      errorFirst.style.display = "none";
+    }
 }
 first.addEventListener("input", function(e){
   validateName(e.target.value);
@@ -90,7 +100,7 @@ first.addEventListener("input", function(e){
 // create a function to validate the last name
 
 function validateLast(lastName){
-  if (lastName === "" || lastName.length < 2 || !(/^[A-Z]{2,25}/gi.test(lastName))){
+  if (lastName === null || lastName.length < 2 || !(/^[A-Z]{2,25}/gi.test(lastName))){
     errorLast.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ Nom";
     errorLast.style.display = "block";
     errorLast.style.color = "red";
@@ -99,7 +109,8 @@ function validateLast(lastName){
     checkedLast = false;
   }else{
     checkedLast = true;
-    last.style.borderColor = "none"
+    fieldfamily = lastName;
+    last.style.border = "none"
     errorLast.style.display ="none";
 
   }
@@ -111,7 +122,7 @@ last.addEventListener("input", function(e){
 // create a function to validate the email
 
 function validateEmail(valEmail){
-  if (valEmail === "" || !(/^[A-Z0-9._-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gi.test(valEmail))){
+  if (valEmail === null || !(/^[A-Z0-9._-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gi.test(valEmail))){
     errorEmail.innerHTML = "Veuillez entrer une adresse e-mail valide";
     errorEmail.style.display = "block";
     errorEmail.style.color = "red";
@@ -120,6 +131,7 @@ function validateEmail(valEmail){
     checkedMail = false;
   }else{
     checkedMail = true;
+    fieldmail = valEmail;
     errorEmail.style.display ="none";
     email.style.border = "none"
   }
@@ -130,22 +142,25 @@ email.addEventListener("input", function(e){
 
 // create a function to validation the birthdate
 
-function validateBirth(){
+function validateBirth(valBirth){
   let birth = document.getElementById("birthdate").value;
   var now = new Date();
-        var month = now.getMonth();
+        var month = now.getMonth()+1;
         var day = now.getDate();
         var year = now.getFullYear();
   var date = year+"-"+month+"-"+day;
+  
   if (birth > date){
     errBirth.innerHTML="Veuillez saisir une date de naissance valide"
     errBirth.style.display = "block"
     errBirth.style.color = "red"
     errBirth.style.fontSize = "12px"
-    birth.style.borderColor = "red"
+    birthDate.style.borderColor = "red"
     checkedBirth = false;
   }else{
-    errBirth.style.display="none";
+    fieldbirth = valBirth;
+    birthDate.style.border = "none"
+    errBirth.style.display= "none";
     checkedBirth = true
   }
 }
@@ -167,9 +182,10 @@ function validateQuantity(valQ){
     quantity.style.borderColor = "red"
     checkedQ = false;
   }else{
+    fieldQ = valQ;
     checkedQ =true;
     errorQuantity.style.display = "none";
-    quantity.style.borderColor = "none"
+    quantity.style.border = "none"
   }
 }
 quantity.addEventListener("input",function(e){
@@ -179,13 +195,19 @@ quantity.addEventListener("input",function(e){
 // create a function to validation radio buttons
 
 function validationLocation(e){
-  if(e.target.checked){
+  console.log(e)
+  if(e != ""){
     errLoc.style.display="none";
-  }else{
-    errLoc.innerHTML="Veuillez sélectionner un lieu";
+    checkedLoc = true
+    fieldLoc = e
+
+    }else{
+    checkedLoc = false
+     errLoc.innerHTML="Veuillez sélectionner un lieu";
     errLoc.style.display="block";
     errLoc.style.color="red";
     errLoc.style.fontSize = "12px"
+    
   }
 }
 
@@ -193,24 +215,24 @@ radios.forEach(radio =>{
   radio.addEventListener("input",function(e){
     console.log("test",e.target.checked)
     checkedLoc=true
-    validationLocation(e);
+    validationLocation(e.target.value);
   })
 })
 
 //creat a function to validate general condition
 
 function validateCondition(valC){
-  if(valC.target.checked){
-    errconditions.style.display="none";
-    checkedCond = false
-  }else{
+  if(condition1.checked){
     checkedCond = true
+    errconditions.style.display="none";
+  }else{
+    fieldCond = valC
+    checkedCond = false    
     errconditions.innerHTML="Veuillez accepter les conditions d'utilisation";
     errconditions.style.display="block";
     errconditions.style.color="red"
-    errconditions.style.fontSize = "12px"
-  }
-}
+    errconditions.style.fontSize = "12px"}}
+  
 
 condition1.addEventListener("input",function(e){
   validateCondition(e.target.value)
@@ -219,45 +241,29 @@ condition1.addEventListener("input",function(e){
 
 //fonctionnement du bouton d'envoi
 
-
 function validate(event) {
 	event.preventDefault();
   btnSubmit.addEventListener("click",function(e){
   // à mettre apres chaque if e.preventDefault();
-  if(!checkedName){
-    e.preventDefault();
-    validateName(e)
-  }
-  if(!checkedLast){
-    e.preventDefault();
-    validateLast(e)
-  }
-  if(!checkedMail){
-    e.preventDefault();
-    validateEmail(e)
-  }
-  if(!checkedBirth){
-    e.preventDefault();
-    validateBirth(e)
-  }
-  if(!checkedQ){
-    e.preventDefault();
-    validateQuantity(e)
-  }
-  if(!checkedLoc){
-    e.preventDefault();
-    validationLocation(e)
-  }
-  if(checkedCond){
-    e.preventDefault();
-    validateCondition(e)
-  }else{
+  if(checkedName && checkedLast && checkedMail && checkedBirth && checkedQ && checkedLoc && checkedCond){
     modalbg.style.display = "none"
     modalConfirm.style.display = "block"
+  }else{
+    validateName(fieldname);
+    validateLast(fieldfamily);
+    validateEmail(fieldmail);
+    validateBirth(fieldbirth);
+    validateQuantity(fieldQ);
+    validationLocation(fieldLoc);
+    validateCondition(fieldCond);
     
   }})}
 
-  document.getElementById("btn-submit-2").addEventListener("click", function(){
+  document.getElementById("second-btn").addEventListener("click", function(){
     modalConfirm.style.display = "none";
   })
+
+  
+  
+  
   
