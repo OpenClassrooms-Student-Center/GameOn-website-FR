e6ce5fb431d72775de84961c2a65256234457731
Jerome Marichez <admin@digitstudio.fr>
2022-07-22 12:09:50 +0200

    Update modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -33,8 +33,8 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Check if i can enable the button submit by checking if "checkbox" is checked
 function stateCheckBox() {
-  // Check if i can enable the button submit by checking if "checkbox" is checked
     if (checkBox1.checked) {
       btnSubmit.classList.remove("btn-disabled");
       return true;
@@ -47,19 +47,20 @@ function stateCheckBox() {
 // Function validate form
 function validate() {
   event.preventDefault(); // Avoid default behavior of form
+
+  // We check only State of checkbox 
   if (stateCheckBox() == true)
   {
     alert("Merci! Votre réservation a été reçue."); // Alert to show réservation is done
+    closeModal(); // Close the form
   }
   else 
   {
     alert("Vous devez accepter les conditions d'utilisation pour valider le formulaire !");
   }
-  //
-  // closeModal(); // Close the form
 }
 
-// Lauch CheckBox Check
+// Lauch CheckBox Check each time we click on the Checkbox1 (T.O.S)
 checkBox1.addEventListener("click", stateCheckBox);
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
