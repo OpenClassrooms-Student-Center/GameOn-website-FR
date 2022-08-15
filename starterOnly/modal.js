ab0214ef60676bf657addae42c82e49d05f15898
FOXEST57 <foxest57@gmail.com>
2022-08-15 15:04:03 +0200

    sauvegarde exercice en cour

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,12 +1,12 @@
 
 
 function editNav() {
-  var x = document.querySelector("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+	var x = document.querySelector("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
 }
 
 // DOM Elements
@@ -19,7 +19,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+	modalbg.style.display = "block";
 }
 
 
@@ -30,7 +30,7 @@ const closeMod = document.querySelector(".close")
 
 //fonction fermeture du formulaire
 function closeModal(){
-  modalbg.style.display = "none";
+	modalbg.style.display = "none";
 }
 
 //evenement qui ferme le modal
@@ -45,115 +45,221 @@ let email          = document.querySelector('#email');
 let birthdate      = document.querySelector('#birthdate');
 let quantity       = document.querySelector('#quantity');
 let avertissements = document.querySelectorAll('.avertissement');
+let submitButton   = document.querySelector('.btn-submit');
+let cities         = document.querySelectorAll(`input[name="location"]`);
 
+
+
+// boucle pour faire disparaitre les messages d'avertissement
 
 avertissements.forEach(function (avertissement){ 
-  avertissement.style.display = "none";
+	avertissement.style.display = "none";
 }) 
 
-firstName.addEventListener('change', function(e) {
-  const value = e.target.value;
-  const avert  = firstName.closest('.formData').querySelector('.avertissement');
-  const avertform  = firstName.closest('.formData').querySelector('.text-control');
-  const regexFirst = /^[a-zA-Z ]{2,30}$/;
-  if (!regexFirst.test(value))
+// vérification regex
+
+function isFirstNameValid()
+	{
+		const regexFirst = /^[a-zA-Z ]{2,30}$/;
+
+		if (!regexFirst.test(firstName.value))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+function isLastNameValid()
+	{
+		const regexLast = /^[a-zA-Z ]{2,30}$/;
+
+		if (!regexLast.test(lastName.value))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+function isEmailValid()
+	{
+		const regexmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+		if (!regexmail.test(email.value))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+function isBirthdateValid()
+{
+	const regexbirthdate = /^(0?[1-9]|1[0-2])[\/](0?[1-9]|[12]\d|3[01])[\/](19|20)\d{2}$/;
+
+	if (!regexbirthdate.test(birthdate.value))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+function isQantityValid()
+{
+	const regexquantity = /^[0-99][0-9]?$/;
+	if(!regexquantity.test(quantity.value))
+	{
+		return false;
+	}
+	return true;
+}
+
+function isCityValid()
+	{
+		let isValid = false;
+		cities.forEach(city =>
+			{
+				if (city.checked)
+				{
+					isValid = true
+				}
+			})
+			return isValid
+	}
+
+// fonction permet de revenir à l'élément parent puis retrouver la class concernée
+
+function findAvertissement(el)
   {
-    avert.style.display = 'block'
-    avert.style.color = 'red'
-    avertform.style.border = '3px solid #FF3012';
-    avertform.style.backgroundColor = '#ff0202bd'
-    }else
-  {
-    avert.style.display ='none';
-    avertform.style.border = '3px solid #13FA39';
-    avertform.style.backgroundColor = '#D6F4DC';
+	  return el.closest('.formData').querySelector('.avertissement');
   }
+
+	function fAvertissement(el)
+  {
+	  return el.closest('.formData').querySelector('.text-control');
+  }
+
+
+//  mise en forme des champs du formulaire en cas erreur ou validation
+function showError(avert, avertform)
+{
+	  avert.style.display = 'block'
+		avert.style.color = 'red'
+		avertform.style.border = '3px solid #FF3012';
+		avertform.style.backgroundColor = '#ff0202bd'		
+}
+
+function hidenError(avert, avertform)
+{
+	avert.style.display ='none';
+		avertform.style.border = '3px solid #13FA39';
+		avertform.style.backgroundColor = '#D6F4DC';
+}
+
+// affiche ou cache le message erreur et modifie la présentation
+	
+firstName.addEventListener('change', function() 
+{	
+	const avert = findAvertissement(firstName);
+	const avertform = fAvertissement(firstName);
+
+	if (!isFirstNameValid())
+	{
+		showError(avert, avertform);
+		
+		}else
+	{
+		hidenError(avert, avertform);
+		
+	}
 })
 
-lastName.addEventListener('change', function(e) {
-  const value = e.target.value;
-  const avert  = lastName.closest('.formData').querySelector('.avertissement');
-  const avertform  = lastName.closest('.formData').querySelector('.text-control');
-  const regexFirst = /^[a-zA-Z ]{2,30}$/;
-  if (!regexFirst.test(value))
-  {
-    avert.style.display = 'block'
-    avert.style.color = 'red'
-    avertform.style.border = '3px solid #FF3012';
-    avertform.style.backgroundColor = '#ff0202bd'; 
-  
-    }else
-  {
-    avert.style.display ='none';
-    avertform.style.border = '3px solid #13FA39';
-    avertform.style.backgroundColor = '#D6F4DC';
-  }
+lastName.addEventListener('change', function() {
+	
+	const avert = findAvertissement(lastName);
+	const avertform = fAvertissement(lastName);
+	
+	if (!isLastNameValid())
+	{
+		showError(avert, avertform);
+		
+		}else
+	{
+		hidenError(avert, avertform);
+		
+	}
+})
+
+
+email.addEventListener('change', function(){
+		
+	const avert = findAvertissement(email);
+	const avertform = fAvertissement(email);
+		if (!isEmailValid())
+	{
+		showError(avert, avertform);
+		
+		}else
+	{
+		hidenError(avert, avertform);
+		
+	}
+	})
+
+birthdate.addEventListener('change', function(){
+		
+	const avert = findAvertissement(birthdate);
+	const avertform = fAvertissement(birthdate);
+		
+		if (isBirthdateValid())
+	{
+		showError(avert, avertform);
+		
+		}else
+	{
+		hidenError(avert, avertform);
+		
+	}
+	})
+
+	quantity.addEventListener('change', function()
+	{
+		const avert  = quantity.closest('.formData').querySelector('.avertissement');
+		const avertform  =  quantity.closest('.formData').querySelector('.text-control');
+		
+		if (!isFirstNameValid())
+	{
+		showError(avert, avertform);
+		
+		}else
+	{
+		hidenError(avert, avertform);
+		
+	}
+	})
+
+
+	
+// validation du formulaire
+
+submitButton.addEventListener('click', function ()
+{
+	
+let formValid = parseInt(isFirstNameValid() && isLastNameValid() && isEmailValid() && isBirthdateValid() && isQantityValid() && isCityValid())
+	// event.preventDefault() 
+if (formValid == true)
+{
+	return alert("test valide");
+	
+}else 
+{
+	return alert("le test n'est pas bon")
+}
+
 })
 
 
 
-
-  email.addEventListener('change', function(e)
-  {
-    const value = e.target.value;
-    const avert  = email.closest('.formData').querySelector('.avertissement');
-    const avertform  = email.closest('.formData').querySelector('.text-control');
-    const regexmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(!regexmail.test(value))
-    {
-    avert.style.display = 'block'
-    avert.style.color = 'red'
-    avertform.style.border = '3px solid #FF3012';
-    avertform.style.backgroundColor = '#ff0202bd'; 
-      
-      }else
-      {
-        avert.style.display ='none';
-        avertform.style.border = '3px solid #13FA39';
-        avertform.style.backgroundColor = '#D6F4DC';
-      }
-  })
-
-  birthdate.addEventListener('change', function(e)
-  {
-    const value = e.target.value;
-    const avert  = birthdate.closest('.formData').querySelector('.avertissement');
-    const avertform  = birthdate.closest('.formData').querySelector('.text-control');
-    const regexbirthdate = /^(0?[1-9]|1[0-2])[\/](0?[1-9]|[12]\d|3[01])[\/](19|20)\d{2}$/;
-    if(regexbirthdate.test(value))
-    {
-    avert.style.display = 'block'
-    avert.style.color = 'red'
-    avertform.style.border = '3px solid #FF3012';
-    avertform.style.backgroundColor = '#ff0202bd'; 
-      
-      }else
-      {
-        avert.style.display ='none';
-        avertform.style.border = '3px solid #13FA39';
-        avertform.style.backgroundColor = '#D6F4DC';
-      }
-  })
-
-  quantity.addEventListener('change', function(e)
-  {
-    const value = e.target.value;
-    const avert  = quantity.closest('.formData').querySelector('.avertissement');
-    const avertform  =  quantity.closest('.formData').querySelector('.text-control');
-    const regexquantity = /^[0-99][0-9]?$/;
-    if(!regexquantity.test(value))
-    {
-    avert.style.display = 'block'
-    avert.style.color = 'red'
-    avertform.style.border = '3px solid #FF3012';
-    avertform.style.backgroundColor = '#ff0202bd'; 
-
-      }else
-      {
-        avert.style.display ='none';
-        avertform.style.border = '3px solid #13FA39';
-        avertform.style.backgroundColor = '#D6F4DC';
-      }
-  })
-
-  
+	
