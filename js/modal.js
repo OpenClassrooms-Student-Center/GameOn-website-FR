373f1391bb9f2c2c9d320e43334113870ec88e76
AxelRaboit <axel@edgco.fr>
2022-08-16 13:54:10 +0200

    [ADD] Close Modal

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName('close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,3 +20,10 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = 'none';
+}
+// Get the closeBtn variable and get the first element to add an listener
+closeBtn[0].addEventListener('click', closeModal);
