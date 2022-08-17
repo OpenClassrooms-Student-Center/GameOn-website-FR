3a9acde675a8fb2c6a64dca475c2144e1518e010
FOXEST57 <foxest57@gmail.com>
2022-08-17 12:20:49 +0200

    modif

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,4 +1,118 @@
+// DOM Elements
+const modalbg   	= document.querySelector(".bground");
+const modalBtn  	= document.querySelectorAll(".modal-btn");
+const formData  	= document.querySelectorAll(".formData");
+let firstName      	= document.querySelector('#first');
+let lastName       	= document.querySelector('#last');
+let email          	= document.querySelector('#email');
+let birthdate      	= document.querySelector('#birthdate');
+let quantity       	= document.querySelector('#quantity');
+let avertissements 	= document.querySelectorAll('.avertissement');
+let submitButton   	= document.querySelector('.btn-submit');
+let cities         	= document.querySelectorAll(`input[name="location"]`);
+const closeMod 		= document.querySelector(".close")
 
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//evenement qui ferme le modal
+closeMod.addEventListener("click", closeModal)
+
+// boucle pour faire disparaitre les messages d'avertissement
+
+avertissements.forEach(function (avertissement){ 
+	avertissement.style.display = "none";
+}) 
+
+firstName.addEventListener('change', function() 
+{	
+	const avert = findEl(firstName, '.avertissement');
+	const avertform = findEl(firstName, '.text-control');
+	hidenError(avert, avertform);
+	
+	if (!isFirstNameValid())
+	{
+		showError(avert, avertform);
+	}
+})
+
+lastName.addEventListener('change', function () 
+{
+	const avert = findEl(lastName, '.avertissement');
+	const avertform = findEl(lastName, '.text-control');
+	hidenError(avert, avertform);
+	
+	if (!isLastNameValid())
+	{
+		showError(avert, avertform);
+	}
+})
+
+
+email.addEventListener('change', function ()
+{
+	const avert = findEl(email, '.avertissement');
+	const avertform = findEl(email, '.text-control');
+	hidenError(avert, avertform);
+	
+	if (!isEmailValid())
+	{
+		showError(avert, avertform);
+	}
+})
+
+birthdate.addEventListener('change', function ()
+{
+	console.log('&&', birthdate.value)
+	const avert = findEl(birthdate, '.avertissement');
+	const avertform = findEl(birthdate, '.text-control');
+	hidenError(avert, avertform);
+		
+	if (!isBirthdateValid())
+	{
+		showError(avert, avertform);
+	}
+})
+
+quantity.addEventListener('change', function()
+{
+	const avert  = quantity.closest('.formData').querySelector('.avertissement');
+	const avertform  =  quantity.closest('.formData').querySelector('.text-control');
+	hidenError(avert, avertform);
+		
+	if (!isQuantityValid())
+	{
+		showError(avert, avertform);
+	}
+})
+
+
+submitButton.addEventListener('click', function (e)
+{
+	e.preventDefault();
+	let formValid = (isFirstNameValid() && isLastNameValid() && isEmailValid() && isBirthdateValid() && isQuantityValid() && isCityValid())
+	console.log('FN', isFirstNameValid())
+	console.log('LN', isLastNameValid())
+	console.log('EM', isEmailValid())
+	console.log('BI', isBirthdateValid())
+	console.log('QTY', isQuantityValid())
+	console.log('CIT', isCityValid())
+		
+	console.log(formValid)
+		
+	// if (formValid)
+	// {
+	// 	return alert("test valide");
+	// }else 
+	// {
+	// 	return alert("le test n'est pas bon");
+	// }
+})
+
+
+function closeModal(){
+	modalbg.style.display = "none";
+}
 
 function editNav() {
 	var x = document.querySelector("myTopnav");
@@ -9,74 +123,10 @@ function editNav() {
 	}
 }
 
-// DOM Elements
-const modalbg   = document.querySelector(".bground");
-const modalBtn  = document.querySelectorAll(".modal-btn");
-const formData  = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-	modalbg.style.display = "block";
-}
-
-
-//fermeture du formulaire
-
-// selection des class concernées pour la fermeture
-const closeMod = document.querySelector(".close")
-
-//fonction fermeture du formulaire
-function closeModal(){
-	modalbg.style.display = "none";
-}
-
-//evenement qui ferme le modal
-closeMod.addEventListener("click", closeModal)
-
-
-//IMPUT création des variables pour lier l'html
-
-let firstName      = document.querySelector('#first');
-let lastName       = document.querySelector('#last');
-let email          = document.querySelector('#email');
-let birthdate      = document.querySelector('#birthdate');
-let quantity       = document.querySelector('#quantity');
-let avertissements = document.querySelectorAll('.avertissement');
-let submitButton   = document.querySelector('.btn-submit');
-let cities         = document.querySelectorAll(`input[name="location"]`);
-
-
-
-// boucle pour faire disparaitre les messages d'avertissement
-
-avertissements.forEach(function (avertissement){ 
-	avertissement.style.display = "none";
-}) 
-
-
-// fonction permet de revenir à l'élément parent puis retrouver la class concernée
-
-function findAvertissement(el)
-  {
-	  return el.closest('.formData').querySelector('.avertissement');
-  }
-
-	function fAvertissement(el)
-  {
-	  return el.closest('.formData').querySelector('.text-control');
-  }
-
-
-//  mise en forme des champs du formulaire en cas erreur ou validation
-function showError(avert, avertform)
+function findEl(el, needle)
 {
-	  avert.style.display = 'block'
-		avert.style.color = 'red'
-		avertform.style.border = '3px solid #FF3012';
-		avertform.style.backgroundColor = '#ff0202bd'		
+	return el.closest('.formData').querySelector(needle);
+	//return el.closest('.formData').querySelector('.avertissement');
 }
 
 function hidenError(avert, avertform)
@@ -86,197 +136,79 @@ function hidenError(avert, avertform)
 		avertform.style.backgroundColor = '#D6F4DC';
 }
 
-// affiche ou cache le message erreur et modifie la présentation
+function isFirstNameValid() 
+{
+	return isNameValid(firstName.value); 
+}
 
-firstName.addEventListener('change', function() 
-{	
-	const avert = findAvertissement(firstName);
-	const avertform = fAvertissement(firstName);
-	console.log(firstName.value)
+function isLastNameValid() 
+{
+	return isNameValid(lastName.value); 
+}
 
+function isNameValid(value) 
+{
+	const regexFirst = /^[a-zA-Z ]{2,30}$/;
 
-	if (!isFirstNameValid())
-	{
-		showError(avert, avertform);
-		return false
-		
-		}else
-	{
-		hidenError(avert, avertform);
-		return true
-		
-	}
-})
-
-lastName.addEventListener('change', function() {
-	
-	const avert = findAvertissement(lastName);
-	const avertform = fAvertissement(lastName);
-	
-	if (!isLastNameValid())
-	{
-		showError(avert, avertform);
-		
-		}else
-	{
-		hidenError(avert, avertform);
-		
-	}
-})
-
-
-email.addEventListener('change', function(){
-		
-	const avert = findAvertissement(email);
-	const avertform = fAvertissement(email);
-		if (!isEmailValid())
-	{
-		showError(avert, avertform);
-		
-		}else
-	{
-		hidenError(avert, avertform);
-		
-	}
-	})
-
-birthdate.addEventListener('change', function(){
-		
-	const avert = findAvertissement(birthdate);
-	const avertform = fAvertissement(birthdate);
-		
-		if (isBirthdateValid())
-	{
-		showError(avert, avertform);
-		
-		}else
-	{
-		hidenError(avert, avertform);
-		
-	}
-	})
-
-	quantity.addEventListener('change', function()
-	{
-		const avert  = quantity.closest('.formData').querySelector('.avertissement');
-		const avertform  =  quantity.closest('.formData').querySelector('.text-control');
-		
-		if (!isFirstNameValid())
-	{
-		showError(avert, avertform);
-		
-		}else
-	{
-		hidenError(avert, avertform);
-		
-	}
-	})
-
-// vérification regex
-console.log(isFirstNameValid())
-function isFirstNameValid()
-	{
-		const regexFirst = /^[a-zA-Z ]{2,30}$/;
-
-		if (!regexFirst.test(firstName.value))
-		{
-			return false;
-		}else{
-			return true;
-		}
-
-		
-	}
-	
-	
-
-function isLastNameValid()
-	{
-		const regexLast = /^[a-zA-Z ]{2,30}$/;
-
-		if (!regexLast.test(lastName.value))
-		{
-			return false;
-		}
-
-		return true;
-	}
+	return regexFirst.test(value);
+}
 
 function isEmailValid()
-	{
-		const regexmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+{
+	const regexmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-		if (!regexmail.test(email.value))
-		{
-			return false;
-		}
-
-		return true;
-	}
+	return regexmail.test(email.value);
+}
 
 function isBirthdateValid()
 {
-	const regexbirthdate = /^(0?[1-9]|1[0-2])[\/](0?[1-9]|[12]\d|3[01])[\/](19|20)\d{2}$/;
-
+	const regexbirthdate = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
+	const currentYear = new Date().getFullYear();
 	if (!regexbirthdate.test(birthdate.value))
 	{
 		return false;
 	}
 
+	if (birthdate.value.split('-')[0] > (currentYear - 20))
+	{
+		return false
+	}
+
+	if (birthdate.value.split('-')[0] < (currentYear - 150))
+	{
+		return false
+	}
+
 	return true;
 }
 
-function isQantityValid()
+function isQuantityValid()
 {
 	const regexquantity = /^[0-99][0-9]?$/;
-	if(!regexquantity.test(quantity.value))
-	{
-		return false;
-	}
-	return true;
+	return regexquantity.test(quantity.value);
 }
 
 function isCityValid()
-	{
-		let isValid = false;
-		cities.forEach(city =>
+{
+	let isValid = false;
+	cities.forEach(city =>
+		{
+			if (city.checked)
 			{
-				if (city.checked)
-				{
-					isValid = true
-				}
-			})
-			return isValid
-	}
-
-	console.log(isCityValid())
-
-// validation du formulaire
-
-// event.preventDefault() 
-
-submitButton.addEventListener('click', function ()
-{
-	
-let formValid = (isFirstNameValid() && isLastNameValid() && isEmailValid() && isBirthdateValid() && isQantityValid() && isCityValid())
-	
-	console.log(formValid)
-	
-if (formValid)
-{
-
-	return alert("test valide");
-	
-}else 
-{
-	
-	return alert("le test n'est pas bon");
-	
-	
+				isValid = true
+			}
+		})
+		return isValid
 }
 
-})
+function launchModal() {
+	modalbg.style.display = "block";
+}
 
-
-
-	
+function showError(avert, avertform)
+{
+	  avert.style.display = 'block'
+		avert.style.color = 'red'
+		avertform.style.border = '3px solid #FF3012';
+		avertform.style.backgroundColor = '#ff0202bd'		
+}
