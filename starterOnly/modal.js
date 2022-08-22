2841dfcfb9b9853eb6aaad28f2fb27aec1025f90
Leandro Barbosa <leandrobarbosa@sfr.fr>
2022-08-22 16:23:08 +0200

    close button js done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,25 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalClose.addEventListener("click", closeModalForm);
 
 // launch modal form
+/* The function `launchModal()` displays the modal background and sets the display property of the modal background to block */
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+// close modal form
+function closeModalForm() {
+  modalbg.style.display = "none";
+  document.getElementById("modal-body").style.display = "block";
+   document.getElementById("modal-body2").style.display = "none";
+}
