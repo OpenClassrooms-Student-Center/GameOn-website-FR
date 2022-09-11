180ebbc0f917649698947699c2f20e61a237118b
Fabian Deneuville <deneuville.fabian@live.fr>
2022-09-11 18:39:28 +0200

    Issue 1 done - Closing modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,13 +2,20 @@
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+modalCloseBtn.addEventListener('click', closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal function
+function closeModal() {
+  modalbg.style.display = "none";
+}
