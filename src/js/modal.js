ad1703fcb8fc2bf432467c04e8a6a172dc9868d7
Fabian Deneuville <deneuville.fabian@live.fr>
2022-09-11 18:36:21 +0200

    Initial commit

---
+++ src/js/modal.js
@@ -0,0 +1,14 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+
