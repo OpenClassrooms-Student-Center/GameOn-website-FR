84038cdd1f088ede60509c6b6753e1ee51da5ff9
arktal <101146331+gpollet@users.noreply.github.com>
2022-09-16 15:02:33 +0200

    Modal close button is now working Labels and inputs are now properly linked in html

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,30 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopnav")
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += " responsive"
   } else {
-    x.className = "topnav";
+    x.className = "topnav"
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector(".bground")
+const modalBtn = document.querySelectorAll(".modal-btn")
+const formData = document.querySelectorAll(".formData")
+const modalCloseButton = document.querySelector(".close")
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "block"
 }
 
+// Close modal event
+modalCloseButton.addEventListener("click", closeModal)
 
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none"
+}
