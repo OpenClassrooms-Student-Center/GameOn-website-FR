b41eb3887bf28a8ab1a1c474a8b13f1ba1fefe88
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-09-19 15:05:56 +0200

    close --> queryselector

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
