83eeb6286daa0fdf0ad255c992a6a8e58ee8807f
gylgyl2000 <gylgyl2000@gmail.com>
2022-09-26 14:02:52 +0200

    Ajout de la fonctionnalité au bouton de fermeture de la modale

--- modal.js
+++ modal.js
@@ -7,17 +7,34 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+// ÉLÉMENTS DOM
 
-// launch modal event
+// Sélectionner l'élément "modale"
+const modalbg = document.querySelector(".bground");
+// Sélectionner l'élément "bouton" qui ouvre la modale
+const modalBtn = document.querySelectorAll(".modal-btn");
+// Sélectionner l'élément "formulaire"
+const formData = document.querySelectorAll(".formData");
+// Sélectionner l'élément "bouton" qui ferme la modale
+const closeModalBtn = document.querySelector(".close");
+
+// Lorsque l'utilisateur clique sur le bouton, ouvre la modale
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Fonction d'ouverture de la modale
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Lorsque l'utilisateur clique sur le bouton, ferme la modale
+closeModalBtn.addEventListener("click", closeModal);
 
+// Fonction de fermeture de la modale
+function closeModal() {
+  modalbg.setAttribute("closing", "");
+  
+  modalbg.addEventListener("animationend", () => {
+    modalbg.removeAttribute("closing");
+    modalbg.style.display= "none";
+  }, {once: true})
+}
