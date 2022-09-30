405a765dad2505c209c33ded17a1106c5ec1d216
Leandro Barbosa <leandrobarbosa@sfr.fr>
2022-09-30 16:43:27 +0200

    dev for inputs forms

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,17 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("form-modal-data");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const subimtBtn = document.getElementById(".btn-submit");
+const inputs = document.getElementsByTagName("input");
+let firstNameRegex = /^[a-zA-Z]+$/;
+let lastNameRegex = /^[a-zA-Z]+$/;
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
