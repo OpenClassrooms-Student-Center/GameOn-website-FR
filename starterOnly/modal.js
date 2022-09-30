029e9806b92a2e9b04a6ab6f6ee2b3f841e91065
Leandro Barbosa <leandrobarbosa@sfr.fr>
2022-09-30 16:31:18 +0200

    Get all the inputs and stock it in a variable

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,9 +8,11 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
+const closeBtn = document.querySelector('.close');
+const formModal = document.querySelector('.form-modal-confirmation');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,3 +29,19 @@ document.querySelector(".close").addEventListener("click", closeModal);
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+//form validation
+
+// DOM form inputs
+
+const form = document.getElementById('form-modal-data');
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+const submit = document.getElementById('btn-submit');
+const inputs = document.querySelectorAll('input');
+let firstnameRegexExp = new RegExp("[0-9]");
+let lastnameRegexExp = new RegExp("[0-9]");
+
