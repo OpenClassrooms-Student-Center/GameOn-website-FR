fb199d481968d6da0e8f4fb9186af3165482a14e
Cem Ekiz <ekizcem.pro@gmail.com>
2022-10-06 13:43:18 +0200

    quick clean js file

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,22 @@
+// Open & close responsive menu
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+	const menu = document.getElementById('myTopnav');
+	if (menu.className === 'topnav') {
+		menu.className += ' responsive';
+	} else {
+		menu.className = 'topnav';
+	}
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// Launch modal event
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
-// launch modal form
+// Launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+	modalbg.style.display = 'block';
 }
-
-
