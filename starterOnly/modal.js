c81df1bb466c2e5b3733934857f052ac04333588
ChrisLamanu <christopher.vrel@gmail.com>
2022-10-15 00:38:43 +0200

    Issue 1 done - Close Modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,7 +17,16 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
+// Getting all close btn in the DOM
+let closeModalBtn = document.querySelectorAll(".close");
 
+// Adding an event on each btn to close
+closeModalBtn.forEach((e) => e.addEventListener("click", closeModal));
+
+// close modal function
+function closeModal() {
+    modalbg.style.display = "none";
+}
