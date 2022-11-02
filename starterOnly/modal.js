0130ef1e2e7b2fe63d7e9529a3f76a751f531989
qhart <quentinhart51@gmail.com>
2022-11-02 17:10:20 +0100

    close modal form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,22 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+modalClose.forEach((close) => close.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
