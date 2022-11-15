10be7e7903448f781f0ef34ed4ed5152ade46224
David ANDRIASON <davidandriason7@gmail.com>
2022-11-15 18:39:47 +0100

    installation of the modal closure when you click on the button x #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,29 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById('myTopnav');
+    if (x.className === 'topnav') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'topnav';
+    }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
+const closeBtn = document.querySelector('.close');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
+// close modal event when you click on the button (x)
+closeBtn.addEventListener('click', closeModal)
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = 'block';
 }
 
-
+// Close modal form
+function closeModal () {
+    modalbg.style.display = 'none';
+};
