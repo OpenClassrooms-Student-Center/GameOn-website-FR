18b2a2a9e2dfb6c49cf093c565dbd2d7f016a421
Toledano Mickael <mikatol06@gmail.com>
2022-11-24 15:59:24 +0100

    js_validation_start

---
+++ js/formValidation.js
@@ -0,0 +1,16 @@
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const quantity = document.getElementById('quantity');
+const birthdate = document.getElementById('birthdate');
+const allLocations = document.getElementById('allLocations');
+const locations = document.querySelectorAll('#allLocations .checkbox-input');
+const checkbox1 = document.getElementById('checkbox1');
+const input = document.getElementsByClassName('text-control');
+const form = document.getElementById('form');
+
+
+function checkFirstName(){
+
+}
+
