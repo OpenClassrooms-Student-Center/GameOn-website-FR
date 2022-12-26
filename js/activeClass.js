190500f77fa51f91ea07e9dd46eb678bbdcd104c
Nausicaa Cresta <nausicaa.cresta@gmail.com>
2022-12-26 17:47:16 +0100

    Ajout_animation_js_barre_de_menu

---
+++ js/activeClass.js
@@ -0,0 +1,18 @@
+// Je récupère les éléments du DOM et les stocke dans des variables
+const navbar=document.querySelector("main-navbar");
+const links=document.querySelectorAll("a");
+console.log(navbar);
+
+// Je crée un tableau qui contient tous mes liens 
+// grâce à forEach et je parcours les éléments
+links.forEach(a => {
+    // J'ajoute un écouteur d'évènement sur chaque lien qui déclenche la fonction au click
+    a.addEventListener('click', function(){
+        // Je parcours mes liens, je supprime la classe active pour chacun d'entre eux
+        links.forEach(a=>a.classList.remove("active"));
+        // J'ajoute la classe active au click sur mon lien
+        this.classList.add("active");
+    });
+
+});
+
--- js/modal.js
+++ js/modal.js
@@ -18,6 +18,51 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  modalbg.style.add(".active");
 }
 
+/*const items = document.getElementsByClassName("img");
+const nbSlide = items.length;
+const suivant = document.querySelector(".right");
+const precedent = document.querySelector(".left");
+
+// système de compteur d'image initialisé à zéro
+let compteur = 0;
+
+// fonction qui incrémente chaque image de 1
+function slideSuivante(){
+    // sélection de la première image du tableau : retrait de la classe active
+    items[compteur].classList.remove("active");
+
+    // on incrémente de 1 si le compteur est <-1
+    if(compteur < nbSlide-1){
+        console.log(compteur++);
+    }else{
+        compteur = 0;
+    }
+
+    // ajout de la classe active : l'image apparaît
+    items[compteur].classList.add("active")
+    console.log(compteur);
+}
+suivant.addEventListener("click", slideSuivante)
+
+// fonction qui décrémente chaque image de 1
+function slidePrecedente(){
+    // sélection de la première image du tableau : retrait de la classe active
+    items[compteur].classList.remove("active");
+
+    // on décrémente de 1 si le compteur est >0
+    if(compteur >0){
+        compteur--;
+    // sinon on recule la slide activée de 1
+    }else{
+        compteur = nbSlide -1;
+    }
+
+    // ajout de la classe active : l'image apparaît
+    items[compteur].classList.add("active")
+    console.log(compteur);
+}
+precedent.addEventListener("click", slidePrecedente)*/
 
