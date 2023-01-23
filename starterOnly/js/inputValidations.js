7caeaba5409a836e19a84336df74f87fd1c96e9c
dina <jdina2004@gmail.com>
2023-01-23 12:33:20 +0100

    fixing error messages

--- starterOnly/js/inputValidations.js
+++ starterOnly/js/inputValidations.js
@@ -9,17 +9,17 @@ const firstNameValidation = () => {
 
     if  (firstName.value.trim() === '' ) {
         firstName.parentElement.setAttribute('data-error-visible', 'true');
-        firstName.parentElement.setAttribute("data-error", "Veuillez entrer votre prenom ");
+        firstName.parentElement.setAttribute("data-error", "Veuillez entrer votre prénom ");
         return false;
     }
     if (!firstName.value.match(/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/)){
         firstName.parentElement.setAttribute('data-error-visible', 'true');
-        firstName.parentElement.setAttribute("data-error", "Veuillez ne pas entrer special characters au nombre");
+        firstName.parentElement.setAttribute("data-error", "Veuillez ne pas entrer de caractères spéciaux ou de nombres");
         return false;
     }
     if (firstName.value.trim().length < 2 ){
         firstName.parentElement.setAttribute('data-error-visible', 'true');
-        firstName.parentElement.setAttribute("data-error", "Veuillez entrer au moin 2 caractères ");
+        firstName.parentElement.setAttribute("data-error", "Veuillez entrer au moins 2 caractères ");
         return false;
     } 
         first.parentElement.setAttribute('data-error-visible', 'false');
@@ -37,12 +37,12 @@ const  lastNameValidation = () => {
     }
     if (!lastName.value.match(/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/)){
         lastName.parentElement.setAttribute('data-error-visible', 'true');
-        lastName.parentElement.setAttribute("data-error", "Veuillez ne pas entrer special characters au nombre");
+        lastName.parentElement.setAttribute("data-error", "Veuillez ne pas entrer de caractères speciaux  ou de nombres");
         return false;
     }
     if (lastName.value.trim().length < 2 ) {
         lastName.parentElement.setAttribute('data-error-visible', 'true');
-        lastName.parentElement.setAttribute("data-error", "Veuillez entrer au moin 2 caractères ");
+        lastName.parentElement.setAttribute("data-error", "Veuillez entrer au moins 2 caractères ");
         return false;
     } 
         lastName.parentElement.setAttribute('data-error-visible', 'false');
@@ -61,7 +61,7 @@ const  emailValidation= () => {
     }
     if (!email.value.trim().match(emailRegex)) {
         email.parentElement.setAttribute('data-error-visible', 'true');
-        email.parentElement.setAttribute("data-error", "Veuillez entrer une  email valid");
+        email.parentElement.setAttribute("data-error", "Veuillez entrer un email valide");
         return false;
     }
         email.parentElement.setAttribute('data-error-visible', 'false');
@@ -105,12 +105,12 @@ const quantityValidation = () => {
 
     if ((quantity.value.trim().length === 0 )|| (isNaN(quantity.value.trim() === true)) ){
         quantity.parentElement.setAttribute('data-error-visible', 'true');
-        quantity.parentElement.setAttribute("data-error", "Veuillez entrer une nombre");
+        quantity.parentElement.setAttribute("data-error", "Veuillez entrer un nombre");
         return false;
     }
     if (quantity.value.trim() < 0 || quantity.value.trim() >= 99){
         quantity.parentElement.setAttribute('data-error-visible', 'true');
-        quantity.parentElement.setAttribute("data-error", "veuillez entrer une nombre entre 0 et 99");
+        quantity.parentElement.setAttribute("data-error", "veuillez entrer un nombre entre 0 et 99");
         return false;
     }
         quantity.parentElement.setAttribute('data-error-visible', 'false');
@@ -127,7 +127,7 @@ const quantityValidation = () => {
            return true;
     }  
            allLocations.setAttribute('data-error-visible', 'true');
-           allLocations.setAttribute("data-error", "veuillez choisir une location");
+           allLocations.setAttribute("data-error", "veuillez choisir une option");
            return false;
 }
 
@@ -141,7 +141,7 @@ const termsValidation = () => {
         return true;
     }
         termsAcceptBtn.parentElement.setAttribute('data-error-visible', 'true');
-        termsAcceptBtn.parentElement.setAttribute("data-error", "veuillez accepter les terms et condition");
+        termsAcceptBtn.parentElement.setAttribute("data-error", "veuillez accepter les termes et conditions");
         return false;
 }
 
