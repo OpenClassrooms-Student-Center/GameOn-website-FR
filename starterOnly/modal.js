cc58547a5efa7f230118ac32ac6de40c7838bf47
dina <jdina2004@gmail.com>
2023-01-23 09:17:46 +0100

    fixing issue #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,29 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName('close');
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+//modal functionalities (launch, close)
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+// 1-- launch modal inscription form
+modalBtn.forEach((btn) => btn.addEventListener('click', function(){
+        modalbg.style.display = 'block';
+}));
+
+// 2-- close modal form
+closeBtn[0].addEventListener('click', function(){
+         modalbg.style.display = 'none';
+});
+
+
+
+// nav editing responsive 
+function editNav() {
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
 }
-
-
