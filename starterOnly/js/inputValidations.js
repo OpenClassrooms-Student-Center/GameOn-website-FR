c4d9bedc95b6f14b189a015bdde65c1b00104251
dina <jdina2004@gmail.com>
2023-01-23 15:22:03 +0100

    improve responsivity

--- starterOnly/js/inputValidations.js
+++ starterOnly/js/inputValidations.js
@@ -9,20 +9,21 @@ const firstNameValidation = () => {
 
     if  (firstName.value.trim() === '' ) {
         firstName.parentElement.setAttribute('data-error-visible', 'true');
-        firstName.parentElement.setAttribute("data-error", "Veuillez entrer votre prénom ");
+        firstName.parentElement.setAttribute("data-error", "Veuillez entrer votre prenom ");
         return false;
     }
     if (!firstName.value.match(/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/)){
         firstName.parentElement.setAttribute('data-error-visible', 'true');
-        firstName.parentElement.setAttribute("data-error", "Veuillez ne pas entrer de caractères spéciaux ou de nombres");
+        firstName.parentElement.setAttribute("data-error", "Veuillez ne pas entrer special characters au nombre");
         return false;
     }
     if (firstName.value.trim().length < 2 ){
         firstName.parentElement.setAttribute('data-error-visible', 'true');
-        firstName.parentElement.setAttribute("data-error", "Veuillez entrer au moins 2 caractères ");
+        firstName.parentElement.setAttribute("data-error", "Veuillez entrer au moin 2 caractères ");
         return false;
     } 
-        first.parentElement.setAttribute('data-error-visible', 'false');
+        firstName.parentElement.setAttribute('data-error-visible', 'false');
+        firstName.parentElement.removeAttribute('data-error');
         return true;
 }
 
@@ -37,15 +38,16 @@ const  lastNameValidation = () => {
     }
     if (!lastName.value.match(/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/)){
         lastName.parentElement.setAttribute('data-error-visible', 'true');
-        lastName.parentElement.setAttribute("data-error", "Veuillez ne pas entrer de caractères speciaux  ou de nombres");
+        lastName.parentElement.setAttribute("data-error", "Veuillez ne pas entrer special characters au nombre");
         return false;
     }
     if (lastName.value.trim().length < 2 ) {
         lastName.parentElement.setAttribute('data-error-visible', 'true');
-        lastName.parentElement.setAttribute("data-error", "Veuillez entrer au moins 2 caractères ");
+        lastName.parentElement.setAttribute("data-error", "Veuillez entrer au moin 2 caractères ");
         return false;
     } 
         lastName.parentElement.setAttribute('data-error-visible', 'false');
+        lastName.parentElement.removeAttribute('data-error');
         return true;
 }
 
@@ -61,10 +63,11 @@ const  emailValidation= () => {
     }
     if (!email.value.trim().match(emailRegex)) {
         email.parentElement.setAttribute('data-error-visible', 'true');
-        email.parentElement.setAttribute("data-error", "Veuillez entrer un email valide");
+        email.parentElement.setAttribute("data-error", "Veuillez entrer une  email valid");
         return false;
     }
         email.parentElement.setAttribute('data-error-visible', 'false');
+        email.parentElement.removeAttribute('data-error');
         return true;
 }
 
@@ -95,6 +98,7 @@ const birthdateValidation = () => {
            return false;
     }
            birthdate.parentElement.setAttribute('data-error-visible', 'false');
+           birthdate.parentElement.removeAttribute('data-error');
            return true;
 }
 
@@ -105,15 +109,16 @@ const quantityValidation = () => {
 
     if ((quantity.value.trim().length === 0 )|| (isNaN(quantity.value.trim() === true)) ){
         quantity.parentElement.setAttribute('data-error-visible', 'true');
-        quantity.parentElement.setAttribute("data-error", "Veuillez entrer un nombre");
+        quantity.parentElement.setAttribute("data-error", "Veuillez entrer une nombre");
         return false;
     }
     if (quantity.value.trim() < 0 || quantity.value.trim() >= 99){
         quantity.parentElement.setAttribute('data-error-visible', 'true');
-        quantity.parentElement.setAttribute("data-error", "veuillez entrer un nombre entre 0 et 99");
+        quantity.parentElement.setAttribute("data-error", "veuillez entrer une nombre entre 0 et 99");
         return false;
     }
         quantity.parentElement.setAttribute('data-error-visible', 'false');
+        quantity.parentElement.removeAttribute('data-error');
         return true;
 }
 
@@ -127,7 +132,7 @@ const quantityValidation = () => {
            return true;
     }  
            allLocations.setAttribute('data-error-visible', 'true');
-           allLocations.setAttribute("data-error", "veuillez choisir une option");
+           allLocations.setAttribute("data-error", "veuillez choisir une location");
            return false;
 }
 
@@ -138,10 +143,11 @@ const termsValidation = () => {
 
     if (termsAcceptBtn.checked) {
         termsAcceptBtn.parentElement.setAttribute('data-error-visible', 'false');
+        termsAcceptBtn.parentElement.removeAttribute('data-error');
         return true;
     }
         termsAcceptBtn.parentElement.setAttribute('data-error-visible', 'true');
-        termsAcceptBtn.parentElement.setAttribute("data-error", "veuillez accepter les termes et conditions");
+        termsAcceptBtn.parentElement.setAttribute("data-error", "veuillez accepter les terms et condition");
         return false;
 }
 
