38ef1540b8e9e8fa0a199cc62261145d3c1f57ec
stephane.malho <stepahen.malho@gmail.com>
2023-02-10 17:40:26 +0100

    Fix media query

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,9 +1,9 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var menuIcon = document.getElementById("myTopnav");
+  if (menuIcon.className === "topnav") {
+    menuIcon.className += " responsive";
   } else {
-    x.className = "topnav";
+    menuIcon.className = "topnav";
   }
 }
 
