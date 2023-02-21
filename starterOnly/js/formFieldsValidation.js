b3bde2198d559d65664041c6f493d232238a434c
Kor7o <dodane.hugo@gmail.com>
2023-02-21 12:52:02 +0100

    all errors done

--- starterOnly/js/formFieldsValidation.js
+++ starterOnly/js/formFieldsValidation.js
@@ -1,50 +1,89 @@
 // DOM ELEMENTS FORM FIELDS VALIDATION
 const firstName = document.getElementById('first');
 const lastName = document.getElementById('last');
-const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ\-\' ]{2,}$/;
-const regex2 = /^[\w-_\.]+@([\w-]+\.)+[\w-]$/;
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
 
-// ------ FORM FIELDS VALIDATION ------ //
-// FIRST NAME AND LAST NAME
+const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ\-\' ]{2,}$/;
+const regex2 =  /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const regex3 =  /^[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$/g;
+
+
+
+// FIRST AND LAST NAME
 
 function checkFirstName() {
     console.log(firstName.value)
     if (firstName.value.trim().length < 2 || firstName.value.trim() === '' || !firstName.value.match(regex)) {
         firstName.parentElement.setAttribute('data-error-visible', 'true');
-        firstName.parentElement.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du nom.')
-        firstName.style.textAlign = 'left';
+        firstName.parentElement.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du nom.');
         firstName.style.border = '2px solid #e54858';
         return false;
     }
     firstName.parentElement.setAttribute('data-error-visible', 'false');
+    firstName.style.border = 'none';
     return true;
 }
 
 function checkLastName() {
+    console.log(lastName.value)
     if (lastName.value.trim().length < 2 || lastName.value.trim() === "" || !lastName.value.match(regex)) {
         lastName.parentElement.setAttribute('data-error-visible', 'true');
-        lastName.parentElement.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du nom.')
+        lastName.parentElement.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
         lastName.style.border = '2px solid #e54858';
         return false;
     }
     lastName.parentElement.setAttribute('data-error-visible', 'false');
+    lastName.style.border = 'none';
     return true;
 }
 
 // MAIL
 
-function checkMail() {
-    console.log(mail.value)
-    if (firstName.value.trim().length < 2 || firstName.value.trim() === '' || !firstName.value.match(regex)) {
-        firstName.parentElement.setAttribute('data-error-visible', 'true');
-        firstName.parentElement.setAttribute('data-error', 'erorr')
-        firstName.style.border = '2px solid #e54858';
+function checkEmail() {
+    console.log(email.value)
+    if (email.value.trim().length < 2 || email.value.trim() === "" || !email.value.match(regex2)) {
+        email.parentElement.setAttribute('data-error-visible', 'true');
+        email.parentElement.setAttribute('data-error', 'Veuillez entrer une adresse mail valide');
+        email.style.border = '2px solid #e54858';
         return false;
     }
-    firstName.parentElement.setAttribute('data-error-visible', 'false');
-    firstName.style.border = 'solid #279e7a 0.19rem';
+    email.parentElement.setAttribute('data-error-visible', 'false');
+    email.style.border = 'none';
     return true;
 }
+
+// BIRTH (impossible dinterdire -18)
+
+function checkBirthdate() {
+    console.log(birthdate.value)
+    if (birthdate.value.trim().length < 2 || birthdate.value.trim() === "" || !birthdate.value.match(regex3)) {
+        birthdate.parentElement.setAttribute('data-error-visible', 'true');
+        birthdate.parentElement.setAttribute('data-error', 'Veuillez entrer une adresse date de naissance valide <br> Vous devez etre majeur');
+        birthdate.style.border = '2px solid #e54858';
+        return false;
+    }
+    birthdate.parentElement.setAttribute('data-error-visible', 'false');
+    birthdate.style.border = 'none';
+    return true;
+}
+
+// QUANTITY
+
+function checkQuantity() {
+    console.log(quantity.value)
+    if (quantity.value.trim().length === 0 || isNaN(quantity.value.trim()) === true || quantity.value.trim() > 100) {
+        quantity.parentElement.setAttribute('data-error-visible', 'true');
+        quantity.parentElement.setAttribute('data-error', 'Veuillez entrer une valeur numerique valide');
+        quantity.style.border = '2px solid #e54858';
+        return false;
+    }
+    quantity.parentElement.setAttribute('data-error-visible', 'false');
+    quantity.style.border = 'none';
+    return true;
+}
+
 
 // FORM FIELDS EVENTS
 function formFieldsValidation(element, method, event) {
@@ -52,12 +91,17 @@ function formFieldsValidation(element, method, event) {
 }
 formFieldsValidation(firstName, checkFirstName, 'focusout');
 formFieldsValidation(lastName, checkLastName, 'focusout');
-
+formFieldsValidation(email, checkEmail, 'focusout');
+formFieldsValidation(birthdate, checkBirthdate, 'focusout');
+formFieldsValidation(quantity, checkQuantity, 'focusout');
 
 
 function formValidation() {
     if (checkFirstName() === true &&
-        checkLastName() === true) {
+        checkLastName() === true &&
+        checkEmail() === true &&
+        checkBirthdate() === true &&
+        checkQuantity() === true ) {
         return true;
     }
     return false;
