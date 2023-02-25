d1403294075b30e46cc90a991e57459a6053a043
Belarif <b.ocine@live.fr>
2023-02-25 16:01:35 +0100

    GO1: adding first input validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,37 @@ closeBtn.addEventListener("click", closeModal);
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+// validation inputs
+function validate()
+{
+
+  function removeSpan(span) {
+    setTimeout(() => {
+      span.style.display = "none";
+    }, 
+    4000);
+  }
+
+  function createSpan(errorMessage) {
+    const error = document.createElement("span");
+    error.textContent = errorMessage;
+    error.style.color = "#cc0000";
+    error.style.fontSize = "1rem";
+    const formData = document.querySelector(".modal-body .formData");
+    const span = formData.appendChild(error);
+
+    removeSpan(span);
+  }
+
+  // first input validation
+  let first = document.getElementById("first").value;
+
+  if (!first.match(/^[a-zA-Z]{2,}$/)) {
+
+    let errorMessage = "Le prénom doit contenir que des lettres et en avoir au moins 2";
+    createSpan(errorMessage);
+
+    return false;
+  }
+}
