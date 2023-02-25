31597d993278f4fd9b0c14ee83fa3a014ae36a4c
DamienL47 <damien.lataste@lapiscine.pro>
2023-02-25 14:05:39 +0100

    #1 ajout du code JS de fermeture de la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+//je cible le bouton de fermeture de la modale
+const modalBtnClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// Ajout d'un évènement au click sur le bouton close et appel de la fonction closeModal
+modalBtnClose.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
