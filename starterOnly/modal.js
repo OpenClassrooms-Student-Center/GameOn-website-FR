89bc72da89f0abd801214e2feb239396c249ed55
jobe8 <jorisbe.pro@gmail.com>
2023-02-28 16:21:54 +0100

    add close modal btn

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +22,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal form
+closeModalBtn.addEventListener("click", closeModal => {
+  modalbg.style.display = "none";
+});
