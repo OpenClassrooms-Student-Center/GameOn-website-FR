e7290254a557b373cd863ce2ee7840bf3f09fa66
Adrien <59972394+AdrienM2b@users.noreply.github.com>
2023-03-05 07:48:20 +0100

    issue 1 ajout de la fermeture de la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +19,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal 
+
+modalClose.onclick = function(){
+  modalbg.style.display = "none";  
 }
 
 
