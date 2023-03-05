6c290f23152a7fe124a13ae448d67aa38844c644
AugustinDmrt <55685689+AugustinDmrt@users.noreply.github.com>
2023-03-05 16:40:29 +0100

    Fix fermeture du modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,6 +10,7 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn2 = document.querySelectorAll(".close-modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
@@ -18,6 +19,14 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal event
+modalBtn2.forEach((btn) => btn.addEventListener("click", closeModal));
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
