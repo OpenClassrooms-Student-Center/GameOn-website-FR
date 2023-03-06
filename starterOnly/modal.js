aab21b3d434b757b9189fa0ed2428ef280ea67c0
Adrien <59972394+AdrienM2b@users.noreply.github.com>
2023-03-06 08:31:33 +0100

    ajout confirmation envoi reussi

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,19 +12,35 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
+const modalSubmit = document.querySelector(".btn-submit");
+const modalBody = document.querySelector(".modal-body");
+const modalThanks = document.querySelector(".thanks-page");
+const modalCloseThanks = document.querySelector(".btn-close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
-function launchModal() {
+function launchModal(event) {
   modalbg.style.display = "block";
+  event.preventDefault();
 }
 
 // close modal 
-
 modalClose.onclick = function(){
   modalbg.style.display = "none";  
 }
 
+// submit form
+modalSubmit.onclick = function (event){
+  modalBody.style.display = "none";
+  modalThanks.style.display = "flex";
+  event.preventDefault();
+}
 
+// close thanks page 
+
+modalCloseThanks.onclick = function (){
+  modalThanks.style.display = "none";
+  modalBody.style.display = "block";
+}
