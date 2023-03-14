a93e496ee0c91ba4f1bd26d41324ffcedf9d972f
rachon34 <rachon3441@gmail.com>
2023-03-14 17:51:48 +0100

    fermeture modale ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,23 @@ function editNav() {
 }
 
 // DOM Elements
+
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+//console.log(btn)
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  
 }
+modalbg.addEventListener("click", closeForm);
 
+function closeForm() {
+  modalbg.style.display= "none";
+}
 
