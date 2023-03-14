2f8e8289bf4c21ee9ea01f665cf2ef102f08fdc3
Devmatrix <devmatrix73@gmail.com>
2023-03-14 13:48:50 +0100

    Fix #1, close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,6 @@
+/***********************************************************
+ *                     TOP NAVIGATION                     *
+ ***********************************************************/
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,17 +10,57 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+/***********************************************************
+ *                       MODAL FORM                       *
+ ***********************************************************/
 
-// launch modal event
+/**
+ * DOM Elements
+ */
+const modalbg       = document.querySelector(".bground");
+const modalBtn      = document.querySelectorAll(".modal-btn");
+const formData      = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close");
+const modalContent  = document.querySelector(".content");
+
+/* ----------------------------------
+     LAUNCH AND CLOSE MODAL EVENT
+   ---------------------------------- */
+
+/**
+ * Launch modal event
+ */
+const launchModal = () => {
+  modalbg.style.display = "block";
+  modalbg.classList.add("visible");
+  const visibleModal = document.querySelector(".bground.visible");
+
+  if (typeof visibleModal !== "undefined") {
+    //Add ARIA atttributes
+    closeModalBtn.setAttribute("aria-label", "Fermer le formulaire");
+    closeModalBtn.setAttribute("title", "Fermer le formulaire");
+    closeModalBtn.setAttribute("aria-expanded", "true");
+  }
+}
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+/**
+ * Close modal event
+ */
+const closeModal = () => {
+  // Add .dismissed for exit animation 
+  modalContent.classList.add("dismissed");
 
+  setTimeout(() => {      
+    modalContent.classList.remove("dismissed");
+    modalbg.classList.remove("visible");
+    modalbg.style.display = "none";
+
+    // Handle ARIA attributes
+    closeModalBtn.removeAttribute("aria-label");
+    closeModalBtn.removeAttribute("title");
+    closeModalBtn.setAttribute("aria-expanded", "false");
+  }, 800); // Same timing as modal animation
+}
+closeModalBtn.addEventListener("click", closeModal);
 
