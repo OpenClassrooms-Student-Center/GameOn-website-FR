5673270c0ed6a4b3a0198d08ac888c9991efdaff
Adrien <59972394+AdrienM2b@users.noreply.github.com>
2023-03-15 08:51:42 +0100

    m

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,7 +14,6 @@ const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
 const modalSubmit = document.querySelector(".btn-submit");
 const modalBody = document.querySelector(".modal-body");
-const modalThanks = document.querySelector(".thanks-page");
 const modalCloseThanks = document.querySelector(".btn-close");
 
 // launch modal event
@@ -31,16 +30,104 @@ modalClose.onclick = function(){
   modalbg.style.display = "none";  
 }
 
-// submit form
-modalSubmit.onclick = function (event){
-  modalBody.style.display = "none";
-  modalThanks.style.display = "flex";
+// valider le formulaire //
+
+function validate(event){
   event.preventDefault();
-}
+    console.log("toto");
+};
+      // valider le first name //
+    function validateFirstName(){
+      var firstName = document.querySelector("#first");
+      const messageErrorFirstName = document.querySelector("#errorFirstName");
+      firstName.addEventListener("input", function(){
+        if(firstName.value === "" || firstName.value.length < 2){
+          messageErrorFirstName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+          messageErrorFirstName.style.color ="red";
+          return false;
+        } else {
+        messageErrorFirstName.innerText = "";
+        return true;
+        }
+      });  
+    };
+    validateFirstName();
 
-// close thanks page 
+    // valider le name //
 
-modalCloseThanks.onclick = function (){
-  modalThanks.style.display = "none";
-  modalBody.style.display = "block";
-}
+    function validateName(){
+      var name = document.querySelector("#last");
+      const messageErrorName = document.querySelector("#errorName");
+      name.addEventListener("input", function(){
+        if(name.value === "" || name.value.length < 2){
+          messageErrorName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+          messageErrorName.style.color ="red";
+          return false;
+        } else {
+        messageErrorName.innerText = "";
+        return true;
+        }
+      });  
+    };
+    validateName();
+
+    // valider l'email //
+
+    function validateEmail(){
+      var email = document.querySelector("#email");
+      const messageErrorEmail = document.querySelector("#errorEmail");
+      email.addEventListener("input", function(){
+        if(!email.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/) || email.value === ""){
+          messageErrorEmail.innerText = "Veuillez entrer un email valide.";
+          messageErrorEmail.style.color ="red";
+          return false;
+        } else {
+        messageErrorEmail.innerText = "";
+        return true;
+        }
+      });  
+    };
+    validateEmail();
+
+    // valider une option //
+    function validateCheckbox(){
+      const radioButtons = document.querySelectorAll('input[name="location"]');
+      for(const radioButton of radioButtons){
+        radioButton.addEventListener('change', showSelected);
+      }        
+            
+      function showSelected() {
+        if (!this.checked) {
+          document.querySelector('#errorCheckbox').innerText = "Vous devez choisir une option.";
+          return false;
+        }else{
+          return true;
+        }
+      }
+    };
+    validateCheckbox();
+
+    function validateCheckboxTerms(){
+      const radioButtons = document.querySelector('#checkbox1');
+        radioButtons.addEventListener('change', function(){
+          if (!this.checked) {
+            const messageErrorTerms = document.querySelector('#errorCheckboxTerms')
+            messageErrorTerms.innerText = "Vous devez vérifier que vous acceptez les termes et conditions.";
+            messageErrorTerms.style.color = "red";
+            return false;
+          }else{
+            document.querySelector('#errorCheckboxTerms').innerText = "";
+            return true;
+          }
+
+        }); 
+      };      
+    validateCheckboxTerms();
+
+
+
+
+
+
+
+  
