42148d275e79edc66020676c683f5ab0d09b4f4b
rachon34 <rachon3441@gmail.com>
2023-03-16 14:43:00 +0100

    modification border bottom lien navigation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,21 +10,36 @@ function editNav() {
 // DOM Elements
 
 const modalbg = document.querySelector(".bground");
+const modalSubmissionOk = document.querySelector(".confirm-submission")
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelector(".close");
+const btnsubmit = document.querySelector(".btn-submit");
 
-// launch modal event
+// launch modal eventListener
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-//console.log(btn)
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
   
 }
-modalbg.addEventListener("click", closeForm);
 
+//Close modal eventListener
+closeModal.addEventListener("click", closeForm);
+
+//Close modal form
 function closeForm() {
   modalbg.style.display= "none";
 }
+
+//Submit btn Listener
+btnsubmit .addEventListener("submit", submitData);
+
+//Submit data function
+//  async function submitData(){
+
+//Function de validation des données des champs input
+
+
 
