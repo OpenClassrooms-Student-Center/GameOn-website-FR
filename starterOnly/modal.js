5e38f92242b99aa6c8998b69eb2f078c3951c26e
rachon34 <rachon3441@gmail.com>
2023-03-18 04:54:58 +0100

    rectification des messages d'erreur en utilisant data-error

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,14 +7,15 @@ function editNav() {
   }
 }
 
-const errorBorder = "1px solid red"
+const errorBorder = "1px solid red";
 
 // Éléments DOM
 
 const modalbg = document.querySelector(".bground");
-const modalSubmissionMsg = document.querySelector(".confirm-msg") // Message de confirmation de soumission du formulaire dans la modale
+const innermodalBody = document.querySelector(".modal-body");  // Corps de la modale
+
 const modalBtn = document.querySelectorAll(".modal-btn"); // Bouton d'ouverture de la modale
-const innermodalBody = document.querySelector(".modal-body")  // Corps de la modale
+
 const regexpEmail = new RegExp(/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/)    // Expression régulière pour la validation de l'adresse e-mail
 const closeModal = document.querySelector(".close", "modal-btn-close"); // Bouton de fermeture de la modale
 const btnsubmit = document.querySelector(".btn-submit");  // Bouton d'envoi du formulaire
@@ -23,33 +24,35 @@ const formLastName = document.forms["reserve"]["last"]; // Champ input nom
 const formEmail = document.forms["reserve"]["email"]; // Champ input e-mail
 const formBirthDate = document.forms["reserve"]["birthdate"]; // Champ input date de naissance
 const formQuantity = document.forms["reserve"]["quantity"]; // Champ input nombre de tournois
-const formfieldsObjects = [ // Tableau d'objets contenant les champs input du formulaire
+const formTerms = document.forms["reserve"]["terms"];
+
+const formfieldsObjects = [
   {
-    "formfield": formName,  // Champ input prénom
-    "condition": formName.value == "" || formName.value.length < 2,  // Condition de validation du champ input
-    "message": 'Mettez votre prénom.' // Message d'erreur
+    formfield: formName,
+    condition: () => formName.value === "" || formName.value.length < 2,
+    message: "Mettez votre prénom."
   },
   {
-    "formfield": formQuantity,
-    "condition": formQuantity.value == "",
-    "message": 'Merci de compléter le formulaire avec le nombre de participation à nos tournois.'
+    formfield: formQuantity,
+    condition: () => formQuantity.value === "",
+    message: "Merci de compléter le formulaire avec le nombre de participation à nos tournois."
   },
   {
-    "formfield": formEmail,
-    "condition": !regexpEmail.test(formEmail.value),
-    "message": 'Mettez une adresse e-mail valide.'
+    formfield: formEmail,
+    condition: () => !regexpEmail.test(formEmail.value),
+    message: "Mettez une adresse e-mail valide."
   },
   {
-    "formfield": formLastName,
-    "condition": formLastName.value == "" || formLastName.value.length < 2,
-    "message": 'Veuillez entrer 2 caractères ou plus pour le champ du nom.'
+    formfield: formLastName,
+    condition: () => formLastName.value === "" || formLastName.value.length < 2,
+    message: "Veuillez entrer 2 caractères ou plus pour le champ du nom."
   },
   {
-    "formfield": formBirthDate,
-    "condition": formBirthDate.value !== true,
-    "message": 'Vous devez entrer votre date de naissance.'
+    formfield: formBirthDate,
+    condition: () => formBirthDate.value === "",
+    message: "Veuillez entrer votre date de naissance."
   }
-]
+];
 
 //////////////////////////////////////////////// GESTION MODALE //////////////////////////////////////////
 
@@ -76,35 +79,47 @@ function closeForm() {
 //// SOUMISSION DE LA MODALE
 
 // Événement d'envoi du formulaire
-//document.forms["reserve"].addEventListener("submit", confirmValidation);
+document.forms["reserve"].addEventListener("submit", confirmValidation);
+// Vérification de la modale
+document.forms["reserve"].addEventListener(
+  "submit", 
+  function (event) {
+    event.preventDefault();
+  }
+);
 
+// Fonction de confirmation de la modale
+// Fonction de confirmation de la modale
 function confirmValidation() {
-  
-    innermodalBody.innerHTML= "";
-    innermodalBody.innerHTML = `<p class="confirm-msg">Merci ! Votre réservation a été reçue.</p>`;
-  
+  if (validate()) {
+    innermodalBody.getElementsByTagName("form")[0].style.display = "none";
+    modalSubmissionDiv.style.display = "block";
+  }
 }
+
+
+//////////////////////////////////////////////// GESTION DU FORMULAIRE //////////////////////////////////////////
+
+let formIsTrue = true;
+
 
 // Fonction de validation des données des champs input
 function validate() {
- 
-  formIsTrue = true;
-  
-  for (let i = 0; i < formfieldsObjects.length; i++) {  // Boucle sur le tableau d'objets contenant les champs input du formulaire
-    // if (formIsTrue === false) {
-    //   formfieldsObjects[i].formfield.nextElementSibling.innerHTML = "";
-    // }
-    if (formfieldsObjects[i].condition) {  // Si la condition de validation du champ input est fausse         
-       formfieldsObjects[i].formfield.style.border = errorBorder;
-       formfieldsObjects[i].formfield.insertAdjacentHTML("afterend", `<p id="error" >${formfieldsObjects[i].message}</p>`);
-       formfieldsObjects[i].formfield.focus();
-       formIsTrue = false;
-     }
-  };
-  if (formIsTrue == false) {
-   return false
+  let formIsTrue = true;
+
+  for (let i = 0; i < formfieldsObjects.length; i++) {
+    let condition = formfieldsObjects[i].condition();
+    let message = formfieldsObjects[i].message;
+
+    if (condition) {
+      formfieldsObjects[i].formfield.parentElement.setAttribute("data-error", message);
+      formfieldsObjects[i].formfield.parentElement.setAttribute("data-error-visible", "true");
+      formfieldsObjects[i].formfield.focus();
+      formIsTrue = false;
+    } else {
+      formfieldsObjects[i].formfield.parentElement.removeAttribute("data-error");
+      formfieldsObjects[i].formfield.parentElement.setAttribute("data-error-visible", "false");
+    }
   }
-  else {
-   return true
-  }
+  return formIsTrue;
 }
