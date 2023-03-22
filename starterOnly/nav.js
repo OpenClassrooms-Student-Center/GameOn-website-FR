22af19f2f1d002537061cd215ca0bbd4359d3e15
rachon34 <rachon3441@gmail.com>
2023-03-22 16:43:32 +0100

    rectification menu text et finalisation editnav.js

--- starterOnly/nav.js
+++ starterOnly/nav.js
@@ -1,6 +1,11 @@
+//Récupération de l'eventlistener pour editer le hamburger responsive
+const iconNav = document.getElementById("IconNavBar");
+iconNav.addEventListener("click", editNav);
+
+
 function editNav() {
     var NavBar = document.getElementById("myTopnav");
-    var iconNavBar = document.getElementById("IconNavBar::active");
+    let iconNavBar = document.getElementById("IconNavBar::active");
     if (NavBar.className === "topnav") {
       NavBar.className += " responsive";
       iconNavBar.style.color = "white";
