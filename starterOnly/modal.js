c3d73d9f96e039386f7bda2cac2e81ee990e75a5
citizensyd <59608029+citizensyd@users.noreply.github.com>
2023-03-31 13:42:48 +0200

    Fix #3 - Add validation or error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -37,7 +37,12 @@ const regexNumberCompetitions = new RegExp("^[0-9]+$");
 const first = document.getElementById("first");
 const last = document.getElementById("last");
 const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
 const numberCompetitions = document.getElementById("quantity");
+const townTournament = document.querySelectorAll('input[type="radio"]');
+const townTournamentId = document.getElementById('location1');
+const checkBoxUse = document.getElementById("checkbox1");
+const form = document.querySelector("form");
 
 // declaration of variables to validate each entry
 let isFirstValid = false;
@@ -51,19 +56,34 @@ let useConditions = false;
 // fist name entry test
 first.addEventListener("input", function (event) {
   const text = event.target.value.trim();
-  isFirstValid = regexFirst.test(text);
+  if (!regexFirst.test(text)) {
+    first.parentElement.setAttribute('data-error-visible', 'true');
+  } else {
+    isFirstValid = true;
+    first.parentElement.setAttribute('data-error-visible', 'false');
+  }
 });
 
 // last name entry test
 last.addEventListener("input", function (event) {
   const text = event.target.value.trim();
-  isLastValid = regexLast.test(text);
+  if (!regexLast.test(text)) {
+    last.parentElement.setAttribute('data-error-visible', 'true');
+  } else {
+    isLastValid = true;
+    last.parentElement.setAttribute('data-error-visible', 'false');
+  }
 });
 
 // email entry test
 email.addEventListener("input", function (event) {
   const text = event.target.value.trim();
-  isEmailValid = regexEmail.test(text);
+  if (!regexEmail.test(text)) {
+    email.parentElement.setAttribute('data-error-visible', 'true');
+  } else {
+    isEmailValid = true;
+    email.parentElement.setAttribute('data-error-visible', 'false');
+  }
 });
 
 // Check that the anniversary date is valid
@@ -85,7 +105,6 @@ numberCompetitions.addEventListener("input", function (event) {
 });
 
 // town radio which entry test tournament
-const townTournament = document.querySelectorAll('input[type="radio"]');
 townTournament.forEach(function (town) {
   town.addEventListener("click", function () {
     if (town.checked) {
@@ -95,11 +114,13 @@ townTournament.forEach(function (town) {
 });
 
 // check that the box for compliance with the terms of use is checked
-const checkBoxUse = document.getElementById("checkbox1");
 useConditions = checkBoxUse.checked;
 checkBoxUse.addEventListener("click", function () {
   if (checkBoxUse.checked) {
     useConditions = true;
+  }
+  else {
+    useConditions = false;
   }
 });
 
@@ -118,8 +139,23 @@ function validate() {
 
 // sending the form
 function submitForm() {
+  validateCase();
   var form = document.forms["reserve"];
   if (validate()) {
     form.submit();
+  }
+}
+
+// Check the validity of radio, date and checkbox inputs
+function validateCase() {
+  const valuesToValidate = [isFirstValid, isLastValid, isEmailValid, isBirthdateValid, isNumberCompetitionsValid, whatTownChecked, useConditions];
+  const elements = [first, last, email, birthdate, numberCompetitions, townTournamentId, checkBoxUse];
+  
+  for (let i = 0; i < valuesToValidate.length; i++) {
+    if (!valuesToValidate[i]) {
+      elements[i].parentElement.setAttribute('data-error-visible', 'true');
+    } else {
+      elements[i].parentElement.setAttribute('data-error-visible', 'false');
+    }
   }
 }
