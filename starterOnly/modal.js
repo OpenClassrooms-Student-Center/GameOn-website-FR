9effc0fd48f65187071af91fd4836d772e7eaca7
citizensyd <59608029+citizensyd@users.noreply.github.com>
2023-04-06 21:57:54 +0200

    Fix #3 -Add validation or error messages description de la correction

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// Function to handle navigation menu
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -11,151 +12,236 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBody = document.querySelector(".modal-body");
 
-// launch modal event
+// Event listener to launch modal form
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Function to show modal window
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// list of regex
-// Firstname
-const regexFirst = new RegExp("^.+\\S{2,}$");
+// declaration of variables to validate each entry
+let formFields = {
+  isFirstValid: false,
+  isLastValid: false,
+  isEmailValid: false,
+  isBirthdateValid: false,
+  isNumberCompetitionsValid: false,
+  whatTownChecked: false,
+  useConditions: false,
+};
 
-// Lastname
-const regexLast = new RegExp("^.+\\S{2,}$");
-
-// email
-const regexEmail = new RegExp("^\\S+@\\S+\\.\\S+$");
-
-// Number of competitions
-const regexNumberCompetitions = new RegExp("^[0-9]+$");
-
-// declaration of the variables containing the value of the inputs
-const first = document.getElementById("first");
-const last = document.getElementById("last");
-const email = document.getElementById("email");
-const birthdate = document.getElementById("birthdate");
-const numberCompetitions = document.getElementById("quantity");
-const townTournament = document.querySelectorAll('input[type="radio"]');
-const townTournamentId = document.getElementById('location1');
-const checkBoxUse = document.getElementById("checkbox1");
 const form = document.querySelector("form");
 
-// declaration of variables to validate each entry
-let isFirstValid = false;
-let isLastValid = false;
-let isEmailValid = false;
-let isBirthdateValid = false;
-let isNumberCompetitionsValid = false;
-let whatTownChecked = false;
-let useConditions = false;
-
-// fist name entry test
-first.addEventListener("input", function (event) {
-  const text = event.target.value.trim();
-  if (!regexFirst.test(text)) {
-    first.parentElement.setAttribute('data-error-visible', 'true');
-  } else {
-    isFirstValid = true;
-    first.parentElement.setAttribute('data-error-visible', 'false');
-  }
-});
-
-// last name entry test
-last.addEventListener("input", function (event) {
-  const text = event.target.value.trim();
-  if (!regexLast.test(text)) {
-    last.parentElement.setAttribute('data-error-visible', 'true');
-  } else {
-    isLastValid = true;
-    last.parentElement.setAttribute('data-error-visible', 'false');
-  }
-});
-
-// email entry test
-email.addEventListener("input", function (event) {
-  const text = event.target.value.trim();
-  if (!regexEmail.test(text)) {
-    email.parentElement.setAttribute('data-error-visible', 'true');
-  } else {
-    isEmailValid = true;
-    email.parentElement.setAttribute('data-error-visible', 'false');
-  }
-});
-
-// Check that the anniversary date is valid
-function checkDateValidity() {
-  const birthdateInput = document.getElementById("birthdate");
-  birthdateInput.addEventListener("input", function () {
-    const dateString = birthdateInput.value;
-    if (dateString !== "") {
-      isBirthdateValid = true;
-    }
+// Listen to input event on form
+const inputFormListening = () => {
+  form.addEventListener("input", (event) => {
+    const target = event.target;
+    console.log(target);
+    target.matches("[required][name='first']")
+      ? validateField(target, ["required", "first"])
+      : target.matches("[required][name='last']")
+      ? validateField(target, ["required", "last"])
+      : target.matches("[required][name='email']")
+      ? validateField(target, ["required", "email"])
+      : target.matches("[required][name='birthdate']")
+      ? validateField(target, ["required", "birthdate"])
+      : target.matches("[required][name='quantity']")
+      ? validateField(target, ["required", "quantity"])
+      : target.matches("[name='location']")
+      ? validateField(target, ["location"])
+      : target.matches("[name='agreement1']")
+      ? validateField(target, ["agreement1"])
+      : null;
   });
-}
-checkDateValidity();
+};
+inputFormListening();
+
+// Display error message for invalid input
+const displayErrorMessage = (field, message) => {
+  console.log(field);
+  const errorMessageSpan = field.closest(".formData").querySelector(".data-error");  
+  console.log(errorMessageSpan);
+  const errorTextfield = field.parentNode.querySelector(".text-control");
+  errorMessageSpan.textContent = message;
+  errorMessageSpan.classList.add("data-error-visible");
+  errorTextfield !== null
+    ? errorTextfield.classList.add("text-control-boder-red")
+    : null;
+};
+
+// Remove error message when input is valid
+const removeErrorMessage = (field) => {
+  const errorMessageSpan = field.closest(".formData").querySelector(".data-error"); 
+  const errorTextfield = field.parentNode.querySelector(".text-control");
+  errorMessageSpan.textContent = "";
+  errorMessageSpan.classList.remove("data-error-visible");
+  errorTextfield !== null
+    ? errorTextfield.classList.remove("text-control-boder-red")
+    : null;
+};
+
+// Validate required field
+const validateRequired = (value) => {
+  return value.trim() !== "";
+};
+
+// Validate name input
+const validateName = (value) => {
+  const nameRegex = /^.+\S{2,}$/;
+  return nameRegex.test(value);
+};
+
+// Validate email input
+const validateEmail = (value) => {
+  const emailRegex = /^[\w\.-]+@[\w-]+\.[\w\.-]+$/;
+  return emailRegex.test(value);
+};
+
+// Validate birthdate input
+const validateBirthday = (value) => {
+  const regex = /^(19|20)\d{2}\-(0[1-9]|1[0-2])\-(0[1-9]|[12][0-9]|3[01])$/;
+  return !regex.test(value)
+    ? false
+    : new Date(value) > Date.now()
+    ? false
+    : true;
+};
 
 // number of competitions entry test
-numberCompetitions.addEventListener("input", function (event) {
-  const text = event.target.value.trim();
-  isNumberCompetitionsValid = regexNumberCompetitions.test(text);
-});
+const validateNumberCompetitions = (value) => {
+  const regexNumberCompetitions = /^[0-9][0-9]?$/;
+  return regexNumberCompetitions.test(value);
+};
 
-// town radio which entry test tournament
-townTournament.forEach(function (town) {
-  town.addEventListener("click", function () {
-    if (town.checked) {
-      whatTownChecked = true;
-    }
-  });
-});
-
-// check that the box for compliance with the terms of use is checked
-useConditions = checkBoxUse.checked;
-checkBoxUse.addEventListener("click", function () {
-  if (checkBoxUse.checked) {
-    useConditions = true;
-  }
-  else {
-    useConditions = false;
-  }
-});
-
-// Check that each box is filled in correctly before validate
-function validate() {
-  return (
-    isFirstValid &&
-    isLastValid &&
-    isEmailValid &&
-    isBirthdateValid &&
-    isNumberCompetitionsValid &&
-    whatTownChecked &&
-    useConditions
+// Checkbox town of competitions
+const validateTownTournament = () => {
+  const selectRadioTownTournament = document.querySelectorAll(
+    'input[type="radio"][name="location"]'
   );
-}
+  const isChecked = Array.from(selectRadioTownTournament).some(
+    (town) => town.checked
+  );
+  return isChecked
+    ? (formFields.whatTownChecked = true)
+    : (displayErrorMessage(selectRadioTownTournament[0],"Vous devez sélectionner un tournoi auqel vous souhaitez participer"));
+};
 
-// sending the form
-function submitForm() {
-  validateCase();
-  var form = document.forms["reserve"];
-  if (validate()) {
+
+// Validate use conditions checkbox
+const validateCheckBoxUse = () => {
+  const selectCheckbox = document.querySelector(
+    'input[type="checkbox"][name="agreement1"]'
+  );
+  const isValid = selectCheckbox.checked
+    ? true
+    : (displayErrorMessage(
+        selectCheckbox,
+        "Vous devez accepter les conditions d'utilisations."
+      ),
+      false);
+  return isValid;
+};
+
+// Objects for rules and error messages
+const validationRules = {
+  required: {
+    validator: validateRequired,
+    errorMessage: "Ce champ est requis",
+    formFieldKey: null,
+  },
+  first: {
+    validator: validateName,
+    errorMessage:
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
+    formFieldKey: "isFirstValid",
+  },
+  last: {
+    validator: validateName,
+    errorMessage: "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+    formFieldKey: "isLastValid",
+  },
+  email: {
+    validator: validateEmail,
+    errorMessage: "Veuillez entrer une adresse email valide.",
+    formFieldKey: "isEmailValid",
+  },
+  birthdate: {
+    validator: validateBirthday,
+    errorMessage: "Vous devez entrer votre date de naissance.",
+    formFieldKey: "isBirthdateValid",
+  },
+  quantity: {
+    validator: validateNumberCompetitions,
+    errorMessage:
+      "Le nombre de tournoi (entre 1 et 99) auquel vous avez participé cette année.",
+    formFieldKey: "isNumberCompetitionsValid",
+  },
+  location: {
+    validator: () => true,
+    formFieldKey: "whatTownChecked",
+  },
+  agreement1: {
+    validator: validateCheckBoxUse,
+    errorMessage: "Veuillez accepter les conditions d'utilisation.",
+    formFieldKey: "useConditions",
+  },
+};
+
+// Validate each field according to its rules
+const validateField = (field, rules) => {
+  for (let rule of rules) {
+    const { validator, errorMessage, formFieldKey } = validationRules[rule];
+    if (!validator(field.value)) {
+      if (errorMessage) {
+        displayErrorMessage(field, errorMessage);
+      }
+      if (formFieldKey) {
+        formFields[formFieldKey] = false;
+      }
+      return false;
+    } else {
+      removeErrorMessage(field);
+      if (formFieldKey) {
+        formFields[formFieldKey] = true;
+      }
+    }
+  }
+  return true;
+};
+
+// Validate all required fields
+const requiredValidate = () => {
+  let isValid = true;
+  const selectFieldsValidation = document.querySelectorAll("input[required]");
+  selectFieldsValidation.forEach((inputField) => {
+    const value = inputField.value.trim();
+    !validateRequired(value)
+      ? (displayErrorMessage(inputField, "Ce champ est requis"),
+        (isValid = false))
+      : null;
+  });
+  validateTownTournament();
+  validateCheckBoxUse();
+
+  return isValid;
+};
+
+// Sending the form
+const submitForm = () => {
+  const form = document.forms["reserve"];
+  if (areAllValid === true) {
     form.submit();
   }
-}
+};
 
-// Check the validity of radio, date and checkbox inputs
-function validateCase() {
-  const valuesToValidate = [isFirstValid, isLastValid, isEmailValid, isBirthdateValid, isNumberCompetitionsValid, whatTownChecked, useConditions];
-  const elements = [first, last, email, birthdate, numberCompetitions, townTournamentId, checkBoxUse];
-  
-  for (let i = 0; i < valuesToValidate.length; i++) {
-    if (!valuesToValidate[i]) {
-      elements[i].parentElement.setAttribute('data-error-visible', 'true');
-    } else {
-      elements[i].parentElement.setAttribute('data-error-visible', 'false');
-    }
+// Event listener on form submission
+form.addEventListener("submit", (event) => {
+  let areAllValid = Object.values(formFields).every((value) => value === true);
+  event.preventDefault();
+  if (requiredValidate() && areAllValid === true) {
+    submitForm();
   }
-}
+});
