6437cc728239af0f4335e62bea73dc6fd5384534
Rachel Barthélery <rachel-barth@live.fr>
2023-04-07 23:17:56 +0200

    Fix Issues 2 (validation des entrées)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,13 +12,21 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
+const firstName = document.querySelector('.formData #first');
+const lastName = document.querySelector('.formData #last');
+const email = document.querySelector('.formData #email');
+const birthdate = document.querySelector('.formData #birthdate');
+const quantity = document.querySelector('.formData #quantity');
+const conditions = document.querySelector('.formData #checkbox1');
+const locationRadio = document.getElementsByName('location');
+const buttonSubmit = document.getElementById('buttonSubmit');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "block";  
 }
 
 // close modal event
@@ -28,3 +36,127 @@ closeBtn.addEventListener('click', closeModal);
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+//firstname entries validation event
+firstName.addEventListener('change', validateFirstName);
+
+//function to validate the firstname
+function validateFirstName() {
+  let validationFirstBool = true;
+  if(firstName.value.length <= 2 || firstName.value.match(/^\s*$/g) || firstName.value.match('[0-9]')) {
+    validationFirstBool = false;
+  } else {
+    validationFirstBool = true;
+  }
+
+  return validationFirstBool;
+}
+
+//lastname entries validate event
+lastName.addEventListener('change', validateLastName);
+
+//function to validate the lastname
+function validateLastName(){
+  let validationLastNameBool = true;
+  if(lastName.value.length <= 2 || lastName.value.match(/^\s*$/g) || lastName.value.match('[0-9]')) {
+    validationLastNameBool = false;
+  } else {
+    validationLastNameBool = true;
+  }
+
+  return validationLastNameBool;
+}
+
+//email entries validate event
+email.addEventListener('change', validateEmail);
+
+//function to validate the email address
+function validateEmail(){
+  let validationEmailBool = true;
+  if(!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) || email.value.match(/^\s*$/g)) {
+    validationEmailBool = false;
+  } else {
+    validationEmailBool = true;
+  }
+
+  return validationEmailBool;
+}
+
+//birthdate entries validate event
+birthdate.addEventListener('change', validateBirthdate);
+
+//function to validate the birthdate
+function validateBirthdate() {
+  let enterDate = birthdate.value.split('-').reverse().join('')
+  let today = new Date().toLocaleDateString().split('/').join('');
+  let validationBirthdateBool = true;
+
+  if(enterDate === today || birthdate.value === ""){
+    validationBirthdateBool = false;
+  } else {
+    validationBirthdateBool = true;
+  }
+
+  return validationBirthdateBool;  
+}
+
+//quantity entires validate event
+quantity.addEventListener('change', validateQuantity);
+
+//function to validate the quantity of martahon already be
+function validateQuantity(){
+  let validationQuantity = true;
+
+  if(quantity.value === ""){
+    validationQuantity = false;
+  } else {
+    validationQuantity = true;
+  }
+
+  return validationQuantity;
+}
+
+//conditions button validate event
+conditions.addEventListener('change', validateConditions);
+
+//function to validate if conditions checkbox if checked or not
+function validateConditions() {
+  let validationConditionsBool = true;
+
+  if(!conditions.checked){
+    validationConditionsBool = false;
+  } else {
+    validationConditionsBool = true;
+  }
+
+  return validationConditionsBool;
+}
+
+//function to verify if location radio is checked
+function validateLocation() {
+  let oneIsCheck = false;
+  let whichIsChecked = "";
+  for(let i = 0 ; i < locationRadio.length ; i++){
+    if(locationRadio[i].checked === true) {
+      oneIsCheck = true;
+      whichIsChecked = locationRadio[i].value;
+    }
+  }
+
+  return [oneIsCheck, whichIsChecked];
+}
+
+buttonSubmit.addEventListener('click', validateAllData);
+
+function validate(){
+  let verifyFirst = validateFirstName();
+  let verifyLast = validateLastName();
+  let verifyEmail = validateEmail();
+  let verifyBirthdate = validateBirthdate();
+  let verifyQuantity = validateQuantity();
+  let verifyConditions = validateConditions();
+  let verifyLocation = validateLocation();
+
+  return verifyFirst && verifyLast && verifyEmail && verifyBirthdate && verifyQuantity && verifyConditions && verifyLocation[0];
+}
+
