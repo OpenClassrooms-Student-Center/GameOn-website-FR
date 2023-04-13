ea9c6c15f921dd866f61f51248ebf4e8537de6c2
Rachel Barthélery <43150113+barthRachel@users.noreply.github.com>
2023-04-13 19:50:06 +0200

    Factorisation du code + test finaux

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,14 +12,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
-const firstName = document.querySelector('.formData #first');
-const lastName = document.querySelector('.formData #last');
-const email = document.querySelector('.formData #email');
-const birthdate = document.querySelector('.formData #birthdate');
-const quantity = document.querySelector('.formData #quantity');
-const conditions = document.querySelector('.formData #checkbox1');
-const locationRadio = document.getElementsByName('location');
-const buttonSubmit = document.getElementById('buttonSubmit');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -37,129 +29,76 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+//generic function to display a validation message according to a condition
+function validateAttribut(condition, querySelector){
+  if(condition) {
+    querySelector.setAttribute('data-error-visible', true)
+  } else {
+    querySelector.setAttribute('data-error-visible', false)
+  }
+
+  return !condition
+}
+
 //firstname validation event
-firstName.addEventListener('change', validateFirstName);
+document.querySelector('#first').addEventListener('change', validateFirstName);
 
 //firstname validation function
 function validateFirstName() {
-  let validationFirstBool = true;
-  if(firstName.value.length <= 2 || firstName.value.match(/^\s*$/g) || firstName.value.match('[0-9]')) {
-    validationFirstBool = false;
-    document.querySelector('.firstname').setAttribute('data-error-visible', true);
-  } else {
-    validationFirstBool = true;
-    document.querySelector('.firstname').setAttribute('data-error-visible', false);
-  }
-
-  return validationFirstBool;
+  return validateAttribut(document.querySelector('#first').value.length <= 2 || document.querySelector('#first').value.match(/^\s*$/g) || document.querySelector('#first').value.match('[0-9]'), document.querySelector('.formData.firstname'));
 }
 
 //lastname validation event
-lastName.addEventListener('change', validateLastName);
+document.querySelector('#last').addEventListener('change', validateLastName);
 
 //lastname validation function
 function validateLastName(){
-  let validationLastNameBool = true;
-  if(lastName.value.length <= 2 || lastName.value.match(/^\s*$/g) || lastName.value.match('[0-9]')) {
-    validationLastNameBool = false;
-    document.querySelector('.lastname').setAttribute('data-error-visible', true);
-  } else {
-    validationLastNameBool = true;
-    document.querySelector('.lastname').setAttribute('data-error-visible', false);
-  }
-
-  return validationLastNameBool;
+  return validateAttribut(document.querySelector('#last').value.length <= 2 || document.querySelector('#last').value.match(/^\s*$/g) || document.querySelector('#last').value.match('[0-9]'), document.querySelector('.formData.lastname'));
 }
 
 //email validation event
-email.addEventListener('change', validateEmail);
+document.querySelector('#email').addEventListener('change', validateEmail);
 
 //email validation function
 function validateEmail(){
-  let validationEmailBool = true;
-  if(!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) || email.value.match(/^\s*$/g)) {
-    validationEmailBool = false;
-    document.querySelector('.email').setAttribute('data-error-visible', true);
-  } else {
-    validationEmailBool = true;
-    document.querySelector('.email').setAttribute('data-error-visible', false);
-  }
-
-  return validationEmailBool;
+  return validateAttribut(!document.querySelector('#email').value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) || document.querySelector('#email').value.match(/^\s*$/g), document.querySelector('.formData.email'));
 }
 
 //birthdate validation event
-birthdate.addEventListener('change', validateBirthdate);
+document.querySelector('#birthdate').addEventListener('change', validateBirthdate);
 
 //birthdate validation function
 function validateBirthdate() {
-  let enterDate = birthdate.value.split('-').reverse().join('')
-  let today = new Date().toLocaleDateString().split('/').join('');
-  let validationBirthdateBool = true;
-
-  if(enterDate === today || birthdate.value === ""){
-    validationBirthdateBool = false;
-    document.querySelector('.birthdate').setAttribute('data-error-visible', true);
-  } else {
-    validationBirthdateBool = true;
-    document.querySelector('.birthdate').setAttribute('data-error-visible', false);
-  }
-
-  return validationBirthdateBool;  
+  return validateAttribut(document.querySelector('#birthdate').value.split('-').reverse().join('') === new Date().toLocaleDateString().split('/').join('') || document.querySelector('#birthdate').value === "", document.querySelector('.formData.birthdate'));
 }
 
 //quantity validation event
-quantity.addEventListener('change', validateQuantity);
+document.querySelector('#quantity').addEventListener('change', validateQuantity);
 
 //quantity validation function
 function validateQuantity(){
-  let validationQuantity = true;
-
-  if(quantity.value === ""){
-    validationQuantity = false;
-    document.querySelector('.formData.quantity').setAttribute('data-error-visible', true);
-  } else {
-    validationQuantity = true;
-    document.querySelector('.formData.quantity').setAttribute('data-error-visible', false);
-  }
-
-  return validationQuantity;
-}
-
-//conditions checkbox validation event
-conditions.addEventListener('change', validateConditions);
-
-//conditions checkbox validation function
-function validateConditions() {
-  let validationConditionsBool = true;
-
-  if(!conditions.checked){
-    validationConditionsBool = false;
-    document.querySelector('.conditions').setAttribute('data-error-visible', true);
-  } else {
-    validationConditionsBool = true;
-    document.querySelector('.conditions').setAttribute('data-error-visible', false);
-  }
-
-  return validationConditionsBool;
+  return validateAttribut(document.querySelector('#quantity').value === "", document.querySelector('.formData.quantity'));
 }
 
 //location radio validation function
 function validateLocation() {
+  const locationRadio = document.getElementsByName('location');
   let oneIsCheck = false;
   for(let i = 0 ; i < locationRadio.length ; i++){
     if(locationRadio[i].checked === true) {
       oneIsCheck = true;
     }
-
-    if(oneIsCheck) {
-      document.querySelector('.location').setAttribute('data-error-visible', false);
-    } else {
-      document.querySelector('.location').setAttribute('data-error-visible', true);
-    }
   }
 
-  return oneIsCheck;
+  return validateAttribut(!oneIsCheck, document.querySelector('.formData.location'));
+}
+
+//conditions checkbox validation event
+document.querySelector('#checkbox1').addEventListener('change', validateConditions);
+
+//conditions checkbox validation function
+function validateConditions() {
+  return validateAttribut(!document.querySelector('#checkbox1').checked, document.querySelector('.formData.conditions'));
 }
 
 //function that will check all infos when the submit button is clicked
@@ -172,9 +111,7 @@ function validate(){
   let verifyConditions = validateConditions();
   let verifyLocation = validateLocation();
 
-  let verifyAll = verifyFirst && verifyLast && verifyEmail && verifyBirthdate && verifyQuantity && verifyConditions && verifyLocation;
-
-  return verifyAll;
+  return verifyFirst && verifyLast && verifyEmail && verifyBirthdate && verifyQuantity && verifyConditions && verifyLocation;
 }
 
 //show confirmation message function
@@ -185,7 +122,7 @@ function validationMessage(){
   let confirmationMessageBloc = document.createElement('div');
   confirmationMessageBloc.classList.add('confimationMessageBloc')
   confirmationMessageBloc.innerHTML = `
-    <p class=""> Merci pour<br>votre inscription </p>
+    <p> Merci pour<br>votre inscription </p>
     <button class="btn-signup modal-btn close-confirmation-btn">
       Fermer
     </button>
