c3bbe81c81661496098d1164adb96eed93d8f56f
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-04-17 15:27:42 +0200

    Issue#3 - Ajout validation ou messages d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,6 +1,6 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
+    var x = document.getElementById("myTopnav");
     x.className += " responsive";
   } else {
     x.className = "topnav";
@@ -11,6 +11,11 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.querySelector("form");
+const submit = document.querySelector(".btn-submit");
+const firstDiv = form.first.parentElement;
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,10 +24,6 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
-// -------------------------------            Issue #1              ----------------------------------//
-// ------------  close modal when click on span class "close" by changing display mode  ---------------//
 
 // creating a function that changes the display value
 function closeModal() {
@@ -34,103 +35,113 @@ const closeBtn = document.querySelector(".close");
 closeBtn.addEventListener("click", closeModal);
 
 
+const formEntries = document.querySelectorAll(".formData");
+let isValid = {
+    "first": false,
+    "last": false,
+    "email": false,
+    "birthdate": false,
+    "quantity": false,
+    "location1": false,
+    "checkbox1": false
+}
 
+function validInputHighlight(input) {
+  input.style.border = "2px solid green";
+}
+function invalidInputHighlight(input) {
+  input.style.border = "2px solid red";
+}
 
-// -------------------------------            Issue #2              ----------------------------------//
-// ------------           form inputs must be valid when clicking on "Submit"           ---------------//
+formEntries.forEach(formEntry => {
+  const input = formEntry.querySelector("input");
+  const inputName = input.id;
+  const inputSpan = document.createElement("span");
+  let errorMessage = "";
 
-// Select all form entries
-const form = document.querySelector("form");
+  if (inputName !== "location1" && inputName !== "checkbox1") {
+    input.addEventListener("blur", function() {
+      switch (inputName) {
+        case "first":
+        case "last":
+          regex = /\w{2,}/;
+          errorMessage = "Le champ doit contenir au moins 2 caractères";
+          break;
+        case "email":
+          regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+          errorMessage = "L'adresse mail doit être valide";
+          break;
+        case "birthdate":
+          regex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
+          errorMessage = "doit contenir une date valide";
+          break;
+        case "quantity":
+          regex = /^0*?[0-9]\d*$/;
+          errorMessage = "Le champ doit contenir une valeur numérique";
+          break;
+      }
+      if (regex.test(input.value)) {
+        validInputHighlight(input);
+        inputSpan.innerText = "";
+        isValid[inputName] = true;
+      } else {
+        invalidInputHighlight(input);
+        isValid[inputName] = false;
+        inputSpan.innerText = errorMessage;
+        formEntry.appendChild(inputSpan);
+        inputSpan.classList.add("form-warning");
+      }
+    });
+    input.addEventListener("focus", function() {
+      inputSpan.innerText = "";
+    });
 
-// Select submit button
-const submit = document.querySelector(".btn-submit");
-
-// By default, disable Submit button
-submit.disabled = true;
-
-// Cancel default behaviour of Submit button
+  } else if (inputName === "location1") {
+    errorMessage = "Vous devez choisir une option."
+    const radioButtons = document.querySelectorAll("[type=radio]");
+    for (const btn of radioButtons) {
+        submit.addEventListener("click", function(event) {
+        if (btn.checked) {
+          isValid[inputName] = true;
+        } else {
+          inputSpan.innerText = errorMessage;
+          inputSpan.classList.add("form-warning");
+          formEntry.appendChild(inputSpan);
+        }
+        btn.addEventListener("change", function(event) {
+          if(this.checked) {
+            inputSpan.innerText = "";
+          }
+        })
+      })
+    }
+  } else if (inputName === "checkbox1") {
+    submit.addEventListener("click", function() {
+      if (input.checked) {
+        inputSpan.innerText = "";
+        isValid[inputName] = true;
+      } else {
+        isValid[inputName] = false;
+        errorMessage = "Vous devez vérifier que vous acceptez les termes et conditions.";
+        inputSpan.innerText = errorMessage;
+        formEntry.appendChild(inputSpan);
+        inputSpan.classList.add("form-warning");
+      }
+    })
+  }
+  input.addEventListener("focus", function() {
+    const inputSpan = document.createElement("span");
+    inputSpan.innerText = "";
+  })
+});
 
 submit.addEventListener("click", function(event) {
   event.preventDefault();
+  Object.entries(isValid).forEach(entry => {
+    console.log(entry[1]);
+    console.log(entry[0]);
+    if (!entry[1]) {
+      invalidInputHighlight(document.getElementById(entry[0]));
+    };
+  })
 })
-
-
-
-
-// ------------                       validates form entries                           ---------------//
-
-
-
-// function to validate "first" entry
-function validateFirst() {
-  const first = form.first;
-  let namesRegex = /\w{2,}/;
-  // checking if input value match the regex pattern, if so return true
-  return namesRegex.test(first.value);
-}
-// validateFirst();
-
-
-function validateLast() {
-  const last = form.last;
-  let namesRegex = /\w{2,}/;
-  return namesRegex.test(last.value);
-}
-
-function validateEmail() {
-  const email = form.email;
-  let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return emailRegex.test(email.value);
-}
-
-function validateBirthdate() {
-  const birthdate = form.birthdate;
-  let dateRegex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
-  return dateRegex.test(birthdate.value);
-}
-
-function validateQuantity() {
-  const quantity = form.quantity;
-  let quantityRegex = /^\d+$/;
-  return quantityRegex.test(quantity.value);
-}
-
-// function to validate if a location is checked
-function validateLocation() {
-  // selecting all the input of type radio
-  const locations = document.querySelectorAll("[type=radio]");
-  //looping on all radio buttons to check if one is checked
-  for (const location of locations) {
-    if (location.checked) {
-      // if so return true
-      return true;
-    }
-  }
-  return false;
-}
-
-function validateTOU() {
-  // selecting the checkbox input managing the terms of use
-  const terms = document.querySelector("#checkbox1");
-  // checking if this is checked, if so return true
-  return terms.checked;
-}
-
-function validateForm() {
-  validateFirst();
-  validateLast();
-  validateEmail();
-  validateBirthdate();
-  validateQuantity();
-  validateLocation();
-  validateTOU();
-  // select the "Submit button"
-  // disable button if all input are not valid
-  if (validateFirst() && validateLast() && validateEmail() && validateBirthdate() && validateQuantity() && validateLocation() && validateTOU()) {
-    submit.disabled = false;
-  } else {
-    submit.disabled = true;
-  }
-};
-
-form.addEventListener("input", validateForm);
