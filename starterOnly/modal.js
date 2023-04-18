86fca381f85bd83a54f418ec74c3c1b78d5e5ced
Astora59 <yaminmoulay12@gmail.com>
2023-04-18 16:23:17 +0200

    Close modal button added

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,11 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelector(".close");
+// getElementsByTagName ?
+
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +25,18 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+
+
+//close modal event
+
+closeModal.addEventListener("click", closeModalWindow)
+
+
+// close modal form
+
+
+function closeModalWindow() {
+  console.log ("zizi")
+  modalbg.style.display = "none";
+}
 
