e1d281114a122363c0c55534520bf99edb84a042
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-04-20 17:27:04 +0200

    issue #5

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,148 +13,189 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const form = document.querySelector("form");
 const submit = document.querySelector(".btn-submit");
-const firstDiv = form.first.parentElement;
-
 const confirmForm = document.querySelector(".confirm");
+const closeBtn = document.querySelectorAll(".close");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+let isTrue = [];
 
-// launch modal form
+let informationObject = {
+  "first": {
+    isValid: false,
+    regex: /\w{2,}/,
+    errorMessage: "Le champ doit contenir au moins 2 caractères"
+  },
+  "last": {
+    isValid: false,
+    regex: /\w{2,}/,
+    errorMessage: "Le champ doit contenir au moins 2 caractères"
+  },
+  "email": {
+    isValid: false,
+    regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    errorMessage: "L'adresse mail doit être valide"
+  },
+  "birthdate": {
+    isValid: false,
+    regex: /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/,
+    errorMessage: "doit contenir une date valide"
+  },
+  "quantity": {
+    isValid: false,
+    regex: /^0*?[0-9]\d*$/,
+    errorMessage: "Le champ doit contenir une valeur numérique"
+  },
+  "location1": {
+    isValid: false,
+    errorMessage: "Vous devez choisir une option."
+  },
+  "checkbox1": {
+    isValid: false,
+    errorMessage: "Vous devez vérifier que vous acceptez les termes et conditions."
+  }
+};
+
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// -------------------------------            Issue #1              ----------------------------------//
-// ------------  close modal when click on span class "close" by changing display mode  ---------------//
-
-// creating a function that changes the display value
 function closeModal() {
   modalbg.style.display = "none";
   confirmForm.style.display = "none";
 }
-// select the close button by className
-const closeBtn = document.querySelectorAll(".close");
-// on click, calling closeModal() function
-closeBtn.forEach(btn => {
-  btn.addEventListener("click", closeModal)
-});
 
-function launchConfirm() {
+function launchConfirmModal() {
   confirmForm.style.display = "block";
-}
-
-const formEntries = document.querySelectorAll(".formData");
-let isValid = {
-    "first": false,
-    "last": false,
-    "email": false,
-    "birthdate": false,
-    "quantity": false,
-    "location1": false,
-    "checkbox1": false
 }
 
 function validInputHighlight(input) {
   input.style.border = "2px solid green";
 }
+
 function invalidInputHighlight(input) {
   input.style.border = "2px solid red";
 }
-function emptyForm() {
+
+function resetForm() {
   form.reset();
+  const input = form.querySelectorAll("input");
+  input.forEach(input => {
+    input.style.border = "none";
+    input.checked = false;
+  })
+  for (const [key, value] of Object.entries(informationObject)) {
+    value.isValid = false
+  }
 }
 
-formEntries.forEach(formEntry => {
-  const input = formEntry.querySelector("input");
+function validateTextInput(input) {
   const inputName = input.id;
   const inputSpan = document.createElement("span");
   inputSpan.classList.add("form-warning");
-  let errorMessage = "";
 
-  if (inputName !== "location1" && inputName !== "checkbox1") {
-    input.addEventListener("input", function() {
-      switch (inputName) {
-        case "first":
-        case "last":
-          regex = /\w{2,}/;
-          errorMessage = "Le champ doit contenir au moins 2 caractères";
-          break;
-        case "email":
-          regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-          errorMessage = "L'adresse mail doit être valide";
-          break;
-        case "birthdate":
-          regex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
-          errorMessage = "doit contenir une date valide";
-          break;
-        case "quantity":
-          regex = /^0*?[0-9]\d*$/;
-          errorMessage = "Le champ doit contenir une valeur numérique";
-          break;
-      }
-      if (regex.test(input.value)) {
-        validInputHighlight(input);
-        isValid[inputName] = true;
-        inputSpan.innerText = "";
-      } else {
-        invalidInputHighlight(input);
-        inputSpan.innerText = errorMessage;
-        formEntry.appendChild(inputSpan);
-        isValid[inputName] = false;
-      }
-    });
-    input.addEventListener("focus", function() {
+  input.addEventListener("input", function() {
+    if (informationObject[inputName].regex.test(input.value)) {
+      validInputHighlight(input);
+      informationObject[inputName].isValid = true;
       inputSpan.innerText = "";
-    });
-
-  } else if (inputName === "location1") {
-    errorMessage = "Vous devez choisir une option."
-    const radioButtons = document.querySelectorAll("[type=radio]");
-    for (const btn of radioButtons) {
-        submit.addEventListener("click", function() {
-        if (btn.checked) {
-          isValid[inputName] = true;
-          errorMessage = "";
-        } else {
-          inputSpan.innerText = errorMessage;
-          formEntry.appendChild(inputSpan);
-        }
-        btn.addEventListener("change", function(event) {
-          if(this.checked) {
-            inputSpan.innerText = "";
-          }
-        })
-      })
+    } else {
+      invalidInputHighlight(input);
+      informationObject[inputName].isValid = false;
+      inputSpan.innerText = informationObject[inputName].errorMessage;
+      input.parentElement.appendChild(inputSpan);
     }
-  } else if (inputName === "checkbox1") {
-    submit.addEventListener("click", function() {
-      if (input.checked) {
-        isValid[inputName] = true;
+  });
+}
+
+function validateLocationInput(input) {
+  const inputSpan = document.createElement("span");
+  inputSpan.classList.add("form-warning");
+  const inputName = input.id;
+  const radioButtons = document.querySelectorAll("[name=location]");
+  let btnValid = [];
+  submit.addEventListener("click", function() {
+    btnValid = []
+    for (const btn of radioButtons) {
+      if (btn.checked) {
+        btnValid.push(true);
       } else {
-        isValid[inputName] = false;
-        errorMessage = "Vous devez vérifier que vous acceptez les termes et conditions.";
-        inputSpan.innerText = errorMessage;
-        formEntry.appendChild(inputSpan);
+        btnValid.push(false);
       }
-    });
-    input.addEventListener("change", function() {
-      if (this.checked) {
+    }
+    if (btnValid.some(input => input)) {
+      informationObject[inputName].isValid = true;
+      inputSpan.innerText = "";
+    } else {
+      inputSpan.innerText = informationObject[inputName].errorMessage;
+      input.parentElement.appendChild(inputSpan);
+    }
+  })
+  radioButtons.forEach(btn => {
+    btn.addEventListener("change", function() {
+      if(this.checked) {
         inputSpan.innerText = "";
       }
     })
+  })
+}
+
+function validateCheckboxInput(input) {
+  const inputSpan = document.createElement("span");
+  inputSpan.classList.add("form-warning");
+  const inputName = input.id;
+  submit.addEventListener("click", function() {
+    if (input.checked) {
+      informationObject[inputName].isValid = true;
+    } else {
+      informationObject[inputName].isValid = false;
+      inputSpan.innerText = informationObject[inputName].errorMessage;
+      input.parentElement.appendChild(inputSpan);
+    }
+  });
+  input.addEventListener("change", function() {
+    if (this.checked) {
+      inputSpan.innerText = "";
+    }
+  });
+}
+
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+closeBtn.forEach(btn => {
+  btn.addEventListener("click", closeModal)
+});
+
+
+formData.forEach(formEntry => {
+  const input = formEntry.querySelector("input");
+  const inputName = input.id;
+
+  if (inputName !== "location1" && inputName !== "checkbox1") {
+    validateTextInput(input);
+  } else if (inputName === "location1") {
+    validateLocationInput(input);
+  } else if (inputName === "checkbox1") {
+    validateCheckboxInput(input);
   }
 });
 
 submit.addEventListener("click", function(event) {
   event.preventDefault();
-  for (const [key, value] of Object.entries(isValid)) {
-    value ? validInputHighlight(document.getElementById(key)) : invalidInputHighlight(document.getElementById(key));
-  }
-  const isTrue = Object.values(isValid).every(value => value)
-  if (isTrue) {
-    closeModal();
-    launchConfirm();
-    emptyForm();
-  }
+  isTrue = []
+  console.log(isTrue);
+
+  for (const [key, value] of Object.entries(informationObject)) {
+    console.log(key, value.isValid);
+    if (value.isValid) {
+      validInputHighlight(document.getElementById(key));
+      isTrue.push(true);
+    } else {
+      invalidInputHighlight(document.getElementById(key));
+      isTrue.push(false)
+    }
+  };
+  if (isTrue.every(value => value)) {
+      closeModal();
+      launchConfirmModal();
+      resetForm();
+  };
 });
