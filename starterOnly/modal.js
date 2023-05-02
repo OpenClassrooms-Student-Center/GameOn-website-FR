2e0724fd63cf0c24c268c4171e8f7ca192c56855
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-05-02 14:15:41 +0200

    new function if input it birthdate

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,11 +13,13 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const form = document.querySelector("form");
 const submit = document.querySelector(".btn-submit");
-const confirmForm = document.querySelector(".confirm");
+const confirmForm = document.querySelector(".modal-confirm");
 const closeBtn = document.querySelectorAll(".close");
+const closeModalBtn = document.querySelector(".btn-close");
 
 let isTrue = [];
 
+// create an object containing all relevant informations to check all form inputs
 let informationObject = {
   "first": {
     isValid: false,
@@ -54,27 +56,36 @@ let informationObject = {
   }
 };
 
+// display the form modal
 function launchModal() {
   modalbg.style.display = "block";
+  form.style.opacity = 1;
+  confirmForm.style.display = "none";
 }
 
+// hide the modal
 function closeModal() {
   modalbg.style.display = "none";
   confirmForm.style.display = "none";
 }
 
+// display the confirmation modal
 function launchConfirmModal() {
+  form.style.opacity = 0;
   confirmForm.style.display = "block";
 }
 
+// highlight the form input's border in green
 function validInputHighlight(input) {
   input.style.border = "2px solid green";
 }
 
+// highlight the form input's border in red
 function invalidInputHighlight(input) {
   input.style.border = "2px solid red";
 }
 
+// reset form and saved values
 function resetForm() {
   form.reset();
   const input = form.querySelectorAll("input");
@@ -87,6 +98,7 @@ function resetForm() {
   }
 }
 
+// check if form input is valid for text input
 function validateTextInput(input) {
   const inputName = input.id;
   const inputSpan = document.createElement("span");
@@ -104,8 +116,32 @@ function validateTextInput(input) {
       input.parentElement.appendChild(inputSpan);
     }
   });
-}
+};
+// check if form input is valid for birthdate input
+function validateBirthdateInput(input) {
+  const inputName = input.id;
+  const inputSpan = document.createElement("span");
+  inputSpan.classList.add("form-warning");
 
+  input.addEventListener("input", function() {
+    let today = new Date();
+    let date = new Date(input.value);
+    if (informationObject[inputName].regex.test(input.value) && (inputName === "birthdate")) {
+      if (date < today) {
+        validInputHighlight(input);
+        informationObject[inputName].isValid = true;
+        inputSpan.innerText = "";
+      } else {
+        invalidInputHighlight(input);
+        informationObject[inputName].isValid = false;
+        inputSpan.innerText = informationObject[inputName].errorMessage;
+        input.parentElement.appendChild(inputSpan);
+      }
+    }
+  });
+};
+
+// check if form input is valid for location input (radio buttons)
 function validateLocationInput(input) {
   const inputSpan = document.createElement("span");
   inputSpan.classList.add("form-warning");
@@ -138,6 +174,7 @@ function validateLocationInput(input) {
   })
 }
 
+// check if form input is valid for terms of use input (checkbox)
 function validateCheckboxInput(input) {
   const inputSpan = document.createElement("span");
   inputSpan.classList.add("form-warning");
@@ -160,29 +197,38 @@ function validateCheckboxInput(input) {
 
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// call closeModal function when clicking the closeBtn
 closeBtn.forEach(btn => {
   btn.addEventListener("click", closeModal)
 });
 
+closeModalBtn.addEventListener("click", closeModal);
 
+
+// testing all the form's inputs
 formData.forEach(formEntry => {
   const input = formEntry.querySelector("input");
   const inputName = input.id;
-
-  if (inputName !== "location1" && inputName !== "checkbox1") {
-    validateTextInput(input);
-  } else if (inputName === "location1") {
-    validateLocationInput(input);
-  } else if (inputName === "checkbox1") {
-    validateCheckboxInput(input);
+  switch (inputName) {
+    case "birthdate":
+      validateBirthdateInput(input);
+      break;
+    case "location1":
+      validateLocationInput(input);
+      break;
+    case "checkbox1":
+      validateCheckboxInput(input);
+      break;
+    default:
+      validateTextInput(input);
+      break;
   }
 });
 
+// when clicking on the submit button, checks if all form entries are valid:
 submit.addEventListener("click", function(event) {
   event.preventDefault();
   isTrue = []
-  console.log(isTrue);
-
   for (const [key, value] of Object.entries(informationObject)) {
     console.log(key, value.isValid);
     if (value.isValid) {
@@ -193,8 +239,8 @@ submit.addEventListener("click", function(event) {
       isTrue.push(false)
     }
   };
+  // if all form entries are valid, calling functions :
   if (isTrue.every(value => value)) {
-      closeModal();
       launchConfirmModal();
       resetForm();
   };
