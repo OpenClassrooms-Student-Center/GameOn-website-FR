ff1f6d1fb847e3ef598acc0f1e91e68d3e5c5c1a
khanh.t.Ubalia <thidankhanh.jolly2022@campus-eni.fr>
2023-05-03 01:24:20 +0200

    ISSUE #2: Implémenter entrées du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -50,4 +50,199 @@ spanClose.addEventListener("click", (e) => {
   }
 });
 
+// ##################################################################################################
+// ##################################################################################################
+// ISSUE #2: Implémenter entrées du formulaire ()
+// ##################################################################################################
 
+// (1) Lier les labels aux entrées dans le HTML en utilisant les attributs "for" et "id" dans le code existant. Corriger le code HTML quand nécessaire.
+// (2) Utiliser du JavaScript pur (pas de jQuery) pour terminer le formulaire :
+
+// Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
+
+// Les données doivent être saisies correctement :
+// (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+// (2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
+// (3) L'adresse électronique est valide.
+// (4) Pour le nombre de concours, une valeur numérique est saisie.
+// (5) Un bouton radio est sélectionné.
+// (6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
+// Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
+
+function Validator(formSelector) {
+  let _this = this;
+
+  /**
+   * (part !!: listen events): from current element => have to get out its parent element (form) => to access to form-message! === search with loop...
+   * cuz theres many parents... => break === match
+   *  */
+
+  /************************************************************************ */
+  function getParent(element, selector) {
+    while (element.parentElement) {
+      /** 
+       * cannot use === selector cuz always true! => undefined 
+       * if (element.parentElement === selector) {
+        return element.parentElement;
+      }
+      element = element.parentElement; */
+
+      if (element.parentElement.matches(selector)) {
+        return element.parentElement;
+      }
+      element = element.parentElement;
+    }
+  }
+
+  /**
+   * (2.1) => our target!
+   */
+  /************************************************************************ */
+  let formRules = {};
+  /**
+     *  expectations: get rules in this format of formRules: 
+     * first: 'required|first',
+     * email: 'required|email',
+     * ...
+     * => need to: get out formRules with the above format & its rules 
+    according to input name then: create functions to validate each rule (function's job)
+    */
+
+  
+
+  /**
+   * (2.3) => steps to get to (1.2) then (1.1)
+   */
+  /************************************************************************ */
+
+  // (2.3.1) get form element in DOM with `formSelector`
+  let formElement = document.querySelector(formSelector);
+
+  // (2.3.2) get inputs => get formRules => get rules with functions to validate at last
+  // only execute IF form element exists in DOM
+  if (formElement) {
+    // (part !):
+    // access to formRules from inputs => with expected format as above
+    let inputs = formElement.querySelectorAll("[name][data-rules]");
+    // (for part !!) inputs => to listen events:
+    for (let input of inputs) {
+      // expectation: get "values": eg: 'required|email' => split the rules with `|` => array with for of (values)
+      let rules = input.getAttribute("data-rules").split("|");
+      // `!!!!!!! separate rules to retreive each rule from each input:` => to use each defined rule function above (...retrieved from a rule array)
+      for (let rule of rules) {
+        let ruleArray; // '!!!'
+        let isRuleHasValue = rule.includes(":");
+
+        // check min: 2 with `includes`or `indexOf`:
+        if (isRuleHasValue) {
+          ruleArray = rule.split(":");
+          rule = ruleArray[0]; // to get only min
+
+          /* if (rule.includes(":")) {
+          let ruleArray = rule.split(":");
+          rule = ruleArray[0]; // to get only min */
+
+          // console.log(validatorRules[rule](ruleArray[1]));
+          // console.log(validatorRules[rule]);
+        }
+        // get into the last function of the nested function? => eg: min: 2
+        let ruleFunc = validatorRules[rule];
+        if (isRuleHasValue) {
+          ruleFunc = ruleFunc(ruleArray[1]);
+        }
+
+        //console.log(rule);
+        // try to get rule functions
+        if (Array.isArray(formRules[input.name])) {
+          // since 2nd run of for => push rules into the array
+          formRules[input.name].push(ruleFunc);
+        } else {
+          // first run of for => else => create an array of validator rules (empty)
+          // console.log(validatorRules[rule]);
+          formRules[input.name] = [ruleFunc];
+        }
+      }
+      // rules is not an official attribute => .getAttribute
+      // formRules[input.name] = input.getAttribute("rules");
+
+      /**
+       * (part !)): create an object to validate each input => DONE!
+       * (part !!)): eventListeners (blur, change, click, focus, input, keydown, keypress, keyup, submit, ...)
+       *  */
+
+      // (!!) => do now: listen events to validate thanks to inputs retrieved above:
+
+      // events:
+      input.onblur = handleValidate;
+      input.onchange = handleValidate;
+      input.oninput = handleClearErrors;
+    }
+
+    //(1)
+    // function to execute validation:
+    // (!!.1)  => get rules (key & functions) out based on events
+    function handleValidate(event) {
+      //expectation => check if err => return err messages:
+      //console.log(event.target); // => get element targeted by event => then get rules
+      //console.log(event.target.value);
+      //console.log(event.target.name);
+      //console.log(formRules[event.target.name]);
+
+      // : each rule above reveives value!
+      let rules = formRules[event.target.name];
+      let errorMessage;
+
+      for (let rule of rules) {
+        if (event.target.type === "checkbox") {
+          errorMessage = rule(event.target.checked);
+        } else {
+          errorMessage = rule(event.target.value);
+        }
+        if (errorMessage) break;
+      }
+      // find => find values in an array => not good to use here!
+      // rules.find(function (rule) {
+      //   errorMessage = rule(event.target.value);
+      //   return errorMessage;
+      // });
+      //console.log(errorMessage);
+
+      // if err => return err messages => UI
+      if (errorMessage) {
+        // input => formData => to get form-message out:
+        // console.log(event.target);
+
+        let formData = getParent(event.target, ".formData");
+        if (formData) {
+          // (!!.3) add class `invalid` => UI errors => CSS ;)
+          formData.classList.add("invalid");
+          let formMessage = formData.querySelector(".form-message");
+          if (formMessage) {
+            formMessage.innerText = errorMessage;
+          }
+        }
+        //console.log(formData);
+      }
+      return !errorMessage; // return true if validate successfully
+    }
+
+    //(2)
+    // function clear error messages:
+    function handleClearErrors(event) {
+      // get formData firstly => to check if formData has class `invalid` => clear if true
+      let formData = getParent(event.target, ".formData");
+      if (formData.classList.contains("invalid")) {
+        formData.classList.remove("invalid");
+
+        // then reset form-message => set === '
+        let formMessage = formData.querySelector(".form-message");
+        if (formMessage) {
+          formMessage.innerText = "";
+        }
+      }
+    }
+  }
+  //console.log(this); //=== instance Validator
+  // console.log(formRules); (part !) => done!
+
+}
