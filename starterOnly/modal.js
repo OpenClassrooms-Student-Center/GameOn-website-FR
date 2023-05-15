33ef5f076b667c3fe7a34dd1cab86c2d604f239a
Swan <swan.marin@epitech.eu>
2023-05-15 19:17:55 +0200

    Fermeture de la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,32 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+  function editNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
   }
-}
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+  // DOM Elements
+  const modalbg = document.querySelector(".bground");
+  const modalBtn = document.querySelectorAll(".modal-btn");
+  const formData = document.querySelectorAll(".formData");
+  const modalExit = document.querySelectorAll(".close");
+  console.log(modalExit);
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+  // launch and close modal event
+  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+  modalExit.forEach((close) => close.addEventListener("click", closeModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+  // launch modal form
+  function launchModal() {
+    modalbg.style.display = "block";
+  }
+
+  //close modal
+  function closeModal() {
+    modalbg.style.display = "none";
+  }
+  
 
 
