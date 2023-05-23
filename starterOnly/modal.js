d720c18da33b185566131d15cf46cab09e778d8a
Chaira <chaira10@hotmail.fr>
2023-05-23 17:11:01 +0200

    add close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,35 @@ function editNav() {
   }
 }
 
+document.addEventListener("DOMContentLoaded", function () {
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+  const modalbg = document.querySelector(".bground");
+  const modalBtn = document.querySelectorAll(".modal-btn");
+  const btnFermer = document.getElementById("btn-fermer");
+  const msg = document.getElementById("msg");
+  const modalBody = document.getElementById("modal-body");
+  const close = document.getElementById("close");
+  const form = document.getElementById("form");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+  // close modal event
+  btnFermer.addEventListener("click", closeModal);
+  close.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+  // close modal form
+  function closeModal() {
+    modalbg.style.display = "none";
+  }
 
+
+
+
+
+});
