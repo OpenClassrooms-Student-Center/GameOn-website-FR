90f2f8d74c9793041cdd38e267749525ae8c3c7d
Arad GHORRESHI <arad.ghorreshi@gmail.com>
2023-05-26 20:53:25 +0200

    Fin implémentation des entrées du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -33,13 +33,61 @@ function closeModal() {
 
   modalcontent.style.animation = closeAnimation;
   modalbg.style.animation = closeAnimation;
-  
+
   //L'élément modalbg disparaît seulement à la fin de l'animation
-  modalbg.addEventListener("animationend", function() {
+  modalbg.addEventListener("animationend", function () {
     if (modalbg.style.animationName === "modalclose") {
       modalbg.style.display = "none";
     }
   });
 }
 
+function validate() {
+  const firstName = document.getElementById("first").value;
+  const lastName = document.getElementById("last").value;
+  const email = document.getElementById("email").value;
+  const quantity = document.getElementById("quantity").value;
+  const locationInputs = document.querySelectorAll('input[name="location"]');
+  const checkbox1 = document.getElementById("checkbox1").checked;
 
+  if (firstName.length < 2 || lastName.length < 2) {
+    alert("Le prénom et le nom doivent comporter au moins 2 caractères.");
+    return false;
+  }
+
+  if (!validateEmail(email)) {
+    alert("Veuillez entrer une adresse e-mail valide.");
+    return false;
+  }
+
+  if (isNaN(quantity) || quantity < 0) {
+    alert(
+      "Veuillez saisir une valeur numérique positive pour le nombre de tournois."
+    );
+    return false;
+  }
+
+  let locationSelected = false;
+  for (let i = 0; i < locationInputs.length; i++) {
+    if (locationInputs[i].checked) {
+      locationSelected = true;
+      break;
+    }
+  }
+  if (!locationSelected) {
+    alert("Veuillez sélectionner une option de tournoi.");
+    return false;
+  }
+
+  if (!checkbox1) {
+    alert("Veuillez accepter les conditions d'utilisation.");
+    return false;
+  }
+
+  return true;
+}
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
