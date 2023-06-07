0dd74d051f99b4f5f86cfafe2dfcc5f77747589f
jamin <gerard.jamin@orange.fr>
2023-06-07 11:48:48 +0200

    gestion des messages erreurs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -33,7 +33,7 @@ function launchModal() {
 /**
  * init all modal's field
  */
-function modalInit(){
+function modalInit() {
   let baliseNom = document.forms.reserve.last
   baliseNom.value = ""
   let balisePrenom = document.getElementById("first")
@@ -41,7 +41,26 @@ function modalInit(){
   let baliseEmail = document.getElementById("email")
   baliseEmail.value = ""
   let baliseQuantity = document.getElementById("quantity")
-  baliseQuantity.value = "" 
+  baliseQuantity.value = ""
+
+  let errorNom = document.getElementById("errorNom")
+  errorNom.setAttribute("data-error-visible", "false")
+  errorNom.setAttribute("data-error", " ")
+  let errorPrenom = document.getElementById("errorPrenom")
+  errorPrenom.setAttribute("data-error-visible", "false")
+  errorPrenom.setAttribute("data-error", " ")
+  let errorEmail = document.getElementById("errorEmail")
+  errorEmail.setAttribute("data-error-visible", "false")
+  errorEmail.setAttribute("data-error", " ")
+  let errorQuantity = document.getElementById("errorQuantity")
+  errorQuantity.setAttribute("data-error-visible", "false")
+  errorQuantity.setAttribute("data-error", " ")
+  let errorBirthday = document.getElementById("errorBirthday")
+  errorBirthday.setAttribute("data-error-visible", "false")
+  errorBirthday.setAttribute("data-error", " ")
+  initBirthday = document.getElementById("birthdate")
+  console.log(initBirthday)
+  initBirthday.value = "jj/mm/aaaa"
 }
 
 /**
@@ -64,8 +83,8 @@ function validerNom(nom) {
   let valid = false
   nom = nom.trim()
   if ((nom.length < 2) || (nom.value === "")) {
-    throw new Error("Le nom est trop court.")
-  }else{
+    throw new Error("#1:Veuillez entrer 2 caractères ou plus pour le champ du nom.")
+  } else {
     valid = true
   }
   return valid
@@ -80,8 +99,8 @@ function validerPrenom(prenom) {
   let valid = false
   prenom = prenom.trim()
   if ((prenom.length < 2) || (prenom.value === " ")) {
-    throw new Error("Le prenom est trop court.")
-  }else{
+    throw new Error("#2:Veuillez entrer 2 caractères ou plus pour le champ du prenom.")
+  } else {
     valid = true
   }
   return valid
@@ -96,9 +115,9 @@ function validerEmail(email) {
   let valid = false
   let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
   if (!emailRegExp.test(email)) {
-    throw new Error("L'email n'est pas valide.")
-  }else{
-     valid = true
+    throw new Error("#3:L'email n'est pas valide.")
+  } else {
+    valid = true
   }
   return valid
 }
@@ -111,12 +130,29 @@ function validerEmail(email) {
 function validerQuantity(quantity) {
   let valid = false
   if (quantity === "") {
-    throw new Error("Vous devez répondre à la question : À combien de tournois GameOn avez-vous déjà participé ?")
-  }else {
+    throw new Error("#4:Vous devez répondre à la question.")
+  } else {
     valid = true
   }
   return valid
 }
+
+/**
+ * La date de naissance dois etre valide.
+ * @param {*} birthday 
+ * @returns 
+ */
+function validerBirthday(birthday) {
+  let valid = false
+  let birthdayRegExp = new RegExp(/^\d{4}-\d{2}-\d{2}$/)
+  if (!birthdayRegExp.test(birthday)) {
+    throw new Error("#5:Vous devez entrer votre date de naissance.")
+  } else {
+    valid = true
+  }
+  return valid
+}
+
 
 /**
  * cette fonction permet de valider l'envoie du formulaire lorsque tous les champs sont
@@ -126,56 +162,110 @@ function validerQuantity(quantity) {
 let form = document.querySelector("form")
 form.addEventListener("submit", (event) => {
   //avoid re-load web page
-    event.preventDefault()
+  event.preventDefault()
 
-    try {
-      let baliseNom = document.forms.reserve.last
-      let nom = baliseNom.value
-      let validName = validerNom(nom)
-     
-      let balisePrenom = document.getElementById("first")
-      let prenom = balisePrenom.value
-      let validPrenom = validerPrenom(prenom)
-  
-      let baliseEmail = document.getElementById("email")
-      let email = baliseEmail.value
-      let validEmail = validerEmail(email)
-  
-      let baliseQuantity = document.getElementById("quantity")
-      let quantity = baliseQuantity.value
-      let validQuantity = validerQuantity(quantity)
-  
-      let formValid = validName && validPrenom && validEmail && validQuantity
-  
-      if (formValid){
-        //all field are correct
-        modalbg.style.display = "none";
-      }
-    } catch (erreur) {
-        console.log(erreur.message) 
-        afficherMessageError(erreur.message)
+  try {
+    let baliseNom = document.getElementById("last")
+    let nom = baliseNom.value
+    let validName = validerNom(nom)
+
+    let balisePrenom = document.getElementById("first")
+    let prenom = balisePrenom.value
+    let validPrenom = validerPrenom(prenom)
+
+    let baliseEmail = document.getElementById("email")
+    let email = baliseEmail.value
+    let validEmail = validerEmail(email)
+
+    let baliseQuantity = document.getElementById("quantity")
+    let quantity = baliseQuantity.value
+    let validQuantity = validerQuantity(quantity)
+
+    let baliseBirthday = document.getElementById("birthdate")
+    let birthdate = baliseBirthday.value
+    let validBirthday = validerBirthday(birthdate)
+
+    let formValid = validName && validPrenom && validEmail && validQuantity && validBirthday
+
+    if (formValid) {
+      //all field are correct
+      modalbg.style.display = "none";
     }
+  } catch (erreur) {
+    console.log(erreur.message)
+    afficherMessageError(erreur.message)
+  }
 })
 
 /** 
- * cette fonction affiche le message d'erreur passé en paramètre.
- * si le span existe déjà, alors il est réutilisé pour ne pas les multiplier 
+ * cette fonction affiche le message d'erreur passé en paramètre et selectionne l'element HTML.
+ * 
  * @param {string} message
 */
 function afficherMessageError(message) {
-  let spanEreurMessage = document.getElementById("erreurMessage")
 
-  if (!spanEreurMessage) {
-    let modalBody = document.querySelector(".modal-body")
-    spanEreurMessage = document.createElement("span")
-    spanEreurMessage.id = "erreurMessage"
-    modalBody.append(spanEreurMessage)
+  let firstTwoCaractere = message.substring(0, 2)
+
+  if (firstTwoCaractere === "#1") {
+    let errorNom = document.getElementById("errorNom")
+    errorNom.setAttribute("data-error-visible", "true")
+    errorNom.setAttribute("data-error", message.substring(3))
+  } else {
+    errorNom.removeAttribute("data-error-visible")
+    errorNom.removeAttribute("data-error")
   }
 
-  spanEreurMessage.innerText = message
+  if (firstTwoCaractere === "#2") {
+    let errorPrenom = document.getElementById("errorPrenom")
+    errorPrenom.setAttribute("data-error-visible", "true")
+    errorPrenom.setAttribute("data-error", message.substring(3))
+  } else {
+    errorPrenom.removeAttribute("data-error-visible")
+    errorPrenom.removeAttribute("data-error")
+  }
+
+  if (firstTwoCaractere === "#3") {
+    let errorEmail = document.getElementById("errorEmail")
+    errorEmail.setAttribute("data-error-visible", "true")
+    errorEmail.setAttribute("data-error", message.substring(3))
+  } else {
+    errorEmail.removeAttribute("data-error-visible")
+    errorEmail.removeAttribute("data-error")
+  }
+
+  if (firstTwoCaractere === "#4") {
+    let errorQuantity = document.getElementById("errorQuantity")
+    errorQuantity.setAttribute("data-error-visible", "true")
+    errorQuantity.setAttribute("data-error", message.substring(3))
+  } else {
+    errorQuantity.removeAttribute("data-error-visible")
+    errorQuantity.removeAttribute("data-error")
+  }
+
+  if (firstTwoCaractere === "#5") {
+    let errorBirthday = document.getElementById("errorBirthday")
+    errorBirthday.setAttribute("data-error-visible", "true")
+    errorBirthday.setAttribute("data-error", message.substring(3))
+  } else {
+    errorBirthday.removeAttribute("data-error-visible")
+    errorBirthday.removeAttribute("data-error")
+  }
 }
 
-function validate(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+function validate() {
 
 }
 
