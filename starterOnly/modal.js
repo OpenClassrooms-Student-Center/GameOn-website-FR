0eb4f3ebecd92be08900043a38f1ef7dfe7555c1
aradgh <arad.ghorreshi@gmail.com>
2023-06-22 21:49:59 +0200

    Factorisation des fonctions de validation des champs du formulaire + Ajout de la fonction de validation du formulaire + Première version CSS de la confirmation de l'inscription

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,8 @@ const modalcontent = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalCloseBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
+const modalForm = document.querySelector("form");
+const modalThanks = document.querySelector(".thanks");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -49,48 +51,18 @@ const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const date = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
+const tournamentLocation = document.getElementById("location1");
 const locationInputs = document.querySelectorAll('input[name="location"]');
-const checkbox1 = document.getElementById("checkbox1").checked;
+const checkbox1 = document.getElementById("checkbox1");
 
-function validateFirstName() {
-  let isValid = false;
-  if (firstName.value.length < 2) {
-    showError(firstName, "Veuillez entrer 2 caractères ou plus.");
-  } else {
-    hideError(firstName);
-  }
-}
+//Date max input
+date.max = new Date().toISOString().split("T")[0];
 
-function validateLastName() {
-  if (lastName.value.length < 2) {
-    showError(lastName, "Veuillez entrer 2 caractères ou plus.");
+function validateInput(inputElement, errorMessage, validationFunction) {
+  if (validationFunction) {
+    showError(inputElement, errorMessage);
   } else {
-    hideError(lastName);
-  }
-}
-
-function validateEmail() {
-  if (!email.checkValidity()) {
-    showError(email, "Veuillez entrer une adresse e-mail valide.");
-  } else {
-    hideError(email);
-  }
-}
-
-function validateDate() {
-  console.log("validateDate()");
-  if (!date.checkValidity()) {
-    showError(date, "Veuillez entrer une date valide.");
-  } else {
-    hideError(date);
-  }
-}
-
-function validateQuantity() {
-  if (!quantity.checkValidity()) {
-    showError(quantity, "Veuillez entrer un nombre entre 0 et 99.");
-  } else {
-    hideError(quantity);
+    hideError(inputElement);
   }
 }
 
@@ -104,43 +76,116 @@ function hideError(inputElement) {
   inputElement.parentNode.setAttribute("data-error", "");
 }
 
-// function validate() {
-//   //Mettre en var à l'extérieur de la fonction avec un scope plus large
-//   //GetDataFromForm() pour tout récupérer
-//   //Factoriser chaque validation de champ function(nomduchamp, fonction validation)
-//   //validateField(input, fonction de validation)
-//   //fichier validationfunctions, ou faire en fonction fléchée directement
+function checkLength(inputElement) {
+  return inputElement.value.length < 2;
+}
 
-//   var isValid = true;
+function checkValidity(inputElement) {
+  return !inputElement.checkValidity();
+}
 
-//   // Validation du champ Prénom
-//   //Faire une fonction générique, retourner un object avec le message d'erreur, et la var isValid boolean
-//   //(field, function fléchée) pour retourner le champ entier avec ou non le message d'erreur (pourquoi pas dans un autre fichier)
-//   //Mettre le curseur à l'intérieur du premier champ où il y a une erreur
+function checkLocationUnselected() {
+  for (let i = 0; i < locationInputs.length; i++) {
+    if (locationInputs[i].checked) {
+      return false;
+    }
+  }
+  return true;
+}
 
-//   if (isNaN(quantity) || quantity < 0) {
-//     alert(
-//       "Veuillez saisir une valeur numérique positive pour le nombre de tournois."
-//     );
-//     return false;
-//   }
+function checkCheckboxSelected() {
+  return !checkbox1.checked;
+}
 
-//   let locationSelected = false;
-//   for (let i = 0; i < locationInputs.length; i++) {
-//     if (locationInputs[i].checked) {
-//       locationSelected = true;
-//       break;
-//     }
-//   }
-//   if (!locationSelected) {
-//     alert("Veuillez sélectionner une option de tournoi.");
-//     return false;
-//   }
+firstName.addEventListener("input", () =>
+  validateInput(
+    firstName,
+    "Veuillez entrer 2 caractères ou plus.",
+    checkLength(firstName)
+  )
+);
+lastName.addEventListener("input", () =>
+  validateInput(
+    lastName,
+    "Veuillez entrer 2 caractères ou plus.",
+    checkLength(lastName)
+  )
+);
+email.addEventListener("input", () =>
+  validateInput(
+    email,
+    "Veuillez entrer une adresse e-mail valide.",
+    checkValidity(email)
+  )
+);
+date.addEventListener("keyup", () =>
+  validateInput(date, "Veuillez entrer une date valide.", checkValidity(date))
+);
+date.addEventListener("change", () =>
+  validateInput(date, "Veuillez entrer une date valide.", checkValidity(date))
+);
+quantity.addEventListener("input", () =>
+  validateInput(
+    quantity,
+    "Veuillez entrer un nombre entre 0 et 99.",
+    checkValidity(quantity)
+  )
+);
 
-//   if (!checkbox1) {
-//     alert("Veuillez accepter les conditions d'utilisation.");
-//     return false;
-//   }
+//Form validation
+function validate() {
+  // Appeler les fonctions de validation individuelles
+  validateInput(
+    firstName,
+    "Veuillez entrer 2 caractères ou plus.",
+    checkLength(firstName)
+  );
+  validateInput(
+    lastName,
+    "Veuillez entrer 2 caractères ou plus.",
+    checkLength(lastName)
+  );
+  validateInput(
+    email,
+    "Veuillez entrer une adresse e-mail valide.",
+    checkValidity(email)
+  );
+  validateInput(date, "Veuillez entrer une date valide.", checkValidity(date));
+  validateInput(
+    quantity,
+    "Veuillez entrer un nombre entre 0 et 99.",
+    checkValidity(quantity)
+  );
+  validateInput(
+    tournamentLocation,
+    "Veuillez sélectionner une ville.",
+    checkLocationUnselected()
+  );
+  validateInput(
+    checkbox1,
+    "Veuillez accepter les conditions d'utilisation.",
+    checkCheckboxSelected()
+  );
 
-//   return isValid;
-// }
+  // Vérifier s'il y a des erreurs de validation
+  var errorElements = document.querySelectorAll(
+    '.formData[data-error-visible="true"]'
+  );
+  if (errorElements.length > 0) {
+    // Empêcher la soumission du formulaire si des erreurs sont présentes
+    //TODO vérifier par quoi remplacer event qui est déprécié
+    event.preventDefault();
+    // Définir le focus sur le premier champ en erreur
+    errorElements[0].querySelector(".text-control").focus();
+  } else {
+    event.preventDefault();
+    // Thanks after validate
+    modalForm.style.display = "none";
+    modalThanks.style.display = "block";
+    // modalbg.style.display = "block";
+    // modalcontent.style.display = "block";
+
+    // Clear inputs after validate
+    modalForm.reset();
+  }
+}
