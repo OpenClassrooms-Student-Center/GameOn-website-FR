b61139b274d2196a6767393edcbd954850263782
aradgh <arad.ghorreshi@gmail.com>
2023-06-23 23:58:46 +0200

    Correction dans le index.html + Simplification des fonctions de validation + Traduction de tous les commentaires en français + Correction du event déprécié + Ajout des étoiles rouges à côté des labels + Changement du bouton Je m'inscrit après la confirmation de l'inscription + Changement du nom de la variable date à birthdate

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,7 +7,7 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// Elements du DOM
 const modalbg = document.querySelector(".bground");
 const modalcontent = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -15,20 +15,20 @@ const modalCloseBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 const modalForm = document.querySelector("form");
 const modalThanks = document.querySelector(".thanks");
+const signUpButtons = document.querySelectorAll(".btn-signup");
 
-// launch modal event
+// Déclencher l'ouverture du formulaire
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Ouvrir le formulaire
 function launchModal() {
   modalbg.style.display = "block";
-  //On réinitialise les propriétés qui ont été ajoutée dans closeModal()
+  // On réinitialise les propriétés qui ont été ajoutée dans closeModal()
   modalbg.style.animationName = "none";
   modalcontent.style.animationName = "modalopen";
 }
-//Faire une toggle, change entre l'état initiale et l'état finale
 
-// close modal event
+// Fermer le formulaire
 modalCloseBtn.addEventListener("click", closeModal);
 
 function closeModal() {
@@ -37,7 +37,7 @@ function closeModal() {
   modalcontent.style.animation = closeAnimation;
   modalbg.style.animation = closeAnimation;
 
-  //L'élément modalbg disparaît seulement à la fin de l'animation
+  // L'élément modalbg disparaît seulement à la fin de l'animation
   modalbg.addEventListener("animationend", function () {
     if (modalbg.style.animationName === "modalclose") {
       modalbg.style.display = "none";
@@ -45,21 +45,22 @@ function closeModal() {
   });
 }
 
-//Form inputs Elements
+// Elements du formulaire
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
-const date = document.getElementById("birthdate");
+const birthdate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
 const tournamentLocation = document.getElementById("location1");
 const locationInputs = document.querySelectorAll('input[name="location"]');
 const checkbox1 = document.getElementById("checkbox1");
 
-//Date max input
-date.max = new Date().toISOString().split("T")[0];
+// Date de naissance maximale assignée à la date d'aujourd'hui
+// Aucune limite d'âge
+birthdate.max = new Date().toISOString().split("T")[0];
 
-function validateInput(inputElement, errorMessage, validationFunction) {
-  if (validationFunction) {
+function validateInput(inputElement, errorMessage) {
+  if (!inputElement.checkValidity()) {
     showError(inputElement, errorMessage);
   } else {
     hideError(inputElement);
@@ -76,116 +77,63 @@ function hideError(inputElement) {
   inputElement.parentNode.setAttribute("data-error", "");
 }
 
-function checkLength(inputElement) {
-  return inputElement.value.length < 2;
-}
-
-function checkValidity(inputElement) {
-  return !inputElement.checkValidity();
-}
-
-function checkLocationUnselected() {
-  for (let i = 0; i < locationInputs.length; i++) {
-    if (locationInputs[i].checked) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function checkCheckboxSelected() {
-  return !checkbox1.checked;
-}
-
+// Déclencher les fonctions de validation à chaque changement dans les 5 premiers champs
 firstName.addEventListener("input", () =>
-  validateInput(
-    firstName,
-    "Veuillez entrer 2 caractères ou plus.",
-    checkLength(firstName)
-  )
+  validateInput(firstName, "Veuillez entrer 2 caractères ou plus.")
 );
 lastName.addEventListener("input", () =>
-  validateInput(
-    lastName,
-    "Veuillez entrer 2 caractères ou plus.",
-    checkLength(lastName)
-  )
+  validateInput(lastName, "Veuillez entrer 2 caractères ou plus.")
 );
 email.addEventListener("input", () =>
-  validateInput(
-    email,
-    "Veuillez entrer une adresse e-mail valide.",
-    checkValidity(email)
-  )
+  validateInput(email, "Veuillez entrer une adresse e-mail valide.")
 );
-date.addEventListener("keyup", () =>
-  validateInput(date, "Veuillez entrer une date valide.", checkValidity(date))
+// birthdate a besoin de 2 triggers
+// onkeyup permet de détecter les entrées au clavier
+birthdate.addEventListener("keyup", () =>
+  validateInput(birthdate, "Veuillez entrer une date valide.")
 );
-date.addEventListener("change", () =>
-  validateInput(date, "Veuillez entrer une date valide.", checkValidity(date))
+// onchange permet de détecter les sélections de date à travers le widget calendrier
+birthdate.addEventListener("change", () =>
+  validateInput(birthdate, "Veuillez entrer une date valide.")
 );
 quantity.addEventListener("input", () =>
-  validateInput(
-    quantity,
-    "Veuillez entrer un nombre entre 0 et 99.",
-    checkValidity(quantity)
-  )
+  validateInput(quantity, "Veuillez entrer un nombre entre 0 et 99.")
 );
 
-//Form validation
-function validate() {
+// Validation du formulaire
+function validate(event) {
   // Appeler les fonctions de validation individuelles
-  validateInput(
-    firstName,
-    "Veuillez entrer 2 caractères ou plus.",
-    checkLength(firstName)
-  );
-  validateInput(
-    lastName,
-    "Veuillez entrer 2 caractères ou plus.",
-    checkLength(lastName)
-  );
-  validateInput(
-    email,
-    "Veuillez entrer une adresse e-mail valide.",
-    checkValidity(email)
-  );
-  validateInput(date, "Veuillez entrer une date valide.", checkValidity(date));
-  validateInput(
-    quantity,
-    "Veuillez entrer un nombre entre 0 et 99.",
-    checkValidity(quantity)
-  );
-  validateInput(
-    tournamentLocation,
-    "Veuillez sélectionner une ville.",
-    checkLocationUnselected()
-  );
-  validateInput(
-    checkbox1,
-    "Veuillez accepter les conditions d'utilisation.",
-    checkCheckboxSelected()
-  );
+  validateInput(firstName, "Veuillez entrer 2 caractères ou plus.");
+  validateInput(lastName, "Veuillez entrer 2 caractères ou plus.");
+  validateInput(email, "Veuillez entrer une adresse e-mail valide.");
+  validateInput(birthdate, "Veuillez entrer une date valide.");
+  validateInput(quantity, "Veuillez entrer un nombre entre 0 et 99.");
+  validateInput(tournamentLocation, "Veuillez sélectionner une ville.");
+  validateInput(checkbox1, "Veuillez accepter les conditions d'utilisation.");
 
-  // Vérifier s'il y a des erreurs de validation
+  // Liste de tous les champs montrant des erreurs de validation
   var errorElements = document.querySelectorAll(
     '.formData[data-error-visible="true"]'
   );
+  // Empêcher la fermeture automatique du formulaire
+  event.preventDefault();
   if (errorElements.length > 0) {
-    // Empêcher la soumission du formulaire si des erreurs sont présentes
-    //TODO vérifier par quoi remplacer event qui est déprécié
-    event.preventDefault();
     // Définir le focus sur le premier champ en erreur
     errorElements[0].querySelector(".text-control").focus();
   } else {
-    event.preventDefault();
-    // Thanks after validate
+    // Afficher la confirmation d'inscription
     modalForm.style.display = "none";
     modalThanks.style.display = "block";
-    // modalbg.style.display = "block";
-    // modalcontent.style.display = "block";
-
-    // Clear inputs after validate
-    modalForm.reset();
+    // Désactiver les boutons "Je m'inscris" (format desktop et mobile)
+    // pour ne pas pouvoir remplir à nouveau le formulaire
+    signUpButtons.forEach((button) => {
+      button.disabled = true;
+      button.textContent = "Merci pour votre inscription";
+      button.style.backgroundColor = "#3876ac";
+      button.style.cursor = "default";
+    });
   }
 }
+modalbg.style.display = "block";
+modalForm.style.display = "none";
+modalThanks.style.display = "block";
