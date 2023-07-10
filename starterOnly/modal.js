3a6446544fc1ae5ed3e578531e0611a63e47ae0e
nburd01 <n.burdon@protonmail.com>
2023-07-10 17:26:57 +0200

    validations functioning in console for 5 fields, need to implement the rest

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,3 +22,48 @@ function launchModal() {
 function hideModal() {
   modalbg.style.display = "none";
 }
+function validate(event) {
+  try {
+    event.preventDefault();
+
+    const fields = [
+      { id: "first", name: "prénom" },
+      { id: "last", name: "nom" },
+      { id: "email", name: "e-mail" },
+      { id: "birthdate", name: "date de naissance" },
+      { id: "quantity", name: "quantité" },
+    ];
+
+    let isValid = true;
+
+    fields.forEach((field) => {
+      const baliseField = document.getElementById(field.id);
+      const valeurField = baliseField.value.trim();
+
+      if (valeurField === "") {
+        console.log(`Le champ ${field.name} est vide`);
+        isValid = false;
+        throw new Error(`Le champ ${field.name} est vide`);
+      }
+    });
+
+    if (isValid) {
+      console.log("All fields are filled");
+
+      // Additional validation logic can be added here
+
+      // If all validations pass, you can submit the form
+      form.submit();
+
+      // Alternatively, you can hide the modal or perform any other desired action
+      hideModal();
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+// Rest of your code...
+
+const form = document.querySelector("form");
+form.addEventListener("submit", validate);
