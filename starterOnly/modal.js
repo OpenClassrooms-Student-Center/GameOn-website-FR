605172ce2b67e99b6424ab961411605bd4b1670f
zerossama <charlotte@anros.fr>
2023-08-10 17:50:57 +0200

    issues 1  close modal done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,30 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach(btn => btn.addEventListener("click", launchModal));
+
+// close modal event
+closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
-
+// close modal form
+function closeModal() {
+    modalbg.style.display = "none";
+}
