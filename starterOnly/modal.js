55db3f3fc049ad0a78c46aa8bc403646c06a3b6f
Antonin81 <antonin.pidet@gmail.com>
2023-08-11 09:42:42 +0200

    Vérifications en js plus poussées - fin

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,10 +15,14 @@ const formDataRadios = document.querySelector(".formData--radios");
 const radios = formDataRadios.querySelectorAll("input");
 const emailInput = document.getElementById("email");
 const regexEmail = new RegExp("^[a-z0-9._-]+@[a-z0-9-_]+\\.[a-z]{2,}$");
+const regexQuantity = new RegExp("^[0-9]+$");
 const checkBoxRequired = document.getElementById("checkbox1");
+const checkBoxNotRequired = document.getElementById("checkbox2");
 const firstnameInput = document.getElementById("first");
 const lastnameInput = document.getElementById("last");
 const closeModalBtn = document.querySelector('.close');
+const birthDateInput = document.getElementById("birthdate")
+const quantityInput = document.getElementById("quantity")
 
 // launches modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -26,10 +30,19 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // submits modal event
 function validate(){
   event.preventDefault();
-  console.log(validateFirstname());
-  console.log(validateLastname());
-  const email = emailInput.value;
-  return (regexEmail.test(email) && radioChecked() && checkBoxRequired.checked);
+  console.log("Prénom : "+firstnameInput.value+", Validité : "+validateFirstname());
+  console.log("Nom : "+lastnameInput.value+", Validité : "+validateLastname());
+  console.log("Email : "+emailInput.value+", Validité : "+validateEmail());
+  console.log("Date de naissance : "+birthDateInput.value);
+  console.log("Nombre de participation à des tournois : "+quantityInput.value+", Validité : "+validateQuantity());
+  console.log("Lieu de participation souhaité : "+radioChecked()+", Validité : "+validateRadioChecked());
+  console.log("Conditions générales cochées : "+checkBoxRequired.checked+", Validité : "+checkBoxRequired.checked);
+  console.log("Checkbox facultative cochée : "+checkBoxNotRequired.checked);
+  if (validateFirstname() && validateLastname() && validateEmail() && validateQuantity() && validateRadioChecked() && checkBoxRequired.checked){
+    console.log("TOUT EST VALIDE");
+  } else {
+    console.log("INVALIDE");
+  }
 }
 
 // close modal event
@@ -43,13 +56,28 @@ function launchModal() {
 }
 
 // Verifies that a radio has been checked
-function radioChecked(){
+function validateRadioChecked(){
   for (radio of radios){
     if(radio.checked){
       return true;
     }
   }
   return false;
+}
+
+// Verifies that a radio has been checked
+function radioChecked(){
+  for (radio of radios){
+    if(radio.checked){
+      return radio.value;
+    }
+  }
+  return "Aucun radio sélectionné";
+}
+
+// Verifies that there is a number inside the quantity input
+function validateQuantity(){
+  return regexQuantity.test(quantityInput.value);
 }
 
 // closes modal form
@@ -67,4 +95,9 @@ function validateFirstname(){
 function validateLastname(){
   let lastname=lastnameInput.value;
   return (lastname.length>=2);
+}
+
+// Verifies that there is a number inside the quantity input
+function validateEmail(){
+  return regexEmail.test(emailInput.value);
 }
