26b22bb574c2aab262b86b8959c1ae9e771643c4
Raphael Sanchez <hello@raphaelsanchez.design>
2023-08-21 14:03:43 +0200

    Add close modal functions - Close when modal (X) is cliqued - Close when "esc" key is pressed - Close when clic outside the modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,62 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopnav")
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += " responsive"
   } else {
-    x.className = "topnav";
+    x.className = "topnav"
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalContainer = document.querySelector(".bground")
+const modalOpenBtn = document.querySelectorAll(".modal-btn")
+const modalCloseBtn = document.querySelectorAll(".close")
+const formData = document.querySelectorAll(".formData")
+
+// MODAL events
+// --------------------------------------------------------------
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalOpenBtn.forEach((btn) => btn.addEventListener("click", launchModal))
+
+// close modal event
+modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal))
+
+// close modal when click outside form
+window.onclick = function (event) {
+  if (event.target == modalContainer) {
+    closeModal()
+  }
+}
+
+// close modal when click escape key a11y recommendation
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeModal()
+  }
+})
+
+// FORM functions
+// --------------------------------------------------------------
+
+// Reset form function
+function resetForm() {
+  // reset form fields by removing value attribute
+  document.querySelector("form").reset()
+}
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  // display modal by adding style attribute with display: block property
+  modalContainer.style.display = "block"
+  // focus first input field when modal open
+  document.querySelector("#first").focus()
 }
 
-
+// close modal form
+function closeModal() {
+  // hide modal by removing style attribute added by launchModal()
+  modalContainer.removeAttribute("style")
+  // reset form
+  resetForm()
+}
