233e86fa35fd9a7b104326b49a164832bb68ebc9
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2023-09-03 23:06:52 +0200

    feat: fermeture du formulaire avec la croix

--- modal.js
+++ modal.js
@@ -11,13 +11,29 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelector(".close");
+const modalBgContent = document.querySelector(".content");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+closeForm.addEventListener("click", closeModal);
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  modalBgContent.classList.remove("closed")
 }
 
 
+// close modal form
+function closeModal() {
+
+  modalBgContent.classList.add("closed")
+
+  setTimeout(() => {
+  modalbg.style.display = "none";
+  }, 800);
+
+}
