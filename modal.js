29057002404ee7b4bc194a5e28a3d48a52cb1926
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2023-09-03 19:37:42 +0200

    #1 Issue resolved: Close the modal

--- modal.js
+++ modal.js
@@ -1,23 +1,31 @@
 function editNav() {
-  let x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".modal-close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+modalCloseBtn.addEventListener("click", closeModal)
+
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
-
+function closeModal(e) {
+    let modalBG = e.target.parentNode.parentNode
+    if (modalBG.style.display === "block") {
+        modalBG.style.display = "none"
+    }
+}
