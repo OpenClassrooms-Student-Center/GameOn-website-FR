3ec5e3e599ed5a56c71a9e8d1fab72dc38f4c1db
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2023-09-04 19:23:40 +0200

    regexp en cours

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,8 +10,10 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+let formData = document.querySelectorAll(".formData");
+const form = document.getElementById("myForm");
 
+console.log(form.first);
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +22,41 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//Ecouter la modification du prénom
+form.first.addEventListener('change',function() {
+  valideFirst(this);
+});
 
+//Fonction vérifier via la RegExp que le prénom est conforme
+const valideFirst = function(inputFirst) {
+  let firstRegExp = new RegExp('^[a-zA-Z-áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ\s\']+$', 'g');
+
+let testFirst = firstRegExp.test(inputFirst.value)
+console.log(testFirst);
+};
+
+//Ecouter la modification du nom
+form.last.addEventListener('change',function() {
+  valideLast(this);
+});
+//Fonction vérifier via la RegExp que le nom est conforme
+const valideLast = function(inputLast) {
+  let lastRegExp = new RegExp(/^[a-zA-Z-áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]+([ \-']?[a-zA-Z-áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]+[ \-']?[a-zA-Z-áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]+[ \-']?)[a-zA-Z-áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]+$/, 'g');
+
+let testLast = lastRegExp.test(inputLast.value)
+console.log(testLast);
+};
+
+
+//Ecouter la modification de l'email
+form.email.addEventListener('change',function() {
+  valideEmail(this);
+});
+
+//Fonction vérifier via la RegExp que l'email est conforme'
+const valideEmail = function(inputEmail) {
+  let emailRegExp = new RegExp('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$', 'gm');
+
+let testEmail = emailRegExp.test(inputEmail.value)
+console.log(testEmail);
+};
