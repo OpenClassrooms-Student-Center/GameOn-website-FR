1e0c67d63dc59c76b58d4a05d4291f381f4a8aef
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2023-09-04 12:13:13 +0200

    #1 Issue resolved: smooth modal animation

--- modal.js
+++ modal.js
@@ -9,9 +9,11 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modal = modalbg.querySelector(".content");
+const modalCloseBtn = modal.querySelector(".modal-close")
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const modalCloseBtn = document.querySelector(".modal-close")
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,11 +23,25 @@ modalCloseBtn.addEventListener("click", closeModal)
 // launch modal form
 function launchModal() {
     modalbg.style.display = "block";
+    modal.animate({
+        opacity: [0, 1], transform: ["translateY(150px)", "translateX(0)"]
+    }, {
+        // temporisation
+        duration: 400, iterations: 1, easing: "cubic-bezier(.35,.43,.18,1.54)"
+    },);
+
 }
 
 function closeModal(e) {
-    let modalBG = e.target.parentNode.parentNode
-    if (modalBG.style.display === "block") {
-        modalBG.style.display = "none"
-    }
+    setTimeout(() => {
+        modalbg.style.display = "none"
+    }, 390)
+    modal.animate(// étapes/keyframes
+        {
+            opacity: [1, 0], transform: ["translateY(0)", "translateY(-150px)"]
+        }, {
+            // temporisation
+            duration: 400, iterations: 1, easing: "cubic-bezier(.35,.43,.18,1.54)"
+        });
+
 }
