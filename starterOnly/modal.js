e3e63ec47ebbdbf5ec0a55111012d8906c2cbd5a
Tom ANDRE <tom.andre.dev@outlook.com>
2023-09-05 17:16:34 +0200

    Comment file modal.js and fix bug

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,83 +7,116 @@ function editNav() {
   }
 }
 
-// DOM Elements
+/*
+    DOM Elements can't change -----------------------------------------------------------
+*/
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 let closeBtn = document.querySelectorAll('.close-btn')
 const modalBody = document.querySelector('.modal-body')
 
+/*
+    DOM Elements can change  ----------------------------------------------------------
+*/
+let formDataFirst = document.getElementById("first")
+let formDataLast = document.getElementById("last")
+let formDataEmail = document.getElementById("email")
+let formDataBirthdate = document.getElementById("birthdate")
+let formDataQuantity = document.getElementById('quantity')
+let formDataLocation = document.querySelectorAll('[name="location"]')
+let formDataCGU = document.getElementById('checkbox1')
+let formDataNewsletter = document.getElementById('checkbox2')
+let thanksContainer = document.querySelector('.thanks-container')
+let formBody = document.getElementById('form')
+let formData = document.querySelectorAll(".formData")
 
-// DOM Elements Form Input
-const formDataFirst = document.getElementById("first")
-const formDataLast = document.getElementById("last")
-const formDataEmail = document.getElementById("email")
-const formDataBirthdate = document.getElementById("birthdate")
-const formDataQuantity = document.getElementById('quantity')
-const formDataLocation = document.querySelectorAll('[name="location"]')
-const formDataCGU = document.getElementById('checkbox1')
-const formDataNewsletter = document.getElementById('checkbox2')
 
 
-// **************  Lunch modal ***************
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+/* 
+    Update Get DOM ------------------------------------------------------------------
+*/
+function updateInputForm() {
+  formDataFirst = document.getElementById("first")
+  formDataLast = document.getElementById("last")
+  formDataEmail = document.getElementById("email")
+  formDataBirthdate = document.getElementById("birthdate")
+  formDataQuantity = document.getElementById('quantity')
+  formDataLocation = document.querySelectorAll('[name="location"]')
+  formDataCGU = document.getElementById('checkbox1')
+  formDataNewsletter = document.getElementById('checkbox2')
 
-// launch modal form
-function launchModal() {
+  thanksContainer = document.querySelector('.thanks-container')
+  formBody = document.getElementById('form')
+  formData = document.querySelectorAll(".formData")
+  closeBtn = document.querySelectorAll('.close-btn')
+  updateListener()
+}
+
+/* 
+    Update addEventListener -----------------------------------------------------------
+*/
+updateListener()                              //Première initialisation
+function updateListener() {
+  formDataFirst.addEventListener('change', verifyData)
+  formDataLast.addEventListener('change', verifyData)
+  formDataBirthdate.addEventListener('change', verifyData)
+  formDataEmail.addEventListener('change', verifyData)
+  formDataQuantity.addEventListener('change', verifyData)
+  formDataLocation.forEach((checkboxLocation) => checkboxLocation.addEventListener('change', verifyData))
+  formDataCGU.addEventListener('change', verifyData)
+
+  closeBtn.forEach((btn) => btn.addEventListener('click', closeModal))
+}
+
+
+
+/*
+    Lunch modal ---------------------------------------------------------------------
+*/
+modalBtn.forEach((btn) =>                       // launch modal event
+  btn.addEventListener("click", launchModal)); 
+
+
+function launchModal() {                        // launch modal form
   modalbg.style.display = "block";
 
 }
 
-// **************  Close modal ***************
-//close modal event
-closeBtn.forEach((btn) => btn.addEventListener('click', closeModal))
+/*
+    Close modal ------------------------------------------------------------------
+*/
+closeBtn.forEach((btn) =>                       // close modal event
+  btn.addEventListener('click', closeModal))
 
-//Met à jour la liste des bouttons close
-function updateCloseBtn() {
-  closeBtn = document.querySelectorAll('.close-btn')
-  closeBtn.forEach((btn) => btn.addEventListener('click', closeModal))
-}
-// close modal form
 function closeModal() {
-  //Vérifie si message remerciement
-  const thanksContainer = document.querySelector('.thanks-container')
-  if (thanksContainer) {
-    thanksContainer.remove()
-    const formBody = document.getElementById('form')
-    formBody.style.display = "block";
+  if (thanksContainer) {                        // SI message remerciement
+    thanksContainer.remove()                    // Enlever remerciement
+    formBody.style.display = "block";           // Afficher Formulaire DOM
   }
-  //fait disparaitre le modal
-  modalbg.style.display = "none"
 
+  modalbg.style.display = "none"                // Fermer la modale
 }
 
 
-// **************  Verify Input ***************
-
-// verify value in input form
-formDataFirst.addEventListener('change', verifyData)
-formDataLast.addEventListener('change', verifyData)
-formDataBirthdate.addEventListener('change', verifyData)
-formDataEmail.addEventListener('change', verifyData)
-formDataQuantity.addEventListener('change', verifyData)
-formDataLocation.forEach((checkboxLocation) => checkboxLocation.addEventListener('change', verifyData))
-formDataCGU.addEventListener('change', verifyData)
-
-// function for control value
+/*
+    Verify Input ------------------------------------------------------------------
+*/
 function verifyData(data) {
-  //different REGEX for verify
+  /*
+      Regex Restriction ----------------------------------
+   */
   const alphabetRegex = new RegExp("^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$")
   const emailRegex = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$")
   const numberRegex = /^\d+$/
   // de 19 ou 20 avec 0-9 et 0-9 ensuite - 0-9 ou 10-12 ensuite - 01-09 ou 10-29 ou 30-31
   const dateRegex = /^(?:(?:19|20)\d\d)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
 
-  //variable resultat si vraie ou faux (faux pas defaut)
-  let resultat = false
-
-  //Ce bloc vérifie si la vérification est vrai ou faux
+  
+  /*
+      Regex Control ----------------------------------
+   */
+  let resultat = false                            // Resultat Regex - Vrai ou Faux
+  console.log(data.target.value)
   if (data.target.id === "first" || data.target.id === "last") {
     resultat = alphabetRegex.test(data.target.value)
 
@@ -107,101 +140,116 @@ function verifyData(data) {
     ne correspond à aucun champ de saisi.`)
   }
 
-  //Ce bloc active ou désactive le message d'erreur
-  if (resultat) {
-    //Si OK je désactive l'erreur
+  /*
+      Active OR Disable -> Message Error --------------------------------------
+   */
+  if (resultat) {  //---------------------------- Si OK je désactive l'erreur
+                                                  
     data.target.parentElement.setAttribute("data-error-visible", "false")
     // console.log(`La valeur ${data.target.value} est validé`)
 
-  } else {
-    //Si NOK j'active l'erreur
+  } else {  //----------------------------------- Si NOK j'active l'erreur
+
     data.target.parentElement.setAttribute("data-error-visible", "true")
     // console.error(`La valeur ${data.target.value} est refusé`)
   } 
 }
 
 
-// **************  Validate Form ***************
-
-//validate form
-function validate(event) {
+/*
+    Validate Form ------------------------------------------------------------------
+*/
+function validate(event) { // ------------------------------ Control if Form Ok - Nok
   try {
-    //******************* Controle si formulaire Ok ou Nok ************************/
+    
+    event.preventDefault()                      //désactive le rafraichissement de la page
 
-    //désactive le rafraichissement de la page
-    event.preventDefault()
-    //initialise un tableau vide pour lister les erreurs
-    let arrayDataError = []
-    //lecture de  tout les input pour afficher les champs avec des erreurs
-    //le bloc ajoute également les valeur au tableau
+    /*
+      Data-Error Control ----------------------------------
+   */    
+    let arrayDataError = []                     //initialise un tableau vide pour lister les erreurs
+
     formData.forEach((element) => {
-      //obtient la valeur de data-error-visible
-      let dataErrorVisible = element.getAttribute('data-error-visible')
-      //ajoute dans le tableau la valeur "data-error-visible" du champ
-      let addDataError = arrayDataError.push(dataErrorVisible)
-      //si des champs n'ont pas été modifié ça affiche une erreur
-      //car les champs erroné sont déjà marqué en erreur
-      if (dataErrorVisible === null) {
+      let dataErrorVisible =                    //obtient la valeur de data-error-visible
+        element.getAttribute('data-error-visible')
+      
+      arrayDataError.push(dataErrorVisible)     //ajoute dans le tableau la valeur "data-error-visible"
+      
+      
+      if (dataErrorVisible === null) {          //si des champs n'ont pas été modifié ça affiche une erreur
+                                                //car les champs erroné sont déjà marqué en erreur
         element.setAttribute("data-error-visible", "true")
       }
     })
 
-    
     //"every" renvoie true si toute les valeurs du tableau sont égale à 'false'
     //C'est à dire aucune error
     const validData = arrayDataError.every(value => value === 'false')
-    //******************* Si Ok ou Nok ************************/
+    
+    /*
+      Validate form if no errors  ----------------------------------
+   */
     if (validData) {
-      //stock dans arrayLocation la ville choisi
+      /*
+        Stock location  ----------------------------------
+      */
       let arrayLocation = []
       formDataLocation.forEach((element) => {
         if (element.checked) {
           arrayLocation.push(element.value) }
       })
-
+      /*
+        Saving form  ----------------------------------
+      */
       const responseForm = {
         firstName: formDataFirst.value,
         lastName: formDataLast.value,
         email: formDataEmail.value,
         birthdate: formDataBirthdate.value,
         numberGameOne: formDataQuantity.value,
-        whatTournament: arrayLocation,
+        whatTournament: arrayLocation,                // Location
         cgu: formDataCGU.checked ? "Accepté" : "Refusé",
         newsletter: formDataNewsletter.checked ? "Accepté" : "Refusé"
-
       }
+
+      formBody.style.display = "none"                 // Désactive le formulaire
+      
+      const thanksMessage =                           // Initialise le message
+      `
+        <div class="thanks-container">
+        <p class="thanks-container-text">
+          Merci pour votre inscription
+        </p>
+        <button class="thanks-container-btn close-btn">
+          Fermer
+        </button>
+        </div>
+      `
+      formBody.reset()                                // Supprime contenu des input
+      
+      formData.forEach((element) => {                 // Enlève le controle des erreur
+        element.removeAttribute("data-error-visible")
+      })
+      //                                            /!\  Change la structure du DOM
+      modalBody.innerHTML += thanksMessage  //           Ajoute au DOM le message remerciement
+      //                                                 += pour par supprimer le contenu de modalBody
 
       console.log(responseForm)
       console.log("Formulaire envoyé.")
+      updateInputForm()                               // Mise à jour du DOM
 
-      //Désactive le formulaire
-      const formBody = document.getElementById('form')
-      formBody.style.display = "none"
-      //Initialise le message
-      const thanksMessage = `
-      <div class="thanks-container">
-      <p class="thanks-container-text">
-        Merci pour votre inscription
-      </p>
-      <button class="thanks-container-btn close-btn">
-        Fermer
-      </button>
-      </div>
-      `
-      //Ajoute au DOM le message remerciement
-      // += pour par supprimer le contenu de modalBody
-      modalBody.innerHTML += thanksMessage
-      //Met à jour les boutton close
-      updateCloseBtn()
-
+    /*
+      Not validate form if errors  ----------------------------------
+   */
     } else {
       console.log("Formulaire non envoyé.")
-      
     }
 
   } catch (error) {
     console.error("Il y a une erreur lors de l'envoi " + error.message)
   }
-
-
 }
+
+
+
+
