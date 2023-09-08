d5bddce9f9e95415b4c3ac46f68c1f5bfd304ce5
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-08 20:52:58 +0200

    Ajout d'une class opened-modal à la modale et création d'une fonction closeModal qui ferme la modale au clic sur la croix

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,31 @@
+// VARIABLES
+const modalForm = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeButton = document.querySelector(".close");
+
+
+
+
+
+
+// ouverture de la modale au clic sur le bouton
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// function pour ouvrir la modale avec ajout d'une class "opened-modal"
+function launchModal() {
+  modalForm.classList.add("opened-modal")
+}
+
+//fermeture de la modale au clic sur la croix
+
+closeButton.addEventListener("click", closeModal)
+
+// function pour fermer la modale
+function closeModal() {
+  modalForm.classList.remove("opened-modal")
+}
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,18 +34,3 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-
