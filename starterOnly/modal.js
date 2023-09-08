3d455995ddb3d21895bcb1825254fdc8ad142256
Spratch <contact@josephclenet.fr>
2023-09-08 11:22:49 +0200

    modal close button

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCross = document.querySelector(".close");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//close modal event
+modalCross.addEventListener("click", closeModal);
