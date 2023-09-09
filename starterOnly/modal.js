74813094dd5eefeb440d84d307c2916efb8ec8cd
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-09 18:06:46 +0200

    Nettoyage du code HTML, ajout des attributs required, pattern, checked et création de la fonction disableSubmit en JS pour vérifier la validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -3,10 +3,8 @@ const modalForm = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeButton = document.querySelector(".close");
-
-
-
-
+const acceptedConditions = document.querySelector("#checkbox1");
+const submitButton = document.querySelector(".btn-submit");
 
 
 // ouverture de la modale au clic sur le bouton
@@ -24,6 +22,21 @@ closeButton.addEventListener("click", closeModal)
 // function pour fermer la modale
 function closeModal() {
   modalForm.classList.remove("opened-modal")
+}
+
+// si les conditions ne sont pas acceptées, le formulaire passe en disabled
+
+acceptedConditions.addEventListener("click", disableSubmit)
+
+function disableSubmit(event) {
+  console.log(event);
+
+  const conditionsAreAccepted = event.target.checked;   //On récupère dans l'événément l'attribut checked dans target. Aperçu dans la console
+  if(conditionsAreAccepted === false) {                 //Si les conditions ne sont pas acceptées, alors on active l'attribut disabled sur le submitButton
+  submitButton.setAttribute("disabled", "true");
+   } else {
+  submitButton.removeAttribute("disabled");       //Si les conditions sont acceptées, alors on retire l'attribut disabled sur le submitButton
+ }
 }
 
 function editNav() {
