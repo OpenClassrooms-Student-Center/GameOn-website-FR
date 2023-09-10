de810d2ad5c2a4803b6cd4c0270a44226d3cdf4c
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-10 22:39:00 +0200

    Création de la modal2 pour le message de remerciement, fermeture de la modal fonctionne

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,9 @@ const email = document.querySelector("#email");
 const birthdate = document.querySelector("#birthdate");
 const radioButton = document.querySelector("#location1");
 const form = document.querySelector("form");
+const modal2 = document.querySelector(".modal2");
+const closeButtonModal2 = document.querySelector(".close-modal2");
+const closeButtonBottomModal2 = document.querySelector(".button-modal2");
 
 // ouverture de la modale au clic sur le bouton
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -130,16 +133,29 @@ function errorOnLocation(event) {
 
 function validate () {   //cette fonction était déjà nommée dans le HTML. Ici on vérifie que le formulaire est valide avec checkValidity
     if(form.checkValidity()) {
-      //closeModal();                   ici, on peut soit appeler la fonction closeModal(), soit ajouter un display = "none";
-      modalForm.style.display = "none";
-      alert("Merci ! Votre réservation a été reçue.")
+      closeModal();                   //ici, on peut soit appeler la fonction closeModal(), soit ajouter un display = "none";
+      modalForm.style.display = 'none';
+      //alert("Merci ! Votre réservation a été reçue.") //
     }
+
 }
 
 
+function openModal2() {
+    modal2.style.display = 'block';
+    modal2.classList.add("opened-modal")
+};
+
+//fermeture de la modale 2 au clic sur la croix ou sur le bouton fermer
+
+closeButtonModal2.addEventListener("click", closeModal2)
+closeButtonBottomModal2.addEventListener("click", closeModal2)
 
 
-
+// function pour fermer la modale
+function closeModal2() {
+  modal2.style.display = "none";
+}
 
 
 function editNav() {
