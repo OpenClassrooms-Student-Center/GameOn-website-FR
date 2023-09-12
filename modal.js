78520938c5ca55d0b879dced457c6ad4b100a708
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2023-09-12 14:17:01 +0200

    Required validation

--- modal.js
+++ modal.js
@@ -15,17 +15,12 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 
 const myForm = modal.querySelector("#modal-form")
 
-myForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-})
 
-
-// launch modal event
+// launch/close modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 modalCloseBtn.addEventListener("click", closeModal)
 
-// launch modal form
+// Animation function of the modal when the modal is oppended
 function launchModal() {
     modalbg.style.display = "block";
     modal.animate({
@@ -37,6 +32,7 @@ function launchModal() {
 
 }
 
+// Animation function of the modal when the modal is closed
 function closeModal(e) {
     setTimeout(() => {
         modalbg.style.display = "none"
@@ -51,7 +47,66 @@ function closeModal(e) {
 
 }
 
+// Prevent the page from reloading when the form is submitted
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
 
+// Handle the validation of the form when the form is submitted
 function validate(form) {
-    console.log(form)
+    let inputs = {
+        firstname: {
+            value: form['first'].value.trim(), required: true, validation: (value) => {
+                isName(value)
+            }
+        }, lastname: {
+            value: form['last'].value.trim(), required: true, validation: (value) => {
+                isName(value)
+            }
+        }, email: {
+            value: form['email'].value.trim(), required: true, validation: (value) => {
+                isEmail(value)
+            }
+        }, birthday: {
+            value: form['birthdate'].value.trim(), required: true, validation: (value) => {
+                isCorrectBirthDate(value)
+            }
+        }, tournament: {
+            value: form['quantity'].value.trim(), required: true,
+        }, location: {
+            value: form['location'].value.trim(), required: true
+        }, termsOfUse: {
+            value: form['termsOfUse'].checked, required: true
+        }, newsletter: {
+            value: form['newsletter'].checked
+        }
+    }
+    for (const [key, input] of Object.entries(inputs)) {
+        const {value, required} = input;
+        if (isRequired(input)) {
+            console.error(`Please complete the form for ${key}`);
+        }
+
+    }
+
+}
+
+
+function isRequired(input) {
+    return (input.required && (input.value === "" || input.value === false))
+
+}
+
+function isEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function isName(name) {
+    const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ-' ]+$/;
+    return nameRegex.test(name);
+}
+
+function isCorrectBirthDate(birthDate) {
+    return new Date(birthDate) < new Date();
 }
