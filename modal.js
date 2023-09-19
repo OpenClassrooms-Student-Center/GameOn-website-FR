7848854f312c68a07d285ca1a02e75d51aa83397
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2023-09-19 19:32:50 +0200

    clean and remake of the validation behaviours

--- modal.js
+++ modal.js
@@ -1,4 +1,4 @@
-function editNav() {
+const editNav = () => {
     let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
@@ -18,23 +18,12 @@ const modalbg = document.querySelector(".bground");
 const modal = document.querySelector(".content");
 const modalCloseBtn = modal.querySelector(".modal-close")
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll('.formInput');
-const form = modal.querySelector("form")
-formData.forEach(form => {
-    let input = form.querySelector("input");
-    input.addEventListener("change", () => {
-        if (form.hasAttribute("data-error")) {
-            delete form.dataset.error;
-        }
-    })
-})
+const inputsWrapper = document.querySelectorAll('.formInput');
+const form = document.querySelector('.modalForm');
 
-// launch/close modal event listeners
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalCloseBtn.addEventListener("click", closeModal)
 
-// Animation function of the modal when the modal is oppended
-function launchModal() {
+// Animation const of the modal when the modal is oppended
+const launchModal = () => {
     modalbg.style.display = "block";
     modal.animate({
         opacity: [0, 1], transform: ["translateY(150px)", "translateX(0)"]
@@ -45,7 +34,7 @@ function launchModal() {
 }
 
 // Animation function of the modal when the modal is closed
-function closeModal() {
+const closeModal = () => {
     setTimeout(() => {
         modalbg.style.display = "none"
     }, 390)
@@ -59,130 +48,262 @@ function closeModal() {
 
 }
 
-// Prevent the page from reloading when the form is submitted
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalCloseBtn.addEventListener("click", closeModal)
+
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-})
-
-// Handle the validation of the form when the form is submitted
-function validate(form) {
-
-    let hasErrors = false;
-    // remove all errors (function?)
-    formData.forEach(element => {
-        if (element.hasAttribute("data-error")) {
-            delete form.dataset.error;
-        }
-    })
-
-    // Get all the input fields values
-    let inputs = {
-        firstname: {
-            value: form['first'].value.trim(), domElement: form['first'], required: true, validation: {
-                validate: value => {
-                    return isName(value)
-                }, message: "Veuillez entrer un prénom valide contenant au moins 2 caractères alphabétiques."
-            }
-        }, lastname: {
-            value: form['last'].value.trim(), domElement: form['last'], required: true, validation: {
-                validate: value => {
-                    return isName(value)
-                }, message: "Veuillez entrer un nom de famille valide contenant au moins 2 caractères alphabétiques."
-            }
-        }, email: {
-            value: form['email'].value.trim(), domElement: form['email'], required: true, validation: {
-                validate: value => {
-                    return isEmail(value)
-                }, message: "Veuillez entrer une adresse email valide."
-            }
-        }, birthday: {
-            value: form['birthdate'].value.trim(), domElement: form['birthdate'], required: true, validation: {
-                validate: value => {
-                    return isValidBirthdate(value)
-                }, message: "Veuillez entrer une date de naissance valide."
-            }
-        }, tournament: {
-            value: form['quantity'].value.trim(), domElement: form['quantity'], required: true, validation: {
-                validate: value => {
-                    return isPositiveInteger(value)
-                }, message: "Veuillez entrer un nombre entier positif pour le nombre de tournois."
-            }
-        }, location: {
-            value: form['location'].value.trim(), domElement: form['location'], required: true  // Validation
-        }, termsOfUse: {
-            value: form['termsOfUse'].checked, domElement: form['termsOfUse'], required: true // Validation
-        }, newsletter: {
-            value: form['newsletter'].checked, domElement: form['newsletter']
-        }
+    if (!form.checkValidity()) {
+        e.preventDefault();
     }
-
-    // For each inputs, handle required and validation
-    for (const key in inputs) {
-        const input = inputs[key];
-        let formInput;
-        const isNodeList = NodeList.prototype.isPrototypeOf(input.domElement);
-        if (isNodeList) {
-            formInput = input.domElement[0].closest('.formInput')
-        } else {
-            formInput = input.domElement.closest('.formInput')
+})
+let formData = {};
+const getAllInputs = () => {
+    formData = {
+        first: form['first'],
+        last: form['last'],
+        email: form['email'],
+        birthdate: form['birthdate'],
+        quantity: form['quantity'],
+        location: form['location'],
+        termsOfUse: form['termsOfUse'],
+        newsletter: form['newsletter']
+    }
+}
+getAllInputs();
+const validate = form => {
+    for (const name in formData) {
+        let input = formData[name];
+        resetError(input);
+        if (isRequired(input)) {
+            handleErrorValidation(input, "Ce champs ne peut pas être vide.");
+            continue;
         }
+        validation(input)
+    }
+}
 
-        console.log(formInput)
+inputsWrapper.forEach(input => {
+    input.addEventListener("input", (e) => {
+        let input = formData[e.target.name];
+        resetError(input);
 
         if (isRequired(input)) {
-            handleErrorValidation(formInput, "Ce champ ne peut pas être vide.")
-            hasErrors = true;
-            continue; // next iteration
+            handleErrorValidation(input, "Ce champs ne peut pas être vide.")
         }
+        validation(input)
+    })
+})
 
-        if (input.hasOwnProperty("validation")) {
-            if (!input.validation.validate(input.value)) {
-                handleErrorValidation(formInput, input.validation.message);
-                hasErrors = true;
-            }
-        }
+const validation = input => {
+    const name = getInputName(input);
+    const value = getInputValue(input);
+    if ((name === "first" || name === "last") && !isValidName(value)) {
+        handleErrorValidation(input, "Nom invalide : minimum 2 caractères et caractères spéciaux non autorisés.")
+    } else if (name === "email" && !isValidEmail(value)) {
+        handleErrorValidation(input, "Adresse e-mail invalide. Veuillez entrer une adresse e-mail valide.")
+    } else if (name === "birthdate" && !isValidBirthdate(value)) {
+        handleErrorValidation(input, "Date de naissance invalide. Veuillez entrer une date de naissance valide.")
+    } else if (name === "quantity" && !isPositiveInteger(value)) {
+        handleErrorValidation(input, "Quantité invalide. Veuillez entrer un nombre entier positif.");
+    } else if (name === "termsOfUse" && !value) {
+        // CHECKBOX ERROR MESSAGE
     }
-
-    if (hasErrors) {
-        modal.classList.add("shake");
-        setTimeout(() => {
-            modal.classList.remove("shake");
-        }, 1000)
-        return false;
-    } else {
-        closeModal()
-        setTimeout(() => {
-            form.submit();
-        }, 1000)
+    let wrapper = getInputWrapper(input);
+    if (!wrapper.hasAttribute("data-error")) {
+        input.classList.add("success");
     }
 }
 
-function handleErrorValidation(input, message) {
-    const errorElement = input.querySelector('.validation-error');
-    input.dataset.error = "true";
-    errorElement ? errorElement.innerText = String(message) : null;
+
+const isValidEmail = value => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(value);
 }
 
-function isRequired(input) {
-    return (input.required && (!input.value))
+const isValidName = value => {
+    const regex = /^[a-zA-Z ,.'-]{2,}$/;
+    return regex.test(value);
 }
 
-function isEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+const isValidBirthdate = value => {
+    const birthdate = new Date(value);
+    return !(birthdate > new Date() || birthdate.getFullYear() < 1900);
 }
 
-function isName(name) {
-    const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ-' ]+$/;
-    return nameRegex.test(name);
-}
-
-function isValidBirthdate(birthDate) {
-    return new Date(birthDate) < new Date();
-}
-
-function isPositiveInteger(value) {
+const isPositiveInteger = value => {
     const number = Number(value);
     return Number.isInteger(number) && number >= 0
 }
+const resetError = input => {
+    const wrapper = getInputWrapper(input);
+    if (wrapper.hasAttribute("data-error")) {
+        wrapper.dataset.error = "false";
+    }
+}
 
+
+const getInputValue = input => {
+    return input.type === "checkbox" ? input.checked : input.value;
+}
+
+const getInputName = input => {
+    const isNodeList = NodeList.prototype.isPrototypeOf(input);
+    if (isNodeList) {
+        return input[0].name
+    } else {
+        return input.name;
+    }
+}
+const isRequired = input => {
+    return input.required && !getInputValue(input);
+}
+const getInputWrapper = input => {
+    let formInput;
+    const isNodeList = NodeList.prototype.isPrototypeOf(input);
+    if (isNodeList) {
+        formInput = input[0].closest('.formInput')
+    } else {
+        formInput = input.closest('.formInput')
+    }
+
+    return formInput;
+}
+const handleErrorValidation = (input, message) => {
+    const inputWrapper = getInputWrapper(input);
+    const errorElement = inputWrapper.querySelector('.validation-error');
+    inputWrapper.dataset.error = "true";
+    errorElement ? errorElement.innerText = String(message) : null;
+}
+
+// const getInputFieldValues = (name, isRequired = false, validate = null) => {
+//     const value = form[name].value.trim();
+//     const domElement = form[name];
+//     let validation = null;
+//
+//     if (typeof validate === 'function') {
+//         validation = {validate};
+//     } else if (typeof validate === 'object' && validate !== null && typeof validate.function === 'function') {
+//         validation = {validate: validate.function, message: validate.message};
+//     }
+//
+//     return {value, domElement, required: isRequired, validation};
+// }
+//
+//
+// // Handle the validation of the form when the form is submitted
+// const validate = form => {
+//     resetErrorMessage();
+//     let hasErrors = false;
+//     let inputs = {
+//         firstname: getInputFieldValues("first", true, {
+//             function: isName, message: "Veuillez entrer un prénom valide."
+//         }),
+//         lastname: getInputFieldValues("last", true, {
+//             function: isName, message: "Veuillez entrer un nom de famille valide."
+//         }),
+//         email: getInputFieldValues("email", true, {
+//             function: isEmail, message: "Veuillez entrer une adresse e-mail valide."
+//         }),
+//         birthday: getInputFieldValues("birthdate", true, {
+//             function: isValidBirthdate, message: "Veuillez entrer une date de naissance valide."
+//         }),
+//         tournament: getInputFieldValues("quantity", true, {
+//             function: isPositiveInteger, message: "Veuillez entrer une valeur numérique positive."
+//         }),
+//         location: getInputFieldValues("location", true),
+//         termsOfUse: getInputFieldValues("termsOfUse", true),
+//         newsletter: getInputFieldValues("newsletter"),
+//     }
+//
+//     // For each inputs, handle required and validation
+//     for (const name in inputs) {
+//         const input = inputs[name];
+//         let formInput;
+//         const isNodeList = NodeList.prototype.isPrototypeOf(input.domElement);
+//         if (isNodeList) {
+//             formInput = input.domElement[0].closest('.formInput')
+//         } else {
+//             formInput = input.domElement.closest('.formInput')
+//         }
+//
+//         if (isRequired(input)) {
+//             console.log(input)
+//             handleErrorValidation(formInput, "Ce champ ne peut pas être vide.")
+//             hasErrors = true;
+//             continue;
+//         }
+//
+//         if (input.validation) {
+//             if (!input.validation.validate(input.value)) {
+//                 let errorMessage = input.validation.message ? input.validation.message : "La valeur saisie est incorrecte. Veuillez vérifier et corriger.";
+//                 handleErrorValidation(formInput, errorMessage)
+//                 hasErrors = true;
+//             }
+//         }
+//     }
+//
+//     checkFormValidity(hasErrors);
+//     inlineValidation();
+//
+// }
+//
+// const handleErrorValidation = (input, message) => {
+//     const errorElement = input.querySelector('.validation-error');
+//     input.dataset.error = "true";
+//     errorElement ? errorElement.innerText = String(message) : null;
+// }
+//
+// const resetErrorMessage = () => {
+//     formData.forEach(form => {
+//         if (form.hasAttribute("data-error")) {
+//             delete form.dataset.error;
+//         }
+//
+//     })
+// }
+//
+// const inlineValidation = () => {
+//     formData.forEach(input => {
+//         input.addEventListener("input", (input) => {
+//             const name = input.target.name;
+//             console.log(getInputFieldValues(name, true, {
+//                 function: isEmail, message: "Veuillez entrer une adresse e-mail valide."
+//             }))
+//         })
+//     })
+// }
+//
+// const isRequired = input => {
+//     return (input.required && (!input.value))
+// }
+//
+// const isEmail = email => {
+//     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(email);
+// }
+//
+// const isName = name => {
+//     const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ-' ]+$/;
+//     return nameRegex.test(name);
+// }
+//
+// const isValidBirthdate = birthDate => {
+//     return new Date(birthDate) < new Date();
+// }
+//
+
+//
+// const checkFormValidity = (errors) => {
+//     if (errors) {
+//         modal.classList.add("shake");
+//         setTimeout(() => {
+//             modal.classList.remove("shake");
+//         }, 1000)
+//         return false;
+//     } else {
+//         closeModal()
+//         setTimeout(() => {
+//             form.submit();
+//         }, 1000)
+//     }
+// }
