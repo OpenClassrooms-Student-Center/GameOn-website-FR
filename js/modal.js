2f7939fba33945e9e249194e0114b725b7f0611d
Maxime Leherquer <max.mo8n@icloud.com>
2023-09-22 12:50:16 +0200

    fix OpenClassrooms-Student-Center/GameOn-website-FR#1

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,9 @@
 function editNav() {
   let x = document.getElementById("myTopnav");
 
-  if (x === null) {
-    return;
-  }
-
-  if (x.className === "topnav") {
+  if (x?.className === "topnav") {
     x.className += " responsive";
-  } else {
+  } else if (x) {
     x.className = "topnav";
   }
 }
@@ -15,12 +11,18 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCloseBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn?.forEach((btn) => btn.addEventListener("click", launchModal));
+modalCloseBtn?.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+function closeModal() {
+  modalbg.style.display = "none";
 }
