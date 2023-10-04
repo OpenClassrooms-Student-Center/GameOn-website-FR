64eba2e3bb107459e46ca409527fbef9b61ece32
hichamecode <hichame.code@gmail.com>
2023-10-04 13:08:09 +0200

    reprise de l'ensemble du code - passage sur de la POO

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,287 +1,181 @@
+// gestion du comportement responsive
+
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var myTopnav = document.getElementById("myTopnav")
+  if (myTopnav.className === "topnav") {
+    myTopnav.className += " responsive"
   } else {
-    x.className = "topnav";
+    myTopnav.className = "topnav"
+  }
+}
+/* on crée la classe Form avec ses attributs et ses méthodes */
+class Form {
+  constructor(formId, modalBtnId, closeBtnId, submitBtnId, modalId) {
+    this.formRequest = document.getElementById(formId)
+    this.openBtn = document.querySelectorAll(modalBtnId)
+    this.closeBtn = document.getElementById(closeBtnId)
+    this.submitBtn = document.querySelector(submitBtnId)
+    this.modalBg = document.getElementById(modalId)
+    this.validationMessage = document.getElementById("validation-message")
+    this.closeValidationBtn = document.getElementById("close-validation-message")
+  }
+  // méthode pour ouvrir la modale
+  openForm() {
+    this.openBtn.forEach((btn) => btn.addEventListener("click", () => {
+      this.modalBg.style.display = "block";
+    }));
+  }
+  // méthode pour fermer la modale
+  closeForm() {
+    this.closeBtn.addEventListener("click", () => {
+      this.modalBg.style.display = "none";
+    });
+  }
+  // méthode pour la gestion des erreurs
+  showError(fieldId, isOk, element) {
+    const errorMessage = document.getElementById(`error-${fieldId}`);
+    if (errorMessage) {
+      if (isOk) {
+        errorMessage.style.display = "none";
+        element.style.border ="none"
+      } else {
+        errorMessage.style.display = "block";
+        element.style.border ="2px solid red"
+      }
+    } 
+  }
+  // méthodes de validation en fonction du type de champ
+  validateText(value, fieldId, element) {
+    const isOk = /^[a-zA-Z-]{2,}$/.test(value); // on teste la valeur du champ avec une regex
+    this.showError(fieldId, isOk, element);
+    return isOk;
+  }
+
+  validateEmail(value, fieldId, element) {
+    const isOk = /^[a-z1-9-_.]+@[a-z1-9-_.]+\.[a-z1-9-_.]+$/.test(value);
+    this.showError(fieldId, isOk, element);
+    return isOk;
+  }
+
+  validateDate(value, fieldId, element) {
+    const currentYear = new Date().getFullYear();
+    const isOk = currentYear - parseInt(value) <= 120 && currentYear - parseInt(value) >= 7; // parseInt() pour convertir string en integer
+    this.showError(fieldId, isOk, element);
+    return isOk;
+  }
+
+  validateNumber(value, fieldId, element) {
+    const isOk = value <= 99 && value !== '';
+    this.showError(fieldId, isOk, element);
+    return isOk;
+  }
+
+  validateRadio(name, fieldId, element) {
+    const radioBtns = document.getElementsByName(name); // on boucle sur les radios pour récupérer une radio ckecked 
+    let isOk = false;
+    for (let i = 0; i < radioBtns.length; i++) {
+      if (radioBtns[i].checked) {
+        isOk = true;
+        break; // break pour casser la boucle quand une valeur true est trouvée
+      }
+    }
+    this.showError("location", isOk, element);
+    return isOk;
+  }
+
+  validateCheckbox(value, fieldId, element) {
+    const isOk = value;
+    this.showError(fieldId, isOk, element);
+    return isOk;
+  }
+  // méthode pour la validation du formulaire avec l'écoute de l'event submit
+  formValidation() {
+    this.formRequest.addEventListener("submit", (event) => {
+      let formIsValid = true; // on initialise la variable à true et on boucle sur tous les champs du formulaire sauf checkbox2 et submitBtn
+
+      for (let i = 0; i < this.formRequest.elements.length; i++) {
+        const element = this.formRequest.elements[i];
+        const formFieldType = element.type;
+        const fieldId = element.id;
+        let isValid = false;
+
+        if (formFieldType === 'text') {
+          isValid = this.validateText(element.value, fieldId, element);
+        } else if (formFieldType === 'email') {
+          isValid = this.validateEmail(element.value, fieldId, element);
+        } else if (formFieldType === 'date') {
+          isValid = this.validateDate(element.value, fieldId, element);
+        } else if (formFieldType === 'number') {
+          isValid = this.validateNumber(element.value, fieldId, element);
+        } else if (formFieldType === 'radio') {
+          isValid = this.validateRadio(element.name, fieldId, element);
+        } else if (formFieldType === 'checkbox' && fieldId === 'checkbox1') {
+          isValid = this.validateCheckbox(element.checked, fieldId, element);
+        } else if (fieldId === 'checkbox2' || formFieldType === 'submit') {
+          continue
+        }
+
+        if (!isValid) {
+          formIsValid = false;
+        }
+      }
+
+      if (formIsValid) {
+        this.modalBg.style.display = "none"
+        this.validationMessage.style.display = "flex";
+        event.preventDefault()
+      } else {
+        event.preventDefault()
+      }
+    });
+  }
+  // méthode pour la validation des champs en temps réel (écoute de l'évenement input)
+  realTimeValidation() {
+    for (let j = 0; j < this.formRequest.elements.length; j++) {
+      const element = this.formRequest.elements[j];
+      const formFieldType = element.type;
+      const fieldId = element.id;
+
+      element.addEventListener("input", () => {
+        let isOk = false;
+        let errorField = fieldId
+        if (formFieldType === 'text') {
+
+          isOk = this.validateText(element.value, fieldId, element);
+        } else if (formFieldType === 'email') {
+          isOk = this.validateEmail(element.value, fieldId, element);
+        } else if (formFieldType === 'date') {
+          isOk = this.validateDate(element.value, fieldId, element);
+        } else if (formFieldType === 'number') {
+          isOk = this.validateNumber(element.value, fieldId, element);
+        } else if (formFieldType === 'radio') {
+          isOk = this.validateRadio(element.name, fieldId, element);
+          errorField = "location" // "location pour la gestion des erreurs sur les radios"
+        } else if (formFieldType === 'checkbox' && fieldId === 'checkbox1') {
+          isOk = this.validateCheckbox(element.checked, fieldId, element);
+        }
+        this.showError(errorField, isOk, element);
+      });
+    }
+  }
+
+  // méthode pour la fermeture du message de validation
+  closeValidationMessage() {
+    this.closeValidationBtn.addEventListener("click", () => {
+      this.validationMessage.style.display = "none";
+    });
+  }
+
+  // méthode init() pour regrouper toutes les méthodes et les exécuter
+  init() {
+    this.openForm();
+    this.closeForm();
+    this.realTimeValidation();
+    this.formValidation();
+    this.closeValidationMessage()
   }
 }
 
-// DOM Elements
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalBg = document.getElementById("modal")
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalBg.style.display = "block";
-}
-
-let contentBg = document.querySelector(".content")
-
-/* FERMETURE DE LA MODALE AU CLIC SUR LA CROIX */
-
-const closeModalBtn = document.getElementById("close-modal-btn")
-
-closeModalBtn.addEventListener("click", () => {
-
-  modalBg.style.display = "none"
-})
-
-/* PRENOM - On crée une var bool si les conditions de validation du prénom sont remplies */
-/* On execute formValidation */
-
-let firstNameOk = false
-let firstName = document.getElementById("first-name")
-let firstNameMessage = document.getElementById("error-first-name")
-
-firstName.addEventListener("input", () => {
-
-  let regexFirstName = new RegExp("[a-zA-Z-]+")
-  let regexFirstNameOk = regexFirstName.test(firstName.value)
-
-  if (firstName.value != "" && firstName.value.length >= 2 && regexFirstNameOk === true) {
-
-    firstNameOk = true
-    firstName.style.border = "2px solid white"
-    firstNameMessage.style.display = "none"
-
-  }
-  else {
-    firstNameOk = false
-    firstName.style.border = "2px solid #fe142f"
-    firstNameMessage.style.display = "block"
-
-  }
-  formValidation()
-})
-
-/* NOM - On crée une var bool si les conditions de validation du nom sont remplies */
-/* On execute formValidation */
-
-let lastNameOk = false
-let lastName = document.getElementById("last-name")
-let lastNameMessage = document.getElementById("error-last-name")
-
-lastName.addEventListener("input", () => {
-
-  let regexLastName = new RegExp("[a-zA-Z-]+")
-  let regexLastNameOk = regexLastName.test(lastName.value)
-
-  if (lastName.value != "" && lastName.value.length >= 2 && regexLastNameOk === true) {
-
-    lastNameOk = true
-    lastName.style.border = "2px solid white"
-    lastNameMessage.style.display = "none"
-  }
-  else {
-    lastNameOk = false
-    lastName.style.border = "2px solid #fe142f"
-    lastNameMessage.style.display = "block"
-  }
-  formValidation()
-})
-
-/* EMAIL - On crée une var bool depuis le regex de validation en récupérant la valeur du test */
-/* On execute formValidation */
-
-let emailUserOk = false
-let emailUser = document.getElementById("email")
-let emailMessage = document.getElementById("error-email")
-
-emailUser.addEventListener("input", () => {
-
-  let regexEmail = new RegExp("[a-z1-9-_.]+@[a-z1-9-_.]+\\.[a-z1-9-_.]+")
-  let regexEmailUserOk = regexEmail.test(emailUser.value)
-
-  if (emailUser.value != "" && regexEmailUserOk == true) {
-    emailUserOk = true
-    emailUser.style.border = "2px solid white"
-    emailMessage.style.display = "none"
-  }
-  else {
-    emailUserOk = false
-    emailUser.style.border = "2px solid #fe142f"
-    emailMessage.style.display = "block"
-  }
-
-  formValidation()
-})
-
-/* AGE - On crée une var bool => != entre année actuelle et les 4 1ers caractères de la date de naissance */
-/* On execute formValidation */
-
-let birthDateOk = false
-let birthDate = document.getElementById("birth-date")
-let birthDateMessage = document.getElementById("error-birth-date")
-
-birthDate.addEventListener("input", () => {
-
-  let birthDateValue = birthDate.value
-  let birthDateYear = birthDateValue.substring(0, 4)
-
-  let currentYear = new Date().getFullYear()
-
-  if (currentYear - birthDateYear <= 100 && currentYear - birthDateYear >= 7) {
-
-    birthDateOk = true
-    birthDate.style.border = "2px solid white"
-    birthDateMessage.style.display = "none"
-  }
-  else {
-    birthDateOk = false
-    birthDate.style.border = "2px solid #fe142f"
-    birthDateMessage.style.display = "block"
-  }
-  formValidation()
-})
-
-/* NBRE DE TOURNOIS - On crée une var bool si les conditions de validation sont remplies  */
-/* On execute formValidation */
-
-let nberTournamentsOk = false
-let nberTournaments = document.getElementById("quantity")
-let nberTournamentsMessage = document.getElementById("error-quantity")
-
-nberTournaments.addEventListener("input", () => {
-
-  if (nberTournaments.value != "" && nberTournaments.value <= 99) {
-
-    nberTournamentsOk = true
-    nberTournaments.style.border = "2px solid white"
-    nberTournamentsMessage.style.display = "none"
-  }
-  else {
-    nberTournamentsOk = false
-    nberTournaments.style.border = "2px solid #fe142f"
-    nberTournamentsMessage.style.display = "block"
-  }
-  formValidation()
-})
-
-/* CHOIX DU TOURNOI - On crée une boucle for pour écouter chaque élement à la suite */
-/* On execute formValidation */
-
-let locationListOk = false
-let locationList = document.querySelectorAll("input[name=location]")
-let locationListMessage = document.getElementById("error-location")
-
-for (let i = 0; i < locationList.length; i++) {
-  locationList[i].addEventListener("input", () => {
-
-    if (locationList[i] != "") {
-
-      locationListOk = true
-      locationListMessage.style.display = "none"
-    }
-    else {
-      locationListOk = false
-    }
-    formValidation()
-  })
-}
-
-/* CONDITIONS D'UTILISATION -  On crée un bool avec la condition checked pour les radios */
-/* On execute formValidation */
-
-let termsAndConditionsOk = false
-let termsAndConditions = document.getElementById("checkbox1")
-let termsAndConditionsMessage = document.getElementById("error-terms-conditions")
-
-termsAndConditions.addEventListener("change", () => {
-  let termsAndConditionsChecked = termsAndConditions.checked
-  if (termsAndConditionsChecked == true) {
-
-    termsAndConditionsOk = true
-    termsAndConditionsMessage.style.display = "none"
-  }
-  else {
-    termsAndConditionsOk = false
-  }
-  formValidation()
-})
-
-
-/* formValidation () : pour chaque bool de chaque champ, on boucle avec forEach sur l'array allResults */
-
-let result = false
-let submitBtn = document.querySelector(".btn-submit")
-let form = document.getElementById("form")
-
-
-function formValidation() {
-
-  let elementValidated = 0
-  let allResults = [firstNameOk, lastNameOk, emailUserOk, birthDateOk, nberTournamentsOk]
-
-  allResults.forEach(element => {
-
-    if (element == true) {
-      elementValidated++
-    }
-
-  })
-
-  let result = allResults.length == elementValidated // si la taille de l'array a la même valeur que le nbre d'elements valides
-
-  /* On écoute l'event submit sur le bouton, ensuite système à 3 conditions pour savoir quel message d'erreur va s'afficher */ 
-
-  form.addEventListener("submit", (event) => {
-
-    console.log("result " + result + " locationListOk " + locationListOk + " termsandconditionsOk " + termsAndConditionsOk)
-
-    if (result == false) {
-
-      console.log("formulaire incomplet")
-      event.preventDefault()
-    }
-
-    else if (result == true && locationListOk == true && termsAndConditionsOk == true) {
-
-      console.log("le résultat est bon")
-      modalBg.style.display = "none"
-      validationMessage.style.display = "flex"
-      locationListMessage.style.display = "none"
-
-    } else if (result == true && locationListOk == true && termsAndConditionsOk == false) {
-
-      console.log("merci d'accepter les conditions générales")
-      event.preventDefault()
-      locationListMessage.style.display = "none"
-      termsAndConditionsMessage.style.display = "block"
-
-    }
-
-    else if (result == true && locationListOk == false && termsAndConditionsOk == false) {
-
-      console.log("merci de remplir les 2 champs manquants")
-      event.preventDefault()
-      locationListMessage.style.display = "block"
-      termsAndConditionsMessage.style.display = "block"
-    }
-
-    else if (result == true && locationListOk == false && termsAndConditionsOk == true) {
-
-      console.log("merci de choisiir une ville")
-      event.preventDefault()
-      locationListMessage.style.display = "block"
-      termsAndConditionsMessage.style.display = "none"
-    }
-
-  })
-
-}
-
-
-// closing the validationMessage 
-
-let validationMessage = document.getElementById("validation-message")
-let closeValidationBtn = document.getElementById("close-validation-message")
-
-closeValidationBtn.addEventListener("click", () => {
-
-  validationMessage.style.display = "none"
-})
+// instanciation de la classe avec newForm, application des méthodes via init()
+const newForm = new Form('form', '.modal-btn', 'close-modal-btn', '.btn-submit', 'modal');
+newForm.init();
