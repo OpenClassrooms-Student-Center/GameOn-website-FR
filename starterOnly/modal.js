501c66bcb186d0844b81ad9a9021462ef30ef76d
Mary P <marylaboitebis@gmail.com>
2023-10-04 12:07:13 +0200

    envoi fichiers

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,12 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const btnClose = document.querySelector(".close");
+const icon = document.querySelector(".icon");
+const  mainNavbarResponsive = document.querySelector(" #myTopnav .main-navbar").firstElementChild;
+
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,10 +16,17 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+// add margin-top on menu responsive
+icon.addEventListener("click", () =>{
+  mainNavbarResponsive.classList.add("main-navbarResponsive");
+});
+
+
+
+//Closing modal
+btnClose.addEventListener("click", () =>{
+  modalbg.style.display = "none";
+});
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +35,3 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
