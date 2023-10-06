2cec65dc12a44186a677303130153a9dd0bd5469
SafaSELLAM <safa.sellam@outlook.fr>
2023-10-06 12:00:08 +0200

    New : close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//close modal event
+closeModal.addEventListener("click", closeModalHandler);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+//close modal form
+function closeModalHandler() {
+  modalbg.style.display = "none";
+}
