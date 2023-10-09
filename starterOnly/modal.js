e5da8e71ca37825f6b5baaef271e704f7384f829
Pierre HENRY <pierre.henry@code99.fr>
2023-10-10 00:08:45 +0200

    #2 - Validation du formulaire et correction HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,3 +27,45 @@ closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+// Vérification des données du formulaire
+const form = document.querySelector("form");
+
+const validators = [
+  { name: "first", pattern: /^\S{2}/ },
+  { name: "last", pattern: /^\S{2}/ },
+  { name: "email", internal: true },
+  { name: "quantity", pattern: /^\d+$/ },
+  { name: "location", pattern: /^.+$/ },
+  { name: "cgu", checked: true },
+];
+
+function isValidate(validator) {
+  const data = form[validator.name];
+  if (!data) {
+    console.log(
+      `Le champs '${validator.name}' ne semble pas disponible dans ce formulaire !`
+    );
+  }
+  if (validator.pattern) {
+    return data.value.match(validator.pattern);
+  } else if (validator.internal) {
+    return data.validity.valid;
+  } else if (validator.checked !== undefined) {
+    return data.checked === validator.checked;
+  }
+  return false;
+}
+
+function validate() {
+  let errors = 0;
+  validators.forEach((validator) => {
+    if (isValidate(validator)) {
+      // Masquer les messages d'erreurs sur le formulaire
+    } else {
+      // Afficher les messages d'erreurs sur le formulaire
+      errors++;
+    }
+  });
+  return errors === 0;
+}
