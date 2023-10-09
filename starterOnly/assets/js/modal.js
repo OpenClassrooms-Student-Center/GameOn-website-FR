471ff3048b2ec6ae94db79973d01418af6fc271d
badgohan <kaori972@hotmail.fr>
2023-10-09 16:17:07 +0200

    fermeture de la modal

--- starterOnly/assets/js/modal.js
+++ starterOnly/assets/js/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,8 +21,19 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event, click x button
+closeBtn.addEventListener("click", closeModal);
+
+// close modal form
 function closeModal() {
-  
+  modalbg.style.display = "none";
 }
+
+// close modal event, click outside modal
+modalbg.addEventListener('click', function (event) {
+  if (event.target === modalbg) {
+    closeModal();
+  }
+});
 
 
