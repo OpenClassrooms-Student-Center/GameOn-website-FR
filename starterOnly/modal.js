4bd92d1a1ee0ed56db4a86d694c5625f02cbc164
Pierre HENRY <pierre.henry@code99.fr>
2023-10-13 23:54:54 +0200

    Simplification des traitements JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,104 +1,42 @@
+// Ajout d'événement personnalisés pour "open" et
+// "close" afin de gérer l'ouverture et/ou la formeture
+// d'une modale (par exemple, pour initialiser un
+// formulaire)
 const openEvent = new CustomEvent("open");
 const closeEvent = new CustomEvent("close");
 
 const observer = new MutationObserver((records) => {
-  for (const { attributeName, target } of records) {
-    if (attributeName === "open") {
-      if (target.getAttribute("open") == "true") {
-        target.dispatchEvent(openEvent);
-      } else {
-        target.dispatchEvent(closeEvent);
-      }
-    }
-  }
+  for (const { attributeName, target } of records)
+    if (attributeName === "open")
+      target.dispatchEvent(
+        target.getAttribute("open") == "true" ? openEvent : closeEvent
+      );
 });
 
-// DOM Elements
-const topNav = document.querySelector("#myTopnav");
+for (const element of document.querySelectorAll(".modal"))
+  observer.observe(element, { attributes: true });
 
-const signupModal = document.querySelector("#modal-signup");
-const signupForm = signupModal.querySelector("form[name=reserve]");
-const formData = signupForm.querySelectorAll(".formData");
-
-const modalDone = document.querySelector("#modal-done");
-
-/**
- * Démarre une modale
- *
- * Assure que la modale soit initialisé a chaque lancement,
- * si c'est nécesaire.
- *
- **/
-function launchModal(modal) {
+// Functions pour gérer les modales
+function showModal(modal) {
   modal.classList.add("show");
   modal.setAttribute("open", true);
 }
 
-/**
- * Ferme une modale
- *
- * Permet le lancement d'un animation pour lors de la fermeture
- * de la modale
- */
 function closeModal(modal) {
   modal.setAttribute("open", false);
   modal.classList.remove("show");
 }
 
-/**
- * Initialisation du formulaire de réservation
- *
- * Permet d'initialiser le formulaire de réservation, afin de
- * revenir a son état d'origine, tout en assurant que la date
- * maximum soit actualisée (vérification de l'âge)
- *
- */
-function initForm(modal) {
-  console.log("Init");
-  signupForm.reset();
-  formData.forEach((element) => {
-    element.dataset.errorVisible = false;
-  });
-  const max = new Date();
-  max.setFullYear(max.getFullYear() - 18);
-  signupForm.birthdate.max = max.toISOString().slice(0, 10);
-}
-
-/**
- * Vérification des données du formulaire
- *
- * Permet de vérifier la validatité d'un formulaire, en se
- * basant sur la présence de l'attribut required, et de la
- * propriété validity des <input>, ainsi, il suffit de
- * spécifier les contraintes directement dans le code HTML
- * quand c'est nécessaire (min, max, pattern, etc...)
- *
- * Permet également d'afficher les messages d'erreurs
- *
- */
-function formValidate(e) {
-  e.preventDefault();
-  const errors = [];
-  formData.forEach((element) => {
-    const input = element.querySelector("input");
-    if (
-      input?.required &&
-      (element.dataset.errorVisible = !input.validity.valid)
-    ) {
-      errors.push(input);
-    }
-  });
-  if (errors.length === 0) {
-    // ...
-    closeModal(signupModal);
-    launchModal(modalDone);
+function toggleModal(modal) {
+  if (isShow(modal)) {
+    showModal(modal);
   } else {
-    errors[0].focus();
+    closeModal(modal);
   }
 }
 
-for (const element of document.querySelectorAll(".modal")) {
-  observer.observe(element, { attributes: true });
+function isShow(modal) {
+  return modal.classList.contains("show");
 }
 
 /***
@@ -111,7 +49,7 @@ for (const element of document.querySelectorAll(
   const target = document.querySelector(element.dataset.target);
   if (target && target.classList.contains("modal")) {
     element.addEventListener("click", (e) => {
-      launchModal(target);
+      showModal(target);
     });
   }
 }
@@ -128,21 +66,15 @@ for (const element of document.querySelectorAll("[data-dismiss=modal]")) {
   }
 }
 
-/**
- * Gestionnaire des événements clavier
- *
- */
-function keyupEvent(e) {
-  switch (e.key) {
-    case "Escape":
-      document.querySelectorAll(".modal").forEach((element) => {
-        if (element.classList.contains("show")) {
-          closeModal(element);
-        }
-      });
-      break;
-  }
-}
+// DOM Elements
+const topNav = document.querySelector("#myTopnav");
+
+const signupModal = document.querySelector("#modal-signup");
+const signupForm = signupModal.querySelector("form[name=reserve]");
+const formDatas = signupForm.querySelectorAll(".formData");
+
+const modalDone = document.querySelector("#modal-done");
+const modalError = document.querySelector("#modal-error");
 
 // Affectation d'un EventListener "click" à tout les .nav-burger
 // permettant d'afficher le menu responsive
@@ -150,20 +82,82 @@ topNav.querySelector(".nav-burger").addEventListener("click", () => {
   topNav.classList.toggle("responsive");
 });
 
+// Ajout de l'événement "change" aux entrées du formulaire
+// de réservation pour afficher ou non les erreurs
+for (const formData of formDatas) {
+  for (const input of formData.querySelectorAll("input")) {
+    if (input.required)
+      input.addEventListener("change", ({ target }) => {
+        formData.dataset.errorVisible = !target.validity.valid;
+      });
+  }
+}
+
 // Affectation d'un EventListener "submit" au formulaire de
-// réservation
-signupForm.addEventListener("submit", formValidate);
-signupModal.addEventListener("open", initForm);
-//signupModal.init = initForm;
+// réservation pour vérifier la validité
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const errors = [];
+  for (const formData of formDatas) {
+    const input = formData.querySelector("input");
+    if (
+      input?.required &&
+      (formData.dataset.errorVisible = !input.validity.valid)
+    ) {
+      errors.push(input);
+    }
+  }
+  if (errors.length === 0) {
+    let success;
+    // Début du traitement de l'envoie des données du formulaire et réception de
+    // la confirmation de la réservation
+    // ...
+    success = true; // Pour l'instant, on met a "true" pour simulier la réussite
+    // ...
+    // Fin du traitement
+    closeModal(signupModal);
+    if (success) {
+      showModal(modalDone);
+    } else {
+      showModal(modalError);
+    }
+  } else {
+    errors[0].focus();
+  }
+});
+
+// Ajout de l'événement "open" (c'est un évenement personalisé)
+// lorsque la modale s'ouvre pour initialiser le formulaire de
+// Réservation
+signupModal.addEventListener("open", (e) => {
+  signupForm.reset();
+  for (const formData of formDatas) {
+    formData.dataset.errorVisible = false;
+  }
+  const maxBirthdate = new Date();
+  maxBirthdate.setFullYear(maxBirthdate.getFullYear() - 18);
+  signupForm.birthdate.max = maxBirthdate.toISOString().slice(0, 10);
+});
 
 // Ajout du gestionnaire du clavier
-document.addEventListener("keyup", keyupEvent);
+window.addEventListener("keyup", ({ key, target }) => {
+  switch (key) {
+    case "Escape":
+      if (target.tagName === "INPUT") {
+        target.blur();
+      } else {
+        for (const modal of document.querySelectorAll(".modal"))
+          if (isShow(modal)) {
+            closeModal(element);
+          }
+      }
+      break;
+  }
+});
 
-document.querySelectorAll("input").forEach((element) => {
-  element.addEventListener("change", (e) => {
-    console.log(e.target.validity);
-    if (e.target.validity.valid) {
-      e.target.closest(".formData").dataset.errorVisible = false;
-    }
-  });
+// Ajout du gestionnaire du click pour fermer les modales
+window.addEventListener("click", ({ target }) => {
+  if (target.classList.contains("modal")) {
+    closeModal(target);
+  }
 });
