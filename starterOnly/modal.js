d7651568b8e2b53f4a9b3f1c79ae4abf50aa283e
Guillaume Donizetti <glm.donizetti@gmail.com>
2023-10-15 00:26:41 +0200

    Activation du bouton de fermeture pour le formulaire modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+// Targeting the close button
+const closeModalBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+//Close modal event
+closeModalBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//Cll the function to close the modal
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
