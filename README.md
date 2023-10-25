93848c5424b33699667955a8ed3d7622854b707a
Guillaume Donizetti <glm.donizetti@gmail.com>
2023-10-25 15:37:07 +0200

    vérification prénom, nom et email

--- README.md
+++ README.md
@@ -3,3 +3,41 @@
 2. Il est conseillé d'utiliser VisualStudio Code et vous pouvez utiliser Docker, mais ce n'est pas obligatoire ;
 3. Il n'y a aucune dépendance ;
 4. Vous ne devez utiliser que du CSS personnalisé et du JavaScript pur, sans jQuery, Bootstrap ou autre librairie.
+
+
+
+
+1. Issue 1 : Terminé
+
+2. Issue 2 :
+
+ (1) Terminé
+(2) Utiliser du JavaScript pur (pas de jQuery) pour terminer le formulaire :
+
+Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
+Les données doivent être saisies correctement :
+(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
+(3) L'adresse électronique est valide.
+(4) Pour le nombre de concours, une valeur numérique est saisie.
+(5) Un bouton radio est sélectionné.
+(6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
+Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
+
+3. Issue 3 : 
+
+Des messages d'erreur spécifiques doivent apparaître sous l'entrée qui n'est pas correcte. Les messages d'erreur doivent s'afficher sous le champ de saisie associé. Exemples :
+
+"Veuillez entrer 2 caractères ou plus pour le champ du nom."
+"Vous devez choisir une option."
+"Vous devez vérifier que vous acceptez les termes et conditions."
+"Vous devez entrer votre date de naissance."
+
+4. Issue 4 :
+
+ Après une validation réussie, inclure un message de confirmation de la soumission réussie pour l'utilisateur (ex. "Merci ! Votre réservation a été reçue.")
+
+ 5. Issue 5 :
+
+ Visualiser et tester l'interface utilisateur dans les dernières versions de Chrome et de Firefox, ainsi que dans les versions mobile et desktop. Corriger les erreurs d'affichage existantes.
+Tester toutes les fonctionnalités des boutons et des entrées de formulaire (tester les valeurs correctes et incorrectes)
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,46 +11,82 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-// Targeting the close button
-const closeModalBtn = document.querySelector(".close");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-//Close modal event
-closeModalBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-//Cll the function to close the modal
+
+//////////////////Fermeture du formulaire///////////
+const closeModalBtn = document.querySelector(".close");
+closeModalBtn.addEventListener("click", closeModal);
+
 function closeModal() {
   modalbg.style.display = "none";
 }
 
-// CONSIGNES et TODO LIST
-// 2) Utiliser du JavaScript pur (pas de jQuery) pour terminer le formulaire :
+//Vérifier que le champ Prénom a un minimum de 2 caractères n'est pas vide.
 
-// Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
-//Mettre en place une fonction qui vérifiera un par un les champs du formulaire.
-//Ils devront tous être conforme et bien remplis, sinon = echec
+//Fonction qui vérifie le prénom et le nom non vide et au moins 2 caractère
 
-// Les données doivent être saisies correctement :
-// (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-//Faire une condition vérifié si longueur prénom >=2 && not null
-// (2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
-//Idem, faire une condition vérifié si longueur nom >=2 && not null
-// (3) L'adresse électronique est valide.
-//Voir si il existe une fonction native pour les email, sinon créer une REGEX
-// (4) Pour le nombre de concours, une valeur numérique est saisie.
-//Condition, vérifier si le nombre est composé de chiffres
-// (5) Un bouton radio est sélectionné.
-//condition, vérifier tout les boutons, si il y en a un checké, avec une boucle
-// (6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée. propriété checked 
-// true or false, utiliser un boléen
-//Consigne peu claire, demander au tuteur, je laisse comme ça?
-// Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation. => utiliser la prorpiété event prevent defaut pour éviter
-//le rechargement au submit du formulaire
+function checkIdentity(identity){
+  if(identity.value.trim() === ""){
+    throw new Error(`Vous devez mettre un ${identity.name}`)
+  }
+  if(identity.value.trim().length < 2){
+    throw new Error(`Vous devez mettre un ${identity.name} d'au moins 2 caractères`)
+  }
+}
+//Fonction qui vérifie le mail avec une regex
 
-//Voir le cours chapitre "récupérez la valeur d'un champ de formulaire"
+function checkEmail(email){
+  const regex = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+  if(email.value.trim() === ""){
+    throw new Error(`Vous devez mettre un ${email.name}`);
+  }
+  if(!regex.test(email.value.trim())){
+    throw new Error(`L'adresse e-mail est invalide`);
+  }
+}
+
+//Fonction qui vérifie la date de naissance (année comprise entre 1900 et 2023)
+
+//Fonction qui vérifie le nombre de tournoi
+
+//Fonction qui vérifie si un tournoi est checké
+
+//Fonction qui vérifie le check des conditions d'utilisation
+
+//Une fois les fonctions mise en place, mettre en place le try/catch
+
+
+const form = document.getElementById("main-form");
+console.log(form);
+
+form.addEventListener("submit", (event)=>{
+
+try{
+event.preventDefault()
+const first = document.getElementById("first");
+checkIdentity(first);
+
+const last = document.getElementById("last");
+checkIdentity(last);
+
+const email = document.getElementById("email");
+checkEmail(email);
+
+}catch(error){
+  console.log(error.message);
+}
+}
+)
+
+
+
+
