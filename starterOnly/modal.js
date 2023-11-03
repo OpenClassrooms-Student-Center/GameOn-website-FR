ae31fd6b1dfa30b9d331b1d302f1519d27c89af6
samakunchan <badgehades@hotmail.com>
2023-11-03 04:44:53 +0100

    feat: modal
    - Add a close modal function
    - Fix double quote to single quote

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,29 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const closeModalBtn = document.querySelector('.close');
+const formData = document.querySelectorAll('.formData');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block';
 }
 
 
+// Close modal form
+const closeModal = () => {
+  modalbg.style.display = 'none';
+}
+closeModalBtn.addEventListener('click', closeModal)
