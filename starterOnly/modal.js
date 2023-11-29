0c7930c09c72befb7573243ee6672b248e4202e5
crislec64 <lecuyer.christine@yahoo.fr>
2023-11-29 21:16:33 +0100

    responsive

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,19 +1,9 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closemodalBtn = document.querySelector(".close");
 const thanks = document.querySelector(".bgThanks")
-
 
 // Form elements
 const form = document.querySelector("form");
@@ -26,49 +16,29 @@ const inputlocation = document.querySelectorAll('input[name="location"]');
 const spanLocation = document.querySelectorAll('.check-icon')
 const condition = document.getElementById("checkbox1");
 const close = document.querySelector(".close");
+
 //form thank
-const message =document.getElementById("message");
+const message = document.getElementById("message");
 const thanksBtn = document.querySelector(".thanksBtn");
 
 //regex
-const regexText = new RegExp("([A-Za-z ]+)?([-]{0,1})?([A-Za-z|\s]{2,15})$")
+const regexText = new RegExp("^[a-zA-ZéèîïÉÈÎÏ]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$")
 const regexEmail = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
 
 //error message
 const error = {
-  text: 'uniquement des caractères',
-  length: 'trop court',
+  text: 'Uniquement les caractères sont autorisés',
+  length: 'Trop court - Veuillez entrer au moins 2 caractères',
   nonValide: 'Veuillez entrer une date valide',
   email: 'Veuillez renseigner une adresse Email valide.',
-  birthdate: 'interdit aux mineurs',
-  quantity: 'Veuillez selectionner le nombre de selection',
+  emailFormat:'Email non valide, veuillez entrer une adresse valide',
+  birthdate: 'Non autorisé - interdit aux mineurs',
+  quantity: 'Veuillez selectionner un nombre de 0 ou plus',
   location: 'Veuillez selectionner une ville',
   condition: 'Veuillez accepter les conditions',
 
 };
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// close modal form
-modalbg.addEventListener("click", (event) => {
-  if (event.target === modalbg) {
-    modalbg.style.display = "none";
-  }
-});
-
-// close modal form x
-closemodalBtn.addEventListener("click", closeModal);
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-//close modal form
-function closeModal() {
-  modalbg.style.display = "none";
-}
 //error visible
 const errorVisible = (element, error) => {
   element.parentElement.setAttribute("data-error-visible", "true");
@@ -79,6 +49,26 @@ const errorHiden = element => {
   element.parentElement.removeAttribute("data-error-visible");
   element.parentElement.removeAttribute("data-error", error);
 }
+
+function editNav() {
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "flex";
+}
+
+//close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 // check length
 function validerInput(element, error) {
   if (element.value.trim().length < 2) {
@@ -98,7 +88,6 @@ function checkInputVide(element, error) {
   }
   errorHiden(element);
   return true;
-
 };
 
 //check input REGEX
@@ -107,7 +96,6 @@ function checkInput(regex, element, error) {
     errorVisible(element, error);
     return false
   }
-
   errorHiden(element);
   return true;
 }
@@ -148,27 +136,51 @@ function checkCondition(element, error) {
 }
 
 //thanks
-function modalthanks(){
+function modalthanks() {
   modalbg.style.display = "none";
   thanks.style.display = "flex";
   form.reset();
 };
 
 //close thanks
-function closeModalThanks(){
+function closeModalThanks() {
   thanks.style.display = "none";
   form.reset();
 };
 
 //eventlistener
-first.addEventListener('change', () => ([checkInput(regexText, first, error.text)] && [validerInput(first, error.length)]));
-last.addEventListener('change', () => ([checkInput(regexText, last, error.text)] && [validerInput(last, error.length)]));
-email.addEventListener('change', () => checkInput(regexEmail, email, error.email));
-birthdate.addEventListener('change', () => ([majeur(birthdate, error.birthdate)] && [checkInputVide(birthdate, error.nonValide)]));
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal form
+modalbg.addEventListener("click", (event) => {
+  if (event.target === modalbg) {
+    modalbg.style.display = "none";
+  }
+});
+
+// close modal form x
+closemodalBtn.addEventListener("click", closeModal);
+
+// close modal thanks
+thanksBtn.addEventListener('click', () => closeModalThanks());
+
+// submit modal ==> check validation
+form.addEventListener('submit', e => validate(e));
+
+
+
+// addEventListener ==> change / imput form
+first.addEventListener('change', () => (checkInput(regexText, first, error.text) && validerInput(first, error.length)));
+last.addEventListener('change', () => (checkInput(regexText, last, error.text) && validerInput(last, error.length)));
+email.addEventListener('change', () => (checkInput(regexEmail, email, error.email) && checkInputVide(email, error.email)));
+birthdate.addEventListener('change', () => (majeur(birthdate, error.birthdate) && checkInputVide(birthdate, error.nonValide)));
 quantity.addEventListener('change', () => checkInputVide(quantity, error.quantity));
 
 inputlocation.forEach(radio => radio.addEventListener('change', () => checkLocation(inputlocation, error.location)));
 condition.addEventListener('change', () => checkCondition(condition, error.condition));
+
 //valide form
 function validate(e) {
   e.preventDefault();
@@ -176,7 +188,7 @@ function validate(e) {
   //check conditions = valide
   const valideFirst = (checkInput(regexText, first, error.text) && validerInput(first, error.length));
   const valideLast = (checkInput(regexText, last, error.text) && validerInput(last, error.length));
-  const valideEmail = checkInput(regexEmail, email, error.email);
+  const valideEmail = (checkInput(regexEmail, email, error.emailFormat) && checkInputVide(email, error.email));
   const valideBirthdate = (majeur(birthdate, error.birthdate) && checkInputVide(birthdate, error.nonValide));
   const valideQuantity = checkInputVide(quantity, error.quantity);
   const valideLocation = checkLocation(inputlocation, error.location);
@@ -185,7 +197,4 @@ function validate(e) {
   if (valideFirst && valideLast && valideEmail && valideBirthdate && valideQuantity && valideLocation && valideCondition) {
     modalthanks();
   }
-};
-thanksBtn.addEventListener('click', () => closeModalThanks());
-
-form.addEventListener('submit', e => validate(e));
+}
