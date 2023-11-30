b1203adf827a031778ff08d34fc93ffca73f5d83
7Flafleur <7nb@hotmail.fr>
2023-11-30 08:31:06 +0100

    closeModal valid first

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,10 +11,15 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closes = document.querySelectorAll(".closes, .exit");
+const closes = document.getElementById("cross");
+
 
 // launch modal event  FUNCTION
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+cross.addEventListener("click",closeModal);
+
+
 
 // launch modal form 
 function launchModal() {
@@ -22,17 +27,50 @@ function launchModal() {
   console.log("test")
 }
 
-//launch switch modal to bye
-closes.addEventListener("click",(e) => closeModal(e));
+// launch switch modal to bye
+
+function closeModal() {
+  modalbg.style.display = "none";
+  console.log("exit");
+
+}
+
+//Form validation functions
+
+
+function validfirst() {
+  if (first.value.length<2){
+    document.getElementById("prenom").style.display="inline";
+    console.log("invalid first name!")
+  }
+  else{
+    document.getElementById("prenom").style.display="none";
+    console.log("valid first name");
+  }
+}
+
+var enterpressed=false;
+
+first.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    console.log('Enter key pressed!');
+    enterpressed=true;
+
+  }
+  if (enterpressed){
+    validfirst();
+  }
+});
+
+
+
 
 
 // switch modal form to bye
 
-function closeModal(e) {
-  console.log(e)
-  e.target.parentElement.parentElement.style.display = "none";
-  console.log("exit modal")
-
-
-}
+// function closeModal(e) {
+//   console.log(e)
+//   e.target.parentElement.parentElement.style.display = "none";
+//   console.log("exit modal")
+// }
 
