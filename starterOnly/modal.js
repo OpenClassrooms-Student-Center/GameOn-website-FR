a1164284d09650b9361fb50fb6ea1dd9b92c639d
nicolas riera <nicolariera@gmail.com>
2023-12-08 11:25:44 +0100

    add DOM elements closeModalBton / event close modal / close moadl form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,21 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeModalBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
