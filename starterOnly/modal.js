a406608de2aef0fd67e41684c3ccd192f5841f35
ChaymaaLabied <chaymaalabied1996@gmail.com>
2024-01-04 11:02:31 +0100

    Ajouter validation ou messages d'erreur #3

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -35,51 +35,122 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
 })
+
+
 function validerNomEtPrenom(nom) {
 
-  if (nom.trim().length >= 2) {
-    return true
+  if (nom.trim().length < 2) {
+    throw new Error("Veuillez entrer 2 caractères ou plus .")
   }
-  return false
 
 }
-function validerChampQuantity (quantity){
-  let quantityRegEx = new RegExp("^\d+$")
-  if ( quantityRegEx.test(quantity)){
-    return true
+
+
+function validerEmail(email) {
+  let emailRegex = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
+  if (!emailRegex.test(email.trim())) {
+    throw new Error("Veuillez entrer un Email valide")
   }
-return false
-// console.log(typeof quantity)
+}
+
+function validerChampQuantity(quantity) {
+  // let quantityRegEx = new RegExp("^\d+$")
+  // !quantityRegEx.test(quantity)
+  if (quantity==="") {
+    throw new Error("Veuillez choisir un nombre")
+  }
+  // console.log(typeof quantity)
   // return !!quantity && !isNaN(quantity)
 }
 
-function validerEmail(email){
-  let emailRegex = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
-  if(emailRegex.test(email.trim())){
-    return true
-  }
-  return false
-}
-function validerBoutonRadio(Var){
-if(Var === ""){
-  return false
-}
-else{
-  return true 
-}
-}
-function validerCheckBox(btn){
-  return (btn.checked) // better way to do the sae as if (tatata) return true 
 
+function validerBoutonRadio(Var) {
+  if (Var === "") {
+    // cette fait la meme chose que li 9bal hh
+    throw new Error("Vous devez choisir une option.")
+  }
+
+}
+function validerCheckBox(btn) {
+  if (!btn.checked){
+    throw new Error("Vous devez vérifier que vous acceptez les termes et conditions.")
+  } // better way to do the same as if (tatata) return true 
+
+
+}
+
+// function afficherMessageErreurPrenom(message){
+//   let SpanMessageErreur = document.getElementById("erreurMessage1")
+//   if(!SpanMessageErreur){
+
+//   let formData = document.querySelector(".formData1")
+//   SpanMessageErreur = document.createElement("span")
+//   SpanMessageErreur.id = "erreurMessage1"
+//   formData.append(SpanMessageErreur)
+// }
+// SpanMessageErreur.innerHTML = message
+// }
+
+function afficherMessageErreur(message, formDataSelector, erreurMessageSelector) {
+  let SpanMessageErreur1 = document.getElementById(erreurMessageSelector);
+
+  if (!SpanMessageErreur1) {
+    let formData = document.querySelector(formDataSelector);
+    SpanMessageErreur1 = document.createElement("div");
+    SpanMessageErreur1.id = erreurMessageSelector;
+    formData.append(SpanMessageErreur1);
+  }
+
+  SpanMessageErreur1.innerHTML = message;
 }
 
 function validate() {
-  console.log("Il n’y a pas eu de rechargement de page");
-  let prenom = document.getElementById("first").value
-  let nom = document.getElementById("last").value
-  let email = document.getElementById("email").value
+  try {
+
+    let prenom = document.getElementById("first").value
+    console.log(prenom)
+    console.log("validation Prenom", validerNomEtPrenom(prenom))
+    // afficherMessageErreurPrenom("")
+    afficherMessageErreur("", ".formData1", "#erreurMessage1")
+  }
+  catch (Error) {
+    // afficherMessageErreurPrenom(Error.message)
+    afficherMessageErreur(Error.message, ".formData1", "#erreurMessage1")
+
+  }
+
+  try {
+    let nom = document.getElementById("last").value
+    console.log(nom)
+    console.log("validation nom", validerNomEtPrenom(nom))
+    afficherMessageErreur("", ".formData2", "#erreurMessage2")
+  } catch (Error) {
+
+    afficherMessageErreur(Error.message, ".formData2", "#erreurMessage2")
+  }
+
+  try {
+    let email = document.getElementById("email").value
+    console.log(email)
+    console.log("validation email", validerEmail(email))
+    afficherMessageErreur("", ".formData3", "#erreurMessage3")
+  } catch (Error) {
+    afficherMessageErreur(Error.message, ".formData3", "#erreurMessage3")
+  }
+
   let birthday = document.getElementById("birthdate").value
-  let quantity = document.getElementById("quantity").value
+  console.log(birthday)
+
+  try {
+    let quantity = document.getElementById("quantity").value
+    console.log(quantity)
+    console.log("validation du champ quantity", validerChampQuantity(quantity))
+    afficherMessageErreur("", ".formData5", "#erreurMessage5")
+  } catch (Error) {
+    afficherMessageErreur(Error.message, ".formData5", "#erreurMessage5")
+  }
+
+try {
   let baliseLocation = document.querySelectorAll('input[name="location"]')
   let location = ""
   for (let i = 0; i < baliseLocation.length; i++) {
@@ -88,26 +159,31 @@ function validate() {
       break
     }
   }
+  console.log(location)
+  console.log("validation bouton radio", validerBoutonRadio(location))
+  afficherMessageErreur("", ".formData6", "#erreurMessage6")
+} catch (Error) {
+  afficherMessageErreur(Error.message, ".formData6", "#erreurMessage6")
+}
+try {
   let checkBox = document.querySelectorAll('input[type="checkbox"]')
   for (let i = 0; i < checkBox.length; i++) {
     checkBoxChecked = checkBox[i].checked
     console.log(checkBoxChecked)
   }
   let checkBox1 = document.getElementById("checkbox1")
-  console.log(prenom)
-  console.log(nom)
-  console.log("validation Prenom",  validerNomEtPrenom(prenom))
-  console.log("validation nom",  validerNomEtPrenom(nom))
-  console.log(email)
-  console.log("validation email" , validerEmail(email))
-  console.log(birthday)
-  console.log(quantity)
-  console.log( "validation du champ quantity" , validerChampQuantity(quantity))
-  console.log(location)
-  console.log("validation bouton radio", validerBoutonRadio(location))
-  console.log( "validation checkbox1", validerCheckBox(checkBox1))
+  console.log("validation checkbox1", validerCheckBox(checkBox1))
+  afficherMessageErreur("", ".formData7", "#erreurMessage7")
 
+} catch (Error) {
+  afficherMessageErreur(Error.message, ".formData7", "#erreurMessage7")
+}
+
+  // code a netoyer , c-a-d supprimer les console log 
+  // return validerNomEtPrenom(prenom) && validerNomEtPrenom(nom) && validerEmail(email)
+  //   && validerChampQuantity(quantity) && validerBoutonRadio(location) && validerCheckBox(checkBox1)
   return true
+
 }
 
 
