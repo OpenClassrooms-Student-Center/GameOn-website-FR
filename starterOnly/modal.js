bb6c4b4195c42d40075f7a35f10dc24c94275fbe
Thomas DOUCHE <Thomas.douche@aol.com>
2024-01-11 20:16:12 +0100

    Modal close functionality

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,27 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalbg  = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
 
