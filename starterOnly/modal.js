403a8428760052202c09b182985465c9112c4e4c
Max FRZ <maxfrz@maxs-mbp.home>
2024-02-12 01:46:08 +0100

    initial commit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,25 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// CLOSING THE FORM
+// close modal function
+function closeModal() {
+  modalbg.style.display = "none";
+}
+// close modal event
+closeForm.addEventListener("click", closeModal);
+
+
 
 
