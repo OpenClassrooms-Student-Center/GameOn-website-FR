002e89ba783bc4c04a8b021323850e2e26b16955
Maxime Danic <maxime.danic@gmail.com>
2024-02-13 16:16:03 +0200

    Add form validation to modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,122 @@ closeBtn.addEventListener('click', closeModal);
 function closeModal() {
   modalbg.style.display = 'none';
 }
+
+// DOM Elements form validation
+const form = document.getElementById('reserveForm');
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+
+const checkbox1 = document.getElementById('checkbox1');
+const checkbox2 = document.getElementById('checkbox2');
+const submit = document.getElementById('submit');
+
+// First name
+function validateFirstName() {
+  if (firstName.value.length < 2) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Last name
+function validateLastName() {
+  if (lastName.value.length < 2) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Email
+function validateEmail() {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!emailRegex.test(email.value)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Birthdate
+function validateBirthdate() {
+  if (birthdate.value.trim() !== '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Quantity
+function validateQuantity() {
+  if (isNaN(quantity.value) && quantity.value < 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Checkbox Location
+function validateCheckboxLocation() {
+  const locationCheckBoxes = document.getElementsByName('location');
+  let checkedValue = null;
+
+  locationCheckBoxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      checkedValue = checkbox.value;
+    }
+  });
+
+  if (checkedValue === null) {
+    return false;
+  } else {
+    return checkedValue;
+  }
+}
+
+// Checkbox Terms of use
+function validateCheckboxTerms() {
+  if (!checkbox1.checked) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Form validation
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  isFormValid();
+});
+
+// Is Form Valid
+
+function isFormValid() {
+  if (
+    validateFirstName() &&
+    validateLastName() &&
+    validateEmail() &&
+    validateQuantity() &&
+    validateCheckboxLocation() &&
+    validateCheckboxTerms() &&
+    validateBirthdate()
+  )
+    console.log(
+      firstName.value,
+      lastName.value,
+      email.value,
+      birthdate.value,
+      quantity.value,
+      validateCheckboxLocation(),
+      checkbox1.checked,
+      checkbox2.checked
+    );
+  else {
+    console.log('not valid');
+  }
+}
