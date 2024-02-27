306a3021763785ca98d1dc344ce9357ddd28fbd6
najar <hajer.najar.ing@gmail.com>
2024-02-27 23:20:48 +0100

    ajouter la fermeture de modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,11 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
+//close modal event
+modalClose.addEventListener("click", () => {
+  modalbg.style.display = "none";
+});
 
 
